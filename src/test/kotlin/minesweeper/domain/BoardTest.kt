f36ffdec5183ab0d7ee765e10311fb8df8c7355a
Parkjoopika <parkjoohan0603@gmail.com>
2020-08-14 19:46:28 +0900

    feat: make coordinate and input x,y can get coordinate

--- src/test/kotlin/minesweeper/domain/BoardTest.kt
+++ src/test/kotlin/minesweeper/domain/BoardTest.kt
@@ -1,6 +1,7 @@
 package minesweeper.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class BoardTest {
@@ -19,5 +20,14 @@ class BoardTest {
 
         assertThat(coordinate.x).isEqualTo(2)
         assertThat(coordinate.y).isEqualTo(3)
+    }
+
+    @Test
+    fun find_error() {
+        val board = Board(10, 10)
+
+        assertThatThrownBy {
+            board.findCoordinate(10, 10)
+        }.isInstanceOf(IllegalArgumentException::class.java).hasMessageContaining("10, 10 좌표는 없습니다.")
     }
 }
