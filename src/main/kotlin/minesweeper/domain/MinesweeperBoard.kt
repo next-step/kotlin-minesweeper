687980a4987b5d9ee049f7053e569d9bc132a848
EthanChae <boschae36@gmail.com>
2020-08-16 01:20:31 +0900

    feat(minesweeper): Shuffle the board

--- src/main/kotlin/minesweeper/domain/MinesweeperBoard.kt
+++ src/main/kotlin/minesweeper/domain/MinesweeperBoard.kt
@@ -1,10 +1,18 @@
 package minesweeper.domain
 
 class MinesweeperBoard(boardSize: BoardSize, mineNumber: MineNumber) {
-    private var _minesweeperBoard: List<List<BoardType>> = List(boardSize.height.length) { List(boardSize.width.length) { BoardType.LOAD } }
+    private var _minBoard = MutableList(boardSize.height.length * boardSize.width.length) { BoardType.LOAD }
+    private var _minesweeperBoard: MutableList<List<BoardType>> = MutableList(boardSize.height.length) { List(boardSize.width.length) { BoardType.LOAD } }
     val minesweeperBoard get() = _minesweeperBoard.toList()
-    init {
 
-        _minesweeperBoard = _minesweeperBoard.shuffled()
+    init {
+        for (i in 0 until mineNumber.mineNumber) {
+            _minBoard[i] = BoardType.MINE
+        }
+        _minBoard = _minBoard.shuffled().toMutableList()
+        for (i in 0 until boardSize.height.length) {
+            val startNumber = i * boardSize.width.length
+            _minesweeperBoard[i] = _minBoard.subList(startNumber, startNumber + boardSize.width.length)
+        }
     }
 }
--- src/test/kotlin/MinesweeperBoardGameTest.kt
+++ src/test/kotlin/MinesweeperBoardGameTest.kt
@@ -1,4 +1,5 @@
 import minesweeper.domain.BoardSize
+import minesweeper.domain.BoardType
 import minesweeper.domain.LengthNumber
 import minesweeper.domain.MineNumber
 import minesweeper.domain.MinesweeperBoard
@@ -22,5 +23,18 @@ class MinesweeperBoardGameTest {
             { assertThat(boardSize.height.length).isEqualTo(boardHeight) },
             { assertThat(boardSize.width.length).isEqualTo(boardWidth) }
         )
+    }
+
+    @Test
+    @DisplayName("만들어진 배열에 랜덤하게 지뢰를 배치")
+    fun checkDistributeMine() {
+        val boardSize = BoardSize(LengthNumber(10), LengthNumber(7))
+        val mineCount = MineNumber(10, boardSize)
+        val mineSweeperBoard = MinesweeperBoard(boardSize, mineCount)
+
+        val mineCountInBoard =
+            mineSweeperBoard.minesweeperBoard.sumBy { it.filter { boardType -> boardType == BoardType.MINE }.count() }
+
+        assertThat(mineCount.mineNumber).isEqualTo(mineCountInBoard)
     }
 }
