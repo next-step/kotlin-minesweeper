d240117d11fc00158f2bbc069e5488947a07de61
zion830 <zion830@honglab.org>
2020-08-17 04:49:26 +0900

    feat(minesweeper): 지뢰 표시 기호 변경

--- src/main/kotlin/game/Board.kt
+++ src/main/kotlin/game/Board.kt
@@ -14,29 +14,30 @@ class Board private constructor(
     private fun initBoard() {
         var count = mineCount
         while (count > 0) {
-            val x = (0 until width).random()
-            val y = (0 until height).random()
+            val x = (0 until height).random()
+            val y = (0 until width).random()
             count -= setMine(x, y)
         }
     }
 
     private fun setMine(x: Int, y: Int): Int {
-        val location = boardInfo[x][y]
-        if (location == SYMBOL_EMPTY) {
+        if (boardInfo[x][y] == SYMBOL_EMPTY) {
             boardInfo[x][y] = SYMBOL_MINE
             return 1
         }
         return 0
     }
 
-    override fun toString() = boardInfo.joinToString("") { it.joinToString("") + "\n" }
+    override fun toString() = boardInfo.joinToString("") { it.joinToString(" ") + "\n" }
 
     companion object {
-        private const val SYMBOL_EMPTY = "ㅁ"
+        private const val SYMBOL_EMPTY = "O"
         private const val SYMBOL_MINE = "X"
 
+        fun isValidMineCount(width: Int, height: Int, mineCount: Int) = mineCount in 0..(width * height)
+
         operator fun invoke(width: Int, height: Int, mineCount: Int): Board? {
-            if (mineCount in 0..(width * height)) {
+            if (isValidMineCount(width, height, mineCount)) {
                 return Board(width, height, mineCount)
             }
             return null
--- src/test/kotlin/BoardTest.kt
+++ src/test/kotlin/BoardTest.kt
@@ -1,12 +1,22 @@
 import game.Board
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class BoardTest {
 
-    @Test
-    fun `높이 * 너비 크기의 판을 생성한다`() {
-        assertThat(Board(2, 3, 0).toString()).isEqualTo("ㅁㅁ\nㅁㅁ\nㅁㅁ\n")
+    @ParameterizedTest
+    @ValueSource(strings = ["O O\nO O\nO O\n"])
+    fun `높이 * 너비 크기의 판을 생성한다`(board: String) {
+        assertThat(Board(2, 3, 0).toString()).isEqualTo(board)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [101, -1])
+    fun `지뢰의 개수는 0 이상 (높이 * 너비) 이하여야 한다`(mineCount: Int) {
+        assertThat(Board.isValidMineCount(10, 10, mineCount)).isFalse()
+        assertThat(Board(10, 10, mineCount)).isNull()
     }
 
     @Test
@@ -14,10 +24,5 @@ class BoardTest {
         assertThat(
             Board(10, 10, 5).toString().count { it.toString() == "X" }
         ).isEqualTo(5)
-    }
-
-    @Test
-    fun `지뢰의 개수는 0 이상 (높이 * 너비) 이하여야 한다`() {
-        assertThat(Board(10, 10, 101)).isNull()
     }
 }
