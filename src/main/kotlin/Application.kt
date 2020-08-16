916dfcb3f279b41fdb2a6aed34cf6f873d96c0bb
zion830 <zion830@honglab.org>
2020-08-17 04:50:57 +0900

    feat(minesweeper): 보드판 View 구현

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,13 @@
+import game.Board
+import view.InputView
+import view.ResultView
+
+fun main() {
+    val width = InputView.readWidth()
+    val height = InputView.readHeight()
+    val mineCount = InputView.readMineCount { Board.isValidMineCount(width, height, it) }
+
+    Board(width, height, mineCount)?.run {
+        ResultView.printBoard(this)
+    }
+}
---
+++ src/main/kotlin/view/ResultView.kt
@@ -0,0 +1,12 @@
+package view
+
+import game.Board
+
+object ResultView {
+    private const val START_GAME = "지뢰찾기 게임 시작"
+
+    fun printBoard(board: Board) {
+        println(START_GAME)
+        println(board)
+    }
+}
