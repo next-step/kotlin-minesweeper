dc0a46e0d5acc9bbb952228537f94c1f034c0a08
bky373 <bo373@naver.com>
2020-08-17 17:57:36 +0900

    feat(view) : create object(InputView) and object(ResultView)

---
+++ src/main/kotlin/minesweeper/view/ResultView.kt
@@ -0,0 +1,15 @@
+package minesweeper.view
+
+import minesweeper.domain.MineMap
+
+const val ANNOUNCE_GAME_START = "지뢰게임 시작"
+
+object ResultView {
+
+    fun showMineMap(mineMap: MineMap) {
+        println("\n$ANNOUNCE_GAME_START")
+
+        val map = mineMap.stateOfMap()
+        map.indices.forEach { println(map[it].joinToString(" ")) }
+    }
+}
