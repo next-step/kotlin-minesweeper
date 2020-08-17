e2e0130cf0be51379a2f4821016215db58034b16
BeokBeok <kekemusa37@nate.com>
2020-08-17 15:13:35 +0900

    feat(LengthOfSide) : DIVIDE - LengthOfSide class

--- src/test/kotlin/LengthOfSideTest.kt
+++ src/test/kotlin/LengthOfSideTest.kt
@@ -12,16 +12,3 @@ class LengthOfSideTest {
         }
     }
 }
-
-class LengthOfSide(length: Int) {
-
-    init {
-        require(length in NUMBER_RANGE) {
-            "높이 혹은 너비는 10 - 30까지만 가능합니다."
-        }
-    }
-
-    companion object {
-        private val NUMBER_RANGE = 10..30
-    }
-}
