aa2bfa931dcb73abf7b0bc6d0d4b771e1c1766ed
BeokBeok <kekemusa37@nate.com>
2020-08-17 15:12:16 +0900

    test(LengthOfSide) : APPLY - 높이 혹은 너비 값이 허용되는 범위에 없는 경우 TC

--- src/test/kotlin/LengthOfSideTest.kt
+++ src/test/kotlin/LengthOfSideTest.kt
@@ -1,1 +1,27 @@
-class LengthOfSideTest
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LengthOfSideTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = [9, 31])
+    fun `높이 혹은 너비 값이 허용되는 범위에 없는 경우`(input: Int) {
+        assertThatIllegalArgumentException().isThrownBy {
+            LengthOfSide(input)
+        }
+    }
+}
+
+class LengthOfSide(length: Int) {
+
+    init {
+        require(length in NUMBER_RANGE) {
+            "높이 혹은 너비는 10 - 30까지만 가능합니다."
+        }
+    }
+
+    companion object {
+        private val NUMBER_RANGE = 10..30
+    }
+}
