d2d1d31912d45ad9160169428a21d2fd18c28075
BeokBeok <kekemusa37@nate.com>
2020-08-18 12:00:48 +0900

    fix(test) : MODIFY - 테스트명 변경
    review url : https://github.com/next-step/kotlin-minesweeper/pull/6#discussion_r471803213

--- src/test/kotlin/model/LengthOfSideTest.kt
+++ src/test/kotlin/model/LengthOfSideTest.kt
@@ -8,7 +8,7 @@ class LengthOfSideTest {
 
     @ParameterizedTest
     @ValueSource(ints = [9, 31])
-    fun `높이 혹은 너비 값이 허용되는 범위에 없는 경우`(input: Int) {
+    fun `높이 혹은 너비 값은 10 이상 30 이하이어야 한다`(input: Int) {
         assertThatIllegalArgumentException().isThrownBy {
             LengthOfSide(input)
         }
