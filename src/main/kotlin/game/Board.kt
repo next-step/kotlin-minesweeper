5544fdd0b10bd2e12ada42263e65dd44733cf636
무슨이윤지 <zion830@naver.com>
2020-08-18 21:46:50 +0900

    Step2 - 지뢰 찾기(지뢰 개수) (#9)
    * refactor(minesweeper): 지뢰 설치 방식 변경
    
    - 매번 random 객체를 재생성하지 않고, 이미 설치된 위치를 중복해서 확인하지 않도록 변경
    
    * docs(minesweeper): step 2 TODO 작성
    
    * feat(minesweeper): 지뢰를 그리는 방식 변경
    
    - 주위 8칸에 있는 지뢰 개수를 표시하도록 변경
    
    * feat(minesweeper): 프로퍼티를 지역변수로 변경

--- src/main/kotlin/game/Board.kt
+++ src/main/kotlin/game/Board.kt
@@ -1,5 +1,8 @@
 package game
 
+import kotlin.math.max
+import kotlin.math.min
+
 class Board private constructor(
     private val width: Int,
     private val height: Int,
@@ -12,27 +15,50 @@ class Board private constructor(
     }
 
     private fun initBoard() {
-        var count = mineCount
-        while (count > 0) {
-            val x = (0 until height).random()
-            val y = (0 until width).random()
-            count -= setMine(x, y)
+        val mineLocations = createMines()
+        for (i in 0 until mineCount) {
+            setMine(mineLocations[i].first, mineLocations[i].second)
         }
     }
 
-    private fun setMine(x: Int, y: Int): Int {
-        if (boardInfo[x][y] == SYMBOL_EMPTY) {
+    private fun createMines() = (0 until height).flatMap { x ->
+        (0 until width).map { y -> Pair(x, y) }
+    }.shuffled().take(mineCount)
+
+    private fun setMine(x: Int, y: Int) {
+        if (boardInfo[x][y] > SYMBOL_MINE) {
             boardInfo[x][y] = SYMBOL_MINE
-            return 1
+            notifySetMine(x, y)
         }
-        return 0
     }
 
-    override fun toString() = boardInfo.joinToString("") { it.joinToString(" ") + "\n" }
+    private fun notifySetMine(x: Int, y: Int) {
+        for (location in findNotifyRange(x, y)) {
+            increaseMineCount(location.first, location.second)
+        }
+    }
+
+    private fun increaseMineCount(x: Int, y: Int) {
+        if (boardInfo[x][y] > SYMBOL_MINE) {
+            boardInfo[x][y] += 1
+        }
+    }
+
+    private fun findNotifyRange(x: Int, y: Int): List<Pair<Int, Int>> {
+        val rangeX = max(0, x - NOTIFY_RANGE)..min(x + NOTIFY_RANGE, height - 1)
+        val rangeY = max(0, y - NOTIFY_RANGE)..min(y + NOTIFY_RANGE, width - 1)
+        return rangeX.flatMap { i -> rangeY.map { j -> Pair(i, j) } }
+    }
+
+    override fun toString() = boardInfo.joinToString("") {
+        it.joinToString(" ").replace(SYMBOL_MINE.toString(), SYMBOL_MINE_CHAR) + "\n"
+    }
 
     companion object {
-        private const val SYMBOL_EMPTY = "O"
-        private const val SYMBOL_MINE = "X"
+        private const val SYMBOL_MINE = -1
+        private const val SYMBOL_MINE_CHAR = "X"
+        private const val SYMBOL_EMPTY = 0
+        private const val NOTIFY_RANGE = 1
 
         fun isValidMineCount(width: Int, height: Int, mineCount: Int) = mineCount in 0..(width * height)
 
