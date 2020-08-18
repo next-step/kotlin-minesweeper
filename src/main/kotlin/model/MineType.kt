7e699f287655446f9f95d99f1a0f384512a935bd
BeokBeok <kekemusa37@nate.com>
2020-08-18 23:55:41 +0900

    feat(MineType) : APPLY - 아스키 코드 값

--- src/main/kotlin/model/MineType.kt
+++ src/main/kotlin/model/MineType.kt
@@ -1,6 +1,6 @@
 package model
 
-enum class MineType(val symbol: Char) {
-    NONE('C'),
-    MINE('*')
+enum class MineType(val symbol: Char, val ascii: Int) {
+    NONE('C', 34),
+    MINE('*', 42)
 }
