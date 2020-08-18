4a62129302a36498f529e0e5a0b97d49dd97bc87
Malibin <nightmare73@naver.com>
2020-08-18 18:42:26 +0900

    refactor(step2): 지뢰 숫자 포함한 지뢰판만들기

--- src/main/kotlin/domain/MineFieldGenerator.kt
+++ src/main/kotlin/domain/MineFieldGenerator.kt
@@ -6,8 +6,19 @@ class MineFieldGenerator(
     fun create(rectangle: Rectangle, minesCount: Int): MineField {
         val positions = rectangle.getAllPositions()
         val minePositions = minePositionsSelectStrategy.getMinePositionsFrom(positions, minesCount)
-        val normalBlocks: List<Block> = (positions - minePositions).map { NormalBlock(it, 0) }
+        val normalBlocks = createNormalBlocks(positions, minePositions)
         val mineBlocks = Mine.from(minePositions)
         return MineField(rectangle, (normalBlocks + mineBlocks).sortedBy { it.position })
+    }
+
+    private fun createNormalBlocks(allPositions: List<Position>, minePositions: List<Position>): List<Block> {
+        val normalPositions = allPositions - minePositions
+        return normalPositions.map { it.surroundings() }
+            .map { countPositionsContainsMines(it, minePositions) }
+            .zip(normalPositions) { minesCount, position -> NormalBlock(position, minesCount) }
+    }
+
+    private fun countPositionsContainsMines(positions: List<Position>, minePositions: List<Position>): Int {
+        return positions.count { minePositions.contains(it) }
     }
 }
--- src/test/kotlin/domain/MineFieldGeneratorTest.kt
+++ src/test/kotlin/domain/MineFieldGeneratorTest.kt
@@ -4,25 +4,37 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class MineFieldGeneratorTest {
+
+    /**
+     *      1 2 *
+     *      * 3 2
+     *      2 * 1
+     */
     @Test
     fun `전체 필드를 제대로 만드는지 확인`() {
         // given
         val mineFieldGenerator = MineFieldGenerator(object : MinePositionsSelectStrategy {
             override fun getMinePositionsFrom(positions: List<Position>, count: Int): List<Position> {
-                return listOf(Position.of(1, 1))
+                return listOf(Position.of(3, 1), Position.of(1, 2), Position.of(2, 3))
             }
         })
         val expectedMineField = MineField(
-            Rectangle(2, 2),
+            Rectangle(3, 3),
             listOf(
-                Mine(1, 1),
-                NormalBlock(2, 1, 0),
-                NormalBlock(1, 2, 0),
-                NormalBlock(2, 2, 0)
+                NormalBlock(1, 1, 1),
+                NormalBlock(2, 1, 2),
+                Mine(3, 1),
+                Mine(1, 2),
+                NormalBlock(2, 2, 3),
+                NormalBlock(3, 2, 2),
+                NormalBlock(1, 3, 2),
+                Mine(2, 3),
+                NormalBlock(3, 3, 1)
             )
         )
+
         // when
-        val mineField = mineFieldGenerator.create(Rectangle(2, 2), 1)
+        val mineField = mineFieldGenerator.create(Rectangle(3, 3), 3)
 
         // then
         assertThat(mineField).isEqualTo(expectedMineField)
