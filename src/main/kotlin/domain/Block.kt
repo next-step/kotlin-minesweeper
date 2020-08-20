88a6958ecd50956c48f8a424075648d78ce72a67
zion830 <zion830@honglab.org>
2020-08-21 03:15:53 +0900

    feat(minesweeper): 보드를 구성하는 Block 구현

---
+++ src/main/kotlin/domain/Block.kt
@@ -0,0 +1,20 @@
+package domain
+
+data class Block(
+    val type: BlockType = BlockType.GENERAL
+) {
+    var isOpened: Boolean = false
+        private set
+    var mineCount = type.defaultValue
+        private set
+
+    fun increaseMineCount() {
+        if (type != BlockType.MINE) {
+            mineCount++
+        }
+    }
+
+    fun open() {
+        isOpened = true
+    }
+}
---
+++ src/test/kotlin/domain/BlockTest.kt
@@ -0,0 +1,21 @@
+package domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class BlockTest {
+
+    @Test
+    fun `일반 블록은 주변에 있는 지뢰 개수를 가진다`() {
+        val general = Block().apply { increaseMineCount() }
+        val mine = Block(BlockType.MINE).apply { increaseMineCount() }
+        assertThat(general.mineCount).isEqualTo(1)
+        assertThat(mine.mineCount).isEqualTo(-1)
+    }
+
+    @Test
+    fun `사용자가 공개할 블록을 선택할 수 있다`() {
+        val block = Block().apply { open() }
+        assertThat(block.isOpened).isTrue()
+    }
+}
