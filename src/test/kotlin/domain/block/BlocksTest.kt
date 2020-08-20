0cea3ced9cac7d87709e5f1f49805f9d331f91eb
Malibin <nightmare73@naver.com>
2020-08-20 17:00:45 +0900

    refactor(step3): Blocks test 늘리기 및 GameException으로 변경

--- src/test/kotlin/domain/block/BlocksTest.kt
+++ src/test/kotlin/domain/block/BlocksTest.kt
@@ -1,5 +1,7 @@
 package domain.block
 
+import domain.GameOverException
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
@@ -19,7 +21,7 @@ class BlocksTest {
 
         // then
         assertThatThrownBy { blocks.open(Position.of(0, 0)) }
-            .isInstanceOf(IllegalArgumentException::class.java)
+            .isInstanceOf(GameOverException::class.java)
             .hasMessage("해당 칸은 지뢰입니다!")
     }
 
@@ -40,5 +42,60 @@ class BlocksTest {
         assertThatThrownBy { blocks.open(invalidPosition) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("${invalidPosition}에 해당하는 칸을 찾을 수 없습니다.")
+    }
+
+    /**
+     *  0   1   1        0   1   □
+     *  1   2   *   =>   1   2   □
+     *  *   2   1        □   □   □
+     */
+    @Test
+    fun `지뢰가 아닌 칸 열었을 때 인접 칸 열리는지 확인`() {
+        // given
+        val blocks = Blocks(
+            listOf(
+                NormalBlock(1, 1, 0),
+                NormalBlock(2, 1, 1),
+                NormalBlock(3, 1, 1),
+                NormalBlock(1, 2, 1),
+                NormalBlock(2, 2, 2),
+                Mine(3, 2),
+                Mine(1, 3),
+                NormalBlock(2, 3, 2),
+                NormalBlock(3, 3, 1)
+            )
+        )
+        val expectedBlocks = Blocks(
+            listOf(
+                NormalBlock(1, 1, 0, false),
+                NormalBlock(2, 1, 1, false),
+                NormalBlock(3, 1, 1),
+                NormalBlock(1, 2, 1, false),
+                NormalBlock(2, 2, 2, false),
+                Mine(3, 2),
+                Mine(1, 3),
+                NormalBlock(2, 3, 2),
+                NormalBlock(3, 3, 1)
+            )
+        )
+
+        // then
+        assertThat(blocks.open(Position.of(1, 1))).isEqualTo(expectedBlocks)
+    }
+
+    @Test
+    fun `지뢰가 아닌 모든 칸이 열렸는지 확인`() {
+        // given
+        val blocks = Blocks(
+            listOf(
+                Mine(0, 0),
+                NormalBlock(1, 0, 1, false),
+                NormalBlock(0, 1, 1, false),
+                NormalBlock(1, 1, 1, false)
+            )
+        )
+
+        // then
+        assertThat(blocks.isAllNormalBlocksOpened()).isTrue()
     }
 }
