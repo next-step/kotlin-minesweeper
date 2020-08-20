f8da35d178e371668a9e744a11f92ffa4f77f1aa
Malibin <nightmare73@naver.com>
2020-08-21 01:12:36 +0900

    refactor(step3): Blocks 내부 찾는 로직 변경
    Block을 기반으로 찾는게 아닌, Position을 기반으로 찾게 모두 변경

--- src/main/kotlin/domain/block/Blocks.kt
+++ src/main/kotlin/domain/block/Blocks.kt
@@ -10,42 +10,57 @@ data class Blocks(
     private fun findAllNonMineBlocks(): List<Block> = values.filter { it !is Mine }
 
     fun open(position: Position): Blocks {
-        val blockToOpen = getIn(position)
+        val blockToOpen = findBlockIn(position)
         if (blockToOpen.isMine()) throw GameOverException("해당 칸은 지뢰입니다!")
-        val visitedBlocks = visitBlocks(listOf(blockToOpen))
-        return Blocks(createOpenedBlocks(visitedBlocks))
+        val visitedPositions = visitPositions(listOf(position))
+        return Blocks(createOpenedBlocks(visitedPositions))
     }
 
-    private fun createOpenedBlocks(visitedBlocks: List<Block>): List<Block> {
+    private fun createOpenedBlocks(visitedPositions: List<Position>): List<Block> {
+        val visitedBlocks = findBlocksIn(visitedPositions)
         val closedBlocks = this.values - visitedBlocks
-        val openedBlocks = visitedBlocks
+        val openedBlocks = visitedBlocks.map { OpenedBlock(it.position, it.getMinesCount()) as Block }
         return (openedBlocks + closedBlocks).sortedBy { it.position }
     }
 
-    private fun getIn(position: Position): Block {
+    private fun findBlockIn(position: Position): Block {
         return values.find { it.isAt(position) }
             ?: throw IllegalArgumentException("${position}에 해당하는 칸을 찾을 수 없습니다.")
     }
 
-    private tailrec fun visitBlocks(blocksToVisit: List<Block>, visitedBlocks: List<Block> = emptyList()): List<Block> {
-        if (blocksToVisit.isEmpty()) return visitedBlocks
-        val newVisitedBlocks = visitedBlocks + blocksToVisit
-        val nextVisitBlocks = getNextVisitBlocks(blocksToVisit) - newVisitedBlocks
-        return visitBlocks(nextVisitBlocks, newVisitedBlocks)
+    private tailrec fun visitPositions(
+        positionsToVisit: List<Position>,
+        visitedPositions: List<Position> = emptyList()
+    ): List<Position> {
+        if (positionsToVisit.isEmpty()) return visitedPositions
+        val newVisitedPositions = visitedPositions + positionsToVisit
+        val nextVisitPositions = getNextVisitPositions(positionsToVisit) - newVisitedPositions
+        return visitPositions(nextVisitPositions, newVisitedPositions)
     }
 
-    private fun getNextVisitBlocks(blocks: List<Block>): List<Block> {
-        return blocks.flatMap { getNextVisitBlocks(it) }.distinct()
+    private fun getNextVisitPositions(positions: List<Position>): List<Position> {
+        return positions.flatMap { getNextVisitPositions(it) }.distinct()
     }
 
-    private fun getNextVisitBlocks(block: Block): List<Block> {
-        require(!block.isMine()) { "탐색하는 블럭이 지뢰일 수 없습니다. $block" }
-        if (block.getMinesCount() == 0) return getSurroundingsOf(block)
+    private fun getNextVisitPositions(position: Position): List<Position> {
+        val minesCount = countMinesAround(position)
+        if (minesCount == 0) return filterInBlocks(position.surroundings())
         return emptyList()
     }
 
-    private fun getSurroundingsOf(block: Block): List<Block> {
-        val surroundPositions = block.getSurroundingPositions()
-        return values.filter { surroundPositions.contains(it.position) }
+    private fun filterInBlocks(positions: List<Position>): List<Position> {
+        return this.values.asSequence()
+            .map { it.position }
+            .filter { positions.contains(it) }
+            .toList()
+    }
+
+    private fun countMinesAround(position: Position): Int {
+        return findBlocksIn(position.surroundings())
+            .count { it.isMine() }
+    }
+
+    private fun findBlocksIn(positions: List<Position>): List<Block> {
+        return this.values.filter { positions.contains(it.position) }
     }
 }
