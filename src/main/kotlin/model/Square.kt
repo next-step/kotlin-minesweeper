7597ccd88bc56f168c14b6843fcd300f663a112a
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-08-21 22:52:49 +0900

    feature(minesweeper-step1) private accessor 로 변경하다

--- src/main/kotlin/model/Square.kt
+++ src/main/kotlin/model/Square.kt
@@ -1,6 +1,6 @@
 package model
 
-class Square(val width: Line, val height: Line) {
+class Square(private val width: Line, private val height: Line) {
     fun make(): MinePlate {
         val rows = Array(height.value) { Array(width.value) { "C" } }
         return MinePlate(rows)
