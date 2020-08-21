b575bee755d56fff7a62430d45be0d9fb73bb215
zion830 <zion830@honglab.org>
2020-08-21 20:52:14 +0900

    feat(minesweeper): 결과 출력 View 구현

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -1,13 +1,17 @@
 package view
 
 import domain.Block
-import domain.BlockType
 import domain.Board
+import domain.Result
 
 object ResultView {
     private const val START_GAME = "지뢰찾기 게임 시작"
     private const val SYMBOL_HIDDEN: String = "?"
     private const val SYMBOL_MINE = "X"
+    private const val RESULT_WIN = "You Win"
+    private const val RESULT_LOSE = "You Lose"
+    private const val RESULT_ALREADY_OPEN = "이미 열어본 칸입니다."
+    private const val RESULT_INVALID = "올바른 좌표가 아닙니다"
 
     fun printStart(board: Board) {
         println(START_GAME)
@@ -22,8 +26,20 @@ object ResultView {
     }
 
     private fun findSymbol(block: Block) = when {
-        block.type == BlockType.MINE -> SYMBOL_MINE
-        block.isOpened -> block.mineCount.toString()
-        else -> SYMBOL_HIDDEN
+        !block.isOpened -> SYMBOL_HIDDEN
+        block.isMine() -> SYMBOL_MINE
+        else -> block.mineCount.toString()
+    }
+
+    fun printResult(result: Result, board: Board) {
+        println(
+            when (result) {
+                Result.WIN -> RESULT_WIN
+                Result.ALREADY_OPEN -> RESULT_ALREADY_OPEN
+                Result.INVALID -> RESULT_INVALID
+                else -> RESULT_LOSE
+            }
+        )
+        printBoard(board)
     }
 }
