8e2e20492086f1d490902ae180f11d48560fb501
bky373 <bo373@naver.com>
2020-08-23 01:52:58 +0900

    refactor(view) : modify object(ResultView)

--- src/main/kotlin/minesweeper/view/ResultView.kt
+++ src/main/kotlin/minesweeper/view/ResultView.kt
@@ -1,18 +1,14 @@
 package minesweeper.view
 
-import minesweeper.domain.MineMap
+import minesweeper.domain.CityMap
 
 private const val ANNOUNCE_GAME_START = "지뢰찾기 게임 시작"
-private const val BLANK_SEPARATOR = " "
 
 object ResultView {
 
-    fun showMineMap(mineMap: MineMap) {
+    fun showCityMap(cityMap: CityMap) {
         println("\n$ANNOUNCE_GAME_START")
 
-        val map = mineMap.getMineCountedMap()
-        map.indices.forEach {
-            println(map[it].joinToString(BLANK_SEPARATOR))
-        }
+        println(cityMap)
     }
 }
