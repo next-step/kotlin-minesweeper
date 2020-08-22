f05f8fe1be6eee29b89ebeded9ea8b9fb74340b5
developer-donghwan <ycdoit@naver.com>
2020-08-23 00:28:17 +0900

    feat:view에 조건 추가

--- src/main/kotlin/view/Input.kt
+++ src/main/kotlin/view/Input.kt
@@ -4,18 +4,24 @@ import kotlin.system.exitProcess
 
 object Input {
     fun inputHeight(): Int {
-        println("높이를 입력하세요.")
-        return readLineToInt()
+        println("높이를 입력하세요. 1보단 커야 합니다.")
+        val height = readLineToInt()
+        if (0 >= height) exitProcess(-1)
+        return height
     }
 
     fun inputWidth(): Int {
-        println("너비를 입력하세요.")
-        return readLineToInt()
+        println("너비를 입력하세요. 1보단 커야 합니다.")
+        val width = readLineToInt()
+        if (0 >= width) exitProcess(-1)
+        return width
     }
 
-    fun inputMine(): Int {
-        println("지뢰는 몇 개인가요?")
-        return readLineToInt()
+    fun inputMine(maxCount: Int): Int {
+        println("지뢰는 몇 개인가요? ${maxCount}를 초과할 순 없습니다.")
+        val mineCount = readLineToInt()
+        if (0 >= mineCount || mineCount > maxCount) exitProcess(-1)
+        return mineCount
     }
 
     private fun readLineToInt(): Int {
