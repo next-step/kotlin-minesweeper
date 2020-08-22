48fb942ce8a9710219201a03eab5764defbb8b4e
Malibin <nightmare73@naver.com>
2020-08-22 18:56:30 +0900

    Revert "refactor(step3): 블록 방문 재귀함수 걷어냄"
    This reverts commit 784dbe1a404f7008fac41be6380b5ccb976d5340.

--- src/main/kotlin/domain/block/Blocks.kt
+++ src/main/kotlin/domain/block/Blocks.kt
@@ -1,28 +1,26 @@
 package domain.block
 
 import domain.GameOverException
-import java.util.LinkedList
 
 data class Blocks(
     val values: List<Block>
 ) {
-    fun isAllNormalBlocksOpened(): Boolean {
-        return values.filter { it !is Mine }
-            .all { !it.isClosed() }
-    }
+    fun isAllNormalBlocksOpened(): Boolean = findAllNonMineBlocks().all { !it.isClosed() }
+
+    private fun findAllNonMineBlocks(): List<Block> = values.filter { it !is Mine }
 
     fun open(position: Position): Blocks {
         val blockToOpen = findBlockIn(position)
         if (blockToOpen.isMine()) throw GameOverException("해당 칸은 지뢰입니다!")
-        val visitedBlocks = visitBlocks(position)
-        val blocks = findClosedBlocks(visitedBlocks) + visitedBlocks
-        return Blocks(blocks.sortedBy { it.position })
+        val visitedPositions = visitPositions(listOf(position))
+        return Blocks(createOpenedBlocks(visitedPositions))
     }
 
-    private fun findClosedBlocks(visitedBlocks: List<Block>): List<Block> {
-        val visitedPositions = visitedBlocks.map { it.position }
-        return this.values
-            .filter { !visitedPositions.contains(it.position) }
+    private fun createOpenedBlocks(visitedPositions: List<Position>): List<Block> {
+        val visitedBlocks = findBlocksIn(visitedPositions)
+        val closedBlocks = this.values - visitedBlocks
+        val openedBlocks = visitedBlocks.map { OpenedBlock(it.position, it.getMinesCount()) as Block }
+        return (openedBlocks + closedBlocks).sortedBy { it.position }
     }
 
     private fun findBlockIn(position: Position): Block {
@@ -30,36 +28,22 @@ data class Blocks(
             ?: throw IllegalArgumentException("${position}에 해당하는 칸을 찾을 수 없습니다.")
     }
 
-    private fun visitBlocks(startPosition: Position): List<Block> {
-        val positionsQueue = LinkedList<Position>(listOf(startPosition))
-        val visitedBlocks = mutableListOf<Block>()
-        while (positionsQueue.isNotEmpty()) {
-            val position = positionsQueue.poll()
-            val minesCount = countMinesAround(position)
-            visitedBlocks.add(OpenedBlock(position, minesCount))
-            val visitedPositions = combineVisitedPositions(visitedBlocks, positionsQueue)
-            positionsQueue.addAll(getNextVisitPositions(position, minesCount) - visitedPositions)
-        }
-        return visitedBlocks
+    private tailrec fun visitPositions(
+        positionsToVisit: List<Position>,
+        visitedPositions: List<Position> = emptyList()
+    ): List<Position> {
+        if (positionsToVisit.isEmpty()) return visitedPositions
+        val newVisitedPositions = visitedPositions + positionsToVisit
+        val nextVisitPositions = getNextVisitPositions(positionsToVisit) - newVisitedPositions
+        return visitPositions(nextVisitPositions, newVisitedPositions)
     }
 
-    private fun countMinesAround(position: Position): Int {
-        return findBlocksIn(position.surroundings())
-            .count { it.isMine() }
+    private fun getNextVisitPositions(positions: List<Position>): List<Position> {
+        return positions.flatMap { getNextVisitPositions(it) }.distinct()
     }
 
-    private fun findBlocksIn(positions: List<Position>): List<Block> {
-        return this.values.filter { positions.contains(it.position) }
-    }
-
-    private fun combineVisitedPositions(visitedBlocks: List<Block>, visitingPositions: List<Position>): List<Position> {
-        return visitedBlocks.asSequence()
-            .map { it.position }
-            .plus(visitingPositions)
-            .toList()
-    }
-
-    private fun getNextVisitPositions(position: Position, minesCount: Int): List<Position> {
+    private fun getNextVisitPositions(position: Position): List<Position> {
+        val minesCount = countMinesAround(position)
         if (minesCount == 0) return filterInBlocks(position.surroundings())
         return emptyList()
     }
@@ -69,5 +53,14 @@ data class Blocks(
             .map { it.position }
             .filter { positions.contains(it) }
             .toList()
+    }
+
+    private fun countMinesAround(position: Position): Int {
+        return findBlocksIn(position.surroundings())
+            .count { it.isMine() }
+    }
+
+    private fun findBlocksIn(positions: List<Position>): List<Block> {
+        return this.values.filter { positions.contains(it.position) }
     }
 }
