ae75c9d39541238f6f886b65163b8518526d0986
BeokBeok <kekemusa37@nate.com>
2020-08-22 23:10:36 +0900

    feat(Winner) : 추가 - 게임에서 승리할 경우 TC

--- src/test/kotlin/model/WinnerTest.kt
+++ src/test/kotlin/model/WinnerTest.kt
@@ -1,6 +1,6 @@
 package model
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class WinnerTest {
@@ -14,6 +14,18 @@ class WinnerTest {
         val gamer = Gamer(board)
         gamer.clickCoordinate(Coordinates(1, 2))
 
-        Assertions.assertThat(Winner.isLose(gamer)).isTrue()
+        assertThat(Winner.isLose(gamer)).isTrue()
+    }
+
+    @Test
+    fun `게임에서 승리할 경우`() {
+        val board = Board(
+            boardSize = BoardSize(row = LengthOfSide(length = 10), col = LengthOfSide(length = 10)),
+            mineIndexes = listOf(99)
+        )
+        val gamer = Gamer(board)
+        gamer.clickCoordinate(Coordinates(0, 0))
+
+        assertThat(Winner.isLose(gamer)).isFalse()
     }
 }
