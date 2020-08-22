056bbd72997e983fb36d5bcb2633f97947843aa3
Malibin <nightmare73@naver.com>
2020-08-23 03:45:50 +0900

    refactor(step3): List 인스턴스가 적게 생기게 리팩터링

--- src/main/kotlin/domain/block/Blocks.kt
+++ src/main/kotlin/domain/block/Blocks.kt
@@ -10,42 +10,60 @@ data class Blocks(
     private fun findAllNonMineBlocks(): List<Block> = values.filter { it !is Mine }
 
     fun open(position: Position): Blocks {
-        val blockToOpen = getIn(position)
+        val blockToOpen = findBlockIn(position)
         if (blockToOpen.isMine()) throw GameOverException("해당 칸은 지뢰입니다!")
-        val visitedBlocks = visitBlocks(listOf(blockToOpen))
-        return Blocks(createOpenedBlocks(visitedBlocks))
+        val openedBlocks = openBlocks(mutableListOf(position))
+        return Blocks(replaceOpenedBlocks(openedBlocks))
     }
 
-    private fun createOpenedBlocks(visitedBlocks: List<Block>): List<Block> {
-        val closedBlocks = this.values - visitedBlocks
-        val openedBlocks = visitedBlocks
-        return (openedBlocks + closedBlocks).sortedBy { it.position }
+    private fun replaceOpenedBlocks(openedBlocks: List<Block>): List<Block> {
+        val visitedPositions = openedBlocks.map { it.position }
+        val closedBlocks = values.filter { !visitedPositions.contains(it.position) }
+        return (closedBlocks + openedBlocks).sortedBy { it.position }
     }
 
-    private fun getIn(position: Position): Block {
+    private fun findBlockIn(position: Position): Block {
         return values.find { it.isAt(position) }
             ?: throw IllegalArgumentException("${position}에 해당하는 칸을 찾을 수 없습니다.")
     }
 
-    private tailrec fun visitBlocks(blocksToVisit: List<Block>, visitedBlocks: List<Block> = emptyList()): List<Block> {
-        if (blocksToVisit.isEmpty()) return visitedBlocks
-        val newVisitedBlocks = visitedBlocks + blocksToVisit
-        val nextVisitBlocks = getNextVisitBlocks(blocksToVisit) - newVisitedBlocks
-        return visitBlocks(nextVisitBlocks, newVisitedBlocks)
+    private tailrec fun openBlocks(
+        positionsToVisit: MutableList<Position>,
+        visitedBlocks: MutableList<Block> = mutableListOf()
+    ): List<Block> {
+        if (positionsToVisit.isEmpty()) return visitedBlocks
+        val openedBlocks = createOpenedBlocks(positionsToVisit)
+        visitedBlocks.addAll(openedBlocks)
+        positionsToVisit.clear()
+        positionsToVisit.addAll(getNextVisitPositions(openedBlocks, visitedBlocks))
+        return openBlocks(positionsToVisit, visitedBlocks)
     }
 
-    private fun getNextVisitBlocks(blocks: List<Block>): List<Block> {
-        return blocks.flatMap { getNextVisitBlocks(it) }.distinct()
+    private fun createOpenedBlocks(positions: List<Position>): List<OpenedBlock> {
+        return positions.map { OpenedBlock(it, countMinesAround(it)) }
     }
 
-    private fun getNextVisitBlocks(block: Block): List<Block> {
+    private fun countMinesAround(position: Position): Int {
+        return findBlocksIn(position.surroundings())
+            .count { it.isMine() }
+    }
+
+    private fun findBlocksIn(positions: List<Position>): List<Block> {
+        return this.values.filter { positions.contains(it.position) }
+    }
+
+    private fun getNextVisitPositions(blocks: List<OpenedBlock>, visitedBlocks: List<Block>): List<Position> {
+        val visitedPositions = visitedBlocks.map { it.position }
+        val positions = blocks.flatMap { getNextVisitPositions(it) }.distinct()
+        return this.values.asSequence()
+            .map { it.position }
+            .filter { positions.contains(it) }
+            .toList() - visitedPositions
+    }
+
+    private fun getNextVisitPositions(block: OpenedBlock): List<Position> {
         require(!block.isMine()) { "탐색하는 블럭이 지뢰일 수 없습니다. $block" }
-        if (block.getMinesCount() == 0) return getSurroundingsOf(block)
+        if (block.getMinesCount() == 0) return block.getSurroundingPositions()
         return emptyList()
-    }
-
-    private fun getSurroundingsOf(block: Block): List<Block> {
-        val surroundPositions = block.getSurroundingPositions()
-        return values.filter { surroundPositions.contains(it.position) }
     }
 }
