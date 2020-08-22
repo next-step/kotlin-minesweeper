b9dc783a23d20b28b43504f02038418c8b4e7059
bky373 <bo373@naver.com>
2020-08-23 01:51:59 +0900

    feat(domain) : create class(CityMap)

---
+++ src/main/kotlin/minesweeper/domain/CityMap.kt
@@ -0,0 +1,16 @@
+package minesweeper.domain
+
+class CityMap(private var streets: Streets, mineCount: Int) {
+
+    init {
+        repeat(mineCount) { streets.layMine() }
+        streets = streets.mineCountedMap()
+    }
+
+    constructor(height: Int, width: Int, mineCount: Int) : this(
+        Streets((0 until height).map { number -> Street(number, width) }),
+        mineCount
+    )
+
+    override fun toString(): String = streets.toString()
+}
