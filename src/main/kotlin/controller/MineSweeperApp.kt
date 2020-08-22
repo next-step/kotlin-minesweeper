929aa5519a6b460cec556f879d4683bed358c869
BeokBeok <kekemusa37@nate.com>
2020-08-22 23:19:46 +0900

    feat(MineSweeperApp) : 적용 - 지뢰찾기 게임 실행

--- src/main/kotlin/controller/MineSweeperApp.kt
+++ src/main/kotlin/controller/MineSweeperApp.kt
@@ -2,20 +2,37 @@ package controller
 
 import model.Board
 import model.BoardSize
+import model.Coordinates
+import model.Gamer
 import model.LengthOfSide
 import model.NumberOfMine
+import model.Winner
 import view.InputView
 import view.ResultView
 
 fun main() {
-    val (row, col) = requestLengthOfSide(InputView.Mode.ROW) to requestLengthOfSide(InputView.Mode.COL)
+    registerGamer().also {
+        ResultView.printStartGame()
+        startGameUntilTheEnd(it)
+        ResultView.printResult(it)
+    }
+}
 
+private fun startGameUntilTheEnd(gamer: Gamer) {
+    while (!Winner.isLose(gamer)) {
+        val coordinates = InputView.requestCoordinates()
+        gamer.clickCoordinate(Coordinates(coordinates[0], coordinates[1]))
+        ResultView.printBoard(gamer)
+    }
+}
+
+private fun registerGamer(): Gamer {
+    val (row, col) = requestLengthOfSide(InputView.Mode.ROW) to requestLengthOfSide(InputView.Mode.COL)
     val boardSize = BoardSize(row, col)
     val mineCount = requestMineCount(boardSize)
 
     val board = Board(boardSize, mineCount.getMineIndexes())
-
-    ResultView.printBoard(board)
+    return Gamer(board)
 }
 
 private fun requestMineCount(boardSize: BoardSize): NumberOfMine =
