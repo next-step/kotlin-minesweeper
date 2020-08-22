f0973c3e5f16700e0e6602d7622f1da98cc4048b
BeokBeok <kekemusa37@nate.com>
2020-08-22 23:16:13 +0900

    feat(ResultView) : 수정 - printStartGame 함수

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -4,11 +4,8 @@ import model.Board
 
 object ResultView {
 
-    fun printBoard(board: Board) {
+    fun printStartGame() {
         println("지뢰 찾기 게임 시작")
-        board.grid.map { row ->
-            println(row.map { it.ascii.toChar() }.joinToString())
-        }
     }
 
     fun printError(exception: Throwable?) {
