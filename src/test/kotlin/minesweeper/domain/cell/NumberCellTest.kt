3702b8df7cbac95427995c4e4ab016bfec5d41ba
namjackson <skawo32167@naver.com>
2020-08-24 00:46:04 +0900

    feat(minesweeper count): change naming

--- src/test/kotlin/minesweeper/domain/cell/NumberCellTest.kt
+++ src/test/kotlin/minesweeper/domain/cell/NumberCellTest.kt
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test
 
 class NumberCellTest {
 
-    private val DEFAULT_POSITION = Pair(1, 1).toPosition()
+    private val defaultPosition = Pair(1, 1).toPosition()
 
     @DisplayName(value = "주변의 폭탄의 수는 0보다 커야 한다. ")
     @Test
     fun checkAroundMineCountSmallTest() {
         Assertions.assertThatExceptionOfType(RuntimeException::class.java)
             .isThrownBy {
-                NumberCell(-1, DEFAULT_POSITION)
+                NumberCell(-1, defaultPosition)
             }
     }
 
@@ -22,7 +22,7 @@ class NumberCellTest {
     fun checkAroundMineCountOverTest() {
         Assertions.assertThatExceptionOfType(RuntimeException::class.java)
             .isThrownBy {
-                NumberCell(9, DEFAULT_POSITION)
+                NumberCell(9, defaultPosition)
             }
     }
 }
