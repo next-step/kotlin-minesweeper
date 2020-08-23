5d44892e45e5fa1baf24d41a114144935f7c7641
LeeYounghyeon <leey9494@icloud.com>
2020-08-23 18:09:27 +0900

    feat(minesweeper): Application.kt 적용

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,7 +1,12 @@
+import domain.MineSweeperGame
 import view.InputView
+import view.OutputView
 
 fun main() {
     val height = InputView.inputHeight()
     val weight = InputView.inputWidth()
-    val mine = InputView.inputMine()
+    val totalBoardCount = height * weight
+    val mineCount = InputView.inputMine(MineSweeperGame.validateMineCount(totalBoardCount))
+    val board = MineSweeperGame.initBoard(weight, height, mineCount)
+    OutputView.drawMine(height, weight, board)
 }
