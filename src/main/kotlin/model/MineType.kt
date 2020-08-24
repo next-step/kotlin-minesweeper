f22a8a9beb878c3a64b941c98c7e2d535d35c6b5
BeokBeok <kekemusa37@nate.com>
2020-08-24 23:13:29 +0900

    fix(MineType) : 분리 - ascii

--- src/main/kotlin/model/MineType.kt
+++ src/main/kotlin/model/MineType.kt
@@ -1,18 +1,7 @@
 package model
 
-enum class MineType(val ascii: Int) {
-    NONE(67),
-    ZERO(48),
-    ONE(49),
-    TWO(50),
-    THREE(51),
-    FOUR(52),
-    FIVE(53),
-    SIX(54),
-    SEVEN(55),
-    EIGHT(56),
-    NINE(57),
-    MINE(42);
+enum class MineType {
+    NONE, ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, MINE;
 
     companion object {
         fun findByOrdinal(ordinal: Int): MineType = values()[ordinal]
