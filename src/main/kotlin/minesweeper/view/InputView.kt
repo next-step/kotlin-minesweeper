31748bf130ec4e8b9683c37d682bfb58e710b4fd
Parkjoopika <parkjoohan0603@gmail.com>
2020-08-26 01:47:37 +0900

    refactor: InputView에서 null을 처리하는 방식으로 리펙토링 하였다.

--- src/main/kotlin/minesweeper/view/InputView.kt
+++ src/main/kotlin/minesweeper/view/InputView.kt
@@ -5,47 +5,44 @@ import minesweeper.domain.Coordinate
 object InputView {
     fun inputLength(): Int {
         println("높이를 입력하세요.")
-        return changeInt(readLine())
+        return changeInt(readLine()) ?: inputLength()
     }
 
     fun inputWidth(): Int {
         println("너비를 입력하세요.")
-        return changeInt(readLine())
+        return changeInt(readLine()) ?: inputWidth()
     }
 
     fun inputMines(): Int {
         println("지뢰는 몇 개인가요?")
-        return changeInt(readLine())
+        return changeInt(readLine()) ?: inputMines()
     }
 
     fun inputCoordinate(): Coordinate {
         print("open:")
-        return try {
-            val inputValue = readLine() ?: throw IllegalArgumentException()
-            val list = inputValue.split(",").map { changeInt(it.trim()) }
-            checkListSize(list)
-            Coordinate(list[0], list[1])
-        } catch (e: Exception) {
-            inputCoordinate()
-        }
+        val inputValue = readLine() ?: return inputCoordinate()
+        val list = inputValue.split(",").map { changeInt(it.trim()) ?: return inputCoordinate() }
+        checkListSize(list) ?: return inputCoordinate()
+        return Coordinate(list[0], list[1])
     }
 
-    private fun changeInt(inputValue: String?): Int {
+    private fun changeInt(inputValue: String?): Int? {
         if (inputValue.isNullOrBlank()) {
-            throw IllegalArgumentException("공백값과 null값은 받을수없습니다.")
+            return null
         }
-        try {
-            return inputValue.toInt()
+        return try {
+            inputValue.toInt()
         } catch (e: Exception) {
-            throw IllegalArgumentException("숫자 이외의 값: $inputValue")
+            null
         }
     }
 
-    private fun checkListSize(list: List<Int>) {
+    private fun checkListSize(list: List<Int>): Boolean? {
         if (list.size != COORDINATE_SIZE) {
-            throw IllegalArgumentException("리스트 크기가 ${list.size}입니다. 좌표는 x, y값만 입력할 수 있습니다.")
+            return null
         }
+        return true
     }
 
-    const val COORDINATE_SIZE = 2
+    private const val COORDINATE_SIZE = 2
 }
