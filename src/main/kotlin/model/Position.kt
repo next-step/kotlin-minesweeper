2138ea9527a501cf93e4c8a97af7b8933f7492b7
developer-donghwan <ycdoit@naver.com>
2020-09-07 00:54:39 +0900

    Postion getAround 메소드 추가, Test 코드 추가

--- src/main/kotlin/model/Position.kt
+++ src/main/kotlin/model/Position.kt
@@ -1,3 +1,16 @@
 package model
 
-data class Position(val x: Int, val y: Int)
+data class Position(val x: Int, val y: Int) {
+    fun getAround(mapSize: MapSize): List<Position> {
+        val operand = listOf(-1, 0, 1)
+        return operand.flatMap { x ->
+            operand.map { y ->
+                Position(this.x + x, this.y + y)
+            }
+        }.filter { positionInvalid(it, mapSize) }
+    }
+
+    private fun positionInvalid(position: Position, mapSize: MapSize): Boolean {
+        return position.x in 0 until mapSize.maxX && position.y in 0 until mapSize.maxY && position != this
+    }
+}
