6e9260dfba7140b5cbd725b465e36cc301c6f494
developer-donghwan <ycdoit@naver.com>
2020-09-06 19:06:09 +0900

    View를 제외하고 다시 설계

--- src/main/kotlin/view/Input.kt
+++ src/main/kotlin/view/Input.kt
@@ -1,6 +1,5 @@
 package view
 
-import model.Position
 import kotlin.system.exitProcess
 
 object Input {
--- src/main/kotlin/view/Output.kt
+++ src/main/kotlin/view/Output.kt
@@ -1,15 +1,9 @@
 package view
 
-import model.Game
-
 object Output {
     fun drawMap(game: Game) {
         println("지뢰찾기 게임 시작")
         println(game.toString())
-    }
-
-    fun lose() {
-        println("you lose")
     }
 
     fun win() {
