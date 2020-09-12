6bb64d457406fcabd8be4becaf0ada1f4f248a57
developer-donghwan <ycdoit@naver.com>
2020-09-12 19:49:16 +0900

    remove all

--- src/main/kotlin/model/MapSize.kt
+++
@@ -1,7 +0,0 @@
-package model
-
-class MapSize(x: Size, y: Size) {
-    val size = x.value * y.value
-    val maxX = x.value
-    val maxY = x.value
-}
--- src/main/kotlin/model/MineCount.kt
+++
@@ -1,7 +0,0 @@
-package model
-
-class MineCount(val number: Int, private val maxCount: Int) {
-    init {
-        if (number !in 1..maxCount) throw Exception("범위가 1 ~ ${maxCount}사이에 존재해야 합니다.")
-    }
-}
