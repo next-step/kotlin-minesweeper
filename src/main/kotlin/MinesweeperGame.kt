fd157433bd1629ab2a74d1e6ca22b2619b5dbf07
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-09-12 15:04:03 +0900

    feature(minesweeper-step2) 피드백 반영

--- src/main/kotlin/MinesweeperGame.kt
+++ src/main/kotlin/MinesweeperGame.kt
@@ -17,12 +17,38 @@ class MinesweeperGame() {
 
         for (y in 0 until rowSize) {
             for (x in 0 until colSize) {
-                minePlate.value[y].column.blocks[x].setMineCount(x, y, minePlate)
+                val mineCount = calculateMineCount(x, y, minePlate)
+                minePlate.value[y].column.blocks[x].mineCount = mineCount
             }
         }
 
         return minePlate
     }
+
+    fun calculateMineCount(x: Int, y: Int, minePlate: MinePlate): Int {
+        var mineCount = 0
+        val startXPosition = x - 1
+        val endXPosition = x + 1
+        val startYPosition = y - 1
+        val endYPosition = y + 1
+
+        for (dy in startYPosition..endYPosition) {
+            for (dx in startXPosition..endXPosition) {
+                try {
+                    if(minePlate.value[dy].column.blocks[dx] == minePlate.value[x].column.blocks[y]) {
+                        continue
+                    }
+                    if (minePlate.value[dy].column.blocks[dx].isMine()) {
+                        mineCount++
+                    }
+                } catch (ex: IndexOutOfBoundsException) {
+                    continue
+                }
+            }
+        }
+        return mineCount;
+    }
+
 
     private fun positionList(minePlate: MinePlate): List<Position> {
         val positionList: MutableList<Position> = mutableListOf()
--- src/main/kotlin/model/Block.kt
+++ src/main/kotlin/model/Block.kt
@@ -7,31 +7,7 @@ class Block(var status: MineStatus = MineStatus.block) {
         this.status = MineStatus.mine
     }
 
-    fun setMineCount(x: Int, y: Int, minePlate: MinePlate) {
-        val startXPosition = x - 1
-        val endXPosition = x + 1
-        val startYPosition = y - 1
-        val endYPosition = y + 1
 
-        for (dy in startYPosition..endYPosition) {
-            for (dx in startXPosition..endXPosition) {
-                setMineCountValue(minePlate, dy, dx)
-            }
-        }
-    }
-
-    private fun setMineCountValue(minePlate: MinePlate, dy: Int, dx: Int) {
-        try {
-            if(minePlate.value[dy].column.blocks[dx] == this) {
-                return
-            }
-            if (minePlate.value[dy].column.blocks[dx].isMine()) {
-                mineCount++
-            }
-        } catch (ex: IndexOutOfBoundsException) {
-            return
-        }
-    }
 
     fun isBlock(): Boolean {
         return this.status == MineStatus.block
