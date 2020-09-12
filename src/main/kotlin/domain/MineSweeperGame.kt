73ff5d02f06dbad75e1ab7c15d2bcb148c069cbe
LeeYounghyeon <leey9494@icloud.com>
2020-09-12 14:58:04 +0900

    feat(minesweeper): 지뢰의 위치를 기준으로 지뢰의 갯수를 나타내도록 한다.

--- src/main/kotlin/domain/MineSweeperGame.kt
+++ src/main/kotlin/domain/MineSweeperGame.kt
@@ -4,14 +4,35 @@ object MineSweeperGame {
     fun initBoard(weight: Int, height: Int, mineCount: Int): MineBoard {
         val safetyBlockCount = weight * height - mineCount
         val blocks = getBlocks(mineCount, safetyBlockCount)
+        val board = getInitBoard(blocks, weight)
+        val minePosition = board.filterValues { it.isMine() }
+        getResultBoard(minePosition, board)
+        return MineBoard(board)
+    }
 
-        val board = blocks.mapIndexed { index, block ->
+    private fun getResultBoard(
+        minePosition: Map<Position, Block>,
+        board: MutableMap<Position, Block>
+    ) {
+        minePosition.forEach { position ->
+            val mineX = position.key.getX()
+            val mineY = position.key.getY()
+
+            Direction.values().forEach {
+                board[it.apply(mineX, mineY)]?.setMineCount()
+            }
+        }
+    }
+
+    private fun getInitBoard(
+        blocks: List<Block>,
+        weight: Int
+    ): MutableMap<Position, Block> {
+        return blocks.mapIndexed { index, block ->
             val x = index / weight
             val y = index % weight
             Position(x, y) to block
-        }.toMap()
-
-        return MineBoard(board)
+        }.toMap().toMutableMap()
     }
 
     fun validateMineCount(totalBoardCount: Int): (Int) -> Boolean {
--- src/main/kotlin/domain/Position.kt
+++ src/main/kotlin/domain/Position.kt
@@ -2,4 +2,5 @@ package domain
 
 data class Position(private val x: Int, private val y: Int) {
     fun getX() = x
+    fun getY() = y
 }
