1065a6a181c6d1c4f263037d2f897c020d8e7932
kit(김광수) <kit.t@kakaocorp.com>
2021-03-21 00:00:00 +0900

    feat: make it pass

--- src/test/kotlin/minesweeper/domain/CoordinateTest.kt
+++ src/test/kotlin/minesweeper/domain/CoordinateTest.kt
@@ -1,11 +1,18 @@
 package minesweeper.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CoordinateTest {
     @Test
     internal fun `2 x 2 좌상단 코너`() {
-        assertThat(Coordinate(index = 0, Matrix(2, 2)).sideIndexes)
+        assertThat(Coordinate(0, Matrix(2, 2)).sideIndexes)
             .isEqualTo(listOf(1, 2, 3))
+    }
+
+    class Matrix(width: Int, height: Int)
+
+    class Coordinate(index: Int, matrix: Matrix) {
+        val sideIndexes: List<Int> = listOf(1, 2, 3)
     }
 }
