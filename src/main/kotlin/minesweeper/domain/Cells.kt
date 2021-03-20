b118471c0f5d3d46f257a4f022817327805a10a9
kit(김광수) <kit.t@kakaocorp.com>
2021-03-20 22:36:40 +0900

    test: 높이와 너비, 지뢰 존재

--- src/main/kotlin/minesweeper/domain/Cells.kt
+++ src/main/kotlin/minesweeper/domain/Cells.kt
@@ -2,4 +2,9 @@ package minesweeper.domain
 
 class Cells(val cells: List<Cell>, val width: Int) : List<Cell> by cells {
     val height: Int = size / width
+
+    init {
+        require(width > 0 && height > 0)
+        requireNotNull(cells.find { it.bomb })
+    }
 }
