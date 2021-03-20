b7c91bb438949be7ac0f1aacd12564fb0b287f2e
kit(김광수) <kit.t@kakaocorp.com>
2021-03-20 22:26:59 +0900

    test: 지뢰는 최소 1개이상이어야 한다

--- src/test/kotlin/minesweeper/domain/MotherCellTest.kt
+++ src/test/kotlin/minesweeper/domain/MotherCellTest.kt
@@ -2,16 +2,22 @@ package minesweeper.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class MotherCellTest {
     @Test
     internal fun `높이 너비만큼 셀을 만든다`() {
-        assertThat(MotherCells(10, 10).cells(0)).hasSize(100)
+        assertThat(MotherCells(10, 10).cells(1)).hasSize(100)
     }
 
     @Test
     internal fun `지뢰 개수만큼 생성한다`() {
         val cells: List<Cell> = MotherCells(10, 10).cells(bomb = 10)
         assertThat(cells.filter { it.bomb }).hasSize(10)
+    }
+
+    @Test
+    internal fun `지뢰는 최소 1개이상이어야 한다`() {
+        assertThrows<IllegalArgumentException> { MotherCells(10, 10).cells(bomb = 0) }
     }
 }
