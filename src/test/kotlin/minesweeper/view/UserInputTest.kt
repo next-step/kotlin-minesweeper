8d629cd071c3e7243bad58ac05154cdc9e4321c0
kit(김광수) <kit.t@kakaocorp.com>
2021-03-20 23:38:56 +0900

    test: 숫자가 아니면 오류가 발생한다

--- src/test/kotlin/minesweeper/view/UserInputTest.kt
+++ src/test/kotlin/minesweeper/view/UserInputTest.kt
@@ -2,11 +2,18 @@ package minesweeper.view
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.util.InputMismatchException
 
 class UserInputTest {
     @Test
     fun `숫자를 받는다`() {
         val userInput = UserInput.Int("질문", "10\n")
         assertThat(userInput.answer()).isEqualTo(10)
+    }
+
+    @Test
+    fun `숫자가 아니면 오류가 발생한다`() {
+        assertThrows<InputMismatchException> { UserInput.Int("질문", "a\n").answer() }
     }
 }
