7f5ea120ed7a91f79a59b11f51884b6e2cdbfc78
kit(김광수) <kit.t@kakaocorp.com>
2021-03-21 11:40:53 +0900

    feat: make it pass

--- src/test/kotlin/minesweeper/domain/CoordinateTest.kt
+++ src/test/kotlin/minesweeper/domain/CoordinateTest.kt
@@ -2,6 +2,7 @@ package minesweeper.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import kotlin.math.max
 
 class CoordinateTest {
     @Test
@@ -16,9 +17,47 @@ class CoordinateTest {
             .isEqualTo(listOf(0, 2, 3))
     }
 
-    class Matrix(width: Int, height: Int)
+    class Matrix(private val width: Int, private val height: Int) {
+        private val base = max(width, height)
+        fun around(index: Int): List<Int> {
+            val (x, y) = index.toString(base)
+                .let {
+                    if (it.length == 1) "0$it" else it
+                }
+                .map { Character.getNumericValue(it) }.toList()
+            return Around.apply(x, y).filter { valid(it) }
+                .map { "${it.x}${it.y}" }.map { it.toInt(base) }
+        }
 
-    class Coordinate(index: Int, matrix: Matrix) {
-        val sideIndexes: List<Int> = listOf(1, 2, 3)
+        private fun valid(position: Position): Boolean {
+            if (position.x < 0 || position.y < 0) {
+                return false
+            }
+            if (position.x > width - 1 || position.y > height - 1) {
+                return false
+            }
+            return true
+        }
+    }
+
+    enum class Around(private val x: Int, private val y: Int) {
+        LEFT_TOP(-1, -1), TOP(0, -1), TOP_RIGHT(1, -1),
+        LEFT(-1, 0), RIGHT(1, 0),
+        LEFT_BOTTOM(-1, 1), BOTTOM(0, 1), BOTTOM_RIGHT(1, 1);
+
+        companion object {
+            fun apply(x: Int, y: Int): List<Position> {
+                return values().map { Position(it.x + x, it.y + y) }
+            }
+        }
+    }
+
+    class Position(val x: Int, val y: Int)
+
+    class Coordinate(private val index: Int, private val matrix: Matrix) {
+        val sideIndexes: List<Int>
+            get() {
+                return matrix.around(index).sorted()
+            }
     }
 }
