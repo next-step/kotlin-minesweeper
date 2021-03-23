3339490a21ffc76a34daf0ce7c700f20314b0b29
kit(김광수) <kit.t@kakaocorp.com>
2021-03-23 23:36:48 +0900

    refactor: delete toString

--- src/main/kotlin/minesweeper/domain/Position.kt
+++ src/main/kotlin/minesweeper/domain/Position.kt
@@ -2,8 +2,4 @@ package minesweeper.domain
 
 data class Position(val x: Int, val y: Int) {
     fun insideOf(width: Int, height: Int): Boolean = !(x > width - 1 || y > height - 1 || x < 0 || y < 0)
-
-    override fun toString(): String {
-        return "$x, $y"
-    }
 }
