353bbb5ab42f8aee46e8f2d7e8a19bb6405f94c3
SeoJinHyuk14 <suhjh14@gmail.com>
2021-03-27 00:59:54 +0900

    셀의 일급 컬렉션을 추가한다

---
+++ src/main/kotlin/Cells.kt
@@ -0,0 +1,7 @@
+class Cells(val cells: List<Cell>, private val width: Int, private val height: Int) {
+
+    init {
+        require(width > 0 && height > 0) { "너비와 높이는 0보다 커야합니다" }
+        require(cells.size == width * height) { "셀의 크기는 너비와 높이를 곱한 값이어야 합니다" }
+    }
+}
