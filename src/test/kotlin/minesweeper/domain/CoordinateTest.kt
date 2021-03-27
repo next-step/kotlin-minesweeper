3f23cef18114d79cf78a66a8de2b353e9d9133e7
hyunssooo <jhs695@gmail.com>
2021-03-27 20:42:51 +0900

    feat: coordinate 추가

---
+++ src/test/kotlin/minesweeper/domain/CoordinateTest.kt
@@ -0,0 +1,18 @@
+package minesweeper.domain
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+
+internal class CoordinateTest {
+
+    @Test
+    fun `coordinate 는 0보다 커야한다 1인 경우 생성 가능`() {
+        assertDoesNotThrow { Coordinate.of(1) }
+    }
+
+    @Test
+    fun `coordinate 는 0보다 커야한다 0인 경우 생성 불가능`() {
+        assertThrows<IllegalArgumentException> { Coordinate.of(0) }
+    }
+}
