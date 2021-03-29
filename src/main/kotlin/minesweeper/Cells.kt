d5c564115add53cadacee0c3f2f26edab2310a7e
SeoJinHyuk14 <suhjh14@gmail.com>
2021-03-29 22:13:17 +0900

    셀에 들어가는 숫자를 구하는 함수를 구현한다

--- src/main/kotlin/minesweeper/Cells.kt
+++ src/main/kotlin/minesweeper/Cells.kt
@@ -6,4 +6,15 @@ class Cells(val cells: List<Cell>, val width: Int, val height: Int) {
         require(width > 0 && height > 0) { "너비와 높이는 0보다 커야합니다" }
         require(cells.size == width * height) { "셀의 크기는 너비와 높이를 곱한 값이어야 합니다" }
     }
+
+    fun getCellValue(index: Int): Int {
+        return Position.values().filter { isExistMine(it, index) }.count()
+    }
+
+    private fun isExistMine(position: Position, index: Int): Boolean {
+        val row: Int = index / width
+        val column: Int = index % width
+        val targetIndex = position.getTargetIndex(index, width)
+        return position.isExist(row, column, width, height) && cells[targetIndex].isMine
+    }
 }
--- src/test/kotlin/minesweeper/CellsTest.kt
+++ src/test/kotlin/minesweeper/CellsTest.kt
@@ -1,8 +1,13 @@
 package minesweeper
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.lang.IllegalArgumentException
+import java.util.stream.Stream
 
 class CellsTest {
 
@@ -33,6 +38,41 @@ class CellsTest {
 
         assertThrows<IllegalArgumentException> {
             Cells(listOf(), width, height)
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("cellValueResultData")
+    fun `셀의 숫자를 정상적으로 가져온다`(index: Int, cellValue: Int) {
+        val mines = listOf(
+            false, false, false, false, false, true, true, false, false, true, true, false,
+            false, false, true, true, false, true, false, false, true, true, false, false, true
+        )
+        val board = Cells(mines.map { Cell(it) }, 5, 5)
+
+        assertThat(board.getCellValue(index)).isEqualTo(cellValue)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun cellValueResultData(): Stream<Arguments?>? {
+            return Stream.of(
+                Arguments.of(0, 2),
+                Arguments.of(1, 2),
+                Arguments.of(2, 1),
+                Arguments.of(3, 1),
+                Arguments.of(4, 1),
+                Arguments.of(7, 1),
+                Arguments.of(8, 2),
+                Arguments.of(11, 5),
+                Arguments.of(12, 2),
+                Arguments.of(13, 3),
+                Arguments.of(16, 5),
+                Arguments.of(18, 3),
+                Arguments.of(19, 2),
+                Arguments.of(22, 2),
+                Arguments.of(23, 2)
+            )
         }
     }
 }
