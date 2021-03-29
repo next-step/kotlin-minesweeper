53c699788e8784645ccc8c005c4a24b9fd306d39
KaSha <30290767+SeoJinHyuk14@users.noreply.github.com>
2021-03-29 14:34:40 +0900

    Step1 지뢰 찾기(그리기) (#43)
    * empty 파일 제거
    
    * 문자열을 정수로 형 변환시 예외가 발생하면 커스텀 예외로직을 따른다
    
    * cell data class 추가
    
    * 셀의 일급 컬렉션을 추가한다
    
    * 랜덤하게 지뢰가 있는 지뢰찾기 게임판을 생성한다
    
    * input 로직을 작성한다
    
    * 결과를 출력한다

---
+++ src/main/kotlin/BoardGenerator.kt
@@ -0,0 +1,12 @@
+class BoardGenerator(private val width: Int, private val height: Int, private val mineCount: Int) {
+    init {
+        require(width > 0 && height > 0 && mineCount > 0) { "너비, 높이, 지뢰 갯수는 0보다 커야합니다" }
+        require(width * height > mineCount) { "지뢰 갯수는 너비와 높이를 곱한 값보다 작아야 합니다" }
+    }
+
+    fun generateRandomCell(): List<Cell> {
+        val cellSize = width * height
+        val initCells = (1..cellSize - mineCount).map { Cell(false) } + (1..mineCount).map { Cell(true) }
+        return initCells.shuffled()
+    }
+}
---
+++ src/main/kotlin/InputView.kt
@@ -0,0 +1,15 @@
+
+fun inputHeight(): String? {
+    println("높이를 입력하세요.")
+    return readLine()
+}
+
+fun inputWidth(): String? {
+    println("너비를 입력하세요.")
+    return readLine()
+}
+
+fun inputMineCount(): String? {
+    println("지뢰는 몇 개인가요?")
+    return readLine()
+}
