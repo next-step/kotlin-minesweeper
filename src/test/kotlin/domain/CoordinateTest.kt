79b48e6e59c8435792e489aae1b1a66bdc66a1bc
bactoria <bactoria@gmail.com>
2021-03-30 04:12:21 +0900

    feat: 둘러싸인 좌표들을 반환하는 기능추가

--- src/test/kotlin/domain/CoordinateTest.kt
+++ src/test/kotlin/domain/CoordinateTest.kt
@@ -2,7 +2,10 @@ package domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class CoordinateTest {
 
@@ -16,5 +19,89 @@ internal class CoordinateTest {
     fun `좌표 생성한다`(x: Int, y: Int) {
         val result = Coordinate(x = x, y = y)
         assertThat(result).isNotNull()
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideSurroundingCoordinates")
+    fun `특정 좌표의 둘러싸는 좌표들을 반환한다`(coordinate: Coordinate, dummyMaxValue: Int, expected: Set<Coordinate>) {
+        val result = coordinate.surroundingCoordinates(maxX = dummyMaxValue, maxY = dummyMaxValue)
+        assertThat(result).containsExactlyInAnyOrder(*expected.toTypedArray())
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideRangedSurroundingCoordinates")
+    fun `특정 좌표의 둘러싸는 좌표들 중 지정한 범위 이내의 좌표만 반환할 수 있다`(
+        coordinate: Coordinate,
+        maxX: Int,
+        maxY: Int,
+        expected: Set<Coordinate>
+    ) {
+        val result = coordinate.surroundingCoordinates(maxX = maxX, maxY = maxY)
+        assertThat(result).containsExactlyInAnyOrder(*expected.toTypedArray())
+    }
+
+    companion object {
+        @JvmStatic
+        private fun provideSurroundingCoordinates(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(
+                    Coordinate(0, 1),
+                    10,
+                    setOf(
+                        Coordinate(1, 1),
+                        Coordinate(1, 2)
+                    )
+                ),
+
+                Arguments.of(
+                    Coordinate(2, 2),
+                    10,
+                    setOf(
+                        Coordinate(1, 1),
+                        Coordinate(1, 2),
+                        Coordinate(1, 3),
+                        Coordinate(2, 1),
+                        Coordinate(2, 3),
+                        Coordinate(3, 1),
+                        Coordinate(3, 2),
+                        Coordinate(3, 3)
+                    )
+                )
+            )
+        }
+
+        @JvmStatic
+        private fun provideRangedSurroundingCoordinates(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(
+                    Coordinate(2, 2),
+                    2,
+                    2,
+                    setOf(
+                        Coordinate(1, 1),
+                        Coordinate(1, 2),
+                        Coordinate(2, 1)
+                    )
+                ),
+                Arguments.of(
+                    Coordinate(1, 1),
+                    1,
+                    1,
+                    emptySet<Coordinate>()
+                ),
+                Arguments.of(
+                    Coordinate(2, 2),
+                    3,
+                    2,
+                    setOf(
+                        Coordinate(1, 1),
+                        Coordinate(2, 1),
+                        Coordinate(3, 1),
+                        Coordinate(1, 2),
+                        Coordinate(3, 2)
+                    )
+                )
+            )
+        }
     }
 }
