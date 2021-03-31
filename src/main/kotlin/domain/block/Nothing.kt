ce27c9f1c2179a901d54376d642fc6df0a496d68
bactoria <bactoria@gmail.com>
2021-03-31 20:18:48 +0900

    refactor: 불필요한 equals/hashcode 제거

--- src/main/kotlin/domain/block/Nothing.kt
+++ src/main/kotlin/domain/block/Nothing.kt
@@ -10,21 +10,6 @@ data class Nothing(private val surroundingMineCount: Int = 0) : Block() {
 
     override fun surroundingMineCount() = surroundingMineCount
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Nothing
-
-        if (surroundingMineCount != other.surroundingMineCount) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return surroundingMineCount
-    }
-
     companion object {
         private const val MIN_SURROUNDING_MINE_COUNT = 0
         private const val MAX_SURROUNDING_MINE_COUNT = 8
