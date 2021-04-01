2f5af686ff47d2a3b03a75963eacc6c34e6304cf
SeoJinHyuk14 <suhjh14@gmail.com>
2021-04-01 23:49:59 +0900

    위치 정보를 입력받아 인덱스로 치환하는 Location을 추가한다

---
+++ src/main/kotlin/minesweeper/Location.kt
@@ -0,0 +1,20 @@
+package minesweeper
+
+class Location private constructor(private val y: Int, private val x: Int) {
+
+    fun getConvertIndex(width: Int): Int {
+        return y * width + x
+    }
+
+    companion object {
+        fun of(location: String?): Location {
+            require(location != null && location.isNotBlank()) { "빈 값을 입력하면 안됩니다." }
+            val locationPart = location.filter { !it.isWhitespace() }.split(",")
+            require(locationPart.size == LOCATION_SIZE) { "숫자 두개를 콤마(,)로 구분하여 입력해주세요" }
+            val locations = locationPart.map { it.toInt() }
+
+            return Location(locations[0], locations[1])
+        }
+        private const val LOCATION_SIZE = 2
+    }
+}
---
+++ src/test/kotlin/minesweeper/LocationTest.kt
@@ -0,0 +1,33 @@
+package minesweeper
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullAndEmptySource
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+
+class LocationTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    fun `빈 값을 입력하면 예외가 발생한다`(location: String?) {
+        assertThrows<IllegalArgumentException> { Location.of(location) }
+    }
+
+    @Test
+    fun `숫자 2개가 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> { Location.of("1,2,3") }
+    }
+
+    @Test
+    fun `숫자와 콤마 이외의 값이 들어오면 예외가 발생한다`() {
+        assertThrows<NumberFormatException> { Location.of("1,ㅁ") }
+    }
+
+    @Test
+    fun `숫자 2개를 올바르게 입력하여 인덱스로 convert 할 수 있다`() {
+        assertThat(Location.of("1, 2").getConvertIndex(5)).isEqualTo(7)
+    }
+}
