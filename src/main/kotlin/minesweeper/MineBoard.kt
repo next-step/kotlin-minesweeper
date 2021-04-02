2e41238ce009838dc30482b78e489fc10e5ffdf1
Joseph <wjsdudtkd95@gmail.com>
2021-04-02 12:16:25 +0900

    feat(minesweeper): 게임 보드를 만들고 마인을 심는 기능 구현

---
+++ src/main/kotlin/minesweeper/MineBoard.kt
@@ -0,0 +1,32 @@
+package minesweeper
+
+import kotlin.random.Random
+
+class MineBoard(
+    val width: Int,
+    val height: Int
+) {
+    val board: Array<Array<String>> = Array<Array<String>>(width) { Array<String>(height) { " C " } }
+
+    fun deployMine(mineNumber: Int) {
+        var deployedMine = 0
+        while(deployedMine < mineNumber) {
+            val row = Random.nextInt(0, height - 1)
+            val column = Random.nextInt(0, width - 1)
+
+            if(deploy(row, column)) deployedMine ++
+        }
+    }
+
+    private fun isDuplicateMine(row: Int, column: Int): Boolean {
+        return board[row][column] == " * "
+    }
+
+    private fun deploy(row: Int, column: Int): Boolean {
+        if(!isDuplicateMine(row, column)) {
+            board[row][column] = " * "
+            return true
+        }
+        return false
+    }
+}
