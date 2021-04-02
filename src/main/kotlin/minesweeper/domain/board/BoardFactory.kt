280faa50a6a276a40a79fe88b033ee236d1b8400
hyunssooo <jhs695@gmail.com>
2021-04-02 16:40:50 +0900

    refactor: BoardFactory 메서드 분리

--- src/main/kotlin/minesweeper/domain/board/BoardFactory.kt
+++ src/main/kotlin/minesweeper/domain/board/BoardFactory.kt
@@ -4,36 +4,42 @@ import minesweeper.domain.position.Coordinate
 import minesweeper.domain.position.Position
 import minesweeper.domain.tile.Blank
 import minesweeper.domain.tile.Mine
+import minesweeper.domain.tile.Tile
 
 object BoardFactory {
 
     fun create(height: Int, width: Int, mineCount: Int): Board {
-        require(height > 0 && width > 0) { "높이와 너비는 0보다 커야한다. 높이: $height, 너비: $width" }
+        val positions = createPositions(width = width, height = height)
 
-        require(mineCount > 0) {
-            "지뢰 개수는 0보다 커야한다."
-        }
+        val tiles = createTiles(height * width, mineCount)
 
-        val tileCount = height * width
+        val elements = positions.mapIndexed { index, position -> position to tiles[index] }
+            .toMap()
+        return Board(elements)
+    }
+
+    private fun createTiles(tileCount: Int, mineCount: Int): List<Tile> {
         require(tileCount >= mineCount) {
-            "지뢰의 개수는 총 타일의 개수보다 같거나 작아야 한다 total: $tileCount, mine: $mineCount "
+            "지뢰의 개수는 총 타일의 개수보다 같거나 작아야 한다 total: $tileCount, mine: $mineCount"
         }
+
+        require(mineCount > 0) { "지뢰 개수는 0보다 커야한다." }
 
         val mines = (1..mineCount).map { Mine() }
 
         val blankCount = tileCount - mineCount
         val blanks = (1..blankCount).map { Blank() }
 
-        val tiles = (mines + blanks).shuffled()
+        return (mines + blanks).shuffled()
+    }
 
-        val positions = (1..width).flatMap { x ->
+    private fun createPositions(width: Int, height: Int): List<Position> {
+        require(height > 0 && width > 0) { "높이와 너비는 0보다 커야한다. 높이: $height, 너비: $width" }
+
+        return (1..width).flatMap { x ->
             (1..height).map { y ->
                 Position(x = Coordinate.of(x), y = Coordinate.of(y))
             }
         }
-
-        val elements = positions.mapIndexed { index, position -> position to tiles[index] }
-            .toMap()
-        return Board(elements)
     }
 }
