4e3d31dbc45c886a782174ec45dd3b782f8132c0
sangw0804 <sangw0804@likelion.org>
2021-04-10 01:12:41 +0900

    style(RandomPositionsBuilder): 변수명 수정

--- src/main/kotlin/model/RandomPositionsBuilder.kt
+++ src/main/kotlin/model/RandomPositionsBuilder.kt
@@ -3,16 +3,13 @@ package model
 import model.board.BoardSize
 
 class RandomPositionsBuilder {
-    fun build(
-        boardSize: BoardSize,
-        mineCount: Int
-    ): Positions {
+    fun build(boardSize: BoardSize, size: Int): Positions {
         return Positions(
             List(boardSize.height) { it }.flatMap { height ->
                 List(boardSize.width) { it }.shuffled().map { width ->
                     Position.get(height, width)
                 }
-            }.shuffled().take(mineCount)
+            }.shuffled().take(size)
         )
     }
 }
