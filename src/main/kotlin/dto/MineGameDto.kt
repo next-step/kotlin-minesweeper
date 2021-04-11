336a7817541172064061a1a4f4998bcb6f5354d1
bactoria <bactoria@gmail.com>
2021-04-11 18:08:44 +0900

    refactor: 한 줄에 점을 하나만 찍는다

--- src/main/kotlin/dto/MineGameDto.kt
+++ src/main/kotlin/dto/MineGameDto.kt
@@ -1,5 +1,6 @@
 package dto
 
+import domain.Coordinate
 import domain.GameState
 import domain.MineBoard
 import domain.block.Block
@@ -7,7 +8,13 @@ import domain.block.Block
 class MineGameDto(gameState: GameState, mineBoard: MineBoard) {
     val state: String = gameState.toString()
     val width: Int = mineBoard.width
-    val board: List<BlockDto> = mineBoard.value.toSortedMap().map { it.value }.map(::BlockDto)
+    val board: List<BlockDto> = toBlockDto(mineBoard.value)
+
+    private fun toBlockDto(board: Map<Coordinate, Block>): List<BlockDto> {
+        return board.toSortedMap()
+            .values
+            .map(::BlockDto)
+    }
 }
 
 class BlockDto(block: Block) {
