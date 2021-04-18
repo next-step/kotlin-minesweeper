9435f7bbda0a7d1cdd9c737d00a4da606d936b4e
taeheon.song <taeheon.song@linecorp.com>
2021-04-18 19:30:33 +0900

    feat(minesweeper) : PositionTest test

---
+++ src/test/kotlin/domain/position/PositionTest.kt
@@ -0,0 +1,17 @@
+package domain.position
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class PositionTest {
+
+    @Test
+    fun aroundPosition() {
+        val aroundPosition = Position(0, 1).aroundPosition()
+
+        Assertions.assertThat(aroundPosition).containsExactlyInAnyOrder(
+            Position(0, 0), Position(0, 2),
+            Position(1, 0), Position(1, 1), Position(1, 2)
+        )
+    }
+}
