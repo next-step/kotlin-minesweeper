cc475c7c906f5d454154f5278fc1c1552a47514a
sangw0804 <sangw0804@likelion.org>
2021-04-18 22:05:33 +0900

    perf(Position): POSITIONS 캐싱 해놓는 객체 갯수 100 * 100 개로 제한

--- src/main/kotlin/model/Position.kt
+++ src/main/kotlin/model/Position.kt
@@ -7,16 +7,13 @@ data class Position private constructor(val heightIndex: Int, val widthIndex: In
     }
 
     companion object {
-        private val POSITIONS = mutableSetOf<Position>()
+        private const val INITIAL_SIZE = 100
+
+        private val POSITIONS = List(INITIAL_SIZE) { it }.flatMap { index -> List(INITIAL_SIZE) { Position(index, it) } }.toSet()
 
         fun get(heightIndex: Int, widthIndex: Int): Position {
-            val position = Position(heightIndex, widthIndex)
-
-            if (!POSITIONS.contains(position)) {
-                POSITIONS.add(position)
-            }
-
-            return POSITIONS.find { it == position }!!
+            return POSITIONS.find { it.heightIndex == heightIndex && it.widthIndex == widthIndex }
+                ?: Position(heightIndex, widthIndex)
         }
     }
 }
