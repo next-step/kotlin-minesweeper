205323d419ee6c9aea6b08ad3e47d2efe6202d5c
sangw0804 <sangw0804@likelion.org>
2021-07-01 01:05:33 +0900

    test(MineScopeTest): 필요없는 ParameterizedTest 제거

--- src/test/kotlin/model/MineScopeTest.kt
+++ src/test/kotlin/model/MineScopeTest.kt
@@ -1,6 +1,7 @@
 package model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -14,13 +15,12 @@ internal class MineScopeTest {
         assertThat(result).isEqualTo(shouldEqual)
     }
 
-    @ParameterizedTest
-    @MethodSource("positionsProvider")
-    fun `getPositions 스코프 범위에 해당하는 모든 Positions 리스트 리턴`(shouldContain: List<Position>, shouldNotContain: List<Position>) {
+    @Test
+    fun `getPositions 스코프 범위에 해당하는 모든 Positions 리스트 리턴`() {
         val mineScope = MineScope(0..2, 1..3)
 
-        assertThat(mineScope.getPositions()).containsAll(shouldContain)
-        assertThat(mineScope.getPositions()).doesNotContainSequence(shouldNotContain)
+        assertThat(mineScope.getPositions()).contains(Position.get(1, 3), Position.get(2, 3), Position.get(0, 1))
+        assertThat(mineScope.getPositions()).doesNotContain(Position.get(0, 0))
     }
 
     companion object {
@@ -49,18 +49,6 @@ internal class MineScopeTest {
                         5,
                         5,
                         MineScope(0..2, 3..4)
-                    )
-                }
-            )
-        }
-
-        @JvmStatic
-        fun positionsProvider(): List<Arguments> {
-            return listOf(
-                Arguments {
-                    arrayOf(
-                        listOf(Position.get(1, 3), Position.get(2, 3), Position.get(0, 1)),
-                        listOf(Position.get(0, 0))
                     )
                 }
             )
