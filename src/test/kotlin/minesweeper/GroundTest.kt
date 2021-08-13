2157ebf5e2312c26eab955c62dd592e2f4146fbd
sgkim94 <tkdrn8578@naver.com>
2021-08-13 22:01:12 +0900

    refactor: 음수 및 0에 대한 값을 입력받는 ParameterizedTest로 변경

--- src/test/kotlin/minesweeper/GroundTest.kt
+++ src/test/kotlin/minesweeper/GroundTest.kt
@@ -1,14 +1,16 @@
 package minesweeper
 
-import org.assertj.core.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class GroundTest {
 
-    @Test
-    fun `높이가 자연수가 아닌경우 예외처리된다`() {
+    @ParameterizedTest
+    @ValueSource(ints = [-10, -5, 0])
+    fun `높이가 자연수가 아닌경우 예외처리된다`(input : Int) {
         assertThatIllegalArgumentException().isThrownBy {
-            Ground(-10)
+            Ground(input)
         }
     }
 }
