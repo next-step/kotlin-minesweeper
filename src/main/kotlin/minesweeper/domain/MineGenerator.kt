056120896e3d9f021cce9e49330dd798a3646bf7
sgkim94 <tkdrn8578@naver.com>
2021-08-19 23:59:46 +0900

    feat: 지뢰 개수가 땅 사이즈보다 같거나 큰 경우 예외처리하도록 로직 및 테스트코드 추가

--- src/main/kotlin/minesweeper/domain/MineGenerator.kt
+++ src/main/kotlin/minesweeper/domain/MineGenerator.kt
@@ -3,6 +3,9 @@ package minesweeper.domain
 class MineGenerator(private val marker: Marker) {
 
     fun generateMinePositions(generator: PositionGenerator, countOfMine: Int): HashSet<Position> {
+        val sizeOfMarker = marker.size()
+        require(countOfMine < sizeOfMarker) { "지뢰는 모든 땅의 개수보다 클 수 없습니다. : $sizeOfMarker"}
+
         val positions = HashSet<Position>()
 
         while (positions.size < countOfMine) {
--- src/test/kotlin/minesweeper/domain/MineGeneratorTest.kt
+++ src/test/kotlin/minesweeper/domain/MineGeneratorTest.kt
@@ -1,5 +1,6 @@
 package minesweeper.domain
 
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -11,5 +12,14 @@ class MineGeneratorTest {
         val positions = mineGenerator.generateMinePositions(RandomPositionGenerator(), 3)
 
         assertThat(positions.size).isEqualTo(3)
+    }
+
+    @Test
+    fun `지뢰의_개수가_땅의_크기보다_큰_경우_예외처리한다`() {
+        val mineGenerator = MineGenerator(Marker(4, 5))
+
+        Assertions.assertThatIllegalArgumentException().isThrownBy {
+            mineGenerator.generateMinePositions(RandomPositionGenerator(), 30)
+        }
     }
 }
