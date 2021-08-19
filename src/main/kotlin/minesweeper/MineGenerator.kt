5baf391fc7b4f13d558ddcedd1620fce6e68a999
sgkim94 <tkdrn8578@naver.com>
2021-08-19 21:16:12 +0900

    feat: 여러개의 mine position을 만들기 위한 generateMinePositions 함수 추가

--- src/main/kotlin/minesweeper/MineGenerator.kt
+++ src/main/kotlin/minesweeper/MineGenerator.kt
@@ -1,7 +1,17 @@
 package minesweeper
 
 object MineGenerator {
-    fun generateMinePosition(generator: PositionGenerator, ground: Ground): Position {
-        return Position(generator.generate(ground.height), ground.vertical)
+
+    fun generateMinePositions(countOfMine: Int, ground: Ground): HashSet<Position> {
+        val positions = HashSet<Position>()
+
+        while (positions.size < countOfMine) {
+            positions.add(generateMinePosition(RandomPositionGenerator(), ground))
+        }
+        return positions
+    }
+
+    private fun generateMinePosition(generator: PositionGenerator, ground: Ground): Position {
+        return Position(generator.generate(ground.height), generator.generate(ground.vertical))
     }
 }
