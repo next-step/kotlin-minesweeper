06fd7668b90174a3304d3bd3996035f0a41fb747
sgkim94 <tkdrn8578@naver.com>
2021-08-19 21:15:34 +0900

    refactor: 0의 매직넘버를 상수로 추출

--- src/main/kotlin/minesweeper/RandomPositionGenerator.kt
+++ src/main/kotlin/minesweeper/RandomPositionGenerator.kt
@@ -5,6 +5,10 @@ import kotlin.random.Random
 class RandomPositionGenerator: PositionGenerator {
 
     override fun generate(end: Int): Int {
-        return Random.nextInt(0, end)
+        return Random.nextInt(MINIMUM_RANDOM_NUMBER, end)
+    }
+
+    companion object {
+        const val MINIMUM_RANDOM_NUMBER = 0
     }
 }
