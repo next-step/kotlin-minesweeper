ac5d185b57c845e66248f27df93aee8b329b52fa
Pyro <rhwjddhks@gmail.com>
2021-11-21 11:47:39 +0900

    feat: CellState 와 MineNumber 의 composition 인 Cell 구현

---
+++ src/main/kotlin/domain/Cell.kt
@@ -0,0 +1,22 @@
+package domain
+
+data class Cell private constructor(private var cellState: CellState, private var mineNumber: MineNumber) {
+    constructor() : this(CellState(), MineNumber())
+
+    fun isOpen(): Boolean = cellState.isOpen()
+    fun isMine(): Boolean = cellState.isMine()
+    fun isBlank(): Boolean = mineNumber.isBlank()
+    fun mineNumber(): Int = mineNumber.number
+
+    fun open() {
+        cellState = cellState.open()
+    }
+
+    fun layMine() {
+        cellState = cellState.layMine()
+    }
+
+    fun increaseMineNumber() {
+        mineNumber++
+    }
+}
