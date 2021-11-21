6640ca4c6ad7b19f29b86df8f7b174fb399cd5d2
Pyro <rhwjddhks@gmail.com>
2021-11-21 12:23:45 +0900

    feat: 지뢰찾기를 실행하는 Controller 구현

---
+++ src/main/kotlin/controller/Controller.kt
@@ -0,0 +1,44 @@
+package controller
+
+import domain.Board
+import domain.MineLayer
+import domain.MineNumber
+import domain.MineSweeper
+import domain.Position
+import domain.RandomPositionSelector
+import dto.BoardDto
+import view.InputView
+import view.OutputView
+
+object Controller {
+    fun run() {
+        val board = askBoard()
+        val selector = RandomPositionSelector(board)
+        val mineLayer = MineLayer(board, selector)
+        val mineSweeper = MineSweeper(board, selector)
+        layMines(mineLayer, mineSweeper)
+        sweepMines(board, mineSweeper)
+    }
+
+    private fun layMines(mineLayer: MineLayer, mineSweeper: MineSweeper) {
+        val mineNumber = askMineNumber()
+        printStart()
+        val position = askPosition()
+        mineLayer.layMines(mineNumber, position)
+        mineSweeper.sweepMine(position)
+    }
+
+    private fun sweepMines(board: Board, mineSweeper: MineSweeper) {
+        do {
+            printBoard(board)
+        } while (!board.isAllOpen() && mineSweeper.sweepMine(askPosition()))
+        printResult(board)
+    }
+
+    private fun askBoard(): Board = Board(InputView.askHeight(), InputView.askWidth())
+    private fun askMineNumber(): MineNumber = MineNumber(InputView.askMineNumber())
+    private fun askPosition(): Position = Position(InputView.askPosition())
+    private fun printStart() = OutputView.printStart()
+    private fun printResult(board: Board) = OutputView.printResult(board.isAllOpen())
+    private fun printBoard(board: Board) = OutputView.printBoard(BoardDto(board))
+}
