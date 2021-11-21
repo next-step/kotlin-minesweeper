cc36a5d3d381e571aa084bbc8c7f331b4b1f0588
Pyro <rhwjddhks@gmail.com>
2021-11-21 11:50:13 +0900

    feat: 지뢰 매설을 위한 랜덤 좌표들을 선택해주는 PositionSelector 구현

---
+++ src/main/kotlin/domain/RandomPositionSelector.kt
@@ -0,0 +1,10 @@
+package domain
+
+class RandomPositionSelector(board: Board) : PositionSelector(board.height(), board.width()) {
+    override fun selectMinePositions(mineNumber: MineNumber, excludedPosition: Position): List<Position> {
+        val positions = allPositions().toMutableList()
+        positions.remove(excludedPosition)
+        return (1..mineNumber.number)
+            .map { positions.removeAt(positions.indices.random()) }
+    }
+}
