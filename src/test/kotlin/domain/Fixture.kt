417c88fbd8cf6ca0b85797cf9f3e07918cef2872
Pyro <rhwjddhks@gmail.com>
2021-11-21 12:16:01 +0900

    test: 테스트 환경 세팅을 위한 Fixture 구현

---
+++ src/test/kotlin/domain/Fixture.kt
@@ -0,0 +1,33 @@
+package domain
+
+import dto.BoardDto
+
+class Fixture {
+    val board = Board(height, width)
+    val selector = object : PositionSelector(height, width) {
+        override fun selectMinePositions(mineNumber: MineNumber, excludedPosition: Position) = minePositions
+    }
+
+    fun drawBoard(): String = BoardDto(board).draw()
+    fun boardMineNumbers(): String = BoardDto(board).mineNumbers()
+    fun renderBoard(): String = BoardDto(board).render()
+
+    companion object {
+        const val height = 10
+        const val width = 10
+        val mineNumber = MineNumber(10)
+        val position = Position()
+        val minePositions = listOf(
+            Position(1, 4),
+            Position(1, 8),
+            Position(2, 3),
+            Position(2, 5),
+            Position(5, 1),
+            Position(6, 7),
+            Position(7, 3),
+            Position(7, 7),
+            Position(8, 7),
+            Position(8, 10)
+        )
+    }
+}
