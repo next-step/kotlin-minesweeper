2298a6dccc3aa0c1deade795115c98b99b244610
Pyro <rhwjddhks@gmail.com>
2021-11-21 11:29:58 +0900

    feat: 입력을 받기 위한 InputView 구현

---
+++ src/main/kotlin/view/InputView.kt
@@ -0,0 +1,33 @@
+package view
+
+object InputView {
+    fun askHeight(): Int {
+        println("\n높이를 입력하세요.")
+        return readNumber()
+    }
+
+    fun askWidth(): Int {
+        println("\n너비를 입력하세요.")
+        return readNumber()
+    }
+
+    fun askMineNumber(): Int {
+        println("\n지뢰는 몇 개인가요?")
+        return readNumber()
+    }
+
+    fun askPosition(): Pair<Int, Int> {
+        print("\nopen: ")
+        val delimiter = ","
+        val positions = readInput().split(delimiter).map { it.toInt() }
+        require(positions.size == 2) { "좌표는 ${delimiter}로 구별되는 2개의 숫자를 입력해야 합니다." }
+        return positions[0] to positions[1]
+    }
+
+    private fun readNumber(): Int = readInput().toInt()
+    private fun readInput(): String {
+        val input = readLine()?.filterNot { it.isWhitespace() }
+        require(!input.isNullOrBlank()) { "빈 값을 입력할 수는 없습니다." }
+        return input
+    }
+}
