2fa9fdc03655b988d28c9b82b296514d62891d9c
Pyro <rhwjddhks@gmail.com>
2021-11-21 12:23:34 +0900

    feat: 지뢰찾기 상황을 출력해주는 OutputView 구현

---
+++ src/main/kotlin/view/OutputView.kt
@@ -0,0 +1,9 @@
+package view
+
+import dto.BoardDto
+
+object OutputView {
+    fun printStart() = println("\n지뢰찾기 게임 시작")
+    fun printResult(win: Boolean) = println("\n${if (win) "Win Game." else "Lose Game."}")
+    fun printBoard(board: BoardDto) = println(board)
+}
