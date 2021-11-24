adeb8f1d113323e38f92a215112f18f5fbf559f1
flamme <flamme1004@gmail.com>
2021-11-24 23:55:34 +0900

    style("minsweeper): boardView에 문구 추가

--- src/main/kotlin/minesweeper/view/ResultView.kt
+++ src/main/kotlin/minesweeper/view/ResultView.kt
@@ -4,8 +4,9 @@ import minesweeper.domain.dto.MineSweeperDTO
 
 object ResultView {
     fun boardView(mineSweeperDTO: MineSweeperDTO) {
+        println("지뢰찾기 게임 시작")
         mineSweeperDTO.rows.forEach {
-            println("$it")
+            println(it)
         }
     }
 }
