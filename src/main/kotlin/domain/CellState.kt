2178317144527b923aacc1e2bf850fbd47e3cf9b
Pyro <rhwjddhks@gmail.com>
2021-11-25 10:35:13 +0900

    refactor: CellState 를 value class 에서 data class 로 변경

--- src/main/kotlin/domain/CellState.kt
+++ src/main/kotlin/domain/CellState.kt
@@ -1,13 +1,11 @@
 package domain
 
-@JvmInline
-value class CellState private constructor(private val state: Pair<OpenState, MineState>) {
-    constructor() : this(OpenState.CLOSED to MineState.NOT_MINE)
+data class CellState(private val open: Boolean = false, private val mine: Boolean = false) {
+    fun isOpen(): Boolean = open
 
-    fun isOpen(): Boolean = state.first == OpenState.OPEN
-    fun isMine(): Boolean = state.second == MineState.MINE
-    fun open(): CellState = CellState(OpenState.OPEN to state.second)
-    fun layMine(): CellState = CellState(state.first to MineState.MINE)
-    private enum class OpenState { OPEN, CLOSED }
-    private enum class MineState { MINE, NOT_MINE }
+    fun isMine(): Boolean = mine
+
+    fun open(): CellState = CellState(true, mine)
+
+    fun layMine(): CellState = CellState(open, true)
 }
