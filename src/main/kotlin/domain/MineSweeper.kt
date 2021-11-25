af5015bfab3fa862d64ac1f9f7137a3621af387a
Pyro <rhwjddhks@gmail.com>
2021-11-25 12:22:32 +0900

    refactor: MineSweeper 의 open 함수를 리팩터링
    - MutableSet 대신 불변 Set 을 사용하도록 함
    - forEach 를 할 때 map 을 통한 함수 체이닝을 더 적극적으로 하도록 함

--- src/main/kotlin/domain/MineSweeper.kt
+++ src/main/kotlin/domain/MineSweeper.kt
@@ -4,18 +4,17 @@ class MineSweeper(private val board: Board, private val selector: PositionSelect
     fun sweepMine(position: Position = Position()): Boolean {
         val cell = board.getCell(position)
         if (!cell.isOpen()) {
-            open(position, mutableSetOf())
+            open(position, setOf())
         }
         return !cell.isMine()
     }
 
-    private fun open(position: Position, visited: MutableSet<Position>) {
+    private fun open(position: Position, visited: Set<Position>) {
         val positions = selector.adjacentPositions(position)
             .filterNot { board.getCell(it).isOpen() || visited.contains(it) }
         positions.forEach { board.getCell(it).open() }
-        positions.filter { board.getCell(it).isBlank() }.forEach {
-            visited.add(it)
-            open(it, visited)
-        }
+        positions.filter { board.getCell(it).isBlank() }
+            .map { it to visited + setOf(it) }
+            .forEach { (position, visited) -> open(position, visited) }
     }
 }
