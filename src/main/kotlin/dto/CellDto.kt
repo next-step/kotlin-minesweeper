e7ebb5d303af48a4b6ea5dd653e2e2610d7c2dc3
Pyro <rhwjddhks@gmail.com>
2021-11-25 22:58:42 +0900

    feat: 지뢰 찾기(지뢰 개수) 구현

--- src/main/kotlin/dto/CellDto.kt
+++ src/main/kotlin/dto/CellDto.kt
@@ -4,7 +4,7 @@ import domain.Cell
 
 @JvmInline
 value class CellDto(private val cell: Cell) {
-    override fun toString(): String = if (cell.hasMine()) MINE else CLOSED
+    override fun toString(): String = if (cell.hasMine()) MINE else cell.mineNumber().toString()
 
     companion object {
         private const val MINE = "*"
