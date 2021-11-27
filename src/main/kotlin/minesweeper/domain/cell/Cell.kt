d7add15f217dcad01a2ff566dea8f74b760f2ab2
flamme <flamme1004@gmail.com>
2021-11-28 00:24:36 +0900

    refactor(minesweeper): cell의 cellState의 수정은 cell에서만 가능하도록 변경

--- src/main/kotlin/minesweeper/domain/cell/Cell.kt
+++ src/main/kotlin/minesweeper/domain/cell/Cell.kt
@@ -3,10 +3,15 @@ package minesweeper.domain.cell
 import minesweeper.domain.position.Position
 import minesweeper.domain.position.Positions
 
-class Cell(val position: Position, val state: CellState) {
+class Cell(val position: Position) {
+
+    lateinit var state: CellState
+        private set
 
     companion object {
         fun of(position: Position, minePositions: Positions): Cell =
-            Cell(position, CellState.from(position, minePositions))
+            Cell(position).apply {
+                this.state = CellState.from(position, minePositions)
+            }
     }
 }
