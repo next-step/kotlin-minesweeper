240e3abd65d3c189a47f2b43b3db7459695c75a9
laco-dev <laco2951@gmail.com>
2021-11-28 23:33:14 +0900

    Column, Row에 대한 증가, 감소 기능 추가

--- src/main/kotlin/minesweeper/model/Row.kt
+++ src/main/kotlin/minesweeper/model/Row.kt
@@ -3,6 +3,10 @@ package minesweeper.model
 @JvmInline
 value class Row(val value: Int) {
 
+    fun increment(): Row = Row(value + 1)
+
+    fun decrement(): Row = Row((value - 1).coerceAtLeast(1))
+
     init {
         require(value > 0)
     }
--- src/test/kotlin/minesweeper/model/ColumnTest.kt
+++ src/test/kotlin/minesweeper/model/ColumnTest.kt
@@ -1,5 +1,7 @@
 package minesweeper.model
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -10,5 +12,23 @@ class ColumnTest {
     @ValueSource(ints = [0, -1, -100, -150])
     fun `열(Column)는 항상 1 이상의 값을 가진다`(value: Int) {
         assertThrows<RuntimeException> { Column(value) }
+    }
+
+    @Test
+    fun `Column(1)을 증가시키면 Column(2)가 된다`() {
+        val column = Column(1)
+        assertThat(column.increment()).isEqualTo(Column(2))
+    }
+
+    @Test
+    fun `Column(2)을 감소시키면 Column(1)이 된다`() {
+        val column = Column(2)
+        assertThat(column.decrement()).isEqualTo(Column(1))
+    }
+
+    @Test
+    fun `Column(1)을 감소시키면 Column(1)이 된다`() {
+        val column = Column(1)
+        assertThat(column.decrement()).isEqualTo(Column(1))
     }
 }
