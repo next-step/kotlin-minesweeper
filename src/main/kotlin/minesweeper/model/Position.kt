32609a248e58f9499cc0c882e097d5ed7f10f7ca
laco-dev <laco2951@gmail.com>
2021-11-28 23:48:29 +0900

    Position의 위치 이동 기능 구현

--- src/main/kotlin/minesweeper/model/Position.kt
+++ src/main/kotlin/minesweeper/model/Position.kt
@@ -2,6 +2,22 @@ package minesweeper.model
 
 data class Position(val row: Row, val column: Column) {
 
+    fun top(): Position = copy(row = row.decrement())
+
+    fun topLeft(): Position = Position(row = row.decrement(), column = column.decrement())
+
+    fun topRight(): Position = Position(row = row.decrement(), column = column.increment())
+
+    fun left(): Position = copy(column = column.decrement())
+
+    fun right(): Position = copy(column = column.increment())
+
+    fun bottom(): Position = copy(row = row.increment())
+
+    fun bottomLeft(): Position = Position(row = row.increment(), column = column.decrement())
+
+    fun bottomRight(): Position = Position(row = row.increment(), column = column.increment())
+
     companion object {
         fun list(width: Width, height: Height): List<Position> = List(width.value * height.value) { index ->
             val row = (index / width.value) + 1
