3331ccd906ba66528fcd5949e2c9c6706b6fbd58
laco-dev <laco2951@gmail.com>
2021-11-28 23:04:13 +0900

    지뢰 개수가 가지는 값 하나를 증가 시킬 수 있다

--- src/test/kotlin/minesweeper/model/MineCountTest.kt
+++ src/test/kotlin/minesweeper/model/MineCountTest.kt
@@ -1,6 +1,7 @@
 package minesweeper.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -10,5 +11,12 @@ class MineCountTest {
     @ValueSource(ints = [-1, -100, -150])
     fun `지뢰 개수가 0보다 작으면 항상 0개가 된다`(count: Int) {
         assertThat(MineCount.valueOf(count)).isEqualTo(MineCount.ZERO)
+    }
+
+    @Test
+    fun `지뢰 개수가 가지는 값 하나를 증가 시킬 수 있다`() {
+        val mineCount = MineCount.ZERO
+        val actual = mineCount.increment()
+        assertThat(actual).isEqualTo(MineCount.valueOf(1))
     }
 }
