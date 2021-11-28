7cc264114ffc8aff50e0defbdd3e81bd8ebc85b8
sangyongchoi <csy7792@naver.com>
2021-11-29 01:16:47 +0900

    step1 apply static method

--- src/test/kotlin/domain/WidthTest.kt
+++ src/test/kotlin/domain/WidthTest.kt
@@ -1,5 +1,6 @@
 package domain
 
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -7,7 +8,7 @@ internal class WidthTest {
     @ValueSource(ints = [-1, 0])
     @ParameterizedTest
     fun `value 의 값이 0 이하라면 에러`(value: Int) {
-        org.junit.jupiter.api.assertThrows<IllegalArgumentException> {
+        assertThrows<IllegalArgumentException> {
             Height(value)
         }
     }
