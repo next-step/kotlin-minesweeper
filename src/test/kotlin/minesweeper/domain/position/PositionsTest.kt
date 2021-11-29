a8e30402da9a327f367b7d866c0ea8686d1cfdba
flamme <flamme1004@gmail.com>
2021-11-30 01:10:22 +0900

    chore(minesweeper): 린트 재설정

--- src/test/kotlin/minesweeper/domain/position/PositionsTest.kt
+++ src/test/kotlin/minesweeper/domain/position/PositionsTest.kt
@@ -2,7 +2,6 @@ package minesweeper.domain.position
 
 import minesweeper.domain.board.BoardSize
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
