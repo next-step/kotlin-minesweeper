4405807074ea9595fa6b8a7c54e328b7d1869e0c
김우재 <kwj1270@naver.com>
2021-12-01 23:52:33 +0900

    feat: Height 실제 비즈니스 로직에 반영

--- src/main/kotlin/minesweeper/MinesweeperApplication.kt
+++ src/main/kotlin/minesweeper/MinesweeperApplication.kt
@@ -2,6 +2,7 @@ package minesweeper
 
 import global.strategy.ConsoleInputStrategy
 import global.strategy.ConsoleOutputStrategy
+import minesweeper.domain.Height
 import minesweeper.ui.ErrorView
 import minesweeper.ui.InputView
 import minesweeper.ui.OutputView
@@ -18,11 +19,23 @@ class MinesweeperApplication(
         val width = inputView.askWidth()
         val numberOfMines = inputView.askNumberOfMines()
 
-        val board = (0 until height)
-            .map { (0 until width).toMutableList() }
+        val board = (0 until height.height)
+            .map { (0 until width).map { 99 }.toMutableList() }
             .toMutableList()
 
-        setMine(numberOfMines, width, height, board)
+        setMine(numberOfMines, width, height.height, board)
+
+        for ((x, mutableList) in board.withIndex()) {
+            for ((y, element) in mutableList.withIndex()) {
+                setNumber(x, y, width, height.height, board)
+            }
+        }
+
+        println(
+            board.joinToString(separator = "\n") {
+                it.fold(StringBuilder()) { acc: StringBuilder, i: Int -> acc.append("$i\t") }
+            }
+        )
 
         outputView.startGame()
         println(
@@ -52,11 +65,50 @@ class MinesweeperApplication(
         }
     }
 
-    private fun askHeight(): Int {
+    fun setNumber(row: Int, column: Int, width: Int, height: Int, board: MutableList<MutableList<Int>>) {
+        if (board[row][column] == 99) {
+            board[row][column] = getMineNumber(row, column, width, height, board);
+        }
+    }
+
+    private fun getMineNumber(
+        row: Int,
+        column: Int,
+        width: Int,
+        height: Int,
+        board: MutableList<MutableList<Int>>
+    ): Int {
+        var mineCnt = 0;
+        if (isExistMine(row - 1, column - 1, width, height, board)) mineCnt++;
+        if (isExistMine(row - 1, column, width, height, board)) mineCnt++;
+        if (isExistMine(row - 1, column + 1, width, height, board)) mineCnt++;
+        if (isExistMine(row, column - 1, width, height, board)) mineCnt++;
+        if (isExistMine(row, column + 1, width, height, board)) mineCnt++;
+        if (isExistMine(row + 1, column - 1, width, height, board)) mineCnt++;
+        if (isExistMine(row + 1, column, width, height, board)) mineCnt++;
+        if (isExistMine(row + 1, column + 1, width, height, board)) mineCnt++;
+        return mineCnt;
+    }
+
+    private fun isExistMine(
+        row: Int,
+        column: Int,
+        width: Int,
+        height: Int,
+        board: MutableList<MutableList<Int>>
+    ): Boolean {
+        if (row < 0 || row >= width || column < 0 || column >= height) {
+            return false;
+        }
+
+        return board[row][column] == -1
+    }
+
+    private fun askHeight(): Height {
         return try {
-            inputView.askHeight()
+            Height(inputView.askHeight())
         } catch (e: Exception) {
-            errorView.alert(e.message!!)
+            errorView.alert(e.message.toString())
             askHeight()
         }
     }
--- src/main/kotlin/minesweeper/domain/Height.kt
+++ src/main/kotlin/minesweeper/domain/Height.kt
@@ -1,6 +1,7 @@
 package minesweeper.domain
 
-class Height(private val height: Int = DEFAULT_HEIGHT) {
+@JvmInline
+value class Height(val height: Int = DEFAULT_HEIGHT) {
     init {
         if (height < DEFAULT_HEIGHT) {
             throw IllegalArgumentException()
