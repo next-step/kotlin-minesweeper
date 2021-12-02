be8984ec0c5868490e11dd4f518bcd21076f3a42
laco-dev <laco2951@gmail.com>
2021-12-02 21:31:10 +0900

    2단계 게임 목적에 맞게 컨트롤러 수정

--- src/main/kotlin/minesweeper/view/resource/StringResource.kt
+++ src/main/kotlin/minesweeper/view/resource/StringResource.kt
@@ -3,7 +3,7 @@ package minesweeper.view.resource
 import minesweeper.model.Cell
 
 fun getString(cell: Cell?): String = when (cell) {
-    is Cell.Zero -> "C"
+    is Cell.Zero -> "0"
     is Cell.Mine -> "*"
     is Cell.Number -> "${cell.adjustMineCount.value}"
     null -> ""
