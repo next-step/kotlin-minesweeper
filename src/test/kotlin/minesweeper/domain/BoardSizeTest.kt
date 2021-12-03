951def273e2cd8dd7828addd5dc9a3716d7b2533
김우재 <kwj1270@naver.com>
2021-12-03 09:39:24 +0900

    test: BoardSize, 넓이를 반환할 수 있다

--- src/test/kotlin/minesweeper/domain/BoardSizeTest.kt
+++ src/test/kotlin/minesweeper/domain/BoardSizeTest.kt
@@ -23,4 +23,16 @@ internal class BoardSizeTest {
             { assertThat(boardSize).isExactlyInstanceOf(BoardSize::class.java) },
         )
     }
+
+    @ParameterizedTest(name = "입력 값: {0}, {1}, {2}")
+    @CsvSource(value = ["1:1:1", "10:1:10", "1:10:10", "10:10:10"], delimiter = ':')
+    fun `넓이를 반환할 수 있다`(widthInt: Int, heightInt: Int) {
+        val width = Width(widthInt)
+        val height = Height(heightInt)
+
+        val boardSize = BoardSize(width, height)
+        val area: Int = boardSize.area()
+
+        assertThat(area).isEqualTo(widthInt * heightInt)
+    }
 }
