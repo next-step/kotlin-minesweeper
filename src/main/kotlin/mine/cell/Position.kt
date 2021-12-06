7dd316d9e479964665d5ca9935092dfe4b336e12
thgus5614@naver.com <thgus5614@naver.com>
2021-12-06 21:38:28 +0900

    improve: position의 nullable 확인하는 메소드 추가

--- src/main/kotlin/mine/cell/Position.kt
+++ src/main/kotlin/mine/cell/Position.kt
@@ -4,21 +4,20 @@ package mine.cell
  * 셀의 위치 정보 관리
  * */
 data class Position(val x: Int, val y: Int) {
-    init {
-        require(x >= MIN_POSITION && y >= MIN_POSITION)
-    }
-
     fun aroundPosition(): List<Position> {
-        val xList = listOf(x - 1, x, x + 1).filter { it >= 0 }
-        val yList = listOf(y - 1, y, y + 1).filter { it >= 0 }
+        val xList = listOf(x - 1, x, x + 1)
+        val yList = listOf(y - 1, y, y + 1)
         return xList
             .flatMap { x ->
-                yList.map { Position(x, it) }
+                yList.map { Position(x, it).ofNullable() }
             }
+            .filterNotNull()
             .filterNot { it.x == x && it.y == y }
     }
 
     companion object {
         const val MIN_POSITION = 0
+
+        fun Position.ofNullable(): Position? = if (x >= MIN_POSITION && y >= MIN_POSITION) Position(x, y) else null
     }
 }
