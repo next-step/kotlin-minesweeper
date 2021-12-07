de54e2ac4dda8a7a07dd4f2b83d99ea62269b4db
김우재 <kwj1270@naver.com>
2021-12-07 14:32:21 +0900

    feat: Directions, 여덟 방향에 대한 좌표값을 계산한다

--- src/test/kotlin/minesweeper/domain/DirectionTest.kt
+++ src/test/kotlin/minesweeper/domain/DirectionTest.kt
@@ -1,5 +1,6 @@
 package minesweeper.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
@@ -9,18 +10,18 @@ import org.junit.jupiter.params.provider.CsvSource
 internal class DirectionTest {
 
     @ParameterizedTest(name = "X값: {0}, Y값: {1}")
-    @CsvSource()
+    @CsvSource(value = ["0:0", "1:0", "0:1", "1:1", "10:10"], delimiter = ':')
     fun `여덟 방향에 대한 좌표값을 계산한다`(x: Int, y: Int) {
 
         assertAll(
-            { Direction.NORTH.nextPosition(x, y).isEqualTo(x to y - 1) },
-            { Direction.NORTH_EAST.nextPosition(x, y).isEqualTo(x + 1 to y) },
-            { Direction.EAST.nextPosition(x, y).isEqualTo(x + 1 to y) },
-            { Direction.SOUTH_EAST.nextPosition(x, y).isEqualTo(x + 1, y) },
-            { Direction.SOUTH.nextPosition(x, y).isEqualTo(x + 1 to y) },
-            { Direction.SOUTH_WEST.nextPosition(x, y).isEqualTo(x + 1 to y) },
-            { Direction.WEST.nextPosition(x, y).isEqualTo(x + 1 to y) },
-            { Direction.NORTH_WEST.nextPosition(x, y).isEqualTo(x + 1 to y) },
+            { assertThat(Direction.NORTH.nextPosition(x, y)).isEqualTo(x to y - 1) },
+            { assertThat(Direction.NORTH_EAST.nextPosition(x, y)).isEqualTo(x + 1 to y - 1) },
+            { assertThat(Direction.EAST.nextPosition(x, y)).isEqualTo(x + 1 to y) },
+            { assertThat(Direction.SOUTH_EAST.nextPosition(x, y)).isEqualTo(x + 1 to y + 1) },
+            { assertThat(Direction.SOUTH.nextPosition(x, y)).isEqualTo(x to y + 1) },
+            { assertThat(Direction.SOUTH_WEST.nextPosition(x, y)).isEqualTo(x - 1 to y + 1) },
+            { assertThat(Direction.WEST.nextPosition(x, y)).isEqualTo(x - 1 to y) },
+            { assertThat(Direction.NORTH_WEST.nextPosition(x, y)).isEqualTo(x - 1 to y - 1) },
         )
     }
 }
