7dfc6bf2e66be13c94659e0bbe5d69966a81793f
김우재 <kwj1270@naver.com>
2021-12-07 15:22:41 +0900

    feat: MineCount, 지뢰 개수는 0과 8사이의 숫자로 이루어진다

--- src/test/kotlin/minesweeper/domain/block/MineCountTest.kt
+++ src/test/kotlin/minesweeper/domain/block/MineCountTest.kt
@@ -12,7 +12,7 @@ internal class MineCountTest {
     @ParameterizedTest(name = "입력 값: {0}")
     @ValueSource(ints = [0, 1, 2, 3, 4, 5, 6, 7, 8])
     fun `지뢰 개수는 0과 8사이의 숫자로 이루어진다`(mineCountInt: Int) {
-        val mineCount = MineCount(mineCountInt)
+        val mineCount = MineCount.from(mineCountInt)
 
         assertAll(
             { assertThat(mineCount).isNotNull },
