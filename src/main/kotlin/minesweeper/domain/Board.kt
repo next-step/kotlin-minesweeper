321bdee4c34a26a7f34e8dda046a6a562da14dba
김우재 <kwj1270@naver.com>
2021-12-11 20:21:07 +0900

    refactor: Step2 피드백, Block 정적 팩토리 메서드로 변경

--- src/main/kotlin/minesweeper/domain/Board.kt
+++ src/main/kotlin/minesweeper/domain/Board.kt
@@ -2,8 +2,6 @@ package minesweeper.domain
 
 import minesweeper.domain.area.Area
 import minesweeper.domain.block.Block
-import minesweeper.domain.block.EmptyBlock
-import minesweeper.domain.block.MineBlock
 import minesweeper.domain.block.Position
 import minesweeper.domain.block.strategy.MineBlockGenerateStrategy
 
@@ -16,7 +14,7 @@ value class Board(val blocks: List<Block>) {
         fun of(area: Area, mineCount: MineCount, mineBlockGenerateStrategy: MineBlockGenerateStrategy): Board {
             val positions = positions(area.width, area.height)
             val minePositions = mineBlockGenerateStrategy.generate(positions, mineCount.mineCount)
-            return Board(positions.map { minesOrCell(it, minePositions) })
+            return Board(positions.map { Block.minesOrCell(it, minePositions) })
         }
 
         private fun positions(width: Int, height: Int): List<Position> =
@@ -25,12 +23,5 @@ value class Board(val blocks: List<Block>) {
                     Position(x, y)
                 }
             }
-
-        private fun minesOrCell(position: Position, minePositions: List<Position>): Block {
-            if (minePositions.contains(position)) {
-                return MineBlock(position)
-            }
-            return EmptyBlock.of(position, minePositions)
-        }
     }
 }
--- src/main/kotlin/minesweeper/domain/block/Block.kt
+++ src/main/kotlin/minesweeper/domain/block/Block.kt
@@ -2,4 +2,13 @@ package minesweeper.domain.block
 
 sealed class Block(open val position: Position) {
     abstract val isMine: Boolean
+
+    companion object {
+        fun minesOrCell(position: Position, minePositions: List<Position>): Block {
+            if (minePositions.contains(position)) {
+                return MineBlock(position)
+            }
+            return EmptyBlock.of(position, minePositions)
+        }
+    }
 }
