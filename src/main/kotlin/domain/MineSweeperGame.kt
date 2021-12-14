aa38e28665f5679797c633ac4630308365095682
sinoso <tpgus123123@naver.com>
2021-12-15 01:07:08 +0900

    refactor : leftMines 제거

--- src/main/kotlin/domain/MineSweeperGame.kt
+++ src/main/kotlin/domain/MineSweeperGame.kt
@@ -1,13 +1,13 @@
 package domain
 
-class MineSweeperGame(val mineField: MineField, private var leftMines: Int) {
+class MineSweeperGame(val mineField: MineField) {
 
     companion object {
         fun newGame(size: FieldSize, numberOfMines: Int): MineSweeperGame {
             val randomIndexs = createRandomIndexsForMine(size, numberOfMines)
                 .map { Point(it % size.width, it / size.width) }
             val mineField = MineField.createByIndexs(randomIndexs, size)
-            return MineSweeperGame(mineField, numberOfMines)
+            return MineSweeperGame(mineField)
         }
 
         private fun createRandomIndexsForMine(size: FieldSize, numberOfMines: Int) =
