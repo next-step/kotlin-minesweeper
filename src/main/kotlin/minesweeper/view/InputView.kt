47be52f88553f02e6cf0ffc2fd3a442c1ecad421
seunghwan <slflfl12@naver.com>
2021-12-15 00:48:31 +0900

    feat (step1): InputView 구현

---
+++ src/main/kotlin/minesweeper/view/InputView.kt
@@ -0,0 +1,24 @@
+package minesweeper.view
+
+object InputView {
+
+    private const val INPUT_HEIGHT_MESSAGE = "높이를 입력하세요."
+    private const val INPUT_WIDTH_MESSAGE = "너비를 입력하세요."
+    private const val INPUT_MINE_COUNT_MESSAGE = "지뢰는 몇 개인가요?"
+    private const val INPUT_NULL_POINTER_EXCEPTION_MESSAGE = "입력은 Null일 수 없습니다."
+
+    fun inputHeight(): String {
+        println(INPUT_HEIGHT_MESSAGE)
+        return readLine() ?: throw IllegalArgumentException(INPUT_NULL_POINTER_EXCEPTION_MESSAGE)
+    }
+
+    fun inputWidth(): String {
+        println(INPUT_WIDTH_MESSAGE)
+        return readLine() ?: throw IllegalArgumentException(INPUT_NULL_POINTER_EXCEPTION_MESSAGE)
+    }
+
+    fun inputMineCount(): String {
+        println(INPUT_MINE_COUNT_MESSAGE)
+        return readLine() ?: throw IllegalArgumentException(INPUT_MINE_COUNT_MESSAGE)
+    }
+}
