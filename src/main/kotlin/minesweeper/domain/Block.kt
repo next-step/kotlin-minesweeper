45aef2a5ad21344bfb0dac77a43e0568d94c3fe0
minji0320 <rlaalswl0320@naver.com>
2021-12-27 16:06:04 +0900

    refactor: Block aroundMineCount 초기값 설정

--- src/main/kotlin/minesweeper/domain/Block.kt
+++ src/main/kotlin/minesweeper/domain/Block.kt
@@ -1,6 +1,6 @@
 package minesweeper.domain
 
-class Block(private val aroundMineCount: Int) : Cell() {
+class Block(private val aroundMineCount: Int = 0) : Cell() {
     override fun toString(): String {
         return aroundMineCount.toString()
     }
