6e1afe67260e67bc87fccce66f67d3e494936f47
pug <pug.gg@kakaocorp.com>
2022-06-15 17:33:03 +0900

    test: TDD 테스트 케이스 선 반영

---
+++ src/test/kotlin/minesweeper/domain/board/MineBoardTest.kt
@@ -0,0 +1,16 @@
+package minesweeper.domain.board
+
+import org.junit.jupiter.api.Test
+
+internal class MineBoardTest {
+
+    @Test
+    fun `지뢰 보드의 높이와 너비, 지뢰 개수는 음수일 수 없다`() {
+        TODO()
+    }
+
+    @Test
+    fun `지뢰 보드의 사이즈를 초과하는 지뢰 개수를 가질 수 없다`() {
+        TODO()
+    }
+}
---
+++ src/test/kotlin/minesweeper/domain/cell/CellTest.kt
@@ -0,0 +1,11 @@
+package minesweeper.domain.cell
+
+import org.junit.jupiter.api.Test
+
+internal class CellTest {
+
+    @Test
+    fun `모든 공간의 위치 x, y 는 음수일 수 없다`() {
+        TODO()
+    }
+}
