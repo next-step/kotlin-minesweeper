ad3947e3f2b48f634313b1284dc7d0cd9cd4564d
Grandelwald <artjr97@naver.com>
2022-06-15 19:51:35 +0900

    1단계 - 지뢰 찾기(그리기) : controller MineSweeper.kt 구현

--- src/main/kotlin/controller/MineSweeper.kt
+++ src/main/kotlin/controller/MineSweeper.kt
@@ -5,12 +5,12 @@ import view.InputView
 import view.OutputView
 
 class MineSweeper {
-    fun start(){
+    fun start() {
         val fieldHeight = InputView.inputHeight()
         val fieldWidth = InputView.inputWidth()
         val mineCount = InputView.inputMineCount()
 
-        val fieldInfo = MineField.FieldInfo(fieldHeight,fieldWidth,mineCount)
+        val fieldInfo = MineField.FieldInfo(fieldHeight, fieldWidth, mineCount)
         val mineField = MineField().buildField(fieldInfo)
         OutputView.printMineField(mineField)
     }
