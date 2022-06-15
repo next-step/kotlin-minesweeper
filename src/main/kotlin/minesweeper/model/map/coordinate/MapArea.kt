cb0f41588f3d82eb2e768fdc868db85e4a6f1cb5
Juatae-Kim <jutaekim@sk.com>
2022-06-15 09:54:45 +0900

    Style: Improve readability

--- src/main/kotlin/minesweeper/model/map/coordinate/MapArea.kt
+++ src/main/kotlin/minesweeper/model/map/coordinate/MapArea.kt
@@ -10,14 +10,11 @@ data class MapArea(override val width: Int, override val height: Int) : Area, It
     operator fun get(index: Int): Position = Position(row = index / this.width, column = index % this.width)
     fun indexOf(position: Position): Int = position.row * this.width + position.column
 
-    override fun iterator(): Iterator<Position> {
+    override fun iterator() = object : Iterator<Position> {
+        private var offset = 0
 
-        return object : Iterator<Position> {
-            private var offset = 0
+        override fun hasNext(): Boolean = offset < this@MapArea.area
 
-            override fun hasNext(): Boolean = offset < this@MapArea.area
-
-            override fun next(): Position = this@MapArea[offset++]
-        }
+        override fun next(): Position = this@MapArea[offset++]
     }
 }
