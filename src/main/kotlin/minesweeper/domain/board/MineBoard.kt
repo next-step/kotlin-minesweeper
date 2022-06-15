5744d87105dece04ff8d1c9239b0577fa4a28ac0
pug <pug.gg@kakaocorp.com>
2022-06-15 17:50:50 +0900

    feat, test: MineBoard 클래스 추가 및 테스트 케이스 구현

---
+++ src/main/kotlin/minesweeper/domain/board/MineBoard.kt
@@ -0,0 +1,14 @@
+package minesweeper.domain.board
+
+class MineBoard(
+    private val width: Int,
+    private val height: Int,
+    private val numberOfMines: Int
+) {
+
+    init {
+        val size = width * height
+        require(width > 0 && height > 0 && numberOfMines > 0) { "width, height and number of mines must be positive." }
+        require(size >= numberOfMines) { "number of mines can't exceed the size of board." }
+    }
+}
--- src/test/kotlin/minesweeper/domain/board/MineBoardTest.kt
+++ src/test/kotlin/minesweeper/domain/board/MineBoardTest.kt
@@ -1,16 +1,46 @@
 package minesweeper.domain.board
 
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class MineBoardTest {
 
-    @Test
-    fun `지뢰 보드의 높이와 너비, 지뢰 개수는 음수일 수 없다`() {
-        TODO()
+    @ParameterizedTest
+    @MethodSource("지뢰 보드의 높이, 너비, 지뢰 개수 중 음수가 존재하는 케이스")
+    fun `지뢰 보드의 높이와 너비, 지뢰 개수는 음수일 수 없다`(width: Int, height: Int, numberOfMines: Int) {
+        assertThatThrownBy { MineBoard(width, height, numberOfMines) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("width, height and number of mines must be positive.")
     }
 
-    @Test
-    fun `지뢰 보드의 사이즈를 초과하는 지뢰 개수를 가질 수 없다`() {
-        TODO()
+    @ParameterizedTest
+    @MethodSource("지뢰 개수가 지뢰 보드의 사이즈를 초과하는 케이스")
+    fun `지뢰 보드의 사이즈를 초과하는 지뢰 개수를 가질 수 없다`(width: Int, height: Int, numberOfMines: Int) {
+        assertThatThrownBy { MineBoard(width, height, numberOfMines) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("number of mines can't exceed the size of board.")
+    }
+
+    companion object {
+        @JvmStatic
+        fun `지뢰 보드의 높이, 너비, 지뢰 개수 중 음수가 존재하는 케이스`() = Stream.of(
+            Arguments.of(-10, 10, 10),
+            Arguments.of(10, -10, 10),
+            Arguments.of(10, 10, -10),
+            Arguments.of(-10, -10, 10),
+            Arguments.of(10, -10, -10),
+            Arguments.of(-10, 10, -10),
+            Arguments.of(-10, -10, -10)
+        )
+
+        @JvmStatic
+        fun `지뢰 개수가 지뢰 보드의 사이즈를 초과하는 케이스`() = Stream.of(
+            Arguments.of(1, 1, 2),
+            Arguments.of(5, 5, 50),
+            Arguments.of(10, 10, 101),
+        )
     }
 }
