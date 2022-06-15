4d32e20d04711af5751e34f09f267bb1d8d65542
Juatae-Kim <jutaekim@sk.com>
2022-06-15 17:26:41 +0900

    refactor(Area): Rename properties to clarify the meaning

--- src/main/kotlin/minesweeper/view/input/ConsoleMineMapBuilder.kt
+++ src/main/kotlin/minesweeper/view/input/ConsoleMineMapBuilder.kt
@@ -7,23 +7,24 @@ import minesweeper.view.input.parser.IntInputParser
 
 object ConsoleMineMapBuilder : MineMapBuilder {
 
-    private const val MESSAGE_FOR_INPUT_HEIGHT = "높이를 입력하세요."
-    private const val MESSAGE_FOR_INPUT_WIDTH = "너비를 입력하세요."
+    private const val MESSAGE_FOR_INPUT_ROW_COUNT = "높이를 입력하세요."
+    private const val MESSAGE_FOR_INPUT_COLUMN_COUNT = "너비를 입력하세요."
     private const val MESSAGE_FOR_INPUT_MINE_COUNT = "지뢰는 몇 개인가요?"
 
-    private const val MIN_WIDTH_OF_MAP = 3
-    private const val MIN_HEIGHT_OF_MAP = 3
-    private const val MIN_MINE_COUNT = 1
+    private const val MIN_COUNT_OF_ROW = 3
+    private const val MIN_COUNT_OF_COLUMN = 3
+    private const val MIN_COUNT_OF_MINE = 1
 
     override fun createNewMap(): MineMap {
 
-        val height = ConsoleReader.read(MESSAGE_FOR_INPUT_HEIGHT, IntInputParser(minValue = MIN_HEIGHT_OF_MAP))
-        val with = ConsoleReader.read(MESSAGE_FOR_INPUT_WIDTH, IntInputParser(minValue = MIN_WIDTH_OF_MAP))
+        val rowCount = ConsoleReader.read(MESSAGE_FOR_INPUT_ROW_COUNT, IntInputParser(minValue = MIN_COUNT_OF_ROW))
+        val columnCount =
+            ConsoleReader.read(MESSAGE_FOR_INPUT_COLUMN_COUNT, IntInputParser(minValue = MIN_COUNT_OF_COLUMN))
 
-        val maxMineCount = height * with
-        val mineCountRange = MIN_MINE_COUNT..maxMineCount
+        val maxMineCount = rowCount * columnCount
+        val mineCountRange = MIN_COUNT_OF_MINE..maxMineCount
 
         val mineCount = ConsoleReader.read(MESSAGE_FOR_INPUT_MINE_COUNT, IntInputParser(mineCountRange))
-        return MineMap.randomMap(MapArea(with, height), mineCount)
+        return MineMap.randomMap(MapArea(columnCount, rowCount), mineCount)
     }
 }
