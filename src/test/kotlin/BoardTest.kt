62a7c7cf427c287af5b54952778045ba34cf3a51
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-06-15 23:42:09 +0900

    test: 마인 근처 숫자체크 테스트

---
+++ src/test/kotlin/BoardTest.kt
@@ -0,0 +1,27 @@
+import domain.Board
+import domain.BoardItem
+import domain.BoardRow
+import domain.BoardRows
+import domain.MinePosition
+import domain.Mines
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class BoardTest {
+    @Test
+    fun `지뢰 주변의 숫자가 근처 지뢰개수를 반영해야한다`() {
+        val mines = Mines(
+            listOf(
+                MinePosition(0, 3),
+                MinePosition(1, 3),
+                MinePosition(2, 7)
+            ).toSet()
+        )
+
+        val board = Board(3, 10, mines)
+
+        assertThat(board.rows[1][4].getNearMineCount()).isEqualTo(2)
+        assertThat(board.rows[2][8].getNearMineCount()).isEqualTo(1)
+    }
+}
