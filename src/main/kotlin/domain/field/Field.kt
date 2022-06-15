794b81f01216f300b5aab5c2b32bae901e3f3545
Grandelwald <artjr97@naver.com>
2022-06-15 21:01:59 +0900

    1단계 - 지뢰 찾기(그리기) : 랜덤성 테스트 코드 작성

--- src/main/kotlin/domain/field/Field.kt
+++ src/main/kotlin/domain/field/Field.kt
@@ -6,7 +6,6 @@ import domain.cell.Mine
 import domain.Mines
 
 class Field {
-
     fun buildField(fieldInfo: FieldInfo): List<List<Cell>> {
         val field: List<MutableList<Cell>> = List(fieldInfo.height) { MutableList(fieldInfo.width) { Empty(0, 0) } }
         val mines: Mines = Mines().buildMines(fieldInfo)
@@ -16,7 +15,6 @@ class Field {
                 field[row][column] = Empty(row, column)
                 if (Mine(row,column) in mines) {
                     field[row][column] = Mine(row, column)
-                    println("yes")
                 }
             }
             line.toList()
