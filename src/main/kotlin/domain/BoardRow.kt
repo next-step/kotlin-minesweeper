cc4b8448d9c26de3d09e55101f69253e5691f85e
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-06-17 00:29:12 +0900

    refactor: 미사용 변수 제거 (size)

--- src/main/kotlin/domain/BoardRow.kt
+++ src/main/kotlin/domain/BoardRow.kt
@@ -5,7 +5,5 @@ class BoardRow(height: Int, width: Int, mines: Mines) {
         BoardItem.getItemType(mines.contains(height, col))
     }
 
-    val size get() = cells.size
-
     operator fun get(idx: Int) = cells[idx]
 }
