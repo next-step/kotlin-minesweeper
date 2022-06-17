df0e0ecafb2c8953bfbe3d73ae0461cf9bd64058
molly <molly.d@kakaocorp.com>
2022-06-17 17:21:38 +0900

    fix: step1 리뷰 반영 (readme 체크, 포지션 x축 y축으로 수정)

--- src/main/kotlin/minesweeper/view/OutputView.kt
+++ src/main/kotlin/minesweeper/view/OutputView.kt
@@ -6,7 +6,7 @@ object OutputView {
 
     fun printGameBoard(gameBoard: GameBoard) {
         println("지뢰찾기 게임 시작")
-        gameBoard.cells.cells.groupBy { it.position.height }.forEach {
+        gameBoard.cells.cells.groupBy { it.position.y }.forEach {
             println(it.value.joinToString(" ") { cell -> cell.cellType.point })
         }
     }
