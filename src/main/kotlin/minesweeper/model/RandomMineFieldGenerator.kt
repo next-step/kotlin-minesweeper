e018a565953a41012c34ed249775b3da0e68d647
YooInKeun <keun0390@naver.com>
2022-06-18 13:47:01 +0900

    chore: 너비, 높이, 지뢰 개수 유효성 검증 기능 추가

--- src/main/kotlin/minesweeper/model/RandomMineFieldGenerator.kt
+++ src/main/kotlin/minesweeper/model/RandomMineFieldGenerator.kt
@@ -1,7 +1,13 @@
 package minesweeper.model
 
 object RandomMineFieldGenerator {
+    private const val MIN_WIDTH = 1
+    private const val MIN_HEIGHT = 1
+    private const val MIN_MINE_COUNT = 1
+
     fun generate(width: Int, height: Int, mineCount: Int): List<Marks> {
+        validate(width, height, mineCount)
+
         val mineMarks = List(mineCount) {
             Mark.mine()
         }
@@ -19,5 +25,22 @@ object RandomMineFieldGenerator {
             val endIndex = startIndex + width
             Marks(mergedMarks.subList(startIndex, endIndex))
         }
+    }
+
+    private fun validate(width: Int, height: Int, mineCount: Int) {
+        validateMinWidth(width)
+        validateMinHeight(height)
+        validateMineCount(width, height, mineCount)
+    }
+
+    private fun validateMinWidth(width: Int) = require(width >= MIN_WIDTH) { "너비는 최소 $MIN_WIDTH 이상이어야 합니다." }
+
+    private fun validateMinHeight(height: Int) = require(height >= MIN_HEIGHT) { "높이는 최소 $MIN_HEIGHT 이상이어야 합니다." }
+
+    private fun validateMineCount(width: Int, height: Int, mineCount: Int) {
+        require(mineCount >= MIN_MINE_COUNT) { "지뢰 개수는 최소 ${MIN_MINE_COUNT}개 이상이어야 합니다." }
+
+        val maxMineCount = width * height
+        require(mineCount <= maxMineCount) { "지뢰 개수는 ${maxMineCount}개를 초과할 수 없습니다." }
     }
 }
--- src/test/kotlin/minesweeper/model/RandomMineFieldGeneratorTest.kt
+++ src/test/kotlin/minesweeper/model/RandomMineFieldGeneratorTest.kt
@@ -3,9 +3,38 @@ package minesweeper.model
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 @DisplayName("랜덤 지뢰판 생성기 테스트")
 class RandomMineFieldGeneratorTest {
+
+    @Test
+    fun `너비가 1미만이면 예외 발생`() {
+        // when, then
+        val exception = assertThrows<IllegalArgumentException> { RandomMineFieldGenerator.generate(0, 2, 1) }
+        assertThat(exception.message).isEqualTo("너비는 최소 1 이상이어야 합니다.")
+    }
+
+    @Test
+    fun `높이가 1미만이면 예외 발생`() {
+        // when, then
+        val exception = assertThrows<IllegalArgumentException> { RandomMineFieldGenerator.generate(2, 0, 1) }
+        assertThat(exception.message).isEqualTo("높이는 최소 1 이상이어야 합니다.")
+    }
+
+    @Test
+    fun `지뢰 개수가 1개 미만이면 예외 발생`() {
+        // when, then
+        val exception = assertThrows<IllegalArgumentException> { RandomMineFieldGenerator.generate(2, 2, 0) }
+        assertThat(exception.message).isEqualTo("지뢰 개수는 최소 1개 이상이어야 합니다.")
+    }
+
+    @Test
+    fun `지뢰 개수가 지뢰판 사이즈를 초과하면 예외 발생`() {
+        // when, then
+        val exception = assertThrows<IllegalArgumentException> { RandomMineFieldGenerator.generate(2, 2, 5) }
+        assertThat(exception.message).isEqualTo("지뢰 개수는 4개를 초과할 수 없습니다.")
+    }
 
     @Test
     fun `랜덤 지뢰판이 정상적으로 생성`() {
