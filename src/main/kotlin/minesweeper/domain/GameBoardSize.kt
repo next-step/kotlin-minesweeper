05a54bca5b79f8870ece9ee64cd588e860861295
molly <molly.d@kakaocorp.com>
2022-06-19 00:25:33 +0900

    fix: 게임보드 프로퍼티에 대한 접근제어자 수정

--- src/main/kotlin/minesweeper/domain/GameBoardSize.kt
+++ src/main/kotlin/minesweeper/domain/GameBoardSize.kt
@@ -1,6 +1,6 @@
 package minesweeper.domain
 
-class GameBoardSize(val width: Int, val height: Int) {
+class GameBoardSize(private val width: Int, private val height: Int) {
     init {
         require(width > 0 && height > 0) { "넓이와 높이는 모두 0보다 커야합니다." }
     }
