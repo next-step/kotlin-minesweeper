270ccebe2d98f2416c87b7e14884c3fb212bf9e2
Jake Son <sonjeabin@gmail.com>
2022-06-19 09:49:25 +0900

    test: cell 테스트 추가

---
+++ src/test/kotlin/domain/CellTest.kt
@@ -0,0 +1,20 @@
+package domain
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.types.shouldBeInstanceOf
+import vo.Point
+
+internal class CellTest : StringSpec({
+
+    "지뢰를 가진 cell 을 생성한다" {
+        val cell = Mine(Coordinate(Point(1), Point(2)))
+
+        cell.shouldBeInstanceOf<Cell>()
+    }
+
+    "지뢰가 없는 cell 을 생성한다" {
+        val cell = Empty(Coordinate(Point(1), Point(2)))
+
+        cell.shouldBeInstanceOf<Cell>()
+    }
+})
