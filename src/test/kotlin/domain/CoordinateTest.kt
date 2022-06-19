d49a0a9c9ad00a93e4d608cdf5222fbcbf5dec5c
Jake Son <sonjeabin@gmail.com>
2022-06-19 09:46:28 +0900

    test: 좌표 생성 테스트 추가

---
+++ src/test/kotlin/domain/CoordinateTest.kt
@@ -0,0 +1,15 @@
+package domain
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import vo.Point
+
+internal class CoordinateTest : StringSpec({
+
+    "좌표를 생성한다" {
+        val coordinate = Coordinate(Point(1), Point(2))
+
+        coordinate.x shouldBe 1
+        coordinate.y shouldBe 2
+    }
+})
