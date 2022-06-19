8af72b514dddb3113cfcc120b0886f8be6d7a875
Jake Son <sonjeabin@gmail.com>
2022-06-19 10:08:48 +0900

    fix: Point 테스트 에러해결

--- src/test/kotlin/domain/CoordinateTest.kt
+++ src/test/kotlin/domain/CoordinateTest.kt
@@ -9,7 +9,7 @@ internal class CoordinateTest : StringSpec({
     "좌표를 생성한다" {
         val coordinate = Coordinate(Point(1), Point(2))
 
-        coordinate.x shouldBe 1
-        coordinate.y shouldBe 2
+        coordinate.x shouldBe Point(1)
+        coordinate.y shouldBe Point(2)
     }
 })
