58ba3c358f72b8109480fe204fc536a23c30a819
Jake Son <sonjeabin@gmail.com>
2022-06-19 11:29:19 +0900

    feat: Board 생성검증 로직추현

--- src/main/kotlin/domain/Board.kt
+++ src/main/kotlin/domain/Board.kt
@@ -1,3 +1,9 @@
 package domain
 
-data class Board(val rows: List<Row>)
+data class Board(private val rows: List<Row>) {
+
+    init {
+        require(rows.isNotEmpty()) { "적어도 하나의 row 가 필요합니다" }
+        require(rows.map { it.size }.distinct().size == 1) { "모든 row 의 크기는 같아야 합니다" }
+    }
+}
