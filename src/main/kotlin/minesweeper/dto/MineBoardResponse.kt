adb2fb7f0d7a084fac71764e14ef8ec865615c97
leeyohan93 <good_1411@naver.com>
2022-06-19 23:58:20 +0900

    feat: App, View, Dto 구현

---
+++ src/main/kotlin/minesweeper/dto/MineBoardResponse.kt
@@ -0,0 +1,21 @@
+package minesweeper.dto
+
+import minesweeper.domain.BoardField
+import minesweeper.domain.MineBoard
+
+class MineBoardResponse(
+    val boardRowResponses: List<BoardRowResponse>
+) {
+    companion object {
+        fun of(mineBoard: MineBoard, width: Int): MineBoardResponse {
+            val boardFields = mineBoard.boardFields
+            val boardRowResponses = boardFields.chunked(width)
+                .map(::BoardRowResponse)
+            return MineBoardResponse(boardRowResponses)
+        }
+    }
+}
+
+data class BoardRowResponse(
+    val boardFields: List<BoardField>
+)
---
+++ src/main/kotlin/minesweeper/view/ResultView.kt
@@ -0,0 +1,25 @@
+package minesweeper.view
+
+import minesweeper.domain.BoardField
+import minesweeper.dto.BoardRowResponse
+import minesweeper.dto.MineBoardResponse
+
+object ResultView {
+
+    fun printBoard(mineBoard: MineBoardResponse) {
+        println("지뢰찾기 게임 시작")
+        mineBoard.boardRowResponses.forEach { println(convertBoardRow(it)) }
+    }
+
+    private fun convertBoardRow(boardRow: BoardRowResponse): String {
+        return boardRow.boardFields.joinToString(" ") { convertBoardField(it) }
+    }
+
+    private fun convertBoardField(boardField: BoardField): String {
+        return if (boardField.isMine) {
+            "*"
+        } else {
+            "C"
+        }
+    }
+}
