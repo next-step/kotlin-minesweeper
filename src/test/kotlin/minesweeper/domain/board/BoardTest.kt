e9d59aa649ddcbcfddfd51909d7183981bd759d0
pug <pug.gg@kakaocorp.com>
2022-06-20 11:45:28 +0900

    test: BoardTest 추가

---
+++ src/test/kotlin/minesweeper/domain/board/BoardTest.kt
@@ -0,0 +1,27 @@
+package minesweeper.domain.board
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+internal class BoardTest {
+
+    @ParameterizedTest
+    @MethodSource("보드의 높이와 너비 중 음수가 존재하는 케이스")
+    fun `보드의 높이와 너비는 음수일 수 없다`(width: Int, height: Int) {
+        val exception = assertThrows<IllegalArgumentException> { Board.of(width, height) }
+        Assertions.assertThat(exception.message).isEqualTo("property must be zero or positive.")
+    }
+
+    companion object {
+        @JvmStatic
+        fun `보드의 높이와 너비 중 음수가 존재하는 케이스`() = Stream.of(
+            Arguments.of(-10, 10),
+            Arguments.of(10, -10),
+            Arguments.of(-10, -10)
+        )
+    }
+}
