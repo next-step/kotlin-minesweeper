68b317013f3c13b59eb090f724f563343e296d3a
inpyo-jeon <inpyo.developer@gmail.com>
2022-06-20 18:04:05 +0900

    [step1] refactor: 코드리뷰 반영 - 불필요했던 응답 필드 제거

--- src/main/kotlin/domain/Minesweeper.kt
+++ src/main/kotlin/domain/Minesweeper.kt
@@ -4,13 +4,8 @@ import domain.strategy.MineAllocationStrategy
 
 class Minesweeper(minesweeperProperty: MinesweeperProperty, mineAllocationStrategy: MineAllocationStrategy) {
 
-    private val _board: Array<Array<Place>> =
+    val board: Array<Array<Place>> =
         Array(minesweeperProperty.height) { Array(minesweeperProperty.width) { Place(PlaceType.NOT_MINE) } }
-
-    val board: Array<Array<Place>>
-        get() {
-            return _board
-        }
 
     init {
         val assignedMineLocation = mineAllocationStrategy.calculate(
@@ -21,7 +16,7 @@ class Minesweeper(minesweeperProperty: MinesweeperProperty, mineAllocationStrate
         assignedMineLocation.map {
             val row = it / minesweeperProperty.height
             val col = it % minesweeperProperty.width
-            _board[row][col].placeType = PlaceType.MINE
+            board[row][col].placeType = PlaceType.MINE
         }
     }
 }
