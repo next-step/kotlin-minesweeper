0690f2b749860aedece867fc1574a2561533b5d5
pug <pug.gg@kakaocorp.com>
2022-06-20 18:32:35 +0900

    test: TDD 테스트 케이스 선 반영

---
+++ src/main/kotlin/minesweeper/domain/board/NearbyMineCounter.kt
@@ -0,0 +1,14 @@
+package minesweeper.domain.board
+
+class NearbyMineCounter {
+
+    companion object {
+        fun count(board: Board): Board {
+            TODO()
+        }
+
+        private fun accNearbyMines(index: Int, board: Board) {
+            TODO()
+        }
+    }
+}
---
+++ src/test/kotlin/minesweeper/domain/board/NearbyMineCounterTest.kt
@@ -0,0 +1,11 @@
+package minesweeper.domain.board
+
+import org.junit.jupiter.api.Test
+
+internal class NearbyMineCounterTest {
+
+    @Test
+    fun `각 셀에 표시될 숫자는 자신을 제외한 주변 8개 셀에 포함된 지뢰의 개수이다`() {
+        TODO()
+    }
+}
