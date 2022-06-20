f68db476299c27c0d9ae069fd5b6c72c8b1960ee
JT-Kim <jtkim95@gmail.com>
2022-06-20 22:15:30 +0900

    refactor: Use Area class for Board.area instead of BoardArea (apply feedback)

--- src/main/kotlin/minesweeper/model/board/Board.kt
+++ src/main/kotlin/minesweeper/model/board/Board.kt
@@ -4,17 +4,16 @@ import minesweeper.model.board.coordinate.Area
 import minesweeper.model.board.coordinate.BoardArea
 import minesweeper.model.board.coordinate.Position
 
-class Board private constructor(private val boardArea: BoardArea, val cells: Cells) : Area by boardArea {
+class Board private constructor(private val area: Area, val cells: Cells) : Area by area {
 
     fun cellsAtRowOrNull(row: Int): Cells? = runCatching {
-        require(Position(row, 0) in boardArea)
         Cells(this.cells.filter { it.row == row })
     }.getOrNull()
 
     companion object {
 
         fun build(boardArea: BoardArea, isMineCell: (Position) -> Boolean) = Board(
-            boardArea = boardArea,
+            area = boardArea,
             cells = Cells(boardArea.map { position -> createCell(position, isMineCell(position)) })
         )
 
