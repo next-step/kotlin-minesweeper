3efd8b6694667785af4bc674f872a8ccd62f5fb5
dongchul <sorkehdcjf@gmail.com>
2022-06-21 10:47:28 +0900

    refactor(Coordinate): 주변 좌표 찾기 Enum 활용

--- src/main/kotlin/minesweeper/domain/field/Coordinate.kt
+++ src/main/kotlin/minesweeper/domain/field/Coordinate.kt
@@ -4,21 +4,16 @@ data class Coordinate(
     val x: CoordinateValue,
     val y: CoordinateValue
 ) {
-    fun findAround(): List<Coordinate> = listOf(east(), west(), south(), north(), northEast(), northWest(), southEast(), southWest()).filter { it.x.value >= 0 && it.y.value >= 0 }
+    fun findAround(): List<Coordinate> = Direction.values().map { Coordinate(x + it.x, y + it.y) }
 
-    private fun east(): Coordinate = Coordinate(x + CoordinateValue.move(), y)
-
-    private fun west(): Coordinate = Coordinate(x - CoordinateValue.move(), y)
-
-    private fun south(): Coordinate = Coordinate(x, y + CoordinateValue.move())
-
-    private fun north(): Coordinate = Coordinate(x, y - CoordinateValue.move())
-
-    private fun northEast(): Coordinate = Coordinate(x + CoordinateValue.move(), y - CoordinateValue.move())
-
-    private fun northWest(): Coordinate = Coordinate(x - CoordinateValue.move(), y - CoordinateValue.move())
-
-    private fun southEast(): Coordinate = Coordinate(x + CoordinateValue.move(), y + CoordinateValue.move())
-
-    private fun southWest(): Coordinate = Coordinate(x - CoordinateValue.move(), y + CoordinateValue.move())
+    private enum class Direction(val x: CoordinateValue, val y: CoordinateValue) {
+        EAST(CoordinateValue.plus, CoordinateValue.stay),
+        WEST(CoordinateValue.minus, CoordinateValue.stay),
+        SOUTH(CoordinateValue.stay, CoordinateValue.plus),
+        NORTH(CoordinateValue.stay, CoordinateValue.minus),
+        NORTH_EAST(CoordinateValue.plus, CoordinateValue.minus),
+        NORTH_WEST(CoordinateValue.minus, CoordinateValue.minus),
+        SOUTH_EAST(CoordinateValue.plus, CoordinateValue.plus),
+        SOUTH_WEST(CoordinateValue.minus, CoordinateValue.plus);
+    }
 }
--- src/main/kotlin/minesweeper/domain/field/CoordinateValue.kt
+++ src/main/kotlin/minesweeper/domain/field/CoordinateValue.kt
@@ -8,6 +8,15 @@ value class CoordinateValue(val value: Int) {
     operator fun minus(other: CoordinateValue): CoordinateValue = CoordinateValue(value - other.value)
 
     companion object {
+        val plus: CoordinateValue
+            get() = CoordinateValue(1)
+
+        val minus: CoordinateValue
+            get() = CoordinateValue(-1)
+
+        val stay: CoordinateValue
+            get() = CoordinateValue(0)
+
         fun move(): CoordinateValue = CoordinateValue(1)
     }
 }
