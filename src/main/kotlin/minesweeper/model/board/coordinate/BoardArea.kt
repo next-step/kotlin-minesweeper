bc47ace921c3f47c5b00006c1ef1971e94eab68a
JT-Kim <jtkim95@gmail.com>
2022-06-22 22:20:53 +0900

    refactor(Area/Position): Move implementations to BoardArea from Area (apply feedback)

--- src/main/kotlin/minesweeper/model/board/coordinate/BoardArea.kt
+++ src/main/kotlin/minesweeper/model/board/coordinate/BoardArea.kt
@@ -2,6 +2,24 @@ package minesweeper.model.board.coordinate
 
 data class BoardArea(override val rowCount: PositiveInt, override val columnCount: PositiveInt) : Area {
 
+    override val cellCount: Int
+        get() = this.rowCount * this.columnCount
+
+    operator fun get(index: Int): Position = Position(row = index / this.columnCount, column = index % this.columnCount)
+
+    fun indexOf(position: Position): Int = position.row * this.columnCount + position.column
+
+    override fun surroundPositionsOf(position: Position): List<Position> =
+        position.surroundPositions.filter { it in this }
+
+    override fun iterator() = object : Iterator<Position> {
+        private var offset = 0
+
+        override fun hasNext(): Boolean = offset < this@BoardArea.cellCount
+
+        override fun next(): Position = this@BoardArea[offset++]
+    }
+
     companion object {
         fun of(rowCount: Int, columnCount: Int) = BoardArea(
             rowCount = PositiveInt(rowCount),
