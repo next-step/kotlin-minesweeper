db110a52d473903faff691ebc43b350802423986
jngcii <concotree@gmail.com>
2022-06-23 19:58:10 +0900

    feat : 지뢰맵 출력 로직 구현 및 로직 main 로직 작성

--- src/main/kotlin/com/nextstep/jngcii/minesweeper/Application.kt
+++ src/main/kotlin/com/nextstep/jngcii/minesweeper/Application.kt
@@ -1,9 +1,24 @@
 package com.nextstep.jngcii.minesweeper
 
+import com.nextstep.jngcii.minesweeper.domain.MineManager
+import com.nextstep.jngcii.minesweeper.domain.MineMapFactory
+import com.nextstep.jngcii.minesweeper.domain.RandomPickStrategy
 import com.nextstep.jngcii.minesweeper.view.InputView
+import com.nextstep.jngcii.minesweeper.view.ResultView
+import kotlin.random.Random
 
 fun main() {
     val rowCount = InputView.getRowCount()
     val columnCount = InputView.getColumnCount()
     val mineCount = InputView.getMineCount()
+
+    val mineManager = MineManager(
+        booleanStrategy = { Random.nextBoolean() },
+        pickStrategy = RandomPickStrategy
+    )
+    val mineMapFactory = MineMapFactory(mineManager)
+
+    val mineMap = mineMapFactory.create(rowCount, columnCount, mineCount)
+
+    ResultView.printMap(mineMap)
 }
