7421f21d6d3d221ca58b68fff1c1cffaf82243cc
pug <pug.gg@kakaocorp.com>
2022-06-23 17:32:21 +0900

    test: TDD 테스트 케이스 선 반영

---
+++ src/test/kotlin/minesweeper/domain/cell/CellTest.kt
@@ -0,0 +1,26 @@
+package minesweeper.domain.cell
+
+import org.junit.jupiter.api.Test
+
+internal class CellTest {
+
+    @Test
+    fun `CLOSE 상태의 셀은 열 수 있다`() {
+        TODO()
+    }
+
+    @Test
+    fun `OPEN 상태의 셀은 다시 열 수 없다`() {
+        TODO()
+    }
+
+    @Test
+    fun `셀을 열어서 안에 무엇이 있는지 확인할 수 있다`() {
+        TODO()
+    }
+
+    @Test
+    fun `지뢰가 없는 인접한 셀이 모두 열리게 된다`() {
+        TODO()
+    }
+}
