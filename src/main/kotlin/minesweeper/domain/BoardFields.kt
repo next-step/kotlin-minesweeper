38d529566d64889d2f114f7479d416fc5f7a6e27
leeyohan93 <good_1411@naver.com>
2022-06-24 01:07:13 +0900

    refactor: 지뢰 판 필드들(BoardFields) 필드 오픈 테스트, 기능 구현

--- src/main/kotlin/minesweeper/domain/BoardFields.kt
+++ src/main/kotlin/minesweeper/domain/BoardFields.kt
@@ -12,4 +12,10 @@ data class BoardFields(val boardFields: List<BoardField>) {
     fun mineCount(): Int {
         return boardFields.count { it is MineField }
     }
+
+    fun open(coordinate: Coordinate) {
+        val boardField = boardFields.find { it.coordinate == coordinate }
+            ?: throw IllegalArgumentException("해당 좌표에 필드가 존재하지 않습니다. (${coordinate.x},${coordinate.y})")
+        boardField.open()
+    }
 }
--- src/test/kotlin/minesweeper/domain/BoardFieldsTest.kt
+++ src/test/kotlin/minesweeper/domain/BoardFieldsTest.kt
@@ -1,5 +1,6 @@
 package minesweeper.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
@@ -54,6 +55,38 @@ class BoardFieldsTest : DescribeSpec({
             )
 
             boardFields.mineCount() shouldBe 3
+        }
+    }
+
+    describe("open") {
+        val boardFields = BoardFields(
+            listOf(
+                BoardField.mine(Coordinate(0, 0)),
+                BoardField.nonMine(Coordinate(0, 1)),
+                BoardField.mine(Coordinate(0, 2)),
+                BoardField.nonMine(Coordinate(1, 0)),
+                BoardField.nonMine(Coordinate(1, 1)),
+                BoardField.nonMine(Coordinate(1, 2)),
+                BoardField.mine(Coordinate(2, 0)),
+                BoardField.nonMine(Coordinate(2, 1)),
+                BoardField.nonMine(Coordinate(2, 2)),
+            )
+        )
+
+        context("필드들에 해당하는 좌표를 입력받아") {
+            it("해당하는 필드를 열 수 있다.") {
+                val coordinate = Coordinate(0, 0)
+                boardFields.open(coordinate)
+
+                val boardField = boardFields.boardFields.find { it.coordinate == coordinate }!!
+                boardField.isOpen shouldBe true
+            }
+        }
+
+        context("필드들에 해당하지 않는 좌표를 입력받으면") {
+            it("IllegalArgumentException 이 발생한다.") {
+                shouldThrow<IllegalArgumentException> { boardFields.open(Coordinate(3, 3)) }
+            }
         }
     }
 })
