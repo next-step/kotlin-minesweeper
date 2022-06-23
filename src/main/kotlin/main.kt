029f7523086995c14676de04e0ef3c8ce604bc92
ujusy <lsyeon1997@ajou.ac.kr>
2022-06-23 22:17:47 +0900

    refacotor: main 함수에 지도 ui 추가

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,20 @@
+import domain.CoordinateRandomGenerator
+import domain.LandMineRandomMap
+import dto.CoordinateGeneratorRequest
 import dto.LandMineMapRequest
 import view.InputView
+import view.LandMineMapUI
 
 fun main() {
     val height = InputView.height()
     val width = InputView.width()
     val landmine = InputView.landmine()
 
-    val landMineMapRequest = LandMineMapRequest.of(height, width, landmine)
+    val landMineMapRequest = LandMineMapRequest.of(height, width)
+    val coordinateGeneratorRequest = CoordinateGeneratorRequest.of(landMineMapRequest.width, landMineMapRequest.height, landmine)
+    val landMineRandomMap = LandMineRandomMap(landMineMapRequest, CoordinateRandomGenerator(coordinateGeneratorRequest))
+
+    val mapUI = LandMineMapUI(landMineRandomMap)
+    mapUI.title()
+    mapUI.map()
 }
