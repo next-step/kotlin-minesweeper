9f5a6783f2ea5f6ae908c8d44c2c6c0d008c2907
austen.lee <austen.lee@kakaocorp.com>
2022-06-23 23:24:06 +0900

    step 1 feat : input 받고 println 한 번 더

--- src/main/kotlin/view/input/InputView.kt
+++ src/main/kotlin/view/input/InputView.kt
@@ -3,6 +3,8 @@ package view.input
 object InputView {
     fun <T> receiveUserInput(userInputRequest: UserInputRequest<T>): T {
         println(userInputRequest.message)
-        return userInputRequest.inputConverter.convert(readln())
+        return userInputRequest.inputConverter.convert(readlnOrNull()).also {
+            println()
+        }
     }
 }
