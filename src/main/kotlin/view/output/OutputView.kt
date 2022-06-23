29881e1e46c43925c62a1c1e057db52659faf288
austen.lee <austen.lee@kakaocorp.com>
2022-06-23 19:50:14 +0900

    step 1 feat : 결과를 출력하는 OutputView

---
+++ src/main/kotlin/view/output/OutputView.kt
@@ -0,0 +1,18 @@
+package view.output
+
+import view.output.converter.OutputConverter
+
+object OutputView {
+    fun printlnOnlyMessage(message: String) {
+        println(message)
+    }
+
+    fun <T> println(printable: T, outputConverter: OutputConverter<T>) {
+        print(printable, outputConverter)
+        println()
+    }
+
+    fun <T> print(printable: T, outputConverter: OutputConverter<T>) {
+        println(outputConverter.convert(printable))
+    }
+}
