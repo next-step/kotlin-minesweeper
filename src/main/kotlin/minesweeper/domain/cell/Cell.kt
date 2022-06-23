7b4a51b554b80653bfca7542b76aa46c783dc890
pug <pug.gg@kakaocorp.com>
2022-06-23 18:18:55 +0900

    feat, test: Cell.open(), 테스트 케이스 구현

--- src/main/kotlin/minesweeper/domain/cell/Cell.kt
+++ src/main/kotlin/minesweeper/domain/cell/Cell.kt
@@ -5,8 +5,16 @@ import minesweeper.domain.board.MineMaker
 sealed class Cell(
     val position: Position,
     val nearbyPositions: Positions,
-    val state: CellState = CellState.CLOSE
-)
+) {
+    var state: CellState = CellState.CLOSE
+        private set
+
+    fun open(): Cell {
+        check(state != CellState.OPEN) { "cell was already opened." }
+        state = CellState.OPEN
+        return this
+    }
+}
 
 class Cells(
     private val cells: List<Cell>
--- src/test/kotlin/minesweeper/domain/cell/CellTest.kt
+++ src/test/kotlin/minesweeper/domain/cell/CellTest.kt
@@ -1,26 +1,65 @@
 package minesweeper.domain.cell
 
+import io.mockk.boxedClass
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatCode
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+import kotlin.reflect.KClass
 
 internal class CellTest {
 
-    @Test
-    fun `CLOSE 상태의 셀은 열 수 있다`() {
-        TODO()
+    @ParameterizedTest
+    @MethodSource("임의의 셀 케이스")
+    fun `CLOSE 상태의 셀은 열 수 있다`(cellType: KClass<Cell>, cell: Cell) {
+        // when, then
+        assertThatCode { cell.open() }.doesNotThrowAnyException()
     }
 
-    @Test
-    fun `OPEN 상태의 셀은 다시 열 수 없다`() {
-        TODO()
+    @ParameterizedTest
+    @MethodSource("임의의 셀 케이스")
+    fun `셀을 열어서 안에 무엇이 있는지 확인할 수 있다`(cellType: KClass<Cell>, cell: Cell) {
+        // when
+        val result = cell.open()
+
+        // then
+        assertThat(result).isEqualTo(cell)
+        assertThat(result.boxedClass()).isEqualTo(cellType)
     }
 
-    @Test
-    fun `셀을 열어서 안에 무엇이 있는지 확인할 수 있다`() {
-        TODO()
+    @ParameterizedTest
+    @MethodSource("열린 상태의 셀 케이스")
+    fun `OPEN 상태의 셀은 다시 열 수 없다`(cell: Cell) {
+        // when
+        val exceptionByCell = assertThrows<IllegalStateException> { cell.open() }
+
+        // then
+        assertThat(exceptionByCell.message).isEqualTo("cell was already opened.")
     }
 
     @Test
     fun `지뢰가 없는 인접한 셀이 모두 열리게 된다`() {
         TODO()
+    }
+
+    companion object {
+        val POSITION = Position(0, 0, 0)
+        val NEARBY_POSITIONS = Positions.from(emptyList())
+
+        @JvmStatic
+        fun `임의의 셀 케이스`() = Stream.of(
+            Arguments.of(Empty::class, Empty(POSITION, NEARBY_POSITIONS)),
+            Arguments.of(Mine::class, Mine(POSITION, NEARBY_POSITIONS))
+        )
+
+        @JvmStatic
+        fun `열린 상태의 셀 케이스`() = Stream.of(
+            Arguments.of(Empty(POSITION, NEARBY_POSITIONS).also { it.open() }),
+            Arguments.of(Mine(POSITION, NEARBY_POSITIONS).also { it.open() })
+        )
     }
 }
