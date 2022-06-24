283800d34d4482d6f287e7d4a43b9497ce708e99
saintbeller96 <rlawhdtjd9@naver.com>
2022-06-24 19:14:52 +0900

    refactor: UI 주석 해제

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,17 @@
+import domain.Board
+import domain.Matrix
 import domain.geometric.Dimension
 import ui.InputUI
+import ui.OutputUI
 
 fun main() {
     val width = InputUI.readWidth()
     val height = InputUI.readHeight()
+    val numberOfMines = InputUI.readNumberOfMines()
 
     val dimension = Dimension(width = width, height = height)
 
-    val numberOfMines = InputUI.readNumberOfMines()
+    val board = Board(Matrix.of(dimension, numberOfMines))
 
-    // val board = Board(Matrix(dimension, numberOfMines))
-
-    // OutputUI.showBoard(board)
+    OutputUI.showBoard(board)
 }
