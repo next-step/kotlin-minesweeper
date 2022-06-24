dba43f2f90e60b464fbf22a2abd2fa38f12995f9
inpyo-jeon <inpyo.developer@gmail.com>
2022-06-24 13:51:19 +0900

    [step1] refactor: 코드리뷰 반영 - 외부 노출하지 않도록 immutable 리스트 반환

--- src/main/kotlin/domain/Minesweeper.kt
+++ src/main/kotlin/domain/Minesweeper.kt
@@ -4,13 +4,16 @@ import domain.strategy.MineAllocationStrategy
 
 class Minesweeper(minesweeperProperty: MinesweeperProperty, mineAllocationStrategy: MineAllocationStrategy) {
 
-    val board: Array<Array<Place>> =
-        Array(minesweeperProperty.height) { row ->
-            Array(minesweeperProperty.width) { col ->
+    private val _board: MutableList<MutableList<Place>> =
+        MutableList(minesweeperProperty.height) { row ->
+            MutableList(minesweeperProperty.width) { col ->
                 val number = (row * minesweeperProperty.width) + col
                 Place(number, PlaceType.NOT_MINE)
             }
         }
+
+    val board: List<List<Place>>
+    get() = _board
 
     init {
         val totalPlaceNumber = minesweeperProperty.width * minesweeperProperty.height
@@ -21,7 +24,7 @@ class Minesweeper(minesweeperProperty: MinesweeperProperty, mineAllocationStrate
             .forEach {
                 val row = it / minesweeperProperty.height
                 val col = it % minesweeperProperty.width
-                board[row][col] = Place(it, PlaceType.MINE)
+                _board[row][col] = Place(it, PlaceType.MINE)
             }
     }
 }
