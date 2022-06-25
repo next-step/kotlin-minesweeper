a2d2e32100fc3219007591843ae472d26e082841
pug <pug.gg@kakaocorp.com>
2022-06-25 11:38:42 +0900

    refactor: MineBoard 내부 검증 로직을 함수로 분리

--- src/main/kotlin/minesweeper/domain/board/MineBoard.kt
+++ src/main/kotlin/minesweeper/domain/board/MineBoard.kt
@@ -21,14 +21,16 @@ class MineBoard private constructor(val cells: Cells) {
             numberOfMines: Int,
             mineMaker: MineMaker
         ): MineBoard {
-            val size = width * height
-
-            require(width >= 0 && height >= 0) { "property must be zero or positive." }
-            require(numberOfMines in (0..size)) { "number of mines must be within range of 0 ~ $size" }
-
+            validate(width, height, numberOfMines)
             val cells = Cells.of(width, height, numberOfMines, mineMaker)
             NearbyMineCounter.count(cells)
             return MineBoard(cells)
+        }
+
+        private fun validate(width: Int, height: Int, numberOfMines: Int) {
+            val size = width * height
+            require(width >= 0 && height >= 0) { "property must be zero or positive." }
+            require(numberOfMines in (0..size)) { "number of mines must be within range of 0 ~ $size" }
         }
     }
 }
