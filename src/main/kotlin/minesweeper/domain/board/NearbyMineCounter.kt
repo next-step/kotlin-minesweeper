09255c34933d2740c94db118563ec366c64b439a
pug <pug.gg@kakaocorp.com>
2022-06-25 11:39:49 +0900

    refactor: NearbyMineCounter 클래스를 object 로 변경

--- src/main/kotlin/minesweeper/domain/board/NearbyMineCounter.kt
+++ src/main/kotlin/minesweeper/domain/board/NearbyMineCounter.kt
@@ -5,23 +5,21 @@ import minesweeper.domain.cell.Empty
 import minesweeper.domain.cell.Mine
 import minesweeper.domain.cell.Positions
 
-class NearbyMineCounter {
+object NearbyMineCounter {
 
-    companion object {
-        fun count(cells: Cells) {
-            val mineCells = cells.filterIsInstance<Mine>()
+    fun count(cells: Cells) {
+        val mineCells = cells.filterIsInstance<Mine>()
 
-            mineCells.forEach { mine ->
-                cells.accNearbyMine(mine.nearbyPositions)
-            }
+        mineCells.forEach { mine ->
+            cells.accNearbyMine(mine.nearbyPositions)
         }
+    }
 
-        private fun Cells.accNearbyMine(nearbyPositions: Positions) {
-            nearbyPositions.forEach { position ->
-                this[position.index]
-                    .takeIf { it is Empty }
-                    ?.let { (it as Empty).accNumberOfNearbyMines() }
-            }
+    private fun Cells.accNearbyMine(nearbyPositions: Positions) {
+        nearbyPositions.forEach { position ->
+            this[position.index]
+                .takeIf { it is Empty }
+                ?.let { (it as Empty).accNumberOfNearbyMines() }
         }
     }
 }
