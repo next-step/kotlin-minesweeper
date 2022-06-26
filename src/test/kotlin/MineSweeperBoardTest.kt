a6d4f01cecbca2a0c14bb5bce00577052ae80384
Gomding <backchida@gmail.com>
2022-06-26 21:43:00 +0900

    [Step1] feature: Board를 구성할 때 validate 추가
    - 높이는 1이상의 정수여야 한다.
    - 너비는 1이상의 정수여야 한다.
    - 지뢰 개수는 1개 이상 존재해야한다.

--- src/test/kotlin/MineSweeperBoardTest.kt
+++ src/test/kotlin/MineSweeperBoardTest.kt
@@ -1,3 +1,4 @@
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
@@ -27,5 +28,35 @@ internal class MineSweeperBoardTest : FunSpec({
         // then
         result.field.count { it is SafeZone } shouldBe 19
         result.field.count { it is Mine } shouldBe 5
+    }
+
+    test("보드를 구성할 때 높이가 0이하의 값이라면 예외가 발생한다.") {
+        // given
+        val height = 0
+        val width = 6
+        val mineCount = 5
+
+        // when then
+        shouldThrow<IllegalArgumentException> { MineSweeperBoard.of(height = height, width = width, mineCount = mineCount) }
+    }
+
+    test("보드를 구성할 때 너비가 0이하의 값이라면 예외가 발생한다.") {
+        // given
+        val height = 10
+        val width = 0
+        val mineCount = 5
+
+        // when then
+        shouldThrow<IllegalArgumentException> { MineSweeperBoard.of(height = height, width = width, mineCount = mineCount) }
+    }
+
+    test("보드를 구성할 때 지뢰 개수가 0이하의 값이라면 예외가 발생한다.") {
+        // given
+        val height = 10
+        val width = 6
+        val mineCount = 0
+
+        // when then
+        shouldThrow<IllegalArgumentException> { MineSweeperBoard.of(height = height, width = width, mineCount = mineCount) }
     }
 })
