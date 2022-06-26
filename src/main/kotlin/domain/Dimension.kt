533e0e59c4461fd635763a494ca82d34c53db10f
keira <keira.j@kakaocorp.com>
2022-06-27 00:30:00 +0900

    feature: 리뷰 반영 lintcheck

--- src/main/kotlin/domain/Dimension.kt
+++ src/main/kotlin/domain/Dimension.kt
@@ -4,8 +4,6 @@ data class Dimension(
     val width: Int,
     val height: Int,
 ) {
-    val area: Int = width * height
-
     init {
         require(width > 0 && height > 0) { INVALID_WIDTH_HEIGHT_INPUT_MESSAGE }
     }
