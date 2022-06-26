b803867df611668c08455e291b936f195425a9da
inpyo-jeon <inpyo.developer@gmail.com>
2022-06-26 20:42:59 +0900

    [step2] refactor: 코드리뷰 반영 - nullable 제거 후 생성자 추가

--- src/main/kotlin/domain/Place.kt
+++ src/main/kotlin/domain/Place.kt
@@ -3,8 +3,10 @@ package domain
 class Place(
     val number: Int,
     val placeType: PlaceType,
-    val nearMineCount: Int? = 0
+    val nearMineCount: Int
 ) {
+
+    constructor(number: Int, placeType: PlaceType) : this(number = number, placeType = placeType, nearMineCount = 0)
 
     fun isMine(): Boolean {
         return placeType == PlaceType.MINE
