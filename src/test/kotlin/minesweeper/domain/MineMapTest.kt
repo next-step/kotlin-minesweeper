5636b243fdcc7bb85b7bd3446b82f995cb94a171
revfactory <revfactory@gmail.com>
2022-06-27 17:51:17 +0900

    doc: update docs

--- src/test/kotlin/minesweeper/domain/MineMapTest.kt
+++ src/test/kotlin/minesweeper/domain/MineMapTest.kt
@@ -6,7 +6,7 @@ import io.kotest.matchers.shouldBe
 
 class MineMapTest : StringSpec({
 
-    "입력받은 높이(height), 너비(weight) 만큼 칸을 만든다." {
+    "지뢰 맵은 입력받은 높이(height), 너비(weight) 만큼 칸을 만든다." {
         val mineMap = MineMap.build(10, 5)
         mineMap.map().size shouldBe  10
         mineMap.map().all { it.size == 5 } shouldBe true
