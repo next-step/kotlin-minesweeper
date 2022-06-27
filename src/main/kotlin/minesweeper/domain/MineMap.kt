0649f17c1d4836498be6d5b2cebf1e643d56c85a
revfactory <revfactory@gmail.com>
2022-06-27 17:56:54 +0900

    feat:  Add mineCount - success

--- src/main/kotlin/minesweeper/domain/MineMap.kt
+++ src/main/kotlin/minesweeper/domain/MineMap.kt
@@ -10,6 +10,8 @@ class MineMap private constructor(private val map: List<List<Boolean>>) {
         fun build(width: Int, height: Int, mineCount: Int = 0): MineMap {
             require(width > 0 && height > 0) { "Invalid Mine Map Size" }
             val array = Array(width * height) { false }
+            array.fill(true, 0, mineCount)
+            array.shuffle()
             return MineMap(array.toList().chunked(height))
         }
     }
