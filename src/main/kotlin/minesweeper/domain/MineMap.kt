81ed215884e547b4f5caead2541446bec2b2a950
revfactory <revfactory@gmail.com>
2022-06-27 18:14:22 +0900

    refactor: Refactoring Code

--- src/main/kotlin/minesweeper/domain/MineMap.kt
+++ src/main/kotlin/minesweeper/domain/MineMap.kt
@@ -7,11 +7,11 @@ class MineMap(width: Int, height: Int, mineCount: Int = 0) {
     init {
         require(width > 0 && height > 0) { "Invalid Mine Map Size" }
         require(mineCount >= 0 && width * height >= mineCount) { "Invalid Mine Count" }
-
-        val array = Array(width * height) { false }
-        array.fill(true, 0, mineCount)
-        array.shuffle()
-        map = array.toList().chunked(height)
+        map = Array(width * height) { false }
+            .apply { fill(true, 0, mineCount) }
+            .apply { shuffle() }
+            .toList()
+            .chunked(height)
     }
 
     fun map(): List<List<Boolean>> {
