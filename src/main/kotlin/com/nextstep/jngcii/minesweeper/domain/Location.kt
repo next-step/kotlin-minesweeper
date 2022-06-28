23470a4ebfe6412cfb1f90e5b51fa4e76b9ba065
jngcii <concotree@gmail.com>
2022-06-28 09:39:47 +0900

    refactor : _isMine 배킹필드 제거

--- src/main/kotlin/com/nextstep/jngcii/minesweeper/domain/Location.kt
+++ src/main/kotlin/com/nextstep/jngcii/minesweeper/domain/Location.kt
@@ -4,17 +4,17 @@ data class Location(
     val x: Int,
     val y: Int,
 ) {
-    private var _isMine: Boolean = false
-    val isMine get() = _isMine
+    var isMine: Boolean = false
+        private set
 
     init {
         require(x >= 0 && y >= 0) { "입력 좌표가 0 이상이어야 합니다." }
     }
 
     fun pick() {
-        if (_isMine) {
+        if (isMine) {
             throw IllegalStateException("이미 지뢰로 선택된 위치입니다.")
         }
-        _isMine = true
+        isMine = true
     }
 }
