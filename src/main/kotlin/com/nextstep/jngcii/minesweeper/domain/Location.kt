182de91effbc467ea15316c904fb7d77602be35a
jngcii <concotree@gmail.com>
2022-06-28 09:38:21 +0900

    refactor : kotlin class coding convention 적용

--- src/main/kotlin/com/nextstep/jngcii/minesweeper/domain/Location.kt
+++ src/main/kotlin/com/nextstep/jngcii/minesweeper/domain/Location.kt
@@ -7,14 +7,14 @@ data class Location(
     private var _isMine: Boolean = false
     val isMine get() = _isMine
 
+    init {
+        require(x >= 0 && y >= 0) { "입력 좌표가 0 이상이어야 합니다." }
+    }
+
     fun pick() {
         if (_isMine) {
             throw IllegalStateException("이미 지뢰로 선택된 위치입니다.")
         }
         _isMine = true
-    }
-
-    init {
-        require(x >= 0 && y >= 0) { "입력 좌표가 0 이상이어야 합니다." }
     }
 }
