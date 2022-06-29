da71e614b124a138de93064e1e6c4e3070045e3d
Jake Son <sonjeabin@gmail.com>
2022-06-29 11:09:06 +0900

    refactor: 코드 위치 조정

--- src/main/kotlin/domain/Board.kt
+++ src/main/kotlin/domain/Board.kt
@@ -2,12 +2,12 @@ package domain
 
 data class Board(val rows: List<Row>) {
 
+    val mineCount = rows.flatMap { it.cells }.count { it is Mine }
+
+    val cellCount = rows.first().size * rows.size
+
     init {
         require(rows.isNotEmpty()) { "적어도 하나의 row 가 필요합니다" }
         require(rows.map { it.size }.distinct().size == 1) { "모든 row 의 크기는 같아야 합니다" }
     }
-
-    val mineCount = rows.flatMap { it.cells }.count { it is Mine }
-
-    val cellCount = rows.first().size * rows.size
 }
