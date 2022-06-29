6da55526d8dfe3e3852b42530ae57f6bb1f23b03
erie0210 <2j00923@gmail.com>
2022-06-29 12:08:23 +0900

    feat: ui 구현

--- src/main/kotlin/minesweeper/ui/Input.kt
+++ src/main/kotlin/minesweeper/ui/Input.kt
@@ -1,4 +1,18 @@
 package minesweeper.ui
 
-class Input {
+object Input {
+  fun getHeight(): Int{
+    println("높이를 입력하세요.")
+    return readln().toInt()
+  }
+
+  fun getWidth(): Int{
+    println("너비를 입력하세요.")
+    return readln().toInt()
+  }
+
+  fun getNumberOfMine(): Int{
+    println("지뢰는 몇 개인가요?")
+    return readln().toInt()
+  }
 }
--- src/main/kotlin/minesweeper/ui/Result.kt
+++ src/main/kotlin/minesweeper/ui/Result.kt
@@ -1,4 +1,10 @@
 package minesweeper.ui
 
+import minesweeper.entity.MapElement
+
 class Result {
+  fun informPlay(mineMap: List<List<MapElement>>) {
+    println("지뢰찾기 게임 시작")
+    mineMap.forEach { row: List<MapElement> -> println(row.joinToString(" ") { element: MapElement -> element.represent }) }
+  }
 }
