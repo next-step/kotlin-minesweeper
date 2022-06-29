9a899ddf26e794029fb62194c367f7d96a7b6eb1
ujusy <lsyeon1997@ajou.ac.kr>
2022-06-29 14:38:50 +0900

    refactor: indent

--- src/main/kotlin/dto/CoordinateGeneratorRequest.kt
+++ src/main/kotlin/dto/CoordinateGeneratorRequest.kt
@@ -1,14 +1,20 @@
 package dto
 
 import domain.Coordinate
+import domain.CoordinatePoint
 import util.ConvertType
 
 class CoordinateGeneratorRequest(val coordinates: List<Coordinate>, val landMine: Int) {
 
     companion object {
         fun of(landMineMapRequest: LandMineMapRequest, landMine: String): CoordinateGeneratorRequest {
-            val coordinates: List<Coordinate> = landMineMapRequest.height.flatMap { y -> landMineMapRequest.width.map { x -> Coordinate(x, y) } }
+            val coordinates: List<Coordinate> = landMineMapRequest.height
+                .flatMap { y -> coordinatesByHeight(y, landMineMapRequest) }
             return CoordinateGeneratorRequest(coordinates, ConvertType.int(landMine))
+        }
+
+        private fun coordinatesByHeight(y: CoordinatePoint, landMineMapRequest: LandMineMapRequest): List<Coordinate> {
+            return landMineMapRequest.width.map { x -> Coordinate(x, y) }
         }
     }
 }
