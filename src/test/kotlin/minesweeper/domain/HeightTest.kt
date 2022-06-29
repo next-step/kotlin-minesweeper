5f578b13a95aed6f02a7f383d6da612900a37a56
Rok93 <kr.kim@mfort.co.kr>
2022-06-30 01:27:52 +0900

    refactor: 테스트 코드 네이밍 변경

--- src/test/kotlin/minesweeper/domain/HeightTest.kt
+++ src/test/kotlin/minesweeper/domain/HeightTest.kt
@@ -4,7 +4,7 @@ import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.StringSpec
 
 class HeightTest : StringSpec({
-    "0이하의 정수를 입력하면 예외를 발생시킨다." {
+    "높이 값으로 0이하의 정수를 입력하면 예외를 발생시킨다." {
         listOf(
             0,
             -1,
