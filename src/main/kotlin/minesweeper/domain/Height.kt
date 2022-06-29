3c2cc46e593a46d20fa8123388d9b4e7d50d310a
Rok93 <kr.kim@mfort.co.kr>
2022-06-30 01:23:28 +0900

    refactor: 매직넘버 상수로 치환

--- src/main/kotlin/minesweeper/domain/Height.kt
+++ src/main/kotlin/minesweeper/domain/Height.kt
@@ -1,10 +1,12 @@
 package minesweeper.domain
 
+private const val MIN_HEIGHT_VALUE = 0
+
 @JvmInline
 value class Height(
     val value: Int,
 ) {
     init {
-        require(value > 0) { "높이는 0보다 큰 정수여야합니다." }
+        require(value > MIN_HEIGHT_VALUE) { "높이는 0보다 큰 정수여야합니다." }
     }
 }
