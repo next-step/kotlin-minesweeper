75b07520a00d6d7afac52419c0460b5d955d5891
austen.lee <austen.lee@kakaocorp.com>
2022-06-30 23:11:30 +0900

    step 1 feat : nearMineCount를 받아서 Land를 반환하는 팩토리 메서드

--- src/main/kotlin/domain/Cell.kt
+++ src/main/kotlin/domain/Cell.kt
@@ -12,6 +12,19 @@ sealed interface Cell {
         FIVE(5),
         SIX(6),
         SEVEN(7),
-        EIGHT(8)
+        EIGHT(8);
+
+        companion object {
+            private const val MESSAGE_INVALID_MINE_COUNT = "근처에 있는 지뢰의 개수는 0부터 8까지만 존재할 수 있습니다."
+
+            fun from(nearMineCount: Int): Land {
+                val land = values().find {
+                    it.nearMineCount == nearMineCount
+                }
+                return requireNotNull(land) {
+                    MESSAGE_INVALID_MINE_COUNT
+                }
+            }
+        }
     }
 }
