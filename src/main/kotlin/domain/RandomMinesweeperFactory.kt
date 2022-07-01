ff533ac0bd7b05115494228160b2020f91892296
austen.lee <austen.lee@kakaocorp.com>
2022-07-01 22:56:18 +0900

    step 1 refactor : 근처에 지뢰가 몇 개인지 표시하도록 변경

--- src/main/kotlin/domain/RandomMinesweeperFactory.kt
+++ src/main/kotlin/domain/RandomMinesweeperFactory.kt
@@ -2,19 +2,40 @@ package domain
 
 object RandomMinesweeperFactory : MinesweeperFactory {
     override fun create(minesweeperInfo: MinesweeperInfo): Minesweeper {
-        val landCount = getLandCount(minesweeperInfo)
-
-        val lands = List(landCount) { Cell.Land.ZERO }
-        val mines = List(minesweeperInfo.mineCount) { Cell.Mine }
-
-        val rows = (lands + mines)
-            .shuffled()
-            .chunked(minesweeperInfo.columnCount)
-            .map { cells ->
-                Row(cells)
+        val matrix: Array<Array<Cell>> = Array(minesweeperInfo.rowCount) {
+            Array(minesweeperInfo.columnCount) {
+                Cell.Land.ZERO
             }
+        }
 
-        return Minesweeper(rows)
+        getMinePositions(minesweeperInfo).forEach { minePosition ->
+            matrix[minePosition.row][minePosition.column] = Cell.Mine
+
+            Direction.values().forEach { direction ->
+                val row = minePosition.row + direction.diffRow
+                val column = minePosition.column + direction.diffColumn
+
+                if (row < 0 || column < 0 || row >= minesweeperInfo.rowCount || column >= minesweeperInfo.columnCount) return@forEach
+                matrix[row][column] = matrix[row][column].increaseMineCount()
+            }
+        }
+
+        return Minesweeper(
+            matrix.map { row -> Row(*row) }
+        )
+    }
+
+    private fun getMinePositions(minesweeperInfo: MinesweeperInfo): List<CellPosition> {
+        val cellCount = minesweeperInfo.rowCount * minesweeperInfo.columnCount
+        val mineIndexes = cellCount.toShuffledIndexes().take(minesweeperInfo.mineCount)
+
+        return mineIndexes.map { mineIndex ->
+            CellPosition.from(mineIndex, minesweeperInfo.columnCount)
+        }
+    }
+
+    private fun Int.toShuffledIndexes(): List<Int> {
+        return (0 until this.coerceAtLeast(1)).shuffled()
     }
 
     private fun getLandCount(minesweeperInfo: MinesweeperInfo): Int {
