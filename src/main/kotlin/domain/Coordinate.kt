922e54239d7334156d7c798d3f2ecd4ba60abde8
Jake Son <sonjeabin@gmail.com>
2022-07-02 13:49:23 +0900

    feat: 주변좌표 여부 메소드 구현

--- src/main/kotlin/domain/Coordinate.kt
+++ src/main/kotlin/domain/Coordinate.kt
@@ -7,7 +7,7 @@ data class Coordinate(
     val y: Point,
 ) {
 
-    val surroundings: List<Coordinate> by lazy {
+    private val surroundings: List<Coordinate> by lazy {
         listOfNotNull(
             // top
             makeCoordinateOrNull(x = x - 1, y = y - 1),
@@ -27,6 +27,9 @@ data class Coordinate(
         require(x.isPositive) { "x 는 1보다 커야합니다" }
         require(y.isPositive) { "y 는 1보다 커야합니다" }
     }
+
+    fun isAdjacentTo(coordinate: Coordinate): Boolean =
+        surroundings.contains(coordinate)
 
     private fun makeCoordinateOrNull(x: Point, y: Point): Coordinate? =
         runCatching { Coordinate(x, y) }.getOrNull()
