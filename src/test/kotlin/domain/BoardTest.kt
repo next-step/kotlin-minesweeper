a47dba9f82e9c17402a9517f538c6eadec1a2cab
Jake Son <sonjeabin@gmail.com>
2022-07-02 12:09:11 +0900

    test: board 생성 성공 테스트 추가

--- src/test/kotlin/domain/BoardTest.kt
+++ src/test/kotlin/domain/BoardTest.kt
@@ -2,6 +2,7 @@ package domain
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 internal class BoardTest : StringSpec({
 
@@ -20,5 +21,16 @@ internal class BoardTest : StringSpec({
             )
             Board.of(rows)
         }
+    }
+
+    "Board 인스턴스를 생성할 수 있다" {
+        val rows = listOf(
+            createRow(3, 1),
+            createRow(3, 2),
+            createRow(3, 3),
+        )
+        val board = Board.of(rows)
+
+        board.cellCount shouldBe 9
     }
 })
