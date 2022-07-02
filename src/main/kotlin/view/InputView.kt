4779710c860ba6ffc0792994cc9167be92e122fb
link-js <link.js@kakaocorp.com>
2022-07-03 02:35:06 +0900

    refactor : 상수는 다 각자 쓰는곳으로...

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,16 +1,10 @@
 package view
 
-import constants.ErrorMessages
-
 /**
  * 입력을 받는 클래스
  * Created by Jaesungchi on 2022.06.28..
  */
-object InputView {
-    private const val WRITE_HEIGHT = "높이를 입력하세요."
-    private const val WRITE_WIDTH = "너비를 입력하세요."
-    private const val WRITE_MINE_COUNT = "지뢰는 몇 개인가요?"
-
+class InputView() {
     fun getHeight(readStringValue: () -> String? = { readlnOrNull() }): Int {
         println(WRITE_HEIGHT)
         return getInt(readStringValue)
@@ -28,8 +22,16 @@ object InputView {
 
     private fun getInt(readStringValue: () -> String?): Int {
         val input = readStringValue()
-        require(!input.isNullOrBlank()) { ErrorMessages.IS_NULL_OR_EMPTY }
-        require(input.toIntOrNull() != null) { ErrorMessages.IS_NOT_NUMBER }
+        require(!input.isNullOrBlank()) { IS_NULL_OR_EMPTY }
+        require(input.toIntOrNull() != null) { IS_NOT_NUMBER }
         return input.toInt()
+    }
+
+    companion object {
+        private const val WRITE_HEIGHT = "높이를 입력하세요."
+        private const val WRITE_WIDTH = "너비를 입력하세요."
+        private const val WRITE_MINE_COUNT = "지뢰는 몇 개인가요?"
+        private const val IS_NULL_OR_EMPTY = "입력된 값이 없습니다."
+        private const val IS_NOT_NUMBER = "숫자가 아닙니다."
     }
 }
