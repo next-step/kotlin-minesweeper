8cbc18f271cd4b1375ef61abbb5c116cb90a9ee8
Jake Son <sonjeabin@gmail.com>
2022-07-02 16:38:05 +0900

    test: empty 열림 처리 관련 테스트 추가

--- src/test/kotlin/domain/CellTest.kt
+++ src/test/kotlin/domain/CellTest.kt
@@ -2,6 +2,8 @@ package domain
 
 import domain.vo.Point
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.types.shouldBeInstanceOf
 
 internal class CellTest : StringSpec({
@@ -16,5 +18,15 @@ internal class CellTest : StringSpec({
         val cell = Empty(Coordinate(Point(1), Point(2)))
 
         cell.shouldBeInstanceOf<Cell>()
+    }
+
+    "지뢰없는 cell 을 open 처리 한다" {
+        val cell = Empty(Coordinate(Point(1), Point(2)))
+
+        cell.opended.shouldBeFalse()
+
+        cell.open()
+
+        cell.opended.shouldBeTrue()
     }
 })
