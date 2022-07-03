91a10745a876e4b71647268b162e5c7cb1d73c9e
YooInKeun <keun0390@naver.com>
2022-07-03 19:41:24 +0900

    fix: lint 오류 해결

--- src/test/kotlin/minesweeper/model/ClosedNonMineTest.kt
+++ src/test/kotlin/minesweeper/model/ClosedNonMineTest.kt
@@ -1,6 +1,5 @@
 package minesweeper.model
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
