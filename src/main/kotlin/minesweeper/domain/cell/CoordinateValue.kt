cfebfffb8df1bcce3db40974c4268ff7d6609ffe
Hyeon9mak <jinha3507@gmail.com>
2022-07-05 20:17:14 +0900

    feat: 좌표 방향 Enum 및 주변 좌표 구하기 기능 구현

--- src/main/kotlin/minesweeper/domain/cell/CoordinateValue.kt
+++ src/main/kotlin/minesweeper/domain/cell/CoordinateValue.kt
@@ -4,10 +4,6 @@ package minesweeper.domain.cell
 value class CoordinateValue(
     val value: Int,
 ) {
-    init {
-        require(value >= 0) { "셀 좌표 값은 음수일 수 없습니다." }
-    }
-
     operator fun plus(other: CoordinateValue): CoordinateValue = CoordinateValue(value + other.value)
 
     operator fun minus(other: CoordinateValue): CoordinateValue = CoordinateValue(value - other.value)
