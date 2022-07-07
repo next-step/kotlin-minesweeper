bae4dea8e392e66c918efd194810445aff2a9d6c
hyeon9mak <jinha3507@gmail.com>
2022-07-07 10:31:43 +0900

    refactor: 메서드 분리를 통한 가독성 개선

--- src/main/kotlin/minesweeper/domain/cell/Coordinates.kt
+++ src/main/kotlin/minesweeper/domain/cell/Coordinates.kt
@@ -13,37 +13,34 @@ class Coordinates(
 
     private fun shuffleAndTake(count: Int): List<Coordinate> {
         require(values.size >= count) { "좌표 개수보다 지뢰 개수가 많을 수 없습니다." }
-        return values.shuffled()
-            .take(count)
+        return values.shuffled().take(count)
     }
 
-    private fun mineAt(mineCoordinates: List<Coordinate>) =
-        values.associate { coordinate ->
-            if (coordinate in mineCoordinates) {
-                coordinate to Mine
-            } else {
-                coordinate to Land(mineCount(coordinate, mineCoordinates))
-            }
+    private fun mineAt(mineCoordinates: List<Coordinate>) = values.associate { coordinate ->
+        if (coordinate in mineCoordinates) {
+            coordinate to Mine
+        } else {
+            coordinate to Land(mineCount(coordinate, mineCoordinates))
         }
+    }
 
     private fun mineCount(
-        coordinate: Coordinate,
-        mineCoordinates: List<Coordinate>
-    ) = MineCount(CoordinateDirection.aroundCoordinates(coordinate)
-        .count { it in mineCoordinates })
+        coordinate: Coordinate, mineCoordinates: List<Coordinate>
+    ) = MineCount(CoordinateDirection.aroundCoordinates(coordinate).count { it in mineCoordinates })
 
     companion object {
         private const val START_INDEX = 0
 
         fun from(height: MineBoardLength, width: MineBoardLength): Coordinates = Coordinates(
             (START_INDEX until height.value).flatMap { xValue ->
-                (START_INDEX until width.value).map { yValue ->
-                    Coordinate(
-                        y = CoordinateValue(value = yValue),
-                        x = CoordinateValue(value = xValue)
-                    )
-                }
+                generateRows(width, xValue)
             }
         )
+
+        private fun generateRows(width: MineBoardLength, xValue: Int) =
+            (START_INDEX until width.value).map { yValue ->
+                Coordinate(y = CoordinateValue(value = yValue), x = CoordinateValue(value = xValue)
+            )
+        }
     }
 }
