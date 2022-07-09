dc77556b0960feb81b51343e3f6e6d71bec79106
austen.lee <austen.lee@kakaocorp.com>
2022-07-09 17:51:48 +0900

    step 1 refactor : Validator 사용

--- src/main/kotlin/domain/MinePositionsFactory.kt
+++ src/main/kotlin/domain/MinePositionsFactory.kt
@@ -1,27 +1,17 @@
 package domain
 
+import domain.validator.DuplicatePositionValidator
+import domain.validator.MineCountValidator
+import domain.validator.OutOfBoundaryValidator
+
 abstract class MinePositionsFactory {
     fun create(minesweeperInfo: MinesweeperInfo): List<CellPosition> {
         return getMinePositionsFrom(minesweeperInfo).also { minePositions ->
-            require(minePositions.distinct().size == minePositions.size) {
-                MESSAGE_DUPLICATE_MINE_POSITIONS
-            }
-            require(minePositions.size == minesweeperInfo.mineCount) {
-                "$MESSAGE_INVALID_MINE_COUNT ${minePositions.size}"
-            }
-            val outOfBoundaryMines =
-                minePositions.filter { minePosition -> !minePosition.isInBoundaryOf(minesweeperInfo) }
-            require(outOfBoundaryMines.isEmpty()) {
-                "$MESSAGE_OUT_OF_BOUNDARY $outOfBoundaryMines"
-            }
+            DuplicatePositionValidator.validate(minePositions, minesweeperInfo)
+            MineCountValidator.validate(minePositions, minesweeperInfo)
+            OutOfBoundaryValidator.validate(minePositions, minesweeperInfo)
         }
     }
 
     protected abstract fun getMinePositionsFrom(minesweeperInfo: MinesweeperInfo): List<CellPosition>
-
-    companion object {
-        private const val MESSAGE_DUPLICATE_MINE_POSITIONS = "중복된 지뢰 좌표가 생성되었습니다."
-        private const val MESSAGE_INVALID_MINE_COUNT = "사용자의 입력과 동일한 개수의 지뢰가 생성되어야 합니다.\n생성된 지뢰 개수: "
-        private const val MESSAGE_OUT_OF_BOUNDARY = "범위를 넘어선 곳에 지뢰를 생성할 수 없습니다.\n유효하지 않은 좌표 목록: "
-    }
 }
