434887d00a5ff39811f1a9fcb80265d869e9e5a2
erie0210 <2j00923@gmail.com>
2022-07-10 21:54:40 +0900

    feat: 지도 요소에 숫자를 추가, 값으로 요소를 찾는 함수 추가

--- src/main/kotlin/minesweeper/entity/MapElement.kt
+++ src/main/kotlin/minesweeper/entity/MapElement.kt
@@ -1,5 +1,17 @@
 package minesweeper.entity
 
 enum class MapElement(val represent: String) {
-    GROUND("C"), MINE("*");
+    GROUND("C"),
+    MINE("*"),
+    ZERO("0"),
+    ONE("1"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4");
+
+    companion object {
+        fun find(countOfMines: Int): MapElement {
+            return MapElement.values().find { it -> (it.represent == countOfMines.toString()) } ?: ZERO
+        }
+    }
 }
--- src/test/kotlin/minesweeper/entity/MapElementTest.kt
+++ src/test/kotlin/minesweeper/entity/MapElementTest.kt
@@ -23,4 +23,69 @@ internal class MapElementTest {
         // then
         Assertions.assertThat(groundRepresent).isEqualTo(expected)
     }
+
+    @Test
+    fun `0 개의 지뢰는 ZERO에 대응된다`() {
+        // given
+        val countOfMines = 0
+        val expected = MapElement.ZERO
+
+        // when
+        val result = MapElement.find(countOfMines)
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `1 개의 지뢰는 ZERO에 대응된다`() {
+        // given
+        val countOfMines = 1
+        val expected = MapElement.ONE
+
+        // when
+        val result = MapElement.find(countOfMines)
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `2 개의 지뢰는 ZERO에 대응된다`() {
+        // given
+        val countOfMines = 2
+        val expected = MapElement.TWO
+
+        // when
+        val result = MapElement.find(countOfMines)
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `3 개의 지뢰는 ZERO에 대응된다`() {
+        // given
+        val countOfMines = 3
+        val expected = MapElement.THREE
+
+        // when
+        val result = MapElement.find(countOfMines)
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `4 개의 지뢰는 ZERO에 대응된다`() {
+        // given
+        val countOfMines = 4
+        val expected = MapElement.FOUR
+
+        // when
+        val result = MapElement.find(countOfMines)
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected)
+    }
 }
