baba3357895446b262feea8ded443979d0787bdb
austen.lee <austen.lee@kakaocorp.com>
2022-07-10 21:36:49 +0900

    step 1 refactor : 이제 필요 없어진 Validator 제거.

--- src/main/kotlin/domain/validator/MinePositionsValidator.kt
+++
@@ -1,9 +0,0 @@
-package domain.validator
-
-import domain.CellPosition
-import domain.MinesweeperInfo
-
-interface MinePositionsValidator {
-    @Throws(IllegalArgumentException::class)
-    fun validate(minePositions: List<CellPosition>, minesweeperInfo: MinesweeperInfo)
-}
