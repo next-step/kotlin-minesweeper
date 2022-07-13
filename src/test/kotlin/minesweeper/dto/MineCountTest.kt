16205aedb6aedf4037d29e85fa890dc609f671d7
Hyeon9mak <jinha3507@gmail.com>
2022-07-13 22:33:23 +0900

    feat: 지뢰 개수가 0인지 상태값 메서드 구현

--- src/test/kotlin/minesweeper/dto/MineCountTest.kt
+++ src/test/kotlin/minesweeper/dto/MineCountTest.kt
@@ -2,7 +2,9 @@ package minesweeper.dto
 
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.data.row
 import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
 
 internal class MineCountTest : FreeSpec({
 
@@ -14,6 +16,19 @@ internal class MineCountTest : FreeSpec({
         ).forAll {
             "$it 은 예외 발생" {
                 shouldThrowExactly<IllegalArgumentException> { MineBoardLength(value = it) }
+            }
+        }
+    }
+
+    "지뢰 개수가 0개인지 알 수 있다." - {
+        listOf(
+            row(MineCount(0), true),
+            row(MineCount(1), false),
+            row(MineCount(10), false),
+            row(MineCount(99), false),
+        ).forEach { (mineCount, result) ->
+            "$mineCount 의 값이 0인지 물으면 $result 를 답한다." {
+                mineCount.isZero() shouldBe result
             }
         }
     }
