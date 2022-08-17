d487f6b2c999de5c18f35fb75b06d08ab5a216e8
GyeongrokKim <kr.kim@mfort.co.kr>
2022-08-17 22:02:54 +0900

    refactor: 지뢰찾기판 Factory 필드에 지뢰찾기판 생성 전략 추가

--- src/main/kotlin/v2minesweeper/domain/MineSweeperBoardFactory.kt
+++ src/main/kotlin/v2minesweeper/domain/MineSweeperBoardFactory.kt
@@ -3,23 +3,31 @@ package v2minesweeper.domain
 class MineSweeperBoardFactory(
     val height: Height,
     val width: Width,
-    val mineNumber: MineNumber
+    val mineNumber: MineNumber,
+    val mineSweeperBoardGenerator: (height: Height, width: Width, mineNumber: MineNumber) -> MineSweeperBoard
 ) {
     init {
         require((height.value * width.value) >= mineNumber.value) { "총 넓이보다 지뢰 갯수가 클 수 없습니다." }
     }
 
-    fun create(): MineSweeperBoard {
-        val totalSize = (height.value * width.value)
-        val mineZones = List(mineNumber.value) { MineZone }
-        val safeZones = List(totalSize - mineNumber.value) { SafeZone }
-
-        return (mineZones + safeZones).shuffled()
-            .chunked(width.value)
-            .flatMapIndexed { x, zones ->
-                zones.mapIndexed { y, zone -> (x + 1 to y + 1) to zone }
-            }
-            .toMap()
-            .let { MineSweeperBoard(it) }
-    }
+    fun operate(): MineSweeperBoard = mineSweeperBoardGenerator(
+        height,
+        width,
+        mineNumber
+    )
 }
+
+fun createRandomMineSweeperBoard(height: Height, width: Width, mineNumber: MineNumber): MineSweeperBoard {
+    val totalSize = (height.value * width.value)
+    val mineZones = List(mineNumber.value) { MineZone }
+    val safeZones = List(totalSize - mineNumber.value) { SafeZone }
+
+    return (mineZones + safeZones).shuffled()
+        .chunked(width.value)
+        .flatMapIndexed { x, zones ->
+            zones.mapIndexed { y, zone -> (x + 1 to y + 1) to zone }
+        }
+        .toMap()
+        .let { MineSweeperBoard(it) }
+}
+
--- src/test/kotlin/v2minesweeper/domain/MineSweeperBoardFactoryTest.kt
+++ src/test/kotlin/v2minesweeper/domain/MineSweeperBoardFactoryTest.kt
@@ -4,6 +4,7 @@ import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeSameInstanceAs
 
 class MineSweeperBoardFactoryTest : FunSpec({
     test("총 zone 갯수(높이 x 너비)보다 지뢰 갯수가 더 많으면 예외를 발생시킨다.") {
@@ -18,36 +19,45 @@ class MineSweeperBoardFactoryTest : FunSpec({
                 height = Height(height),
                 width = Width(width),
                 mineNumber = MineNumber(invalidMineNumber)
-            )
+            ) { _, _, _ ->
+                MineSweeperBoard(emptyMap())
+            }
         }
     }
 
     test("지정한 높이, 너비, 지뢰 갯수에 맞게 지뢰판을 생성한다.") {
         // given
-        val height = 3
-        val width = 3
-        val mineNumber = 4
+        val height = 2
+        val width = 2
+        val mineNumber = 2
         val mineSweeperBoardFactory = MineSweeperBoardFactory(
             height = Height(height),
             width = Width(width),
             mineNumber = MineNumber(mineNumber)
-        )
+        ) { height, width, mineNumber ->
+            MineSweeperBoard(
+                mapOf(
+                    (1 to 1) to MineZone,
+                    (1 to 2) to SafeZone,
+                    (2 to 1) to SafeZone,
+                    (2 to 2) to MineZone
+                )
+            )
+        }
 
         // when
-        val mineSweeperBoard = mineSweeperBoardFactory.create()
+        val mineSweeperBoard = mineSweeperBoardFactory.operate()
 
         // then
         assertSoftly(mineSweeperBoard) {
-            // TODO 2022-08-14 경록: 랜덤한 요소에 의존 중인 테스트 (개선 필요)
             zones.containsKey(1 to 1) shouldBe true
+            zones[1 to 1] shouldBeSameInstanceAs MineZone
             zones.containsKey(1 to 2) shouldBe true
-            zones.containsKey(1 to 3) shouldBe true
+            zones[1 to 2] shouldBeSameInstanceAs SafeZone
             zones.containsKey(2 to 1) shouldBe true
+            zones[2 to 1] shouldBeSameInstanceAs SafeZone
             zones.containsKey(2 to 2) shouldBe true
-            zones.containsKey(2 to 3) shouldBe true
-            zones.containsKey(3 to 1) shouldBe true
-            zones.containsKey(3 to 2) shouldBe true
-            zones.containsKey(3 to 3) shouldBe true
+            zones[2 to 2] shouldBeSameInstanceAs MineZone
         }
     }
 })
