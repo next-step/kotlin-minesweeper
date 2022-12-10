ad88c61bd843bee0394a7533a9c18f957abbfe11
devcs96 <katd6@naver.com>
2022-12-10 19:35:58 +0900

    :recycle: refactor(domain) : point -> dot으로 네이밍 변경

--- src/main/kotlin/domain/Board.kt
+++ src/main/kotlin/domain/Board.kt
@@ -6,11 +6,11 @@ class Board(width: Int, height: Int, private val generator: MineGenerator) {
 
     init {
         board = (1..width).map {
-            createPoint(it, height)
+            createDot(it, height)
         }.flatten().toMap()
     }
 
-    private fun createPoint(width: Int, height: Int): List<Pair<Dot, DotState>> {
+    private fun createDot(width: Int, height: Int): List<Pair<Dot, DotState>> {
         return (1..height).map {
             Dot(width, it) to generator.generate()
         }
