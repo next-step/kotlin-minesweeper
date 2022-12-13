174884214208bd37fa03ea472e6e17de95079954
wonjun3991 <wonjun.choi@meshkorea.net>
2022-12-13 22:13:37 +0900

    feature(Map): add print map method

--- src/main/kotlin/minesweeper/Map.kt
+++ src/main/kotlin/minesweeper/Map.kt
@@ -1,4 +1,27 @@
 package minesweeper
 
-class Map(val maxSize: Position) {
+class Map(val maxSize: Position, val mines: List<Mine>) {
+    fun print(): String {
+        var string = ""
+
+        for (height in 1 .. maxSize.height) {
+            for (width in 1 .. maxSize.width) {
+                string += if (isMine(Position(width, height))) MINE_STRING else SAFE_STRING
+                string += " "
+            }
+            string += "\n"
+        }
+        return string
+    }
+
+    private fun isMine(position: Position): Boolean {
+        return mines.any {
+            it.position == position
+        }
+    }
+
+    companion object {
+        private const val MINE_STRING = "*"
+        private const val SAFE_STRING = "C"
+    }
 }
--- src/test/kotlin/minesweeper/MapTest.kt
+++ src/test/kotlin/minesweeper/MapTest.kt
@@ -6,10 +6,28 @@ import org.junit.jupiter.api.Test
 
 internal class MapTest {
     @Test
-    fun `지도는 높이와 너비를 가진다`() {
-        val map = Map(Position(10, 10))
+    fun `지도는 높이와 너비와 지뢰들을 가진다`() {
+        val map = Map(Position(10, 10), listOf(Mine(Position(10, 10))))
 
         map.maxSize.height shouldBe 10
         map.maxSize.width shouldBe 10
+    }
+
+    @Test
+    fun `지도 출력`() {
+        val map = Map(Position(10, 10), listOf(Mine(Position(10, 10))))
+        val expectedString =
+            "C C C C C C C C C C \n" +
+            "C C C C C C C C C C \n" +
+            "C C C C C C C C C C \n" +
+            "C C C C C C C C C C \n" +
+            "C C C C C C C C C C \n" +
+            "C C C C C C C C C C \n" +
+            "C C C C C C C C C C \n" +
+            "C C C C C C C C C C \n" +
+            "C C C C C C C C C C \n" +
+            "C C C C C C C C C * \n"
+
+        map.print() shouldBe expectedString
     }
 }
