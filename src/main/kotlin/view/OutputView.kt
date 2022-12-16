cf8a15ec4dbe894ce458130ab2f24e31d7f3085f
chayoung-choi <chayoung.dev@gmail.com>
2022-12-17 00:07:34 +0900

    refactor: printMineField()

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,20 +1,38 @@
 package view
 
+import domain.Block
 import domain.MineField
 
 object OutputView {
     private const val MARK_MINE_BLOCK = "*"
     private const val MARK_NORMAL_BLOCK = "C"
+    private const val SEPARATOR_ROW = "\n"
+    private const val SEPARATOR_BLOCK = " "
+
     fun printGameStart(mineField: MineField) {
         println("지뢰찾기 게임 시작")
         printMineField(mineField)
     }
 
     private fun printMineField(mineField: MineField) {
-        println(mineField.blocks.map { getBlockMark(it.isMine()) })
+        println(
+            parseListAsBlockMark(mineField.blocks)
+                .chunked(mineField.rectangle.width)
+                .joinMineField()
+        )
+    }
+
+    private fun parseListAsBlockMark(blocks: List<Block>): List<String> {
+        return blocks.map { getBlockMark(it.isMine()) }
+    }
+
+    private fun List<List<String>>.joinMineField(): String {
+        return this.joinToString(SEPARATOR_ROW) { it.joinToString(SEPARATOR_BLOCK) }
     }
 
     private fun getBlockMark(isMine: Boolean): String {
         return if (isMine) MARK_MINE_BLOCK else MARK_NORMAL_BLOCK
     }
+
+
 }
