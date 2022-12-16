6d9a05a920b25a0b79e0fb4c4289a898de921fba
chayoung-choi <chayoung.dev@gmail.com>
2022-12-16 18:07:31 +0900

    feat: OutputView

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,3 +1,20 @@
 package view
 
-object OutputView
+import domain.MineField
+
+object OutputView {
+    private const val MARK_MINE_BLOCK = "*"
+    private const val MARK_NORMAL_BLOCK = "C"
+    fun printGameStart(mineField: MineField) {
+        println("지뢰찾기 게임 시작")
+        printMineField(mineField)
+    }
+
+    private fun printMineField(mineField: MineField) {
+        println(mineField.blocks.map { getBlockMark(it.isMine()) })
+    }
+
+    private fun getBlockMark(isMine: Boolean): String {
+        return if (isMine) MARK_MINE_BLOCK else MARK_NORMAL_BLOCK
+    }
+}
