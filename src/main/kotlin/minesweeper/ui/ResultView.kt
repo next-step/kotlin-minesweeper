110f2595ffdd371ee8b095ad8400452beb553473
Fog the Great <dreamboy1339@gmail.com>
2022-12-20 20:43:31 +0900

    minesweeper(step1): Controller 제거

--- src/main/kotlin/minesweeper/ui/ResultView.kt
+++ src/main/kotlin/minesweeper/ui/ResultView.kt
@@ -1,6 +1,6 @@
 package minesweeper.ui
 
-import minesweeper.controller.Controller
+import minesweeper.domain.Coordinate
 import minesweeper.domain.MineBoard
 
 class ResultView {
@@ -8,6 +8,31 @@ class ResultView {
     fun showBoard(board: MineBoard) {
         println()
         println("지뢰찾기 게임 시작")
-        print(Controller.makeBoardText(board))
+        print(makeBoardText(board))
+    }
+
+    private fun makeBoardText(board: MineBoard): String {
+        var result = ""
+        repeat(board.rowSize) { rowIndex ->
+            result += getColumnText(board, rowIndex) + "\n"
+        }
+        return result
+    }
+
+    private fun getColumnText(board: MineBoard, rowIndex: Int): String {
+        var result = ""
+        val level = rowIndex * board.columnSize
+
+        repeat(board.columnSize) { columnIndex ->
+            val coordinate = board.coordinates[level + columnIndex]
+            result += getCoordinateText(coordinate)
+        }
+
+        return result
+    }
+
+    private fun getCoordinateText(coordinate: Coordinate): String {
+        if (coordinate.isMine()) return "* "
+        return "C "
     }
 }
