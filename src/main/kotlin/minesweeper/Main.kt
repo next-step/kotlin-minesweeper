37ac51e25993bc6a5febfa2657854d6615c06ea8
Fog the Great <dreamboy1339@gmail.com>
2022-12-20 20:29:25 +0900

    minesweeper(step1): InputView 에서 카운트 값에 대해 객체로 리턴하도록 변경

--- src/main/kotlin/minesweeper/Main.kt
+++ src/main/kotlin/minesweeper/Main.kt
@@ -1,8 +1,8 @@
 package minesweeper
 
 import minesweeper.domain.ColumnCount
-import minesweeper.domain.MineCount
 import minesweeper.domain.MineBoard
+import minesweeper.domain.MineCount
 import minesweeper.domain.RowCount
 import minesweeper.ui.InputView
 import minesweeper.ui.ResultView
@@ -10,11 +10,11 @@ import minesweeper.ui.ResultView
 fun main() {
     val inputView = InputView()
 
-    val rowCount: Int = inputView.inputRowCount()
-    val columnCount: Int = inputView.inputColumnCount()
-    val mineCount: Int = inputView.inputMineCount()
+    val rowCount: RowCount = inputView.inputRowCount()
+    val columnCount: ColumnCount = inputView.inputColumnCount()
+    val mineCount: MineCount = inputView.inputMineCount()
 
-    val board = MineBoard(RowCount(rowCount), ColumnCount(columnCount), MineCount(mineCount))
+    val board = MineBoard(rowCount, columnCount, mineCount)
 
     val resultView = ResultView()
     resultView.showBoard(board)
--- src/main/kotlin/minesweeper/ui/InputView.kt
+++ src/main/kotlin/minesweeper/ui/InputView.kt
@@ -1,27 +1,31 @@
 package minesweeper.ui
 
+import minesweeper.domain.ColumnCount
+import minesweeper.domain.MineCount
+import minesweeper.domain.RowCount
+
 class InputView {
 
-    fun inputRowCount(): Int {
+    fun inputRowCount(): RowCount {
         println("높이를 입력하세요.")
         val rowCount: Int = readlnOrNull()?.toIntOrNull() ?: 0
         if (rowCount <= 0) return inputRowCount()
-        return rowCount
+        return RowCount(rowCount)
     }
 
-    fun inputColumnCount(): Int {
+    fun inputColumnCount(): ColumnCount {
         println()
         println("너비를 입력하세요.")
         val columnCount: Int = readlnOrNull()?.toIntOrNull() ?: 0
         if (columnCount <= 0) return inputColumnCount()
-        return columnCount
+        return ColumnCount(columnCount)
     }
 
-    fun inputMineCount(): Int {
+    fun inputMineCount(): MineCount {
         println()
         println("지뢰는 몇 개인가요?")
         val mineCount: Int = readlnOrNull()?.toIntOrNull() ?: 0
         if (mineCount <= 0) return inputMineCount()
-        return mineCount
+        return MineCount(mineCount)
     }
 }
