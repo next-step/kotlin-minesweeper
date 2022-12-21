4f60272c95923f717371f9854177fd7eaee280da
wrallee <wrallee@naver.com>
2022-12-21 21:16:13 +0900

    refactor(MineSweeper): readLine!! -> readln 으로 변경

--- src/main/kotlin/minesweeper/view/InputView.kt
+++ src/main/kotlin/minesweeper/view/InputView.kt
@@ -3,16 +3,16 @@ package minesweeper.view
 object InputView {
     fun readHeight(): Int {
         println("높이를 입력하세요.")
-        return readLine()!!.toInt()
+        return readln().toInt()
     }
 
     fun readWidth(): Int {
         println("너비를 입력하세요.")
-        return readLine()!!.toInt()
+        return readln().toInt()
     }
 
     fun readMineCount(): Int {
         println("지뢰는 몇 개인가요?")
-        return readLine()!!.toInt()
+        return readln().toInt()
     }
 }
