cacee2133cf683aa48673f128aa999741f457de0
giyeon95 <giyeon15@gmail.com>
2022-12-22 20:39:13 +0900

    refactor: ResultView 네이밍 개선
            `print`는 출력할것만 같아요! 하지만 `String`을 만드는거죠! 조금 고민해볼까요?
    
    _Originally posted by @ohgillwhan in https://github.com/next-step/kotlin-minesweeper/pull/186#discussion_r1051581317_

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -11,13 +11,13 @@ object ResultView {
         println()
         println("지뢰찾기 게임 시작")
 
-        val printFields = field.print()
+        val printFields = field.stringForPrint()
 
         println(printFields)
     }
 
-    private fun Field.print(): String = this.rows.joinToString(separator = "\n") { row -> row.print() }
-    private fun Row.print(): String = this.cells.joinToString(separator = " ") { it.desc() }
+    private fun Field.stringForPrint(): String = this.rows.joinToString(separator = "\n") { row -> row.stringForPrint() }
+    private fun Row.stringForPrint(): String = this.cells.joinToString(separator = " ") { it.desc() }
     private fun Cell.desc() = when (this.block) {
         is Land -> "C"
         is Mine -> "O"
