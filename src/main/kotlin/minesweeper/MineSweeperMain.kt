d52aed2a420d3080a32f46371a64f0430eb10963
harris.yi <harris.yi@kakaostyle.com>
2022-12-24 13:51:12 +0900

    feat: Apply input validation rule

--- src/main/kotlin/minesweeper/MineSweeperMain.kt
+++ src/main/kotlin/minesweeper/MineSweeperMain.kt
@@ -7,7 +7,7 @@ import kotlin.random.Random
 fun main() {
     val height = InputView.askHeight()
     val width = InputView.askWidth()
-    val mineCount = InputView.askMineCount()
+    val mineCount = InputView.askMineCount(height * width)
 
     val mineCells = initMineCells(height, width, mineCount)
     val mineBoard = MineBoard(mineCells)
--- src/main/kotlin/minesweeper/view/InputView.kt
+++ src/main/kotlin/minesweeper/view/InputView.kt
@@ -1,19 +1,23 @@
 package minesweeper.view
 
+import java.lang.IllegalArgumentException
+
 object InputView {
     fun askHeight(): Int {
         println("높이를 입력하세요.")
-        return readln().toInt()
+        return readln().toIntOrNull() ?: throw IllegalArgumentException("높이는 숫자를 입력해야 합니다.")
     }
 
     fun askWidth(): Int {
         println("너비를 입력하세요.")
-        return readln().toInt()
+        return readln().toIntOrNull() ?: throw IllegalArgumentException("너비는 숫자를 입력해야 합니다.")
 
     }
 
-    fun askMineCount(): Int {
+    fun askMineCount(maximumCount: Int): Int {
         println("지뢰는 몇 개인가요?")
-        return readln().toInt()
+        val mineCount = readln().toIntOrNull() ?: throw IllegalArgumentException("지뢰의 개수는 숫자를 입력해야 합니다.")
+        if (mineCount > maximumCount) throw IllegalArgumentException("지뢰의 개수가 너무 많습니다.")
+        return mineCount
     }
 }
