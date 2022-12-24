5504a72fee3dfdd9c3833ce9652f6a4c6a707ae9
Leedo <leedo@deali.net>
2022-12-24 17:34:31 +0900

    feat: 메소드 네이밍 변경

--- src/main/kotlin/model/Cells.kt
+++ src/main/kotlin/model/Cells.kt
@@ -9,19 +9,19 @@ class Cells(val rows: Int, val cols: Int, val mineCount: Int) {
 
     init {
         val mines = createMine(rows, cols, mineCount, mutableSetOf())
-        cells = createCell(rows, cols, mines)
+        cells = createCells(rows, cols, mines)
     }
 
-    private fun createCell(rows: Int, cols: Int, mines: Set<Location>) =
-        Array(rows) { row -> createCols(cols, mines, row) }
+    private fun createCells(rows: Int, cols: Int, mines: Set<Location>) =
+        Array(rows) { row -> createCell(cols, mines, row) }
 
-    private fun createCols(
+    private fun createCell(
         cols: Int,
         mines: Set<Location>,
         row: Int
-    ) = Array(cols) { col -> creatCell(mines, row, col) }
+    ) = Array(cols) { col -> cellType(mines, row, col) }
 
-    private fun creatCell(mines: Set<Location>, row: Int, col: Int): Cell = when {
+    private fun cellType(mines: Set<Location>, row: Int, col: Int): Cell = when {
         mines.contains(Location(row, col)) -> Cell(MINE_SIGN)
         else -> Cell()
     }
