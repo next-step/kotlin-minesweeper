97556eddfaaf5bfe384fac8260e118d5eb6d005a
wrallee <wrallee@naver.com>
2022-12-24 14:19:39 +0900

    refactor(MineSweeper): MineBoard 메서드 순서 변경

--- src/main/kotlin/minesweeper/model/MineBoard.kt
+++ src/main/kotlin/minesweeper/model/MineBoard.kt
@@ -6,10 +6,10 @@ class MineBoard(val map: MineMap, private val mines: Mines) {
         require(checkBounds(map, mines)) { "맵 크기를 벗어나는 곳에 지뢰를 배치할 수 없습니다." }
     }
 
+    private fun checkCount(map: MineMap, mines: Mines) = mines.size < map.rowSize * map.columnSize
+    private fun checkBounds(map: MineMap, mines: Mines) = mines.all(map::checkBounds)
+
     fun checkMine(cell: Cell): Boolean {
         return mines.contains(cell)
     }
-
-    private fun checkCount(map: MineMap, mines: Mines) = mines.size < map.rowSize * map.columnSize
-    private fun checkBounds(map: MineMap, mines: Mines) = mines.all(map::checkBounds)
 }
