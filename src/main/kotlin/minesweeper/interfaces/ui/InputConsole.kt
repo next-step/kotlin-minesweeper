9c8601d2ae7b4247238cfb94e8cbac8991c95b39
EP <kjuioqq8@gmail.com>
2022-12-24 16:43:41 +0900

    docs: 문서 내용 변경

--- src/main/kotlin/minesweeper/interfaces/ui/InputConsole.kt
+++ src/main/kotlin/minesweeper/interfaces/ui/InputConsole.kt
@@ -2,31 +2,29 @@ package minesweeper.interfaces.ui
 
 object InputConsole {
     fun queryHeight(): Int =
-        try {
+        queryNumber {
             println("높이를 입력하세요.")
-            queryNumber()
-        } catch (e: NumberFormatException) {
-            println("숫자를 입력해주세요")
-            queryNumber()
         }
 
     fun queryWidth(): Int =
-        try {
+        queryNumber {
             println("너비를 입력하세요.")
-            queryNumber()
-        } catch (e: NumberFormatException) {
-            println("숫자를 입력해주세요")
-            queryNumber()
         }
 
     fun queryMineCount(): Int =
-        try {
+        queryNumber {
             println("지뢰는 몇 개인가요?")
+        }
+
+    private fun queryNumber(lambda:() -> Unit): Int {
+        return try {
+            lambda.invoke()
             queryNumber()
         } catch (e: NumberFormatException) {
             println("숫자를 입력해주세요")
-            queryNumber()
+            queryNumber {  }
         }
+    }
 
     private fun queryNumber(): Int = readln().trim().toInt()
 }
