250472afef18c6046f2b4741e02f29dacc3052b5
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-26 02:11:26 +0900

    Refactor(Coordinate): 팩터리 생성자 추가

--- src/main/kotlin/domain/Coordinate.kt
+++ src/main/kotlin/domain/Coordinate.kt
@@ -1,3 +1,9 @@
 package domain
 
-data class Coordinate(val x: Row, val y: Column)
+data class Coordinate(val x: Row, val y: Column) {
+    companion object {
+        fun from(x: Int, y: Int): Coordinate {
+            return Coordinate(Row(x), Column(y))
+        }
+    }
+}
