3d3259b5d2581136f25a2f50f4c176e6ccd5af99
harris.yi <harris.yi@kakaostyle.com>
2022-12-25 15:40:36 +0900

    test: Validate mine count when create MineBoard

---
+++ src/test/kotlin/minesweeper/domain/MineBoardTest.kt
@@ -0,0 +1,17 @@
+package minesweeper.domain
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class MineBoardTest : StringSpec({
+    "지뢰 보드 생성 시 지뢰 개수를 10개로 설정하면 10개가 생성 됨을 확인한다." {
+        //given
+        val mineBoard = MineBoard.createBoard(5, 5, 10)
+        //when
+        val mineCount = mineBoard.mineCells.sumOf {
+            it.mineCells.filter { cell -> cell.state == "*" }.size }
+        //then
+        mineCount shouldBe 10
+    }
+
+})
