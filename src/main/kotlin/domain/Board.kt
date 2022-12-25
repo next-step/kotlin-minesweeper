dfe09637148df5f66a1b205d773ec5ec0dfbc852
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-25 18:00:14 +0900

    feature(Board): 인자로 받은 좌표가 지뢰인지 아닌지 판별한다.

--- src/main/kotlin/domain/Board.kt
+++ src/main/kotlin/domain/Board.kt
@@ -7,6 +7,10 @@ class Board(cells: List<Cell>) {
         this.cells = cells.sortedWith(compareBy({ it.coordinate.x.value }, { it.coordinate.y.value }))
     }
 
+    fun isMineCell(coordinate: Coordinate): Boolean {
+        return cells.find { it.coordinate == coordinate } is Mine
+    }
+
     companion object {
         fun from(row: Row, column: Column, mineCount: MineCount): Board {
             val cellSize = row * column
@@ -14,9 +18,10 @@ class Board(cells: List<Cell>) {
             val locations = List(cellSize) { it }
             val randomMineLocations = (0 until cellSize).shuffled().take(mineCount.value)
             val blankLocations = locations - randomMineLocations.toSet()
-            val cellGenerator = CellGenerator(blankLocations, randomMineLocations, row)
+            val mineGenerator = MineGenerator(randomMineLocations, row)
+            val blankGenerator = BlankGenerator(blankLocations, row)
 
-            return Board(cellGenerator.minesGenerate() + cellGenerator.blanksGenerate())
+            return Board(mineGenerator.generate() + blankGenerator.generate())
         }
     }
 }
--- src/test/kotlin/domain/BoardTest.kt
+++ src/test/kotlin/domain/BoardTest.kt
@@ -1,6 +1,7 @@
 package domain
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
 internal class BoardTest : StringSpec({
@@ -22,5 +23,32 @@ internal class BoardTest : StringSpec({
         val board = Board.from(row, column, mineCount)
 
         board.cells.filterIsInstance<Blank>().size shouldBe (row * column) - mineCount.value
+    }
+
+    "인자로 받은 좌표가 지뢰라면 true 를 반환한다." {
+        val mineLocations = listOf(0)
+        val generator = MineGenerator(locations = mineLocations, row = Row(3))
+
+        val mineCells = generator.generate()
+        val board = Board(mineCells)
+        val result = board.isMineCell(coordinateOf(1, 1))
+
+        result shouldBe true
+    }
+
+    "인자로 받은 좌표가 빈칸이거나 없는 좌표라면 false 를 반환한다." {
+        val blankLocations = listOf(0)
+        val generator = BlankGenerator(locations = blankLocations, row = Row(3))
+
+        val blankCells = generator.generate()
+        val board = Board(blankCells)
+
+        val coordinates = coordinateListOf(
+            1 to 1, 5 to 3, 3 to 3
+        )
+        coordinates.forAll {
+            val result = board.isMineCell(it)
+            result shouldBe false
+        }
     }
 })
