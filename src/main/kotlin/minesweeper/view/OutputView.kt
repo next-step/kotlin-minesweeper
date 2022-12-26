58e2a977e59d56b0920ce8814b1e6236adb6ffcb
harris.yi <harris.yi@kakaostyle.com>
2022-12-26 22:53:04 +0900

    feat: Change print logic with nearMineCount

--- src/main/kotlin/minesweeper/view/OutputView.kt
+++ src/main/kotlin/minesweeper/view/OutputView.kt
@@ -1,12 +1,22 @@
 package minesweeper.view
 
+import minesweeper.domain.Cell
+import minesweeper.domain.CleanCell
 import minesweeper.domain.MineBoard
+import minesweeper.domain.MineCell
 
 object OutputView {
     fun printMineBoard(mineBoard: MineBoard) {
         println("지뢰찾기 게임 시작")
         mineBoard.mineCells.forEach { row ->
-            println(row.mineCells.joinToString(separator = " ") { it.state})
+            println(row.mineCells.joinToString(separator = " ") {
+                mineCellAsString(it)
+            })
         }
+    }
+
+    private fun mineCellAsString(cell: Cell) = when (cell) {
+        is MineCell -> cell.state
+        is CleanCell -> cell.nearMineCount.toString()
     }
 }
