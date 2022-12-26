2ccc5e9ccc765aa692c8c7b2e9c6000dc3575048
harris.yi <harris.yi@kakaostyle.com>
2022-12-26 22:46:51 +0900

    feat: Add width as property

--- src/main/kotlin/minesweeper/domain/MineBoardRow.kt
+++ src/main/kotlin/minesweeper/domain/MineBoardRow.kt
@@ -1,6 +1,9 @@
 package minesweeper.domain
 
 class MineBoardRow(val mineCells: Array<Cell>) {
+    val width: Int
+        get() = mineCells.size
+
     fun plantMine(width: Int) {
         mineCells[width] = MineCell()
     }
