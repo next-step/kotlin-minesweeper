ded6558056ac4f12128339a22e0f1ba3f1bc3d85
harris.yi <harris.yi@kakaostyle.com>
2022-12-26 22:48:39 +0900

    feat: Add rangeIn function and getNearPositions function in Position class

--- src/main/kotlin/minesweeper/domain/Position.kt
+++ src/main/kotlin/minesweeper/domain/Position.kt
@@ -1,3 +1,17 @@
 package minesweeper.domain
 
-data class Position(val height: Int, val width: Int)
+data class Position(val height: Int, val width: Int) {
+    fun rangeIn(
+        maxHeight: Int,
+        maxWidth: Int,
+    ): Boolean = this.height in 0 until maxHeight && this.width in 0 until maxWidth
+
+    fun getNearPositions(): List<Position> =
+        (this.height - 1..this.height + 1).flatMap { height ->
+            (this.width - 1..this.width + 1).map { width ->
+                Position(height, width)
+            }
+        }
+}
+
+
