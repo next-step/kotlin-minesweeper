43469448dfaa4d88814ec11d9eadc0fd51bdff85
SangHyunGil <zizon5941@gmail.com>
2022-12-27 19:33:56 +0900

    feat: 게임 종료되었는지 확인하는 메서드 및 지뢰인지 확인하는 메서드 및 Board 여는 메서드 구현 및 테스트

--- src/main/kotlin/domain/Board.kt
+++ src/main/kotlin/domain/Board.kt
@@ -2,6 +2,7 @@ package domain
 
 import domain.strategy.BoardGenerateStrategy
 import domain.strategy.RandomMineBoardGenerateStrategy
+import dto.FieldWithCoordinate
 
 class Board(
     val height: Height,
@@ -18,6 +19,40 @@ class Board(
 
     fun getNearByMineCount(height: Int, width: Int): Int {
         val coordinate = Coordinate(height, width)
-        return fields.getNearByFields(coordinate).filterIsInstance<Mine>().count()
+        return fields.getNearByFields(coordinate).count { it.field is Mine }
+    }
+
+    fun isMine(height: Int, width: Int): Boolean {
+        val coordinate = Coordinate(height, width)
+        return fields.getField(coordinate) is Mine
+    }
+
+    fun isGameOver(): Boolean {
+        return fields.isLandAllOpened()
+    }
+
+    tailrec fun open(height: Int, width: Int) {
+        // 해당 좌표 열기
+        val coordinate = Coordinate(height, width)
+        fields.open(coordinate)
+
+        // 인근에 지뢰가 아닌 좌표 열기
+        getNotOpenedLandAndHasNoMineWithCoordinate(coordinate).forEach { land ->
+            open(land.coordinate.row, land.coordinate.col)
+        }
+    }
+
+    private fun getNotOpenedLandAndHasNoMineWithCoordinate(coordinate: Coordinate): List<FieldWithCoordinate> {
+        if (hasMineNearby(coordinate))
+            return emptyList()
+        return getNotOpenedLand(coordinate)
+    }
+
+    private fun hasMineNearby(coordinate: Coordinate): Boolean {
+        return fields.getNearByFields(coordinate).count { it.field is Mine } > 0
+    }
+
+    private fun getNotOpenedLand(coordinate: Coordinate): List<FieldWithCoordinate> {
+        return fields.getNearByFields(coordinate).filter { it.field is Land && it.field.isOpened.not() }
     }
 }
--- src/test/kotlin/domain/BoardTest.kt
+++ src/test/kotlin/domain/BoardTest.kt
@@ -34,6 +34,42 @@ internal class BoardTest : BehaviorSpec({
                 testBoard.getNearByMineCount(5, 5) shouldBe 6
             }
         }
+
+        val testBoard2 = Board(Height(10), Width(10), MineCnt(10), TestMineBoardGenerateStrategy())
+        When("모든 땅이 열려있지 않다면 ") {
+            val isGameOver = testBoard2.isGameOver()
+            Then("게임이 끝나지 않았다.") {
+                isGameOver shouldBe false
+            }
+        }
+
+        When("모든 땅이 열렸다면 ") {
+            (0..10).forEach() { h ->
+                (0..10).forEach() { w ->
+                    when (val field = testBoard2.getField(h, w)) {
+                        is Land -> field.open()
+                        is Mine -> Unit
+                    }
+                }
+            }
+            val isGameOver = testBoard2.isGameOver()
+            Then("게임이 끝난다.") {
+                isGameOver shouldBe true
+            }
+        }
+
+        val testBoard3 = Board(Height(10), Width(10), MineCnt(10), SecondColMineBoardGenerateStrategy())
+        When("땅을 연다면 주의에 지뢰가 없다면 ") {
+            testBoard3.open(0, 0)
+            Then("인근 땅도 다 열린다.") {
+                val openedLandCount = (0..10).count { w ->
+                    val field = testBoard3.getField(0, w)
+                    field is Land && field.isOpened
+                }
+
+                openedLandCount shouldBe 11
+            }
+        }
     }
 })
 
@@ -44,6 +80,23 @@ class TestMineBoardGenerateStrategy : BoardGenerateStrategy {
                 (0..width.value).map { w ->
                     val coordinate = Coordinate(h, w)
                     val field = when (h % 2 == 0) {
+                        true -> Mine()
+                        false -> Land()
+                    }
+                    coordinate to field
+                }
+            }.toMap()
+        )
+    }
+}
+
+class SecondColMineBoardGenerateStrategy : BoardGenerateStrategy {
+    override fun generate(height: Height, width: Width, mineCnt: MineCnt): Fields {
+        return Fields(
+            (0..height.value).flatMap { h ->
+                (0..width.value).map { w ->
+                    val coordinate = Coordinate(h, w)
+                    val field = when (h == 2) {
                         true -> Mine()
                         false -> Land()
                     }
