349eb35a756a441f7627e489ffff91163bb68fd1
wrallee <wrallee@naver.com>
2022-12-27 21:22:38 +0900

    refactor(MineSweeper): MineMap 내 cells 필드 제거
    cells를 저장할 이유가 없다고 판단해서 제거

--- src/main/kotlin/minesweeper/model/MineMap.kt
+++ src/main/kotlin/minesweeper/model/MineMap.kt
@@ -4,10 +4,6 @@ class MineMap private constructor(
     val rowSize: Int,
     val columnSize: Int
 ) {
-    private val cells = List(rowSize) { y -> List(columnSize) { x -> Cell(x, y) } }
-        .flatten()
-        .sortedWith(compareBy({ it.y }, { it.x }))
-
     init {
         require(rowSize >= MINIMUM_SIZE) { "행의 크기는 0이 될 수 없습니다." }
         require(columnSize >= MINIMUM_SIZE) { "열의 크기는 0이 될 수 없습니다." }
@@ -18,11 +14,13 @@ class MineMap private constructor(
             cell.y in INIT_INDEX until INIT_INDEX + rowSize
 
     fun selectAllCells(): List<Cell> {
-        return cells
+        return List(rowSize) { y -> List(columnSize) { x -> Cell(x, y) } }
+            .flatten()
+            .sortedWith(compareBy({ it.y }, { it.x }))
     }
 
     fun selectRandomCells(cellCount: Int): Mines {
-        val shuffledMines = cells
+        val shuffledMines = selectAllCells()
             .shuffled()
             .take(cellCount)
             .toSet()
