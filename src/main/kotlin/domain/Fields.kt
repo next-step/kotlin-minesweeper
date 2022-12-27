0f742aa8136c42b19ee04117f34176775a59cc16
SangHyunGil <zizon5941@gmail.com>
2022-12-27 19:32:05 +0900

    feat: Fields 모든 땅이 열렸는지 확인하는 메서드 및 여는 메서드 구현 및 테스트

--- src/main/kotlin/domain/Fields.kt
+++ src/main/kotlin/domain/Fields.kt
@@ -1,6 +1,6 @@
 package domain
 
-import java.lang.IllegalArgumentException
+import dto.FieldWithCoordinate
 
 class Fields(
     private val fields: Map<Coordinate, Field>
@@ -9,12 +9,29 @@ class Fields(
         return fields[coordinate] ?: throw IllegalArgumentException(NOT_EXIST_FIELD)
     }
 
-    fun getNearByFields(coordinate: Coordinate): List<Field> {
+    fun getNearByFields(coordinate: Coordinate): List<FieldWithCoordinate> {
         val nearByCoordinates = coordinate.getNearByCoordinates()
-        return nearByCoordinates.mapNotNull { fields[it] }
+        return nearByCoordinates.mapNotNull { coordinate ->
+            fields[coordinate]?.let { field -> FieldWithCoordinate(coordinate, field) }
+        }
+    }
+
+    fun isLandAllOpened(): Boolean {
+        val notOpenedLandCount = fields.values.filterIsInstance<Land>()
+            .count { it.isOpened.not() }
+
+        return notOpenedLandCount == 0
+    }
+
+    fun open(coordinate: Coordinate) {
+        when (val field = this.getField(coordinate)) {
+            is Land -> field.open()
+            is Mine -> throw IllegalStateException(CANNOT_OPEN_MINE)
+        }
     }
 
     companion object {
         private const val NOT_EXIST_FIELD = "해당 좌표에는 필드가 존재하지 않아요."
+        private const val CANNOT_OPEN_MINE = "지뢰 좌표는 열 수 없어요."
     }
 }
