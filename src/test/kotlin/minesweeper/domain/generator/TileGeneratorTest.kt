46b4b6abaaa78389164e2a21b43c70a18cbafd23
namonak <namonak@gmail.com>
2022-12-27 20:42:42 +0900

    refactor(step1): change the generation strategy of the TileGenerator test code to MineFirstGenerateStrategy

--- src/test/kotlin/minesweeper/domain/generator/TileGeneratorTest.kt
+++ src/test/kotlin/minesweeper/domain/generator/TileGeneratorTest.kt
@@ -2,7 +2,7 @@ package minesweeper.domain.generator
 
 import minesweeper.domain.MineCount
 import minesweeper.domain.tile.pos.Position
-import minesweeper.domain.tile.strategy.RandomGenerateStrategy
+import minesweeper.domain.tile.strategy.MineFirstGenerateStrategy
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -11,7 +11,7 @@ class TileGeneratorTest {
     fun `TileGenerator - 타일 생성 테스트`() {
         // given
         val tileGenerator = TileGenerator(
-            RandomGenerateStrategy(
+            MineFirstGenerateStrategy(
                 Position(FIXED_POSITION),
                 Position(FIXED_POSITION),
                 MineCount(FIXED_MINE_COUNT, FIXED_CALIBRATED_POSITION * FIXED_CALIBRATED_POSITION)
@@ -20,9 +20,11 @@ class TileGeneratorTest {
 
         // when
         val tiles = tileGenerator.generate()
+        val mines = tiles.subList(0, FIXED_MINE_COUNT)
 
         // then
         assertThat(FIXED_MINE_COUNT).isEqualTo(tiles.count { it.isMine })
+        assertThat(mines).allMatch { it.isMine }
     }
 
     companion object {
