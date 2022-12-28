0f69f1664e4bf2a241c19d3315ace040eaf19657
harris.yi <harris.yi@kakaostyle.com>
2022-12-28 15:02:01 +0900

    feat: Add getAllPosition and getRandomPositions methods in Position

--- src/main/kotlin/minesweeper/domain/Position.kt
+++ src/main/kotlin/minesweeper/domain/Position.kt
@@ -7,7 +7,20 @@ data class Position(val row: Int, val column: Int) {
             (this.column - 1..this.column + 1).map { column ->
                 Position(row, column)
             }
-        }
+        }.filter { it != this }
+
+    companion object {
+        fun getAllPosition(height: Int, width: Int): List<Position> =
+            (0 until height).flatMap { row ->
+                (0 until width).map { column ->
+                    Position(row, column)
+                }
+            }
+
+        fun getRandomPositions(height: Int, width: Int, mineCount: Int) = (0 until height * width).shuffled()
+            .map { Position(it / width, it % width) }
+            .take(mineCount)
+    }
 }
 
 
