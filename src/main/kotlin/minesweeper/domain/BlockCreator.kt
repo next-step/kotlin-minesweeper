a4d271fea70e8b48f6a5f2d667ff29b31d04ad14
이상인/wefu0000 <wefu.child@gmail.com>
2022-12-28 21:51:06 +0900

    refactor: 생성한 블록을 생성자로 가지고 있지 않고 클래스 내에서 생성 한다.

--- src/main/kotlin/minesweeper/domain/BlockCreator.kt
+++ src/main/kotlin/minesweeper/domain/BlockCreator.kt
@@ -4,17 +4,16 @@ class BlockCreator(
     height: Int,
     width: Int,
     mineCount: Int,
-    val blocks: List<Block> = (createNormalBlocks(height, width, mineCount) + createMineBlocks(mineCount)).shuffled()
 ) {
 
-    companion object {
-        private fun createNormalBlocks(height: Int, width: Int, mineCount: Int): List<Block.Normal> {
-            val blockCount = height * width - mineCount
-            return List(blockCount) { Block.Normal }
-        }
+    val blocks: List<Block> = (createNormalBlocks(height, width, mineCount) + createMineBlocks(mineCount)).shuffled()
 
-        private fun createMineBlocks(mineCount: Int): List<Block.LandMine> {
-            return List(mineCount) { Block.LandMine }
-        }
+    private fun createNormalBlocks(height: Int, width: Int, mineCount: Int): List<Block.Normal> {
+        val blockCount = height * width - mineCount
+        return List(blockCount) { Block.Normal }
+    }
+
+    private fun createMineBlocks(mineCount: Int): List<Block.LandMine> {
+        return List(mineCount) { Block.LandMine }
     }
 }
