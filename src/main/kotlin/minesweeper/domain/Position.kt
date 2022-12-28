db6c097e70d177eeda367dd4efc5e3de43301c6d
harris.yi <harris.yi@kakaostyle.com>
2022-12-28 12:23:44 +0900

    chore: Change height & width to row and height in position class

--- src/main/kotlin/minesweeper/domain/Position.kt
+++ src/main/kotlin/minesweeper/domain/Position.kt
@@ -1,15 +1,15 @@
 package minesweeper.domain
 
-data class Position(val height: Int, val width: Int) {
+data class Position(val row: Int, val column: Int) {
     fun rangeIn(
-        maxHeight: Int,
-        maxWidth: Int,
-    ): Boolean = this.height in 0 until maxHeight && this.width in 0 until maxWidth
+        height: Int,
+        width: Int,
+    ): Boolean = this.row in 0 until height && this.column in 0 until width
 
     fun getNearPositions(): List<Position> =
-        (this.height - 1..this.height + 1).flatMap { height ->
-            (this.width - 1..this.width + 1).map { width ->
-                Position(height, width)
+        (this.row - 1..this.row + 1).flatMap { row ->
+            (this.column - 1..this.column + 1).map { column ->
+                Position(row, column)
             }
         }
 }
