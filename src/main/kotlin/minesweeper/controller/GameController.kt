19bdc2c275b62c3cb1ea2b9c22d046aaf09cae2b
adforus_hjp <hjp281@adforus.co.kr>
2022-12-29 13:13:45 +0900

    refactor: 보드 자체를 반환하도록 변경

--- src/main/kotlin/minesweeper/controller/GameController.kt
+++ src/main/kotlin/minesweeper/controller/GameController.kt
@@ -1,9 +1,7 @@
 package minesweeper.controller
 
-import minesweeper.domain.Block
 import minesweeper.domain.MineSweeperBoard
 import minesweeper.domain.MineSweeperException
-import minesweeper.domain.Point
 import minesweeper.view.InputView
 import minesweeper.view.InputViewImpl
 import minesweeper.view.ResultView
@@ -13,11 +11,11 @@ class GameController(
     private val inputView: InputView = InputViewImpl(),
     private val resultView: ResultView = ResultViewImpl()
 ) {
-    fun run(): Map<Point, Block>? {
+    fun run(): MineSweeperBoard? {
         return try {
             val gameBoard = setUp()
             resultView.renderInitialBoard(gameBoard.state)
-            gameBoard.state
+            gameBoard
         } catch (e: MineSweeperException) {
             resultView.printKnownException(e)
             null
