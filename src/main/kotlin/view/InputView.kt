969745127b46531e9fbb0a040140a123d85e4bab
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-29 21:43:08 +0900

    feature(InputView): Row, Column 객체 생성 방식 원복

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -7,24 +7,17 @@ import domain.Row
 object InputView {
     fun inputHeight(): Row {
         println("높이를 입력하세요.")
-        val input = readln().toInt()
-        return Row(validateInput(input))
+        return Row(readln().toInt())
     }
 
     fun inputWidth(): Column {
         println("너비를 입력하세요.")
-        val input = readln().toInt()
-        return Column(validateInput(input))
+        return Column(readln().toInt())
     }
 
     fun inputMineCount(row: Row, column: Column): MineCount {
         println("지뢰는 몇 개인가요?")
         val number = readln().toInt()
         return MineCount.from(row, column, number)
-    }
-
-    private fun validateInput(input: Int): Int {
-        require(input > 0) { "1 이상의 수여야 합니다." }
-        return input
     }
 }
