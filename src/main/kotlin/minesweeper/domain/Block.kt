176e8347916da42deb3236147adc1addfdabed0a
이상인/wefu0000 <wefu.child@gmail.com>
2022-12-29 17:47:00 +0900

    refactor: sealed 워닝 수정

--- src/main/kotlin/minesweeper/domain/Block.kt
+++ src/main/kotlin/minesweeper/domain/Block.kt
@@ -2,5 +2,13 @@ package minesweeper.domain
 
 sealed interface Block {
     object LandMine : Block
-    class Normal : Block
+    class Normal : Block {
+        override fun equals(other: Any?): Boolean {
+            return this === other
+        }
+
+        override fun hashCode(): Int {
+            return System.identityHashCode(this)
+        }
+    }
 }
