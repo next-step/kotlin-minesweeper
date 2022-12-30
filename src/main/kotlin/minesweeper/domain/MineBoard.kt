8a0ac9b2a398cf339b140218137075fda4452043
harris.yi <harris.yi@kakaostyle.com>
2022-12-30 16:05:40 +0900

    feat: Separate create map logic to MineMap from MineBoard

--- src/main/kotlin/minesweeper/domain/MineBoard.kt
+++ src/main/kotlin/minesweeper/domain/MineBoard.kt
@@ -1,32 +1,24 @@
 package minesweeper.domain
 
-import minesweeper.domain.Position.Companion.getAllPositions
-import minesweeper.domain.Position.Companion.getRandomPositions
-
 class MineBoard(
-    private val mineCells: Map<Position, Cell>,
-    private val height: Int,
-    private val width: Int,
+    private val mineMap: MineMap,
 ) {
-    fun snapshot() = (0 until height).map { row ->
-        (0 until width).map { column ->
-            mineCells[Position(row, column)]
-                ?: throw IllegalStateException("해당 지뢰 셀을 찾을 수 없습니다. 위치: (${row},${column})")
+    fun openCell(position: Position): CellOpenResult {
+        val cell = mineMap.openCell(position)
+        val cellOpenResult = cell.openResult()
+        if (cellOpenResult.noNearMine()) {
+            spreadOpenCell(position)
         }
+        return cellOpenResult
     }
 
-    companion object {
-        fun createBoard(height: Int, width: Int, mineCount: Int): MineBoard {
-            val mineCells: HashMap<Position, Cell> = linkedMapOf(
-                *positionToCell(getAllPositions(height, width), getRandomPositions(height, width, mineCount))
-            )
-            return MineBoard(mineCells, height, width)
-        }
-
-        private fun positionToCell(
-            allPositions: List<Position>,
-            minePositions: List<Position>,
-        ) = allPositions.map { it to if (minePositions.contains(it)) MineCell(it) else CleanCell(it, minePositions) }
-            .toTypedArray()
+    private fun spreadOpenCell(position: Position) {
+        mineMap.getClosedNearPositions(position)
+            .forEach { openCell(it) }
     }
+
+    fun snapshot() = mineMap.snapshot()
+
+    fun win(): Boolean = mineMap.countClosedCellEqualsMineCell()
+
 }
---
+++ src/main/kotlin/minesweeper/domain/MineMap.kt
@@ -0,0 +1,44 @@
+package minesweeper.domain
+
+class MineMap(
+    private val mineCells: HashMap<Position, Cell>,
+) {
+    private val mineCount: Int
+        get() = mineCells.values
+            .count { it is MineCell }
+
+    fun openCell(position: Position): Cell = getCell(position).run {
+        mineCells[position] = this.copyWithOpen()
+        this.copyWithOpen()
+    }
+
+    private fun getCell(position: Position): Cell =
+        mineCells[position]
+            ?: throw IllegalStateException("해당 위치의 셀을 찾을 수 없습니다. 위치: (${position.row},${position.column})")
+
+
+    fun getClosedNearPositions(position: Position) = position.getNearPositions()
+        .filter { mineCells.keys.contains(it) }
+        .filter { getCell(it).state == CellState.CLOSED }
+
+    fun snapshot() = mineCells.keys.sortedBy { it.column }
+        .sortedBy { it.row }
+        .map { getCell(it) }
+
+    fun countClosedCellEqualsMineCell() = mineCells.values.count { it.state == CellState.CLOSED } == mineCount
+
+    companion object {
+        fun createMineMap(height: Int, width: Int, mineCount: Int): MineMap {
+            val positions = Positions(height, width)
+            val mineCells: HashMap<Position, Cell> = linkedMapOf(
+                *createPositionToCells(positions.all(), positions.getRandoms(mineCount))
+            )
+            return MineMap(mineCells)
+        }
+
+        private fun createPositionToCells(
+            allPositions: List<Position>,
+            minePositions: List<Position>,
+        ): Array<Pair<Position, Cell>> = allPositions.map { PositionToCell(it, minePositions).pair }.toTypedArray()
+    }
+}
