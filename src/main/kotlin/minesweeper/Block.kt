e752d45225472108e8547d1b39539fa77be2d703
jay <jaeykweon@naver.com>
2022-12-31 02:10:05 +0900

    Block을 sealed interface로 변경 Block, CleanBlock, MineBlock을 하나의 파일로 합침

--- src/main/kotlin/minesweeper/Block.kt
+++ src/main/kotlin/minesweeper/Block.kt
@@ -1,6 +1,27 @@
 package minesweeper
 
-interface Block {
+sealed interface Block {
     fun getNearbyMineCount(): Int
     fun setNearbyMineCount(mineCount: Int)
+}
+
+class CleanBlock(
+    private var _nearbyMineCount: Int = 0
+) : Block {
+    override fun getNearbyMineCount(): Int {
+        return _nearbyMineCount
+    }
+
+    override fun setNearbyMineCount(mineCount: Int) {
+        _nearbyMineCount = mineCount
+    }
+}
+
+class MineBlock(
+    private var _nearbyMineCount: Int = 0
+) : Block {
+    override fun setNearbyMineCount(mineCount: Int) {}
+    override fun getNearbyMineCount(): Int {
+        return _nearbyMineCount
+    }
 }
