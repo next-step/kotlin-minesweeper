c2f954a616ce77cb02fbcdfcab1b176318e992cc
jay <jaeykweon@naver.com>
2022-12-30 17:39:02 +0900

    GameMap 생성 팩토리 (2단계)

--- src/main/kotlin/minesweeper/GameMap.kt
+++ src/main/kotlin/minesweeper/GameMap.kt
@@ -2,4 +2,25 @@ package minesweeper
 
 class GameMap(
     val blockTable: BlockTable
-)
+) {
+
+    companion object {
+        fun of(height: Int, width: Int, mineCount: Int): GameMap {
+            return GameMap(
+                BlockTable(
+                    mapOf(
+                        (0 to 0) to CleanBlock(),
+                        (0 to 1) to CleanBlock(),
+                        (0 to 2) to CleanBlock(),
+                        (1 to 0) to CleanBlock(),
+                        (1 to 1) to CleanBlock(),
+                        (1 to 2) to CleanBlock(),
+                        (2 to 0) to CleanBlock(),
+                        (2 to 1) to MineBlock(),
+                        (2 to 2) to MineBlock(),
+                    )
+                )
+            )
+        }
+    }
+}
--- src/test/kotlin/minesweeper/GameMapTest.kt
+++ src/test/kotlin/minesweeper/GameMapTest.kt
@@ -1,9 +1,24 @@
 package minesweeper
 
+import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.ExpectSpec
+import io.kotest.matchers.shouldBe
 
 /**
  * @see GameMap
  */
 class GameMapTest : ExpectSpec({
+
+    expect("높이, 너비, 지뢰 개수를 입력하면 해당하는 GameMap을 생성한다") {
+        val height = 3
+        val width = 3
+        val mineCount = 2
+
+        val gameMap = GameMap.of(height, width, mineCount)
+
+        assertSoftly(gameMap.blockTable.record.values) { blocks ->
+            blocks.count { it is CleanBlock } shouldBe 7
+            blocks.count { it is MineBlock } shouldBe 2
+        }
+    }
 })
