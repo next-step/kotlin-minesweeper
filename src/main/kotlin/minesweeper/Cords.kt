ec04bb2e2a252b7213b44dcb2c489aba4202db79
jay <jaeykweon@naver.com>
2022-12-30 23:01:31 +0900

    Cords 테스트 케이스 추가 및 오류 수정

--- src/main/kotlin/minesweeper/Cords.kt
+++ src/main/kotlin/minesweeper/Cords.kt
@@ -6,8 +6,8 @@ class Cords(
 
     companion object {
         fun of(height: Int, width: Int): Cords {
-            val xCords = 0 until height
-            val yCords = 0 until width
+            val xCords = 0 until width
+            val yCords = 0 until height
 
             return Cords(
                 CordBuilder()
--- src/test/kotlin/minesweeper/CordsTest.kt
+++ src/test/kotlin/minesweeper/CordsTest.kt
@@ -5,7 +5,7 @@ import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
 
 /**
- * @see
+ * @see Cords
  */
 class CordsTest : ExpectSpec({
 
@@ -26,6 +26,29 @@ class CordsTest : ExpectSpec({
             Cord(2, 0),
             Cord(2, 1),
             Cord(2, 2),
+        )
+    }
+
+    expect("직사각형의 경우에도 좌표 묶음을 생성할 수 있다.") {
+        val height = 4
+        val width = 3
+
+        val cords = Cords.of(height, width)
+
+        cords.cords.size shouldBe 12
+        cords.cords shouldContainAll listOf(
+            Cord(0, 0),
+            Cord(0, 1),
+            Cord(0, 2),
+            Cord(0, 3),
+            Cord(1, 0),
+            Cord(1, 1),
+            Cord(1, 2),
+            Cord(1, 3),
+            Cord(2, 0),
+            Cord(2, 1),
+            Cord(2, 2),
+            Cord(2, 3),
         )
     }
 })
