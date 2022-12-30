bdd871c980adabe6137fdbdc3fdbdda9ee7664bb
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-31 03:24:50 +0900

    refactor: 셀을 이동하는 행위를 Coordinate 객체에 메세지를 보내도록 변경

--- src/main/kotlin/domain/Coordinate.kt
+++ src/main/kotlin/domain/Coordinate.kt
@@ -2,4 +2,8 @@ package domain
 
 data class Coordinate(val x: Number, val y: Number) {
     constructor(value: Pair<Int, Int>) : this(Number(value.first), Number(value.second))
+
+    fun movedCoordinate(directions: Directions): Coordinate {
+        return Coordinate((this.x + directions.coordinate.x).value to (this.y + directions.coordinate.y).value)
+    }
 }
