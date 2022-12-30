1f706e798cdf2d8e2a99cea0ecaf2d1b4f47edc7
jay <jaeykweon@naver.com>
2022-12-30 19:18:13 +0900

    GameMap 생성 팩토리 (3단계)

--- src/main/kotlin/minesweeper/BlockTable.kt
+++ src/main/kotlin/minesweeper/BlockTable.kt
@@ -19,4 +19,20 @@ class BlockTable(
             record[cord]?.setNearbyMineCount(mineCount)
         }
     }
+
+    companion object {
+        fun of(height: Int, width: Int, mineCount: Int): BlockTable {
+            val cords = (0 until height).map { y ->
+                (0 until width).map { x -> (y to x) }
+            }.flatten()
+            val blocks = List(height * width - mineCount) { CleanBlock() } +
+                List(mineCount) { MineBlock() }
+
+            return BlockTable(
+                cords.zip(blocks) { cord, block ->
+                    cord to block
+                }.toMap()
+            )
+        }
+    }
 }
--- src/main/kotlin/minesweeper/GameMap.kt
+++ src/main/kotlin/minesweeper/GameMap.kt
@@ -7,19 +7,7 @@ class GameMap(
     companion object {
         fun of(height: Int, width: Int, mineCount: Int): GameMap {
             return GameMap(
-                BlockTable(
-                    mapOf(
-                        (0 to 0) to CleanBlock(),
-                        (0 to 1) to CleanBlock(),
-                        (0 to 2) to CleanBlock(),
-                        (1 to 0) to CleanBlock(),
-                        (1 to 1) to CleanBlock(),
-                        (1 to 2) to CleanBlock(),
-                        (2 to 0) to CleanBlock(),
-                        (2 to 1) to MineBlock(),
-                        (2 to 2) to MineBlock(),
-                    )
-                )
+                BlockTable.of(height, width, mineCount)
             )
         }
     }
