7ef3a639b21d29fe65718b4f3816f71e11c3f795
jay.lee <stkment@gmail.com>
2023-01-01 00:33:13 +0900

    :white_check_mark: Width, Height, MineCount test unit 재정의

---
+++ src/test/kotlin/minesweeper/model/WidthTest.kt
@@ -0,0 +1,15 @@
+package minesweeper.model
+
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class WidthTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1, 0])
+    fun `너비는 1 이상이어야 한다`(width: Int) {
+        assertThatIllegalArgumentException().isThrownBy { Width(width) }
+    }
+}
