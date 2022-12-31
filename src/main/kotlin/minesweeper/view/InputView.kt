5bd18231ead40a705dce3f389a6fb413f3197d97
Choiyounho <maxosa78@gmail.com>
2022-12-31 22:35:07 +0900

    refactor : 인풋 뷰 코드 수정

--- src/main/kotlin/minesweeper/view/InputView.kt
+++ src/main/kotlin/minesweeper/view/InputView.kt
@@ -6,17 +6,17 @@ object InputView {
     private const val MESSAGE_INPUT_WIDTH = "너비를 입력하세요."
     private const val MESSAGE_INPUT_MINE_COUNT = "지뢰의 수를 입력하세요."
 
-    fun inputHeight(input: Int): Int {
+    fun inputHeight(): Int {
         println(MESSAGE_INPUT_HEIGHT)
         return readLine()?.toIntOrNull() ?: 0
     }
 
-    fun inputWidth(input: Int): Int {
+    fun inputWidth(): Int {
         println(MESSAGE_INPUT_WIDTH)
         return readLine()?.toIntOrNull() ?: 0
     }
 
-    fun inputMineCount(input: Int): Int {
+    fun inputMineCount(): Int {
         println(MESSAGE_INPUT_MINE_COUNT)
         return readLine()?.toIntOrNull() ?: 0
     }
