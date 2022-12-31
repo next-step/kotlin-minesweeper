dfd254097f858cd115680eb001f8f5e0a09e5283
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-31 18:50:25 +0900

    feature(View): 좌표를 입력받고 결과를 출력하는 뷰 추가

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -5,14 +5,24 @@ import domain.Column
 import dto.BoardDto
 
 object ResultView {
-    fun printBoard(boardInfo: BoardInfo, boardDto: BoardDto) {
+    fun printStart() {
         println("지뢰찾기 게임 시작")
-
+    }
+    fun printBoard(boardInfo: BoardInfo, boardDto: BoardDto) {
         boardDto.cells.forEachIndexed { index, it ->
             print("$it ")
 
             if (isSpace(index, boardInfo.column)) println()
         }
+    }
+
+    fun printLose() {
+        println("Lose Game.")
+    }
+
+    fun printWin(boardInfo: BoardInfo, boardDto: BoardDto) {
+        println("Win Game!")
+        printBoard(boardInfo, boardDto)
     }
 
     private fun isSpace(index: Int, column: Column): Boolean {
