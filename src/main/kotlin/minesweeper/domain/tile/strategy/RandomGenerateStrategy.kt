be24e2ab0f939407b627341bcf8eb2e84d723297
namonak <namonak@gmail.com>
2022-12-31 15:33:29 +0900

    fix(step2): fix the problem that coordinate values are shuffled in RandomGenerateStrategy

--- src/main/kotlin/minesweeper/domain/tile/strategy/RandomGenerateStrategy.kt
+++ src/main/kotlin/minesweeper/domain/tile/strategy/RandomGenerateStrategy.kt
@@ -8,12 +8,26 @@ import minesweeper.domain.tile.state.Tile
 class RandomGenerateStrategy(override val width: Position, override val height: Position, override val mineCount: MineCount) : GenerateStrategy {
     override fun generate(): List<Tile> {
         val tiles = mutableListOf<Tile>()
-        var mountCount = mineCount.value
+        var mineList = mutableListOf<Boolean>()
+        var mineCount = mineCount.value
+
+        for (index in 0 until width.getCalibratedPosition() * height.getCalibratedPosition()) {
+            addMine(mineCount--, mineList)
+        }
+        mineList = mineList.shuffled().toMutableList()
 
         for (x in 0..width.value) for (y in 0..height.value) {
             val coordinate = Coordinate(Position(x), Position(y))
-            tiles.add(Tile.of(coordinate, mountCount--))
+            tiles.add(Tile.of(coordinate, mineList.removeAt(0)))
         }
-        return tiles.shuffled().toList()
+        return tiles.toList()
+    }
+
+    private fun addMine(mineCount: Int, mineList: MutableList<Boolean>) {
+        if (mineCount > 0) {
+            mineList.add(true)
+            return
+        }
+        mineList.add(false)
     }
 }
