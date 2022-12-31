1d857d80c8ae9354b208710a6f3d630f4a0e9dc6
namonak <namonak@gmail.com>
2022-12-31 22:20:50 +0900

    refactor(step2): add SurroundingTiles sealed class for calculating surrounding tile coordinates

--- src/main/kotlin/minesweeper/domain/Land.kt
+++ src/main/kotlin/minesweeper/domain/Land.kt
@@ -2,6 +2,7 @@ package minesweeper.domain
 
 import minesweeper.domain.tile.Marking
 import minesweeper.domain.tile.Marking.Companion.toMarkingAsInt
+import minesweeper.domain.tile.SurroundingTiles
 import minesweeper.domain.tile.Tiles
 import minesweeper.domain.tile.pos.Coordinate
 import minesweeper.domain.tile.pos.Position
@@ -24,27 +25,18 @@ data class Land(private val width: Position, private val height: Position, priva
 
     fun getMineCount(coordinate: Coordinate): Int {
         var mineCount = 0
-        val positionX = coordinate.getPositionX()
-        val positionY = coordinate.getPositionY()
-
-        if (isExistMine(positionX - 1, positionY - 1)) mineCount++
-        if (isExistMine(positionX - 1, positionY)) mineCount++
-        if (isExistMine(positionX - 1, positionY + 1)) mineCount++
-        if (isExistMine(positionX, positionY - 1)) mineCount++
-        if (isExistMine(positionX, positionY + 1)) mineCount++
-        if (isExistMine(positionX + 1, positionY - 1)) mineCount++
-        if (isExistMine(positionX + 1, positionY)) mineCount++
-        if (isExistMine(positionX + 1, positionY + 1)) mineCount++
-
+        for (surroundingTiles in SurroundingTiles.values()) {
+            val (x, y) = coordinate.getSurroundTilesCoordinateAsPair(surroundingTiles)
+            if (isExistMine(x, y)) mineCount++
+        }
         return mineCount
     }
 
     private fun isExistMine(positionX: Int, positionY: Int): Boolean {
-        return if (positionX < ZERO || positionX > width.value || positionY < ZERO || positionY > height.value) {
-            false
-        } else {
-            _tiles.isMine(Coordinate.of(positionX, positionY))
+        if (positionX < ZERO || positionX > width.value || positionY < ZERO || positionY > height.value) {
+            return false
         }
+        return _tiles.isMine(Coordinate.of(positionX, positionY))
     }
 
     companion object {
--- src/main/kotlin/minesweeper/domain/tile/pos/Coordinate.kt
+++ src/main/kotlin/minesweeper/domain/tile/pos/Coordinate.kt
@@ -1,9 +1,20 @@
 package minesweeper.domain.tile.pos
 
-data class Coordinate(private val positionX: Position, private val positionY: Position) {
-    fun getPositionX() = positionX.value
+import minesweeper.domain.tile.SurroundingTiles
 
-    fun getPositionY() = positionY.value
+data class Coordinate(private val positionX: Position, private val positionY: Position) {
+    fun getSurroundTilesCoordinateAsPair(surroundingTiles: SurroundingTiles): Pair<Int, Int> {
+        return when (surroundingTiles) {
+            SurroundingTiles.TopLeft -> Pair(positionX.value - 1, positionY.value - 1)
+            SurroundingTiles.TopMiddle -> Pair(positionX.value - 1, positionY.value)
+            SurroundingTiles.TopRight -> Pair(positionX.value - 1, positionY.value + 1)
+            SurroundingTiles.MiddleLeft -> Pair(positionX.value, positionY.value - 1)
+            SurroundingTiles.MiddleRight -> Pair(positionX.value, positionY.value + 1)
+            SurroundingTiles.BottomLeft -> Pair(positionX.value + 1, positionY.value - 1)
+            SurroundingTiles.BottomMiddle -> Pair(positionX.value + 1, positionY.value)
+            SurroundingTiles.BottomRight -> Pair(positionX.value + 1, positionY.value + 1)
+        }
+    }
 
     companion object {
         fun of(positionX: Int, positionY: Int) = Coordinate(Position(positionX), Position(positionY))
