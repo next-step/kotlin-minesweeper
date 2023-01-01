900812daf44acddf31a6bae1e31d05c1cc0817c7
EP <kjuioqq8@gmail.com>
2023-01-02 01:21:33 +0900

    refactor: 불필요한 인터페이스 제거

--- src/main/kotlin/minesweeper/domain/board/MineBoard.kt
+++ src/main/kotlin/minesweeper/domain/board/MineBoard.kt
@@ -3,6 +3,7 @@ package minesweeper.domain.board
 import minesweeper.domain.MineGenerator
 import minesweeper.domain.Mines
 import minesweeper.domain.component.Component
+import minesweeper.domain.component.Components
 import minesweeper.domain.component.DefaultComponent
 import minesweeper.domain.coordinate.BaseCoordinateSystem
 import minesweeper.domain.coordinate.MineCoordinateSystemDecorator
@@ -12,14 +13,14 @@ class MineBoard(
     width: Int,
     mineCount: Int,
     mineGenerator: MineGenerator
-) : Board {
+) : Components {
     private val coordinateSystem: MineCoordinateSystemDecorator
     private val mineList: Mines
         get() = coordinateSystem.mines
 
-    override val height: Int
+    val height: Int
         get() = coordinateSystem.height
-    override val width: Int
+    val width: Int
         get() = coordinateSystem.width
 
     init {
