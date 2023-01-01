579bdfdec16235827174a44624ade8b1ca273167
EP <kjuioqq8@gmail.com>
2023-01-02 01:15:23 +0900

    refactor: data class 활용

--- src/main/kotlin/minesweeper/domain/Position.kt
+++ src/main/kotlin/minesweeper/domain/Position.kt
@@ -2,25 +2,7 @@ package minesweeper.domain
 
 import minesweeper.common.value.CoordinateValue
 
-class Position(
+data class Position(
     val x: CoordinateValue,
     val y: CoordinateValue
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Position
-
-        if (x != other.x) return false
-        if (y != other.y) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = x.hashCode()
-        result = 31 * result + y.hashCode()
-        return result
-    }
-}
+)
