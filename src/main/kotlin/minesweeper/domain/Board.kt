da924f818d251a9acb61b5cfa9f8c4f96028e45d
wonjun3991 <wonjun.choi@meshkorea.net>
2023-01-02 17:04:17 +0900

    refactor(Board, Matrix): refactor Board, Matrix Class
    Separate Logic

--- src/main/kotlin/minesweeper/domain/Board.kt
+++ src/main/kotlin/minesweeper/domain/Board.kt
@@ -1,84 +1,36 @@
 package minesweeper.domain
 
 class Board(val matrix: Matrix, mineCount: Int) {
-    init {
-        createRandomMine(mineCount)
-        createSafe()
-    }
+    private val mineCoordinates: List<Coordinate>
+    private val safeCoordinates: List<Coordinate>
 
-    private fun createRandomMine(count: Int) {
-        require(count <= matrix.width() * matrix.height()) {
+    init {
+        require(mineCount <= matrix.width * matrix.height) {
             "지뢰 개수는 지도에 존재하는 모든 필드의 수보다 클 수 없습니다."
         }
 
-        coordinates().shuffled()
-            .take(count)
-            .forEach { matrix.rows[it.rows][it.cols] = Mine() }
+        mineCoordinates = getMineCoordinates(mineCount)
+        safeCoordinates = getSafeCoordinates()
+        makeMap()
     }
-
     fun open(coordinate: Coordinate) {
-        val field = matrix.rows[coordinate.rows][coordinate.cols]
-        field.open()
-
-        if (field is Safe && field.aroundMineCount == 0) this.openAround(coordinate)
-
-        canOpenFields(coordinate)
-            .filter { noMinesAround(it) }
-            .forEach { this.openAround(it) }
+        matrix.open(coordinate)
     }
 
-    fun isWin(): Boolean {
-        return coordinates().filter { matrix.rows[it.rows][it.cols] is Safe }
-            .all { matrix.rows[it.rows][it.cols].isOpened() }
+    fun isWin() = safeCoordinates.all { matrix.rows[it.rows][it.cols].isOpened() }
+    fun isLose() = mineCoordinates.any { matrix.rows[it.rows][it.cols].isOpened() }
+
+    private fun makeMap() {
+        mineCoordinates.forEach { matrix.landMine(it) }
+        safeCoordinates.forEach { matrix.rows[it.rows][it.cols].landSafe(matrix.aroundMineCount(it)) }
     }
 
-    fun isLose(): Boolean {
-        return coordinates().filter { matrix.rows[it.rows][it.cols] is Mine }
-            .any { matrix.rows[it.rows][it.cols].isOpened() }
-    }
+    private fun getSafeCoordinates() = matrix.coordinates
+        .filter { it !in mineCoordinates }
 
-    private fun openAround(coordinate: Coordinate) {
-        matrix.rows[coordinate.rows][coordinate.cols].open()
-
-        canOpenFields(coordinate)
-            .filter { !noMinesAround(it) }
-            .forEach { matrix.rows[it.rows][it.cols].open() }
-
-        canOpenFields(coordinate)
-            .filter { noMinesAround(it) }
-            .forEach { openAround(it) }
-    }
-
-    private fun canOpenFields(coordinate: Coordinate) =
-        CoordinateDirection.around(coordinate)
-            .asSequence()
-            .filter { it.rows in 0 until matrix.height() && it.cols in 0 until matrix.width() }
-            .filter { !matrix.rows[it.rows][it.cols].isOpened() }
-            .toList()
-
-    private fun noMinesAround(coordinate: Coordinate) =
-        matrix.rows[coordinate.rows][coordinate.cols] is Safe && (matrix.rows[coordinate.rows][coordinate.cols] as Safe).aroundMineCount == 0
-
-    private fun createSafe() {
-        coordinates()
-            .forEach {
-                if (matrix.rows[it.rows][it.cols] !is Mine)
-                    matrix.rows[it.rows][it.cols] = Safe(aroundMineCount(it))
-            }
-    }
-
-    private fun aroundMineCount(coordinate: Coordinate): Int {
-        return CoordinateDirection.around(coordinate)
-            .filter { it.rows in 0 until matrix.width() && it.cols in 0 until matrix.height() }
-            .count { matrix.rows[it.rows][it.cols] is Mine }
-    }
-
-    private fun coordinates() = matrix.rows
-        .indices
-        .flatMap { rows: Int -> rowsCoordinates(rows) }
-
-    private fun rowsCoordinates(rows: Int) =
-        (0 until matrix.rows[rows].size).map { cols: Int -> Coordinate(rows, cols) }
+    private fun getMineCoordinates(mineCount: Int) = matrix.coordinates
+        .shuffled()
+        .take(mineCount)
 
     companion object {
         fun of(width: Int, height: Int, mineCount: Int) =
--- src/main/kotlin/minesweeper/domain/Matrix.kt
+++ src/main/kotlin/minesweeper/domain/Matrix.kt
@@ -1,15 +1,68 @@
 package minesweeper.domain
 
-class Matrix(val rows: List<MutableList<Field>>) {
-    fun width() = rows[0].size
+import minesweeper.domain.field.Field
+import minesweeper.domain.field.Mine
+import minesweeper.domain.field.Safe
 
-    fun height() = rows.size
+class Matrix(val rows: List<List<Field>>) {
+    val width = rows[0].size
+    val height = rows.size
+    val coordinates = rows
+        .indices
+        .flatMap { rows: Int -> rowsCoordinates(rows) }
+
+    fun open(coordinate: Coordinate) {
+        val field = rows[coordinate.rows][coordinate.cols]
+        field.open()
+
+        if (noMinesAround(coordinate)) this.openAround(coordinate)
+
+        canOpenFields(coordinate)
+            .filter { noMinesAround(it) }
+            .forEach { this.openAround(it) }
+    }
+
+    fun landMine(coordinate: Coordinate) {
+        rows[coordinate.rows][coordinate.cols].landMine()
+    }
+
+    fun aroundMineCount(coordinate: Coordinate): Int {
+        return CoordinateDirection.around(coordinate)
+            .filter { it.rows in 0 until width && it.cols in 0 until height }
+            .count { rows[it.rows][it.cols].land is Mine }
+    }
+
+    private fun openAround(coordinate: Coordinate) {
+        rows[coordinate.rows][coordinate.cols].open()
+
+        canOpenFields(coordinate)
+            .filter { !noMinesAround(it) }
+            .forEach { rows[it.rows][it.cols].open() }
+
+        canOpenFields(coordinate)
+            .filter { noMinesAround(it) }
+            .forEach { openAround(it) }
+    }
+
+    private fun canOpenFields(coordinate: Coordinate) =
+        CoordinateDirection.around(coordinate)
+            .asSequence()
+            .filter { it.rows in 0 until height && it.cols in 0 until width }
+            .filter { rows[it.rows][it.cols].isClosed() }
+            .toList()
+
+    private fun noMinesAround(coordinate: Coordinate) =
+        rows[coordinate.rows][coordinate.cols].land is Safe && rows[coordinate.rows][coordinate.cols].aroundMineCount() == 0
+
+    private fun rowsCoordinates(rowIndex: Int) =
+        (0 until rows[rowIndex].size).map { cols: Int -> Coordinate(rowIndex, cols) }
 
     companion object {
         fun of(width: Int, height: Int) = Matrix(createRows(height, width))
 
-        private fun createRows(height: Int, width: Int) = List(height) { createCols(width) }
+        private fun createRows(height: Int, width: Int): List<List<Field>> =
+            List(height) { createCols(width) }
 
-        private fun createCols(width: Int) = MutableList(width) { Field() }
+        private fun createCols(width: Int): List<Field> = List(width) { Field() }
     }
 }
