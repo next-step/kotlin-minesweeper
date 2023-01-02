542427a2e8de8c0b49df622b419051d2577e020e
이상인/wefu0000 <wefu.child@gmail.com>
2023-01-02 18:52:23 +0900

    refactor: 출력 함수 정리, 로직 단순화

--- src/main/kotlin/minesweeper/ui/ConsoleResultView.kt
+++ src/main/kotlin/minesweeper/ui/ConsoleResultView.kt
@@ -8,14 +8,18 @@ class ConsoleResultView : ResultView {
         println("지뢰찾기 게임 시작")
         val blockList = blocks.blockBoard.values.map { it }
         blockList.forEach { rowBlocks ->
-            println(
-                rowBlocks.joinToString {
-                    when (it) {
-                        is Block.LandMine -> "*"
-                        is Block.Normal -> blocks.getAroundMineCount(it).toString()
-                    }
-                }.replace(",", "")
-            )
+            drawRowBlocks(rowBlocks, blocks)
+        }
+    }
+
+    private fun drawRowBlocks(rowBlocks: List<Block>, blocks: Blocks) {
+        println(rowBlocks.joinToString(" ") { getDrawBlockText(it, blocks) })
+    }
+
+    private fun getDrawBlockText(block: Block, blocks: Blocks): String {
+        return when (block) {
+            is Block.LandMine -> "*"
+            is Block.Normal -> blocks.getAroundMineCount(block).toString()
         }
     }
 }
