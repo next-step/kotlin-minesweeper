34e72e665df60f7aece1bb55f84341cfd30fa458
jay.lee <stkment@gmail.com>
2023-01-02 23:47:19 +0900

    :iphone: 지뢰찾기 게임 시작, 진행, 결과에 대한 입출력 기능 구현

--- src/main/kotlin/minesweeper/ResultView.kt
+++ src/main/kotlin/minesweeper/ResultView.kt
@@ -3,9 +3,22 @@ package minesweeper
 import minesweeper.domain.BlockRow
 
 object ResultView {
-    fun printBlocks(blocks: List<BlockRow>) {
+
+    fun printStartGame() {
         println()
         println("지뢰찾기 게임 시작")
+    }
+
+    fun printBlocks(blocks: List<BlockRow>) {
         blocks.forEach { println(it) }
+        println()
+    }
+
+    fun printWin() {
+        println("Win Game.")
+    }
+
+    fun printLose() {
+        println("Lose Game.")
     }
 }
