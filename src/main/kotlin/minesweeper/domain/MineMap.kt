b189210a693eb44789abfbed9475f3baf1025c0b
harris.yi <harris.yi@kakaostyle.com>
2023-01-03 19:29:53 +0900

    feat: Change map to list

--- src/main/kotlin/minesweeper/domain/MineMap.kt
+++ src/main/kotlin/minesweeper/domain/MineMap.kt
@@ -1,32 +1,36 @@
 package minesweeper.domain
 
 class MineMap(
-    private val mineCells: MutableMap<Position, Cell>,
+    private val mineCells: MutableList<Cell>,
 ) {
     private val mineCount: Int
-        get() = mineCells.values
-            .count { it is MineCell }
+        get() = mineCells.count { it is MineCell }
 
-    fun openCell(position: Position): Cell = getCell(position).run {
-        mineCells[position] = this.copyWithOpen()
-        this.copyWithOpen()
+    private val allPositions: List<Position>
+        get() = mineCells.map { it.position }
+
+    fun openCell(position: Position): Cell = getCellWithIndex(position).let {
+        mineCells[it.index] = it.value.copyWithOpen()
+        mineCells[it.index]
     }
 
-    private fun getCell(position: Position): Cell =
-        mineCells[position]
-            ?: throw IllegalStateException("해당 위치의 셀을 찾을 수 없습니다. 위치: (${position.row},${position.column})")
-
+    private fun getCellWithIndex(position: Position): IndexedValue<Cell> = mineCells.withIndex()
+        .firstOrNull { it.value.position == position }
+        ?: throw IllegalStateException("해당 위치의 셀을 찾을 수 없습니다. 위치: (${position.row},${position.column})")
 
     fun getClosedNearPositions(position: Position) = position.getNearPositions()
-        .filter { mineCells.keys.contains(it) }
+        .filter { allPositions.contains(it) }
         .filter { getCell(it).state == CellState.CLOSED }
 
-    fun snapshot() = mineCells.keys.sortedBy { it.column }
+    private fun getCell(position: Position): Cell =
+        mineCells.first { it.position == position }
+
+    fun snapshot() = allPositions.sortedBy { it.column }
         .sortedBy { it.row }
         .map { getCell(it) }
 
     fun isNotGameClear(): Boolean {
-        val closedCells = mineCells.values.filter { it.state == CellState.CLOSED }
+        val closedCells = mineCells.filter { it.state == CellState.CLOSED }
         return !(closedCells.all { it is MineCell } && closedCells.count() == mineCount)
     }
 
@@ -34,11 +38,10 @@ class MineMap(
         fun createMineMap(height: Int, width: Int, mineCount: Int): MineMap {
             val positions = Positions(height, width)
             val minePositions = positions.values.shuffled().take(mineCount)
-            val mineCells: MutableMap<Position, Cell> = positions.values
-                .associateWith { createCell(minePositions, it) }
-                .toMutableMap()
+            val mineCells = positions.values
+                .map { createCell(minePositions, it) }
 
-            return MineMap(mineCells)
+            return MineMap(mineCells.toMutableList())
         }
 
         private fun createCell(
