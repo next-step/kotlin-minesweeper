e6f08f01211c9f35725c172eb828a6d2960043a6
namonak <namonak@gmail.com>
2023-01-05 00:58:47 +0900

    refactor(step3): add NUMBER_OF_COORDINATES constant to InputParser class

--- src/main/kotlin/minesweeper/controller/InputParser.kt
+++ src/main/kotlin/minesweeper/controller/InputParser.kt
@@ -4,6 +4,7 @@ object InputParser {
     private const val MINIMUM_INPUT_POSITION = 1
     private const val MAXIMUM_OUTPUT_POSITION = 20
     private const val CORRECTION_VALUE = 1
+    private const val NUMBER_OF_COORDINATES = 2
 
     fun parseNumber(inputNumber: String): Int {
         return inputNumber.toIntOrNull() ?: throw IllegalArgumentException("숫자를 입력해주세요.")
@@ -17,7 +18,7 @@ object InputParser {
 
     fun parseCoordinate(input: String): Pair<Int, Int> {
         val (x, y) = input.split(", ").let {
-            require(it.size == 2) { "좌표는 x, y 형식으로 입력해주세요." }
+            require(it.size == NUMBER_OF_COORDINATES) { "좌표는 x, y 형식으로 입력해주세요." }
             it.map(::parsePosition)
         }
         return Pair(x, y)
