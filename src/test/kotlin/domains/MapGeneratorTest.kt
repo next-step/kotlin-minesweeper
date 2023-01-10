a212e5b3251e4b367e539a78e94d101f1411f708
hojihun5516 <hojihun5516@daum.net>
2023-01-11 08:45:24 +0900

    feat: Map generator

---
+++ src/test/kotlin/domains/MapGeneratorTest.kt
@@ -0,0 +1,30 @@
+package domains
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+internal class MapGeneratorTest {
+    @Test
+    @DisplayName("게임 사이즈를 통해서 게임 맵을 생성한다")
+    fun `sut generate game map by gameSize`() {
+        // Arrange
+        val gameSize = GameSize(10, 10)
+        val minePositions = Positions(
+            listOf(
+                Position(0, 0),
+                Position(1, 2),
+                Position(3, 5),
+                Position(4, 7),
+                Position(8, 8),
+            )
+        )
+        val sut = MapGenerator(gameSize, minePositions)
+
+        // Act
+        val actual = sut.generateMap()
+
+        // Assert
+        assertThat(actual.value.size).isEqualTo(100)
+    }
+}
