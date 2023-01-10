da27c9631a6ece5af0e8da1b4fe22432600bd9ce
hojihun5516 <hojihun5516@daum.net>
2023-01-11 08:45:35 +0900

    feat: Mine position generator

---
+++ src/main/kotlin/domains/MinePositionGenerator.kt
@@ -0,0 +1,25 @@
+package domains
+
+import kotlin.random.Random
+
+class MinePositionGenerator(
+    private val gameSize: GameSize,
+    private val mineCount: Int
+) {
+    fun generateMinePositions(): Positions {
+        val minePositions: MutableList<Position> = mutableListOf()
+        while (minePositions.size != mineCount) {
+            val position = generatePosition()
+            if (!minePositions.contains(position)) {
+                minePositions.add(position)
+            }
+        }
+        return Positions(minePositions.toList())
+    }
+
+    private fun generatePosition(): Position {
+        val x = Random.nextInt(gameSize.width)
+        val y = Random.nextInt(gameSize.height)
+        return Position(x, y)
+    }
+}
