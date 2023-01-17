1c4ee1129e70dbf388770906c296787d7b6e3c77
adforus_hjp <hjp281@adforus.co.kr>
2023-01-17 14:27:48 +0900

    refactor: 지뢰가 0일 때 0 나누기 에러 나오는 현상 수정

--- src/main/kotlin/minesweeper/domain/plant_strategy/RemainderPlantStrategy.kt
+++ src/main/kotlin/minesweeper/domain/plant_strategy/RemainderPlantStrategy.kt
@@ -7,6 +7,8 @@ class RemainderPlantStrategy : PlantStrategy {
     override fun createMines(width: Int, height: Int, mineCount: Int): Set<Point> {
         val minePoints = mutableSetOf<Point>()
 
+        if (mineCount == 0) return minePoints
+
         val term = width * height / mineCount
         val maxNumber = term * mineCount
         for (i: Int in 0 until maxNumber step term) {
--- src/test/kotlin/minesweeper/domain/plant_strategy/RemainderPlantStrategyTest.kt
+++ src/test/kotlin/minesweeper/domain/plant_strategy/RemainderPlantStrategyTest.kt
@@ -1,20 +1,26 @@
 package minesweeper.domain.plant_strategy
 
 import minesweeper.domain.Point
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class RemainderPlantStrategyTest {
 
     @Test
     fun `원하는 만큼의 지뢰가 나왔는지 테스트 한다`() {
-        Assertions.assertThat(RemainderPlantStrategy().createMines(width = 5, height = 5, mineCount = 10).size)
+        assertThat(RemainderPlantStrategy().createMines(width = 5, height = 5, mineCount = 10).size)
             .isEqualTo(10)
     }
 
     @Test
+    fun `지뢰가 0개면 빈 배열을 반환한다`() {
+        assertThat(RemainderPlantStrategy().createMines(width = 5, height = 5, mineCount = 0).size)
+            .isEqualTo(0)
+    }
+
+    @Test
     fun `3 * 3의 배열에서 5개를 심으면 아래와 같은 모양으로 지뢰를 심는다`() {
-        Assertions.assertThat(RemainderPlantStrategy().createMines(width = 3, height = 3, mineCount = 5))
+        assertThat(RemainderPlantStrategy().createMines(width = 3, height = 3, mineCount = 5))
             .isEqualTo(setOf(Point(0, 0), Point(1, 0), Point(2, 0), Point(1, 1), Point(0, 1)))
     }
 }
