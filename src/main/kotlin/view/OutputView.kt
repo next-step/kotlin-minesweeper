5a21e297578dfe240e1f6a3ba32e5c9e70edc1d2
James <bookwriterseo@gmail.com>
2023-01-27 02:20:03 +0900

    refactor: 출력 뷰 리팩터링

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -3,11 +3,15 @@ package view
 class OutputView {
     fun showBoard(board: List<String>, width: Int) {
         println("지뢰찾기 게임 시작")
-        for (index in board.indices) {
-            print("${board[index]} ")
-            if ((index + 1) % width == 0) {
-                println()
-            }
+        board.forEachIndexed { index, tile ->
+            print("$tile ")
+            addLine(index, width)
+        }
+    }
+
+    private fun addLine(index: Int, width: Int) {
+        if ((index + 1) % width == 0) {
+            println()
         }
     }
 }
