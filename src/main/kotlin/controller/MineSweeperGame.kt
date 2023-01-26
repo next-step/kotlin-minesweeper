ad8086c2b5d48d8fdda405db62f1c433947ad137
James <bookwriterseo@gmail.com>
2023-01-27 02:12:56 +0900

    feat: 지뢰 찾기 보드를 출력한다

--- src/main/kotlin/controller/MineSweeperGame.kt
+++ src/main/kotlin/controller/MineSweeperGame.kt
@@ -5,15 +5,18 @@ import model.BoardMaker
 import model.BoardWidth
 import model.Mine
 import view.InputView
+import view.OutputView
 
 class MineSweeperGame {
     private val inputView = InputView()
+    private val outputView = OutputView()
 
     fun run() {
         val boardHeight = BoardHeight(inputView.getHeight())
         val boardWidth = BoardWidth(inputView.getWidth())
         val mine = Mine(inputView.getMineNumber())
         val boardMaker = BoardMaker()
-        boardMaker.run(boardHeight, boardWidth, mine)
+        val board = boardMaker.run(boardHeight, boardWidth, mine)
+        outputView.showBoard(board, boardWidth.width)
     }
 }
