a79264baf4efbdeca25df38e5db504471b668a4b
ljw0829 <olo_jinwon@naver.com>
2023-06-21 22:50:43 +0900

    feat: 양수 객체 생성

---
+++ src/main/kotlin/minesweeper/PositiveNumber.kt
@@ -0,0 +1,15 @@
+package minesweeper
+
+@JvmInline
+value class PositiveNumber(val value: Int) {
+
+    init {
+        require(value > MINIMUM_NUMBER) {
+            "숫자는 ${MINIMUM_NUMBER}보다 큰 값을 입력해야 합니다. 입력값 : $value"
+        }
+    }
+
+    companion object {
+        private const val MINIMUM_NUMBER: Int = 0
+    }
+}
