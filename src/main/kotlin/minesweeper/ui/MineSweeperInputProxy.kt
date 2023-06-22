81f1739ad214b7614f1bc6e0ee32e57deff87e30
catsbi <killfine1988@gmail.com>
2023-06-23 00:18:00 +0900

    feat(MineSweeper): 지뢰찾기 설정 및 I/O 구현

--- src/main/kotlin/minesweeper/ui/MineSweeperInputProxy.kt
+++ src/main/kotlin/minesweeper/ui/MineSweeperInputProxy.kt
@@ -3,14 +3,35 @@ package minesweeper.ui
 class MineSweeperInputProxy(private val target: MineSweeperInput) : MineSweeperInput {
 
     override fun requestWidth(): Int {
-        return target.requestWidth()
+        val width = target.requestWidth()
+
+        return if(width > 0) {
+            width
+        } else {
+            println("너비는 0 이상의 자연수를 입력해주세요 [Input: $width]")
+            requestWidth()
+        }
     }
 
     override fun requestHeight(): Int {
-        return target.requestHeight()
+        val height = target.requestWidth()
+
+        return if(height > 0) {
+            height
+        } else {
+            println("높이는 0 이상의 자연수를 입력해주세요 [Input: $height]")
+            requestHeight()
+        }
     }
 
     override fun requestMineCapacity(): Int {
-        return target.requestMineCapacity()
+        val mineCapacity = target.requestMineCapacity()
+
+        return if(mineCapacity > 0) {
+            mineCapacity
+        } else {
+            println("지뢰의 갯수는 0 이상의 자연수를 입력해주세요 [Input: $mineCapacity]")
+            requestMineCapacity()
+        }
     }
 }
