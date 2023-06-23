f2a2a564299e282128ddcdaa00a772b2eabf5744
ljw0829 <olo_jinwon@naver.com>
2023-06-23 23:04:12 +0900

    refactor: 불필요한 지시자, 파일 삭제

--- src/main/kotlin/minesweeper/domain/Coordinate.kt
+++ src/main/kotlin/minesweeper/domain/Coordinate.kt
@@ -5,7 +5,7 @@ data class Coordinate(val x: Point, val y: Point) {
     operator fun compareTo(other: Coordinate): Int = compareValuesBy(
         a = this,
         b = other,
-        { it.x.value },
-        { it.y.value },
+        { x.value },
+        { y.value },
     )
 }
