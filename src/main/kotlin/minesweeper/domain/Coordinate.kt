14e2b9b97d008c5c63f7dc1a79fd59d4d0e4b408
ljw0829 <olo_jinwon@naver.com>
2023-06-24 21:56:28 +0900

    feat: 좌표 객체에서 주변 좌표를 구할 수 있도록 기능 추가

--- src/main/kotlin/minesweeper/domain/Coordinate.kt
+++ src/main/kotlin/minesweeper/domain/Coordinate.kt
@@ -2,10 +2,23 @@ package minesweeper.domain
 
 data class Coordinate(val x: Int, val y: Int) {
 
+    fun aroundCoordinates(): List<Coordinate> = x.aroundCoordinates()
+        .flatMap(::createAroundCoordinates)
+        .filter { this != it }
+
+    private fun createAroundCoordinates(x: Int): List<Coordinate> =
+        y.aroundCoordinates().map { y -> Coordinate(x = x, y = y) }
+
+    private fun Int.aroundCoordinates(): List<Int> = listOf(this, this - AROUND_BLOCK, this + AROUND_BLOCK)
+
     operator fun compareTo(other: Coordinate): Int = compareValuesBy(
         a = this,
         b = other,
         { x },
         { y },
     )
+
+    companion object {
+        private const val AROUND_BLOCK: Int = 1
+    }
 }
