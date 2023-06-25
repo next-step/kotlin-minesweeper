4bd00adaaa1569d4eb0827e247fea4dc4ae7fc70
catsbi <killfine1988@gmail.com>
2023-06-25 16:02:29 +0900

    refactor(AdjacentPoints): 매직넘버 상수화

--- src/main/kotlin/minesweeper/domain/finder/AdjacentPoints.kt
+++ src/main/kotlin/minesweeper/domain/finder/AdjacentPoints.kt
@@ -5,28 +5,30 @@ import minesweeper.domain.Point
 
 typealias movePoint = (MinePoint) -> Point
 
+private const val MOVING_POINT = 1
+
 enum class AdjacentPoints(val movePoint: movePoint) {
     // 북서쪽
-    NORTHWESTWARD({ Point(x = it.x - 1, y = it.y - 1) }),
+    NORTHWESTWARD({ Point(x = it.x - MOVING_POINT, y = it.y - MOVING_POINT) }),
 
     // 북쪽
-    NORTH({ Point(x = it.x, y = it.y - 1) }),
+    NORTH({ Point(x = it.x, y = it.y - MOVING_POINT) }),
 
     // 북동쪽
-    NORTHEASTWARD({ Point(x = it.x + 1, y = it.y - 1) }),
+    NORTHEASTWARD({ Point(x = it.x + MOVING_POINT, y = it.y - MOVING_POINT) }),
 
     // 동쪽
-    EAST({ Point(x = it.x + 1, y = it.y) }),
+    EAST({ Point(x = it.x + MOVING_POINT, y = it.y) }),
 
     // 남동쪽
-    SOUTHEASTWARD({ Point(x = it.x + 1, y = it.y + 1) }),
+    SOUTHEASTWARD({ Point(x = it.x + MOVING_POINT, y = it.y + MOVING_POINT) }),
 
     // 남쪽
-    SOUTH({ Point(x = it.x, y = it.y + 1) }),
+    SOUTH({ Point(x = it.x, y = it.y + MOVING_POINT) }),
 
     // 남서쪽
-    SOUTHWEST({ Point(x = it.x - 1, y = it.y + 1) }),
+    SOUTHWEST({ Point(x = it.x - MOVING_POINT, y = it.y + MOVING_POINT) }),
 
     // 서쪽
-    WEST({ Point(x = it.x - 1, y = it.y) });
+    WEST({ Point(x = it.x - MOVING_POINT, y = it.y) });
 }
