c08a45f75ce76dc0dd5211b78f2c1d295cda8f5a
catsbi <killfine1988@gmail.com>
2023-06-29 15:58:31 +0900

    refactor: 불필요 메서드 제거

--- src/main/kotlin/minesweeper/domain/RemainPointCount.kt
+++ src/main/kotlin/minesweeper/domain/RemainPointCount.kt
@@ -4,9 +4,6 @@ data class RemainPointCount(
     private var count: Int,
     val allowNegative: Boolean = false
 ) {
-
-    fun current(): Int = count
-
     fun decreaseAndGet(input: Int = 1): Int {
         check(isReductionPossible(input)) {
             "더 이상 값을 감소시킬 수 없습니다: Current: $count"
