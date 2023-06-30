4a31c20f01e8b6b1512a9f763aa575c237d8b3f6
genos-lee <genos.lee@kakaocorp.com>
2023-07-01 00:16:44 +0900

    feature : 높이를 BoardHeight로 추상화하고 테스트코드 작성

---
+++ src/main/kotlin/next/step/minesweeper/domain/BoardHeight.kt
@@ -0,0 +1,12 @@
+package next.step.minesweeper.domain
+
+data class BoardHeight(val height: Int) {
+    init {
+        require(height > MIN_HEIGHT) { "높이는 ${MIN_HEIGHT}보다 커야합니다." }
+    }
+
+    companion object {
+        private const val MIN_HEIGHT = 0
+        fun of(height: Int): BoardHeight = BoardHeight(height)
+    }
+}
