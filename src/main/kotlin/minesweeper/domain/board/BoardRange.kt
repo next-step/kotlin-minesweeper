2b34ecb00b2ae7eb97cf8db78359fd7322455ce9
nam-sangyun <nsun9505@naver.comm>
2023-07-01 01:42:36 +0900

    feature: 보드 범위의 너비에 0 이하 숫자가 입력되면 IllegalArgumentException 이 발생

--- src/main/kotlin/minesweeper/domain/board/BoardRange.kt
+++ src/main/kotlin/minesweeper/domain/board/BoardRange.kt
@@ -3,7 +3,11 @@ package minesweeper.domain.board
 class BoardRange(private val height: Int, private val width: Int) {
 
     init {
-        require(height >= 1) { "높이는 1 이상이어야 합니다." }
-        require(width >= 1) { "너비는 1 이상이어야 합니다." }
+        require(height >= RANGE_MIN) { "높이는 1 이상이어야 합니다." }
+        require(width >= RANGE_MIN) { "너비는 1 이상이어야 합니다." }
+    }
+
+    companion object {
+        private const val RANGE_MIN = 1
     }
 }
--- src/test/kotlin/minesweeper/domain/board/BoardRangeTest.kt
+++ src/test/kotlin/minesweeper/domain/board/BoardRangeTest.kt
@@ -16,4 +16,15 @@ class BoardRangeTest {
             BoardRange(height = height, width = width)
         }
     }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1, 0",
+        "1, -1"
+    )
+    fun `보드 범위의 너비가 0 이하면 IllegalArgumentException 이 발생`(height: Int, width: Int) {
+        shouldThrow<IllegalArgumentException> {
+            BoardRange(height = height, width = width)
+        }
+    }
 }
