67f708e1192483e7020312ea555e0df17cfe72ba
genos-lee <genos.lee@kakaocorp.com>
2023-07-01 17:11:52 +0900

    test : BoardRow 빠진 테스트코드 추가

--- src/test/kotlin/next/step/minesweeper/domain/board/BoardRowTest.kt
+++ src/test/kotlin/next/step/minesweeper/domain/board/BoardRowTest.kt
@@ -3,9 +3,11 @@ package next.step.minesweeper.domain.board
 import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
 import next.step.minesweeper.domain.board.state.CoveredState
 import next.step.minesweeper.domain.board.state.MineState
 import next.step.minesweeper.domain.mine.MinePosition
+import org.junit.jupiter.api.assertThrows
 
 class BoardRowTest : DescribeSpec({
 
@@ -31,6 +33,14 @@ class BoardRowTest : DescribeSpec({
                     BoardPoint(MineState),
                     BoardPoint(CoveredState)
                 )
+            }
+        }
+
+        context("지뢰 최대 너비 넘어가게 심으면") {
+            it("예외 발생") {
+                assertThrows<IllegalArgumentException> {
+                    BoardRow.covered(3).plantMine(MinePosition(3, 0))
+                }.shouldHaveMessage("지뢰 x 위치는 3 보다 작아야 합니다.")
             }
         }
 
