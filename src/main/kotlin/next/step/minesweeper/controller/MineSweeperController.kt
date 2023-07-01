a4455d548c78a3b6710ac595e5c6ff9d4bdb0e7a
genos-lee <genos.lee@kakaocorp.com>
2023-07-01 17:21:21 +0900

    refactor : main 변경된 설계에 맞게 수정

--- src/main/kotlin/next/step/minesweeper/controller/MineSweeperController.kt
+++ src/main/kotlin/next/step/minesweeper/controller/MineSweeperController.kt
@@ -2,16 +2,14 @@ package next.step.minesweeper.controller
 
 import next.step.blackjack.view.OutputView
 import next.step.minesweeper.domain.board.Board
-import next.step.minesweeper.domain.mine.MineBoard
 import next.step.minesweeper.domain.mine.generator.RandomMineGenerator
 import next.step.minesweeper.view.InputView
 
 fun main() {
     runCatching {
-        val board = Board(InputView.readHeight(), InputView.readWidth())
-        val minePoints = RandomMineGenerator.generate(board, InputView.readMineCnt())
-        val mineBoard = MineBoard(board, minePoints)
-        OutputView.showMineBoard(mineBoard)
+        val board = Board.covered(InputView.readHeight(), InputView.readWidth())
+        board.plantMines(RandomMineGenerator.generate(board, InputView.readMineCnt()))
+        OutputView.showBoard(board)
     }.onFailure { e ->
         OutputView.showError(e.message)
         main()
