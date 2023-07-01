b19c1d349ffdbbc96f65cada9fdc7d3f91fb6894
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-02 07:03:04 +0900

    refactor: style refactor

--- src/main/kotlin/minesweeper/view/CellView.kt
+++ src/main/kotlin/minesweeper/view/CellView.kt
@@ -11,7 +11,7 @@ enum class CellView(
     ;
 
     companion object {
-        fun from(cellType: CellType): CellView = values().firstOrNull() { it.cellType == cellType }
+        fun from(cellType: CellType): CellView = values().firstOrNull { it.cellType == cellType }
             ?: throw IllegalArgumentException("뷰에서 지원하지 않는 cell 타입입니다.")
     }
 }
