066934e4832e7346ec3753058e4ea29afe5c39d7
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 00:25:02 +0900

    refactor : method 정의 순서 변경

--- src/main/kotlin/next/step/minesweeper/domain/board/BoardHeight.kt
+++ src/main/kotlin/next/step/minesweeper/domain/board/BoardHeight.kt
@@ -2,11 +2,12 @@ package next.step.minesweeper.domain.board
 
 @JvmInline
 value class BoardHeight(private val height: Int) {
-    fun range(): IntRange = MIN_HEIGHT until height
-
+    
     init {
         require(height > MIN_HEIGHT) { "높이는 ${MIN_HEIGHT}보다 커야합니다." }
     }
+
+    fun range(): IntRange = MIN_HEIGHT until height
 
     companion object {
         const val MIN_HEIGHT = 0
--- src/main/kotlin/next/step/minesweeper/domain/board/BoardWidth.kt
+++ src/main/kotlin/next/step/minesweeper/domain/board/BoardWidth.kt
@@ -2,11 +2,12 @@ package next.step.minesweeper.domain.board
 
 @JvmInline
 value class BoardWidth(private val width: Int) {
-    fun range(): IntRange = MIN_WIDTH until width
-
+    
     init {
         require(width > MIN_WIDTH) { "너비는 ${MIN_WIDTH}보다 커야합니다." }
     }
+
+    fun range(): IntRange = MIN_WIDTH until width
 
     companion object {
         const val MIN_WIDTH = 0
