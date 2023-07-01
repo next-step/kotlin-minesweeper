2a342f3b4dab8e804cd56f9a2351e263106f126c
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 02:59:37 +0900

    feature : MineCount 개수 증가 operator override하고 테스트코드 작성

--- src/main/kotlin/next/step/minesweeper/domain/mine/MineCount.kt
+++ src/main/kotlin/next/step/minesweeper/domain/mine/MineCount.kt
@@ -2,6 +2,7 @@ package next.step.minesweeper.domain.mine
 
 @JvmInline
 value class MineCount(val count: Int) {
+    operator fun inc(): MineCount = MineCount(count + 1)
 
     init {
         require(count > MIN_COUNT) { "지뢰 개수는 ${MIN_COUNT}보다 커야합니다." }
--- src/test/kotlin/next/step/minesweeper/domain/mine/MineCountTest.kt
+++ src/test/kotlin/next/step/minesweeper/domain/mine/MineCountTest.kt
@@ -2,6 +2,7 @@ package next.step.minesweeper.domain.mine
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.assertThrows
 
 class MineCountTest : DescribeSpec({
@@ -12,6 +13,15 @@ class MineCountTest : DescribeSpec({
                 listOf(-1, 0)
             ) { cnt ->
                 assertThrows<IllegalArgumentException> { MineCount(cnt) }
+            }
+        }
+        context("++ 시키면") {
+            it("개수가 증가") {
+                var count = MineCount(1)
+
+                count++
+
+                count shouldBe MineCount(2)
             }
         }
     }
