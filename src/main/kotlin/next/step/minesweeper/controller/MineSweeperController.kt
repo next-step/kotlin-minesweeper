8a1db994574ae48e0d0544339c4bbd4c05a58881
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 03:26:56 +0900

    refactor : retry logic util로 분리

--- src/main/kotlin/next/step/minesweeper/controller/MineSweeperController.kt
+++ src/main/kotlin/next/step/minesweeper/controller/MineSweeperController.kt
@@ -3,24 +3,15 @@ package next.step.minesweeper.controller
 import next.step.blackjack.view.OutputView
 import next.step.minesweeper.domain.board.Board
 import next.step.minesweeper.domain.mine.generator.RandomMineGenerator
+import next.step.minesweeper.utils.retryOnFailure
 import next.step.minesweeper.view.InputView
 
-fun main() {
-    runCatching {
-        val board = Board.mineFree(InputView.readHeight(), InputView.readWidth())
-        plantRandomMines(board)
-        OutputView.showBoardPoints(board.points())
-    }.onFailure { e ->
-        OutputView.showError(e.message)
-        main()
-    }
+fun main() = retryOnFailure {
+    val board = Board.mineFree(InputView.readHeight(), InputView.readWidth())
+    plantRandomMines(board)
+    OutputView.showBoardPoints(board.points())
 }
 
-private fun plantRandomMines(board: Board) {
-    runCatching {
-        board.plantMines(RandomMineGenerator.generate(board, InputView.readMineCnt()))
-    }.onFailure { e ->
-        OutputView.showError(e.message)
-        plantRandomMines(board)
-    }
+private fun plantRandomMines(board: Board) = retryOnFailure {
+    board.plantMines(RandomMineGenerator.generate(board, InputView.readMineCnt()))
 }
