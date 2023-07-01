7371cf2d5ad7f357ef451591c8c7d4e40e0bb639
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 03:09:25 +0900

    feature : BoardPoint에 지뢰 없는 상태로 초기화하는 로직 추가하고, 지뢰를 알려주면 상태를 업데이트 하는 기능 추가하고 테스트코드 작성

--- src/main/kotlin/next/step/minesweeper/domain/board/BoardPoint.kt
+++ src/main/kotlin/next/step/minesweeper/domain/board/BoardPoint.kt
@@ -2,7 +2,9 @@ package next.step.minesweeper.domain.board
 
 import next.step.minesweeper.domain.board.state.BoardPointState
 import next.step.minesweeper.domain.board.state.CoveredState
+import next.step.minesweeper.domain.board.state.MineFreeState
 import next.step.minesweeper.domain.board.state.MineState
+import next.step.minesweeper.domain.board.state.NearMineState
 
 data class BoardPoint(private var state: BoardPointState) {
 
@@ -12,7 +14,18 @@ data class BoardPoint(private var state: BoardPointState) {
 
     fun state() = state
 
+    fun notifyMine() {
+        when (state) {
+            MineFreeState -> state = NearMineState.one()
+            is NearMineState -> (state as NearMineState).increase()
+            else -> {}
+        }
+    }
+
     companion object {
-        fun covered() = BoardPoint(CoveredState)
+
+        fun covered(): BoardPoint = BoardPoint(CoveredState)
+
+        fun mineFree(): BoardPoint = BoardPoint(MineFreeState)
     }
 }
--- src/test/kotlin/next/step/minesweeper/domain/board/BoardPointTest.kt
+++ src/test/kotlin/next/step/minesweeper/domain/board/BoardPointTest.kt
@@ -1,21 +1,18 @@
 package next.step.minesweeper.domain.board
 
-import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import next.step.minesweeper.domain.board.state.CoveredState
 import next.step.minesweeper.domain.board.state.MineState
+import next.step.minesweeper.domain.board.state.NearMineState
+import next.step.minesweeper.domain.mine.MineCount
 
 class BoardPointTest : DescribeSpec({
 
     describe("BoardPoint") {
         context("생성") {
             it("covered 상태로 생성") {
-                val point = BoardPoint.covered()
-
-                assertSoftly {
-                    point.state() shouldBe CoveredState
-                }
+                BoardPoint.covered().state() shouldBe CoveredState
             }
         }
 
@@ -25,9 +22,41 @@ class BoardPointTest : DescribeSpec({
 
                 point.plantMine()
 
-                assertSoftly {
-                    point.state() shouldBe MineState
+                point.state() shouldBe MineState
+            }
+        }
+
+        context("주변에 지뢰가 있다고 알림받으면") {
+            it("처음 알림 받은 거면 주변 지뢰 개수 1인 NearMineState로 바뀜") {
+                val point = BoardPoint.mineFree()
+
+                point.notifyMine()
+
+                point.state() shouldBe NearMineState(MineCount(1))
+            }
+            it("이전에도 알림을 받았으면 NearMineState의 지뢰 개수를 증가") {
+                val point = BoardPoint.mineFree()
+
+                repeat(3) {
+                    point.notifyMine()
                 }
+
+                point.state() shouldBe NearMineState(MineCount(3))
+            }
+            it("covered 상태에 대해서는 알림이 와도 반응하지 않음") {
+                val point = BoardPoint.covered()
+
+                point.notifyMine()
+
+                point.state() shouldBe CoveredState
+            }
+            it("지뢰가 심어진 상태에 대해서는 알림이 와도 반응하지 않음") {
+                val point = BoardPoint.covered()
+
+                point.plantMine()
+                point.notifyMine()
+
+                point.state() shouldBe MineState
             }
         }
     }
