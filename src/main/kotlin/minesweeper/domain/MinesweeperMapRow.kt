13dcaf9a66db482b3a2c79e59bfda7c68ab69d04
konempty <jameswebb@naver.com>
2023-07-01 21:58:09 +0900

    refactor: 에러처리 및 미흡한 테스트 추가

--- src/main/kotlin/minesweeper/domain/MinesweeperMapRow.kt
+++ src/main/kotlin/minesweeper/domain/MinesweeperMapRow.kt
@@ -14,7 +14,7 @@ class MinesweeperMapRow(private val mapRow: List<MapElement>) : Iterable<MapElem
     }
 
     fun getNumber(colNumber: Int): Int {
-        return (mapRow[colNumber] as NumberMapElement).value
+        return requireNotNull((mapRow[colNumber] as? NumberMapElement)?.value) { NOT_NUMBER_MAP_ELEMENT_ERROR_MESSAGE }
     }
 
     fun getCoveredNumberMapElementCount(): Int {
@@ -23,6 +23,7 @@ class MinesweeperMapRow(private val mapRow: List<MapElement>) : Iterable<MapElem
 
     companion object {
         private const val INVALID_WIDTH_ERROR_MESSAGE = "너비가 0이하일 수 없습니다"
+        private const val NOT_NUMBER_MAP_ELEMENT_ERROR_MESSAGE = "지뢰가 아닌 MapElement만 지뢰 숫자를 가져올 수 있습니다"
         fun of(rowNumber: Int, width: Int, mineLocations: MineLocations): MinesweeperMapRow {
             require(width > 0) { INVALID_WIDTH_ERROR_MESSAGE }
             val mapRow = List(width) { colNumber -> mineLocations.getMapElement(Point(colNumber, rowNumber)) }
