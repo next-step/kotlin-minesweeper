41ae770ebdb7b8c9a4da8c4a1f049336cca4f617
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-02 04:56:48 +0900

    feat: 지뢰 요청 갯수가 현재 셀보다 많으면 예외 추가 및 테스트

--- src/main/kotlin/minesweeper/MineBoard.kt
+++ src/main/kotlin/minesweeper/MineBoard.kt
@@ -4,7 +4,8 @@ class MineBoard(
     val cells: List<Cell>,
 ) {
     fun placeMine(mineCount: Int) {
-        check(mineCount >= MINIMUM_MINE_COUNT) { "지뢰 갯수는 ${MINIMUM_MINE_COUNT}이상이어야 합니다." }
+        require(mineCount >= MINIMUM_MINE_COUNT) { "지뢰 갯수는 ${MINIMUM_MINE_COUNT}이상이어야 합니다." }
+        require(mineCount < cells.size) { "지뢰 갯수는 현재 cell크기 ${cells.size}보다 작은 값을 입력하여야 합니다." }
         check(cells.none { it.isMine() }) { "이미 지뢰가 배치되어 있습니다." }
     }
 
--- src/test/kotlin/minesweeper/MineBoardTest.kt
+++ src/test/kotlin/minesweeper/MineBoardTest.kt
@@ -10,18 +10,35 @@ class MineBoardTest : FunSpec({
         test("지뢰 갯수가 0보다 작은 경우 예외가 발생한다.") {
             val mineBoard = MineBoard(
                 listOf(
-                    Cell(Row(0), Column(0), CellType.MINE),
+                    Cell(Row(0), Column(0), CellType.NONE),
                 ),
             )
 
-            val exception = shouldThrowExactly<IllegalStateException> { mineBoard.placeMine(0) }
+            val exception = shouldThrowExactly<IllegalArgumentException> { mineBoard.placeMine(0) }
             exception shouldHaveMessage "지뢰 갯수는 1이상이어야 합니다."
+        }
+
+        test("지뢰 갯수가 cell 갯수보다 큰 경우 예외가 발생한다.") {
+            val mineBoard = MineBoard(
+                listOf(
+                    Cell(Row(0), Column(0), CellType.NONE),
+                    Cell(Row(0), Column(1), CellType.NONE),
+                    Cell(Row(1), Column(0), CellType.NONE),
+                    Cell(Row(1), Column(1), CellType.NONE),
+                ),
+            )
+
+            val exception = shouldThrowExactly<IllegalArgumentException> { mineBoard.placeMine(4) }
+            exception shouldHaveMessage "지뢰 갯수는 현재 cell크기 4보다 작은 값을 입력하여야 합니다."
         }
 
         test("이미 지뢰가 배치되어 있는 경우 예외가 발생한다.") {
             val mineBoard = MineBoard(
                 listOf(
                     Cell(Row(0), Column(0), CellType.MINE),
+                    Cell(Row(0), Column(1), CellType.NONE),
+                    Cell(Row(1), Column(0), CellType.NONE),
+                    Cell(Row(1), Column(1), CellType.NONE),
                 ),
             )
 
