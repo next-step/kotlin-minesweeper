5a9c7dc603847886f8f814cfb61f0c7216b0a80e
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 03:01:29 +0900

    feature : 지뢰 주변의 변경분을 가지는 NearMineDeltas 추가

---
+++ src/main/kotlin/next/step/minesweeper/domain/mine/NearMineDeltas.kt
@@ -0,0 +1,16 @@
+package next.step.minesweeper.domain.mine
+
+import next.step.minesweeper.domain.position.Delta
+
+object NearMineDeltas {
+    val deltas: List<Delta> = listOf(
+        Delta(-1, -1),
+        Delta(0, -1),
+        Delta(1, -1),
+        Delta(-1, 0),
+        Delta(1, 0),
+        Delta(-1, 1),
+        Delta(0, 1),
+        Delta(1, 1)
+    )
+}
