362e61654b1f74485608c575d445e6e8bf28ed2d
genos-lee <genos.lee@kakaocorp.com>
2023-07-01 17:21:05 +0900

    refactor : 출력 로직 변경된 설계에 맞게 수정

--- src/main/kotlin/next/step/minesweeper/view/OutputView.kt
+++ src/main/kotlin/next/step/minesweeper/view/OutputView.kt
@@ -1,28 +1,24 @@
 package next.step.blackjack.view
 
-import next.step.minesweeper.domain.mine.MineBoard
-import next.step.minesweeper.domain.position.Position
+import next.step.minesweeper.domain.board.Board
 
 object OutputView {
 
     private const val UNKNOWN_ERR_MSG = "알 수 없는 에러가 발생했습니다."
-    private const val MINE_BOARD_TITLE = "지뢰찾기 게임 시작"
-    private const val MINE_DESC = "*"
-    private const val PLAIN_DESC = "C"
+    private const val BOARD_TITLE = "지뢰찾기 게임 시작"
+
+    fun showBoard(board: Board) {
+        println()
+        println(BOARD_TITLE)
+        board.descs().forEach { printBoardRow(it) }
+    }
+
+    private fun printBoardRow(it: List<String>) {
+        println(it.joinToString(" "))
+    }
 
     fun showError(msg: String?) {
         println(msg ?: UNKNOWN_ERR_MSG)
         println()
     }
-
-    fun showMineBoard(mineBoard: MineBoard) {
-        println()
-        println(MINE_BOARD_TITLE)
-        (Position.base().y until mineBoard.height()).forEach { println(row(mineBoard, it)) }
-    }
-
-    private fun row(mineBoard: MineBoard, y: Int): String =
-        (Position.base().x until mineBoard.width()).joinToString(" ") { x ->
-            if (mineBoard.isMineAt(Position(x, y))) MINE_DESC else PLAIN_DESC
-        }
 }
