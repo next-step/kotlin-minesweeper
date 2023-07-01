bfc9888251190353ba017624889c17c16ee28daf
genos-lee <genos.lee@kakaocorp.com>
2023-07-01 17:18:07 +0900

    feature : Board가 List<BoardRow> 를 가지도록 수정하고 지뢰심는 기능 추가하고 테스트코드 추가 및 수정

--- src/main/kotlin/next/step/minesweeper/domain/board/Board.kt
+++ src/main/kotlin/next/step/minesweeper/domain/board/Board.kt
@@ -1,14 +1,36 @@
 package next.step.minesweeper.domain.board
 
-data class Board(private val height: BoardHeight, private val width: BoardWidth) {
+import next.step.minesweeper.domain.mine.MinePosition
+import next.step.minesweeper.domain.mine.MinePositions
 
-    fun width(): Int = width.width
-
-    fun height(): Int = height.height
+@JvmInline
+value class Board(private val rows: List<BoardRow>) {
 
     fun area(): Int = width() * height()
 
+    fun width(): Int = rows.firstOrNull()?.size() ?: 0
+
+    fun height(): Int = rows.size
+
+    fun plantMines(positions: MinePositions): List<List<BoardPoint>> {
+        require(positions.size <= area()) { "지뢰 개수는 ${area()}개보다 많을 수 없습니다." }
+        return positions.map { plantMine(it) }
+    }
+
+    private fun plantMine(position: MinePosition): List<BoardPoint> {
+        require(position.y < height()) { "지뢰 y 위치는 ${height()} 보다 작아야 합니다." }
+        return rows[position.y].plantMine(position)
+    }
+
+    fun descs(): List<List<String>> = rows.map { it.descs() }
+
+
     companion object {
-        fun of(height: Int, width: Int): Board = Board(BoardHeight(height), BoardWidth(width))
+        const val BASE_Y = 0
+
+        fun covered(height: Int, width: Int): Board = covered(BoardHeight(height), BoardWidth(width))
+
+        fun covered(height: BoardHeight, width: BoardWidth): Board =
+            Board((BASE_Y until height.height).map { BoardRow.covered(width.width) })
     }
 }
