a93383fdffff8da9131fd42eba204531ca277fe1
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-02 06:46:31 +0900

    refactor: mineCount validation 로직 메소드 분리

--- src/main/kotlin/minesweeper/domain/MineBoard.kt
+++ src/main/kotlin/minesweeper/domain/MineBoard.kt
@@ -4,14 +4,17 @@ class MineBoard(
     val cells: List<Cell>,
 ) {
     fun placeMine(mineCount: Int) {
-        require(mineCount >= MINIMUM_MINE_COUNT) { "지뢰 갯수는 ${MINIMUM_MINE_COUNT}이상이어야 합니다." }
-        require(mineCount < cells.size) { "지뢰 갯수는 현재 cell크기 ${cells.size}보다 작은 값을 입력하여야 합니다." }
-        check(cells.none { it.isMine() }) { "이미 지뢰가 배치되어 있습니다." }
-
+        validateMineCount(mineCount)
         repeat(mineCount) { cells.filterNot { it.isMine() }.random().changeToMine() }
     }
 
     fun currentBoard(): CellInfos = CellInfos(height = height(), width = width(), cellInfos = cells.map { CellInfo.from(it) })
+
+    private fun validateMineCount(mineCount: Int) {
+        require(mineCount >= MINIMUM_MINE_COUNT) { "지뢰 갯수는 ${MINIMUM_MINE_COUNT}이상이어야 합니다." }
+        require(mineCount < cells.size) { "지뢰 갯수는 현재 cell크기 ${cells.size}보다 작은 값을 입력하여야 합니다." }
+        check(cells.none { it.isMine() }) { "이미 지뢰가 배치되어 있습니다." }
+    }
 
     private fun height(): Int = cells.maxOf { it.row.value } + 1
 
