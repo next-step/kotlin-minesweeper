15f23ef8ef2c0c40098c4c985e69be43b36f7b7f
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 03:07:50 +0900

    feature : 위치 y가 BoardHeight에 포함되는지 확인하는 기능 추가하고 테스트 코드 작성

--- src/main/kotlin/next/step/minesweeper/domain/board/BoardHeight.kt
+++ src/main/kotlin/next/step/minesweeper/domain/board/BoardHeight.kt
@@ -1,13 +1,17 @@
 package next.step.minesweeper.domain.board
 
+import next.step.minesweeper.domain.position.Position
+
 @JvmInline
 value class BoardHeight(private val height: Int) {
-    
+
     init {
         require(height > MIN_HEIGHT) { "높이는 ${MIN_HEIGHT}보다 커야합니다." }
     }
 
     fun range(): IntRange = MIN_HEIGHT until height
+
+    fun inRange(position: Position) = position.y in range()
 
     companion object {
         const val MIN_HEIGHT = 0
--- src/test/kotlin/next/step/minesweeper/domain/board/BoardHeightTest.kt
+++ src/test/kotlin/next/step/minesweeper/domain/board/BoardHeightTest.kt
@@ -2,6 +2,8 @@ package next.step.minesweeper.domain.board
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+import next.step.minesweeper.domain.position.Position
 import org.junit.jupiter.api.assertThrows
 
 class BoardHeightTest : DescribeSpec({
@@ -12,6 +14,20 @@ class BoardHeightTest : DescribeSpec({
                 listOf(-1, 0)
             ) { height ->
                 assertThrows<IllegalArgumentException> { BoardHeight(height) }
+            }
+        }
+        context("Position의 y가 범위에 없으면, false") {
+            withData(
+                listOf(Position(0, 10), Position(0, -1))
+            ) { position ->
+                BoardHeight(10).inRange(position) shouldBe false
+            }
+        }
+        context("Position의 y가 범위에 있으면, true") {
+            withData(
+                listOf(Position(0, 9), Position(0, 0))
+            ) { position ->
+                BoardHeight(10).inRange(position) shouldBe true
             }
         }
     }
