75eeba4c40abc3ca99f104679cbdbeaaf5dcf4ea
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 03:07:30 +0900

    feature : 위치 x가 BoardWidth에 포함되는지 확인하는 기능 추가하고 테스트 코드 작성

--- src/main/kotlin/next/step/minesweeper/domain/board/BoardWidth.kt
+++ src/main/kotlin/next/step/minesweeper/domain/board/BoardWidth.kt
@@ -1,13 +1,17 @@
 package next.step.minesweeper.domain.board
 
+import next.step.minesweeper.domain.position.Position
+
 @JvmInline
 value class BoardWidth(private val width: Int) {
-    
+
     init {
         require(width > MIN_WIDTH) { "너비는 ${MIN_WIDTH}보다 커야합니다." }
     }
 
     fun range(): IntRange = MIN_WIDTH until width
+
+    fun inRange(position: Position) = position.x in range()
 
     companion object {
         const val MIN_WIDTH = 0
--- src/test/kotlin/next/step/minesweeper/domain/board/BoardWidthTest.kt
+++ src/test/kotlin/next/step/minesweeper/domain/board/BoardWidthTest.kt
@@ -2,6 +2,8 @@ package next.step.minesweeper.domain.board
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+import next.step.minesweeper.domain.position.Position
 import org.junit.jupiter.api.assertThrows
 
 class BoardWidthTest : DescribeSpec({
@@ -12,6 +14,20 @@ class BoardWidthTest : DescribeSpec({
                 listOf(-1, 0)
             ) { width ->
                 assertThrows<IllegalArgumentException> { BoardWidth(width) }
+            }
+        }
+        context("Position의 x가 범위에 없으면, false") {
+            withData(
+                listOf(Position(10, 0), Position(-1, 0))
+            ) { position ->
+                BoardWidth(10).inRange(position) shouldBe false
+            }
+        }
+        context("Position의 x가 범위에 있으면, true") {
+            withData(
+                listOf(Position(9, 0), Position(0, 0))
+            ) { position ->
+                BoardWidth(10).inRange(position) shouldBe true
             }
         }
     }
