9608ae0a1db458641ecdac91821a30d34dcb38dc
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 03:02:46 +0900

    feature : 지뢰 위치로부터 주변 위치를 제공하는 기능을 MinePosition에 추가하고 테스트코드 작성

--- src/main/kotlin/next/step/minesweeper/domain/mine/MinePosition.kt
+++ src/main/kotlin/next/step/minesweeper/domain/mine/MinePosition.kt
@@ -2,8 +2,11 @@ package next.step.minesweeper.domain.mine
 
 import next.step.minesweeper.domain.board.BoardHeight
 import next.step.minesweeper.domain.board.BoardWidth
+import next.step.minesweeper.domain.position.Position
 
 data class MinePosition(val x: Int, val y: Int) {
+    fun nearMinePositions(): List<Position> = NearMineDeltas.deltas.map { Position(x + it.dx, y + it.dy) }
+
     init {
         require(x >= BoardWidth.MIN_WIDTH) { "위치 x는 ${BoardWidth.MIN_WIDTH} 보다 작을 수 없습니다." }
         require(y >= BoardHeight.MIN_HEIGHT) { "위치 y는 ${BoardHeight.MIN_HEIGHT} 보다 작을 수 없습니다." }
