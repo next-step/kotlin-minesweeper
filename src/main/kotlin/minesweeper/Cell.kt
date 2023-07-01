9b607ef2dd3bf6f6b4b718c4a2cf3d3ecdaa9561
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-02 04:28:56 +0900

    feat: row, column 변경

---
+++ src/main/kotlin/minesweeper/Cell.kt
@@ -0,0 +1,6 @@
+package minesweeper
+
+enum class Cell {
+    NONE,
+    MINE,
+}
--- src/test/kotlin/minesweeper/RowTest.kt
+++ src/test/kotlin/minesweeper/RowTest.kt
@@ -4,12 +4,12 @@ import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.throwable.shouldHaveMessage
 
-class WidthTest : FunSpec({
+class RowTest : FunSpec({
 
     context("init") {
         test("너비가 제한값보다 작은 경우 예외가 발생한다.") {
-            val exception = shouldThrowExactly<IllegalArgumentException> { Width(0) }
-            exception shouldHaveMessage "너비는 0보다 커야 합니다."
+            val exception = shouldThrowExactly<IllegalArgumentException> { Row(-1) }
+            exception shouldHaveMessage "행은 0이상 이어야 합니다."
         }
     }
 })
