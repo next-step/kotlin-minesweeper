64b9483788dd584c2dfaacc5e6da22dfb6888d09
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-02 04:53:06 +0900

    feat: 지뢰 요청 갯수가 0이하인 경우 예외 추가 및 테스트

--- src/main/kotlin/minesweeper/MineBoard.kt
+++ src/main/kotlin/minesweeper/MineBoard.kt
@@ -3,7 +3,12 @@ package minesweeper
 class MineBoard(
     val cells: List<Cell>,
 ) {
-    fun placeMine() {
+    fun placeMine(mineCount: Int) {
+        check(mineCount >= MINIMUM_MINE_COUNT) { "지뢰 갯수는 ${MINIMUM_MINE_COUNT}이상이어야 합니다." }
         check(cells.none { it.isMine() }) { "이미 지뢰가 배치되어 있습니다." }
+    }
+
+    companion object {
+        private const val MINIMUM_MINE_COUNT = 1
     }
 }
--- src/test/kotlin/minesweeper/MineBoardTest.kt
+++ src/test/kotlin/minesweeper/MineBoardTest.kt
@@ -2,11 +2,22 @@ package minesweeper
 
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
 
 class MineBoardTest : FunSpec({
 
     context("placeMine") {
+        test("지뢰 갯수가 0보다 작은 경우 예외가 발생한다.") {
+            val mineBoard = MineBoard(
+                listOf(
+                    Cell(Row(0), Column(0), CellType.MINE),
+                ),
+            )
+
+            val exception = shouldThrowExactly<IllegalStateException> { mineBoard.placeMine(0) }
+            exception shouldHaveMessage "지뢰 갯수는 1이상이어야 합니다."
+        }
+
         test("이미 지뢰가 배치되어 있는 경우 예외가 발생한다.") {
             val mineBoard = MineBoard(
                 listOf(
@@ -14,8 +25,8 @@ class MineBoardTest : FunSpec({
                 ),
             )
 
-            val exception = shouldThrowExactly<IllegalStateException> { mineBoard.placeMine() }
-            exception.message shouldBe "이미 지뢰가 배치되어 있습니다."
+            val exception = shouldThrowExactly<IllegalStateException> { mineBoard.placeMine(1) }
+            exception shouldHaveMessage "이미 지뢰가 배치되어 있습니다."
         }
     }
 })
