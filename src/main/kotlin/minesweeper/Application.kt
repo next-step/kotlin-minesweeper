7e2f9fbe40abc6c25afb26a1b89d5f9f6976c57e
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-02 05:40:28 +0900

    feat: 어플리케이션 지뢰갯수 받아 생성

--- src/main/kotlin/minesweeper/Application.kt
+++ src/main/kotlin/minesweeper/Application.kt
@@ -2,8 +2,10 @@ package minesweeper
 
 import minesweeper.domain.MineBoard.Companion.generateNewMineBoard
 import minesweeper.view.inputHeight
+import minesweeper.view.inputMineCount
 import minesweeper.view.inputWidth
 
 fun main() {
     val mineBoard = generateNewMineBoard(height = inputHeight(), width = inputWidth())
+    mineBoard.placeMine(mineCount = inputMineCount())
 }
