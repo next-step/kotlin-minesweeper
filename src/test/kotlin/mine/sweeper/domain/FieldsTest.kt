f53874a4d42bb82233581aea211bdc8f18dbc149
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-07-01 22:40:50 +0900

    feat: 지뢰가 설치될때 8방에 값들에 1을 더해준다.

--- src/test/kotlin/mine/sweeper/domain/FieldsTest.kt
+++ src/test/kotlin/mine/sweeper/domain/FieldsTest.kt
@@ -5,10 +5,8 @@ import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import mine.sweeper.Fixture.createMineMap
-import mine.sweeper.application.MapInitializer
 import mine.sweeper.domain.value.Height
 import mine.sweeper.domain.value.Width
-import mine.sweeper.view.dto.MapSize
 import mine.sweeper.view.dto.Position
 
 class FieldsTest : StringSpec({
