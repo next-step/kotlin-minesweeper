161c4f3cbb4730b709c43669cc21429f0b86f275
neity16 <neity16@daum.net>
2023-07-02 18:58:55 +0900

    [step2] refactor: 최초 입력을 제외한 MineCountNumber 에서 0도 허용할 수 있도록 수정

--- src/main/kotlin/ErrorCode.kt
+++ src/main/kotlin/ErrorCode.kt
@@ -1,4 +1,5 @@
 enum class ErrorCode(val msg: String) {
     NOT_POSITIVE_NUMBER_ERROR("number 값이 양수가 아닙니다"),
-    INVALID_MINE_COUNT_NUMBER_ERROR("지뢰의 수가 영역의 최대 개수를 초과합니다")
+    INVALID_MINE_COUNT_NUMBER_ERROR("지뢰의 수가 영역의 최대 개수를 초과합니다"),
+    INVALID_MINE_COUNT_NUMBER_ZERO_ERROR("지뢰의 수는 0이 될 수 없습니다")
 }
