d74adc5575f6bf361264677f3a3867136dce8463
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 22:03:51 +0900

    feature : position 입력 기능 추가

--- src/main/kotlin/next/step/minesweeper/view/InputView.kt
+++ src/main/kotlin/next/step/minesweeper/view/InputView.kt
@@ -4,12 +4,14 @@ import next.step.blackjack.view.OutputView
 import next.step.minesweeper.domain.board.BoardHeight
 import next.step.minesweeper.domain.board.BoardWidth
 import next.step.minesweeper.domain.mine.MineCount
+import next.step.minesweeper.domain.position.Position
 
 object InputView {
 
     private const val ENTER_HEIGHT = "높이를 입력하세요."
     private const val ENTER_WIDTH = "너비를 입력하세요."
     private const val ENTER_MINE_CNT = "지뢰는 몇 개인가요?"
+    private const val ENTER_POSITION = "open: "
 
     fun readHeight(): BoardHeight = read(ENTER_HEIGHT) { BoardHeight(it.toInt()) }
 
@@ -29,4 +31,9 @@ object InputView {
     fun readWidth(): BoardWidth = read(ENTER_WIDTH) { BoardWidth(it.toInt()) }
 
     fun readMineCnt(): MineCount = read(ENTER_MINE_CNT) { MineCount(it.toInt()) }
+
+    fun readPosition(): Position = read(ENTER_POSITION) { entered ->
+        val tokens = entered.split(",").map { it.trim().toInt() }
+        Position(tokens[0], tokens[1])
+    }
 }
