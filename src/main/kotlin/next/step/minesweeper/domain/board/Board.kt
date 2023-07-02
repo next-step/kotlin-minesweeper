1d540564c55453be30143ba233b44470eb12eaeb
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 22:00:05 +0900

    feature : Board에 play 기능 추가하고, 테스트코드 작성

--- src/main/kotlin/next/step/minesweeper/domain/board/Board.kt
+++ src/main/kotlin/next/step/minesweeper/domain/board/Board.kt
@@ -20,14 +20,50 @@ data class Board(private val rows: List<BoardRow>, val area: BoardArea) {
     }
 
     private fun notifyMine(position: MinePosition) =
-        position.nearMinePositions().filter { it in area }.forEach { pointAt(it).notifyMine() }
+        area.nearForEach(position.x, position.y) { pointAt(it).notifyMine() }
 
-    private fun pointAt(position: Position) = rows[position.y].pointAt(position.x)
+    private fun pointAt(position: Position): BoardPoint = rows[position.y].pointAt(position.x)
 
     fun points(): List<List<BoardPoint>> = rows.map { it.points() }
 
     fun cover() = rows.forEach { it.cover() }
-    
+
+    fun play(
+        selector: () -> Position, selectError: (Throwable) -> Unit,
+        announce: (List<List<BoardPoint>>) -> Unit,
+        success: (List<List<BoardPoint>>) -> Unit,
+        fail: (List<List<BoardPoint>>) -> Unit
+    ) {
+        while (canUncover()) {
+            val position = area.select(selector, selectError)
+            if (uncover(position)) return fail(points())
+            announce(points())
+        }
+        success(points())
+    }
+
+    private fun canUncover(): Boolean = rows.any { it.canUncover() }
+
+    private fun uncover(position: Position): Boolean {
+        val point = pointAt(position)
+        point.uncover()
+        uncoverNear(point, position)
+        return point.isUncoveredMine()
+    }
+
+    private fun uncoverNear(point: BoardPoint, position: Position) {
+        if (point.isUncoveredMineFree())
+            area.nearForEach(position.x, position.y) { uncoverNotMine(it) }
+    }
+
+    private fun uncoverNotMine(position: Position) {
+        val point = pointAt(position)
+        if (point.canUncover()) {
+            point.uncover()
+            uncoverNear(point, position)
+        }
+    }
+
     companion object {
 
         fun mineFree(area: BoardArea): Board =
--- src/test/kotlin/next/step/minesweeper/domain/board/BoardTest.kt
+++ src/test/kotlin/next/step/minesweeper/domain/board/BoardTest.kt
@@ -1,5 +1,6 @@
 package next.step.minesweeper.domain.board
 
+import io.kotest.assertions.fail
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.throwable.shouldHaveMessage
@@ -10,6 +11,7 @@ import next.step.minesweeper.domain.board.state.NearMineState
 import next.step.minesweeper.domain.mine.MineCount
 import next.step.minesweeper.domain.mine.MinePosition
 import next.step.minesweeper.domain.mine.MinePositions
+import next.step.minesweeper.domain.position.Position
 import org.junit.jupiter.api.assertThrows
 
 class BoardTest : DescribeSpec({
@@ -17,16 +19,7 @@ class BoardTest : DescribeSpec({
     describe("Board") {
         context("method") {
             it("지뢰 심기") {
-                val board = Board.mineFree(BoardArea(BoardHeight(3), BoardWidth(3)))
-                
-                board.plantMines(
-                    MinePositions(
-                        setOf(
-                            MinePosition(1, 0), MinePosition(0, 1),
-                            MinePosition(2, 2)
-                        )
-                    )
-                )
+                val board = minePlantedBoard()
 
                 board.points() shouldBe listOf(
                     listOf(
@@ -95,6 +88,124 @@ class BoardTest : DescribeSpec({
                     ),
                 )
             }
+            it("uncover할 수 없으면 게임 success로 바로 종료") {
+                val board = mineFullPlantedBoard()
+
+                board.play({ Position(1, 1) }, {},
+                    { fail("호출 안됨") },
+                    {
+                        it shouldBe listOf(
+                            listOf(
+                                BoardPoint(CoveredState(MineState)),
+                                BoardPoint(CoveredState(MineState)),
+                            ),
+                            listOf(
+                                BoardPoint(CoveredState(MineState)),
+                                BoardPoint(CoveredState(MineState)),
+                            ),
+                        )
+                    },
+                    { fail("호출 안됨") })
+            }
+            it("지뢰찾기 플레이 중 지뢰만 남기고 uncover하면 success로 종료됨") {
+                val board = mineCoveredBoard()
+                val selects = mutableListOf(
+                    Position(0, 0),
+                    Position(0, 2),
+                    Position(1, 1),
+                    Position(1, 2),
+                    Position(2, 0),
+                    Position(2, 1)
+                )
+
+                board.play({ selects.removeAt(0) },
+                    { }, {},
+                    {
+                        it shouldBe listOf(
+                            listOf(
+                                BoardPoint(NearMineState(MineCount(2))),
+                                BoardPoint(CoveredState(MineState)),
+                                BoardPoint(NearMineState(MineCount(1)))
+                            ),
+                            listOf(
+                                BoardPoint(CoveredState(MineState)),
+                                BoardPoint(NearMineState(MineCount(3))),
+                                BoardPoint(NearMineState(MineCount(2)))
+                            ),
+                            listOf(
+                                BoardPoint(NearMineState(MineCount(1))),
+                                BoardPoint(NearMineState(MineCount(2))),
+                                BoardPoint(CoveredState(MineState)),
+                            ),
+                        )
+                    },
+                    { fail("호출 안됨") })
+            }
+            it("지뢰찾기 플레이 중 지뢰를 uncover하면 fail로 종료됨") {
+                val board = mineCoveredBoard()
+                val selects = mutableListOf(
+                    Position(2, 2)
+                )
+
+                board.play({ selects.removeAt(0) }, { }, { },
+                    { fail("호출 안됨") },
+                    {
+                        it shouldBe listOf(
+                            listOf(
+                                BoardPoint(CoveredState(NearMineState(MineCount(2)))),
+                                BoardPoint(CoveredState(MineState)),
+                                BoardPoint(CoveredState(NearMineState(MineCount(1))))
+                            ),
+                            listOf(
+                                BoardPoint(CoveredState(MineState)),
+                                BoardPoint(CoveredState(NearMineState(MineCount(3)))),
+                                BoardPoint(CoveredState(NearMineState(MineCount(2))))
+                            ),
+                            listOf(
+                                BoardPoint(CoveredState(NearMineState(MineCount(1)))),
+                                BoardPoint(CoveredState(NearMineState(MineCount(2)))),
+                                BoardPoint(MineState),
+                            ),
+                        )
+                    }
+                )
+            }
         }
+
     }
 })
+
+private fun minePlantedBoard(): Board {
+    val board = Board.mineFree(BoardArea(BoardHeight(3), BoardWidth(3)))
+
+    board.plantMines(
+        MinePositions(
+            setOf(
+                MinePosition(1, 0), MinePosition(0, 1),
+                MinePosition(2, 2)
+            )
+        )
+    )
+    return board
+}
+
+private fun mineCoveredBoard(): Board {
+    val board = minePlantedBoard()
+    board.cover()
+    return board
+}
+
+private fun mineFullPlantedBoard(): Board {
+    val board = Board.mineFree(BoardArea(BoardHeight(2), BoardWidth(2)))
+
+    board.plantMines(
+        MinePositions(
+            setOf(
+                MinePosition(1, 0), MinePosition(0, 1),
+                MinePosition(0, 0), MinePosition(1, 1),
+            )
+        )
+    )
+    board.cover()
+    return board
+}
