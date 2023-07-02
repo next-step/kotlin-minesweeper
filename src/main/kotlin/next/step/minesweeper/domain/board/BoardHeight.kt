a4896b5c13eeeea5defb6d59d0f88c74bedc6a99
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 18:02:29 +0900

    feature : BoardHeight에 범위 포함 여부 require 구문 추가, rangeMap 기능 추가하고 테스트코드 작성

--- src/main/kotlin/next/step/minesweeper/domain/board/BoardHeight.kt
+++ src/main/kotlin/next/step/minesweeper/domain/board/BoardHeight.kt
@@ -7,9 +7,13 @@ value class BoardHeight(private val height: Int) {
         require(height > MIN_HEIGHT) { "높이는 ${MIN_HEIGHT}보다 커야합니다." }
     }
 
+    fun requireInRange(y: Int) = require(inRange(y)) { "y 위치는 ${MIN_HEIGHT}보다 크고, $height 보다 작아야 합니다." }
+
     fun inRange(y: Int) = y in range()
 
-    fun range(): IntRange = MIN_HEIGHT until height
+    private fun range(): IntRange = MIN_HEIGHT until height
+
+    fun <T> rangeMap(transform: (Int) -> T): List<T> = range().map { transform(it) }
 
     fun height(): Int = height
 
--- src/test/kotlin/next/step/minesweeper/domain/board/BoardHeightTest.kt
+++ src/test/kotlin/next/step/minesweeper/domain/board/BoardHeightTest.kt
@@ -3,6 +3,7 @@ package next.step.minesweeper.domain.board
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
 import org.junit.jupiter.api.assertThrows
 
 class BoardHeightTest : DescribeSpec({
@@ -13,6 +14,15 @@ class BoardHeightTest : DescribeSpec({
                 listOf(-1, 0)
             ) { height ->
                 assertThrows<IllegalArgumentException> { BoardHeight(height) }
+            }
+        }
+        context("Position의 y가 범위에 없으면, 예외발생") {
+            withData(
+                listOf(10, -1)
+            ) { y ->
+                assertThrows<IllegalArgumentException> {
+                    BoardHeight(10).requireInRange(y)
+                }.shouldHaveMessage("y 위치는 0보다 크고, 10 보다 작아야 합니다.")
             }
         }
         context("Position의 y가 범위에 없으면, false") {
@@ -31,11 +41,11 @@ class BoardHeightTest : DescribeSpec({
         }
         context("method") {
             val height = BoardHeight(10)
-            it("range 제공") {
-                height.range() shouldBe (0 until 10)
-            }
             it("height 제공") {
                 height.height() shouldBe 10
+            }
+            it("rangeMap으로 변환 제공") {
+                height.rangeMap { it + 3 } shouldBe listOf(3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
             }
         }
     }
