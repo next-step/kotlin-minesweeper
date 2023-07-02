7b5351b1e1065e6f854ac786e5e01762de97785d
genos-lee <genos.lee@kakaocorp.com>
2023-07-02 18:15:17 +0900

    refactor : BoardArea를 먼저 만들도록 main 수정

--- src/main/kotlin/next/step/minesweeper/controller/MineSweeperController.kt
+++ src/main/kotlin/next/step/minesweeper/controller/MineSweeperController.kt
@@ -2,16 +2,18 @@ package next.step.minesweeper.controller
 
 import next.step.blackjack.view.OutputView
 import next.step.minesweeper.domain.board.Board
+import next.step.minesweeper.domain.board.BoardArea
 import next.step.minesweeper.domain.mine.generator.RandomMineGenerator
 import next.step.minesweeper.utils.retryOnFailure
 import next.step.minesweeper.view.InputView
 
 fun main() = retryOnFailure {
-    val board = Board.mineFree(InputView.readHeight(), InputView.readWidth())
+    val boardArea = BoardArea(InputView.readHeight(), InputView.readWidth())
+    val board = Board.mineFree(boardArea)
     plantRandomMines(board)
     OutputView.showBoardPoints(board.points())
 }
 
 private fun plantRandomMines(board: Board) = retryOnFailure {
-    board.plantMines(RandomMineGenerator.generate(board, InputView.readMineCnt()))
+    board.plantMines(RandomMineGenerator.generate(board.area, InputView.readMineCnt()))
 }
