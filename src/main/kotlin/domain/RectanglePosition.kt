d16ab71d7f41ebd2d13b3824d15de7ad601ff351
neity16 <neity16@daum.net>
2023-07-02 19:57:39 +0900

    [step2] refactor: reactangle position 별도 enum으로 분리

---
+++ src/main/kotlin/domain/RectanglePosition.kt
@@ -0,0 +1,12 @@
+package domain
+
+enum class RectanglePosition(val row: Int, val column: Int) {
+    LEFT_TOP(-1, -1),
+    TOP(-1, 0),
+    RIGHT_TOP(-1, 1),
+    LEFT(0, -1),
+    RIGHT(0, 1),
+    LEFT_BOTTM(1, -1),
+    BOTTOM(1, 0),
+    RIGHT_BOTTOM(1, 1),
+}
