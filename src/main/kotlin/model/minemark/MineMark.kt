ddd46e873987cd6418d72554dc6a9aacb4330b44
devyonghee <devyonghee@gmail.com>
2023-07-03 21:46:23 +0900

    feat(MineMark): 인터페이스 분리

--- src/main/kotlin/model/minemark/MineMark.kt
+++ src/main/kotlin/model/minemark/MineMark.kt
@@ -1,11 +1,27 @@
 package model.minemark
 
-sealed class MineMark
+sealed interface MineMark {
+    fun next(count: Int): MineMark
+}
 
-object Safety : MineMark()
-object Mine : MineMark()
-data class MineCount(val count: Int) : MineMark() {
+object Safety : MineMark {
+    override fun next(count: Int): MineMark {
+        return MineCount(count)
+    }
+}
+
+object Mine : MineMark {
+    override fun next(count: Int): MineMark {
+        throw IllegalStateException("mine can not be next mark")
+    }
+}
+
+data class MineCount(val count: Int) : MineMark {
     init {
         require(count >= 0) { "count must be greater than or equal to 0. but provided `$count`" }
+    }
+
+    override fun next(count: Int): MineMark {
+        throw IllegalStateException("mine count can not be next mark")
     }
 }
