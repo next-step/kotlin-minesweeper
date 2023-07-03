79f5682eea49162c05b0e5ff69d5b87030acd519
genos-lee <genos.lee@kakaocorp.com>
2023-07-04 00:25:27 +0900

    refactor : BoardRow에서도 cover 기능 제거

--- src/main/kotlin/next/step/minesweeper/domain/board/BoardRow.kt
+++ src/main/kotlin/next/step/minesweeper/domain/board/BoardRow.kt
@@ -3,18 +3,16 @@ package next.step.minesweeper.domain.board
 @JvmInline
 value class BoardRow(private val points: List<BoardPoint>) {
 
+    fun pointAt(x: Int): BoardPoint = points[x]
+
+    fun points() = points.toList()
+
+    fun canUncover(): Boolean = points.any { it.canUncover() }
+
     fun plantMine(x: Int) {
         require(x < size()) { "지뢰 x 위치는 ${size()} 보다 작아야 합니다." }
         points[x].plantMine()
     }
 
     private fun size() = points.size
-
-    fun pointAt(x: Int): BoardPoint = points[x]
-
-    fun points() = points.toList()
-
-    fun cover() = points.forEach { it.cover() }
-
-    fun canUncover(): Boolean = points.any { it.canUncover() }
 }
--- src/test/kotlin/next/step/minesweeper/domain/board/BoardRowTest.kt
+++ src/test/kotlin/next/step/minesweeper/domain/board/BoardRowTest.kt
@@ -14,14 +14,14 @@ class BoardRowTest : DescribeSpec({
         context("지뢰 심으면") {
             it("특정 위치만 MineState로 바뀜") {
                 val boardRow =
-                    BoardRow(listOf(BoardPoint(MineFreeState), BoardPoint(MineFreeState), BoardPoint(MineFreeState)))
+                    BoardRow(listOf(BoardPoint.mineFree(), BoardPoint.mineFree(), BoardPoint.mineFree()))
 
                 boardRow.plantMine(1)
 
                 boardRow.points() shouldBe listOf(
-                    BoardPoint(MineFreeState),
-                    BoardPoint(MineState),
-                    BoardPoint(MineFreeState),
+                    BoardPoint(CoveredState(MineFreeState)),
+                    BoardPoint(CoveredState(MineState)),
+                    BoardPoint(CoveredState(MineFreeState)),
                 )
             }
         }
@@ -29,26 +29,11 @@ class BoardRowTest : DescribeSpec({
         context("지뢰 최대 너비 넘어가게 심으면") {
             it("예외 발생") {
                 val boardRow =
-                    BoardRow(listOf(BoardPoint(MineFreeState), BoardPoint(MineFreeState), BoardPoint(MineFreeState)))
+                    BoardRow(listOf(BoardPoint.mineFree(), BoardPoint.mineFree(), BoardPoint.mineFree()))
 
                 assertThrows<IllegalArgumentException> {
                     boardRow.plantMine(3)
                 }.shouldHaveMessage("지뢰 x 위치는 3 보다 작아야 합니다.")
-            }
-        }
-
-        context("cover") {
-            it("Point가 모두 CoveredState가 됨") {
-                val boardRow =
-                    BoardRow(listOf(BoardPoint(MineFreeState), BoardPoint(MineFreeState), BoardPoint(MineFreeState)))
-
-                boardRow.cover()
-
-                boardRow.points() shouldBe listOf(
-                    BoardPoint(CoveredState(MineFreeState)),
-                    BoardPoint(CoveredState(MineFreeState)),
-                    BoardPoint(CoveredState(MineFreeState)),
-                )
             }
         }
 
