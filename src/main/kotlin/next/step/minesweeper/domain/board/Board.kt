9e25edf7d48b98c51d4d59f4b33e20a6828c674c
genos-lee <genos.lee@kakaocorp.com>
2023-07-04 01:51:55 +0900

    refactor : BoardRows 일급 컬렉션으로 분리

--- src/main/kotlin/next/step/minesweeper/domain/board/Board.kt
+++ src/main/kotlin/next/step/minesweeper/domain/board/Board.kt
@@ -5,7 +5,7 @@ import next.step.minesweeper.domain.mine.generator.MineGenerator
 import next.step.minesweeper.domain.position.Position
 import next.step.minesweeper.utils.retryOnFailure
 
-data class Board(private val rows: List<BoardRow>, val area: BoardArea) {
+data class Board(private val rows: BoardRows, val area: BoardArea) {
 
     fun play(
         selector: () -> Position,
@@ -14,60 +14,26 @@ data class Board(private val rows: List<BoardRow>, val area: BoardArea) {
         onLose: (List<List<BoardPoint>>) -> Unit,
         onFailure: (Throwable) -> Unit,
     ) {
-        while (canUncover()) {
+        while (rows.canUncover()) {
             val position = retryOnFailure({ area.select(selector) }, onFailure)
-            if (uncover(position)) return onLose(points())
+            if (rows.uncover(position)) return onLose(points())
             onEachStep(points())
         }
         onWin(points())
     }
 
-    private fun canUncover(): Boolean = rows.any { it.canUncover() }
-
-    private fun uncover(position: BoardPosition): Boolean {
-        val point = pointAt(position)
-        point.uncover()
-        uncoverNear(point, position)
-        return point.isUncoveredMine()
-    }
-
-    private fun pointAt(position: BoardPosition): BoardPoint = rows[position.y()].pointAt(position.x())
-
-    private fun uncoverNear(point: BoardPoint, position: BoardPosition) {
-        if (point.isUncoveredMineFree()) {
-            position.near().forEach { uncoverNotMine(it) }
-        }
-    }
-
-    private fun uncoverNotMine(position: BoardPosition) {
-        val point = pointAt(position)
-        if (point.canUncover()) {
-            point.uncover()
-            uncoverNear(point, position)
-        }
-    }
-
-    fun points(): List<List<BoardPoint>> = rows.map { it.points() }
+    fun points(): List<List<BoardPoint>> = rows.points()
 
     companion object {
 
         fun of(area: BoardArea, mineGenerator: MineGenerator, mineCount: MineCount): Board {
             area.checkMaxCount(mineCount.count())
-            val rows = area.rangeMap({ BoardRow(it) }) { _, _ -> BoardPoint.mineFree() }
+            val rows = BoardRows(area.rangeMap({ BoardRow(it) }) { _, _ -> BoardPoint.mineFree() })
             mineGenerator.generate(area, mineCount).forEach { mine ->
-                plantMine(rows, mine)
-                notifyMine(rows, mine)
+                rows.plantMine(mine)
+                rows.notifyMine(mine)
             }
             return Board(rows, area)
-        }
-
-        private fun plantMine(rows: List<BoardRow>, mine: BoardPosition) = rows[mine.y()].plantMine(mine.x())
-
-        private fun notifyMine(
-            rows: List<BoardRow>,
-            mine: BoardPosition,
-        ) {
-            mine.near().forEach { rows[it.y()].pointAt(it.x()).notifyMine() }
         }
     }
 }
