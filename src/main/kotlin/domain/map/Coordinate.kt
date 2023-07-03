708e70a623c557d292c10dcc376e4d9b742538a6
chad.yoon <chad.yoon@kakaocorp.com>
2023-07-03 18:59:39 +0900

    feat: 특정 좌표의 주변 좌표를 계산하는 기능 구현

--- src/main/kotlin/domain/map/Coordinate.kt
+++ src/main/kotlin/domain/map/Coordinate.kt
@@ -10,4 +10,41 @@ data class Coordinate(
             "coordinate must be greater than or equal to zero. your input : x - $x, y - $y"
         }
     }
+
+    fun aroundCoordinates(): Set<Coordinate> {
+        val left = leftOrNull()
+        val leftTop = left?.topOrNull()
+        val top = topOrNull()
+        val rightTop = top?.rightOrNull()
+        val right = rightOrNull()
+        val rightBottom = right?.bottomOrNull()
+        val bottom = bottomOrNull()
+        val leftBottom = bottom?.leftOrNull()
+        return setOfNotNull(
+            left,
+            leftTop,
+            top,
+            rightTop,
+            right,
+            rightBottom,
+            bottom,
+            leftBottom
+        )
+    }
+
+    private fun leftOrNull(): Coordinate? {
+        return runCatching { copy(x = x - 1) }.getOrNull()
+    }
+
+    private fun rightOrNull(): Coordinate? {
+        return runCatching { copy(x = x + 1) }.getOrNull()
+    }
+
+    private fun topOrNull(): Coordinate? {
+        return runCatching { copy(y = y - 1) }.getOrNull()
+    }
+
+    private fun bottomOrNull(): Coordinate? {
+        return runCatching { copy(y = y + 1) }.getOrNull()
+    }
 }
