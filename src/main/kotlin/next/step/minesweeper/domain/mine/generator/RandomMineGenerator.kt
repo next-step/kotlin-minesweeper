19c7a3c770ac68df8bf3538d9af876bf7e03f70c
genos-lee <genos.lee@kakaocorp.com>
2023-07-04 00:21:16 +0900

    refactor : RandomMineGenerator에서는 validation은 제거하고 테스트코드도 제거
    BoardPosition 자체적으로 특정 위치에 생성되게 만들어져서 RandomMineGenerator에서는 보드 위치를 벗어나거나 더 많은 개수를 만들 수 없음

--- src/main/kotlin/next/step/minesweeper/domain/mine/generator/RandomMineGenerator.kt
+++ src/main/kotlin/next/step/minesweeper/domain/mine/generator/RandomMineGenerator.kt
@@ -1,16 +1,19 @@
 package next.step.minesweeper.domain.mine.generator
 
 import next.step.minesweeper.domain.board.BoardArea
+import next.step.minesweeper.domain.board.BoardPosition
+import next.step.minesweeper.domain.board.BoardPositions
 import next.step.minesweeper.domain.mine.MineCount
-import next.step.minesweeper.domain.mine.MinePosition
-import next.step.minesweeper.domain.mine.MinePositions
+import next.step.minesweeper.domain.position.Position
 
 object RandomMineGenerator : MineGenerator {
 
-    override fun generate(area: BoardArea, count: MineCount): MinePositions {
-        area.checkMinePlantable(count.count())
-        return MinePositions(
-            area.rangeMap({ it }) { x, y -> MinePosition(x, y) }.flatten().shuffled().take(count.count()).toSet(),
+    override fun generate(area: BoardArea, count: MineCount): BoardPositions =
+        BoardPositions(
+            area.rangeMap({ it }) { x, y -> BoardPosition(Position(x, y), area) }
+                .flatten()
+                .shuffled()
+                .take(count.count())
+                .toSet(),
         )
-    }
 }
