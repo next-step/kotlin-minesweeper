bb6cd21a52dc5abf655a1782381d7ffb9de9ade5
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-04 19:44:58 +0900

    refactor: 타입 명시

--- src/main/kotlin/minesweeper/domain/strategy/RandomMinePlacementStrategy.kt
+++ src/main/kotlin/minesweeper/domain/strategy/RandomMinePlacementStrategy.kt
@@ -3,5 +3,5 @@ package minesweeper.domain.strategy
 import minesweeper.domain.cell.Cell
 
 class RandomMinePlacementStrategy : MinePlacementStrategy {
-    override fun findPlantTargetCell(cells: List<Cell>) = cells.filterNot { it.isMine() }.random()
+    override fun findPlantTargetCell(cells: List<Cell>): Cell = cells.filterNot { it.isMine() }.random()
 }
