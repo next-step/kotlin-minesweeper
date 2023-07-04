e5e9313b1a93da8d85475c52b635284ea4554278
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-05 02:08:48 +0900

    refactor: 공백 제거

--- src/main/kotlin/minesweeper/domain/CoordinateFinder.kt
+++ src/main/kotlin/minesweeper/domain/CoordinateFinder.kt
@@ -7,21 +7,13 @@ enum class CoordinateFinder(
     val find: (Coordinate) -> Coordinate,
 ) {
     SOUTH({ it.down() }),
-
     SOUTH_WEST({ it.down().left() }),
-
     WEST({ it.left() }),
-
     NORTH_WEST({ it.up().left() }),
-
     NORTH({ it.up() }),
-
     NORTH_SOUTH({ it.up().right() }),
-
     EAST({ it.right() }),
-
     SOUTH_EAST({ it.down().right() }),
-
     ;
 
     companion object {
