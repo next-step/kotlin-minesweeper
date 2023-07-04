7bee3cb4b4385805821db7293004fffdcbe1d4e9
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-05 00:38:37 +0900

    feat: cell type 변환 기능 구현 및 테스트

--- src/main/kotlin/minesweeper/domain/cell/CellType.kt
+++ src/main/kotlin/minesweeper/domain/cell/CellType.kt
@@ -1,6 +1,34 @@
 package minesweeper.domain.cell
 
+import java.lang.IllegalArgumentException
+
 enum class CellType {
     NONE,
+    ZERO,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
     MINE,
+    ;
+
+    companion object {
+        fun parseToCellType(value: Int): CellType =
+            when (value) {
+                0 -> ZERO
+                1 -> ONE
+                2 -> TWO
+                3 -> THREE
+                4 -> FOUR
+                5 -> FIVE
+                6 -> SIX
+                7 -> SEVEN
+                8 -> EIGHT
+                else -> throw IllegalArgumentException("지원하지 않는 셀 타입입니다.")
+            }
+    }
 }
---
+++ src/test/kotlin/minesweeper/domain/cell/CellTypeTest.kt
@@ -0,0 +1,45 @@
+package minesweeper.domain.cell
+
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
+import minesweeper.domain.cell.CellType.Companion.parseToCellType
+import minesweeper.domain.cell.CellType.EIGHT
+import minesweeper.domain.cell.CellType.FIVE
+import minesweeper.domain.cell.CellType.FOUR
+import minesweeper.domain.cell.CellType.ONE
+import minesweeper.domain.cell.CellType.SEVEN
+import minesweeper.domain.cell.CellType.SIX
+import minesweeper.domain.cell.CellType.THREE
+import minesweeper.domain.cell.CellType.TWO
+import minesweeper.domain.cell.CellType.ZERO
+
+class CellTypeTest : FunSpec({
+
+    context("parseToCellType") {
+        test("지원하지 않는 셀값이 입력된 경우 예외가 발생한다.") {
+            val exception = shouldThrowExactly<IllegalArgumentException> { CellType.parseToCellType(9) }
+            exception shouldHaveMessage "지원하지 않는 셀 타입입니다."
+        }
+
+        forAll(
+            row(0, ZERO),
+            row(1, ONE),
+            row(2, TWO),
+            row(3, THREE),
+            row(4, FOUR),
+            row(5, FIVE),
+            row(6, SIX),
+            row(7, SEVEN),
+            row(8, EIGHT),
+        ) { input, expected ->
+            test("${input}은 ${expected}로 변환된다.") {
+                val actual = parseToCellType(input)
+                actual shouldBe expected
+            }
+        }
+    }
+})
