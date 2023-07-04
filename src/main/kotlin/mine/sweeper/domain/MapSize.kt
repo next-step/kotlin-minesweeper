9a22b6ee5af1e1dc6d1496bf77368fdd3910ea37
박세준 <sjpark@selab.co.kr>
2023-07-04 15:34:56 +0900

    feat: MapSize의 필드들을 게터가 아닌 바로 접근 가능하도록 변경

--- src/main/kotlin/mine/sweeper/domain/MapSize.kt
+++ src/main/kotlin/mine/sweeper/domain/MapSize.kt
@@ -4,17 +4,9 @@ import mine.sweeper.domain.value.Height
 import mine.sweeper.domain.value.Width
 
 class MapSize(
-    private val height: Height,
-    private val width: Width
+    val height: Height,
+    val width: Width,
 ) {
-    fun height(): Int {
-        return height.value
-    }
-
-    fun width(): Int {
-        return width.value
-    }
-
     fun area(): Int {
         return height.value * width.value
     }
