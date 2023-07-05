af429544bf89bd40e5acfff23217033feb4754d1
박세준 <sjpark@selab.co.kr>
2023-07-05 18:34:12 +0900

    feat: 위치의 조회에 따라 출력값이 달라지면서, 게임의 승패를 출력한다.

--- src/main/kotlin/mine/sweeper/view/OutputView.kt
+++ src/main/kotlin/mine/sweeper/view/OutputView.kt
@@ -1,11 +1,22 @@
 package mine.sweeper.view
 
-import mine.sweeper.field.domain.Field
+import mine.sweeper.application.value.GameStatus
+import mine.sweeper.domain.Field
+import mine.sweeper.domain.MineField
+import mine.sweeper.domain.SafeField
+import mine.sweeper.view.dto.Fields
 
 object OutputView {
-    fun printMap(fields: List<Field>) {
+    fun noticeGameStart() {
         println("지뢰찾기 게임 시작")
-        val sortedFields = fields.sortedWith(compareBy({ it.position.x }, { it.position.y }))
+    }
+
+    fun noticeWrongPosition() {
+        println("다시 입력해주세요.")
+    }
+
+    fun printMap(fields: Fields) {
+        val sortedFields = fields.fields.sortedWith(compareBy({ it.position.x }, { it.position.y }))
 
         var currentRow = sortedFields.first().position.x
         for (field in sortedFields) {
@@ -13,8 +24,24 @@ object OutputView {
                 println()
                 currentRow = field.position.x
             }
-            print("${field.value} ")
+            printFieldStatus(field)
         }
         println()
+    }
+
+    private fun printFieldStatus(field: Field) {
+        when {
+            !field.checked -> print("C ")
+            field is SafeField -> print("${field.value} ")
+            field is MineField -> print("* ")
+        }
+    }
+
+    fun noticeGameResult(status: GameStatus) {
+        if (status == GameStatus.COMPLETE) {
+            println("Win Game.")
+        } else if (status == GameStatus.GAME_OVER) {
+            println("Lose Game.")
+        }
     }
 }
