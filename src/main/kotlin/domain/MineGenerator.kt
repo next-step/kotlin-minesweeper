465c30cd8f5b992f6fd535746594ea4c8cede86a
celine.kk <celine.kk@kakaocorp.com>
2023-07-05 21:17:09 +0900

    feat. 리뷰 반영: object 클래스로 변경

--- src/main/kotlin/domain/MineGenerator.kt
+++ src/main/kotlin/domain/MineGenerator.kt
@@ -2,31 +2,29 @@ package domain
 
 import kotlin.random.Random
 
-class MineGenerator {
-    companion object {
-        private val lists: MutableList<Location> = mutableListOf()
-
-        fun create(height: Int, width: Int, count: Int): List<Location> {
-            for (i in 1..count) {
-                lists.add(getNewLocation(height, width))
-            }
-            return lists
+object MineGenerator {
+    private val lists: MutableList<Location> = mutableListOf()
+    fun create(height: Int, width: Int, count: Int): List<Location> {
+        for (i in 1..count) {
+            lists.add(getNewLocation(height, width))
         }
-
-        private fun getNewLocation(height: Int, width: Int): Location {
-            var location = Location(getRandomHeight(height), getRandomWidth(width))
-            while (lists.contains(location)) {
-                location = Location(getRandomHeight(height), getRandomWidth(width))
-            }
-            return location
-        }
-
-        private fun getRandomWidth(width: Int): Int {
-            return Random.nextInt(width)
-        }
-
-        private fun getRandomHeight(height: Int): Int {
-            return Random.nextInt(height)
-        }
+        return lists
     }
+
+    private fun getNewLocation(height: Int, width: Int): Location {
+        var location = Location(getRandomHeight(height), getRandomWidth(width))
+        while (lists.contains(location)) {
+            location = Location(getRandomHeight(height), getRandomWidth(width))
+        }
+        return location
+    }
+
+    private fun getRandomWidth(width: Int): Int {
+        return Random.nextInt(width)
+    }
+
+    private fun getRandomHeight(height: Int): Int {
+        return Random.nextInt(height)
+    }
+
 }
