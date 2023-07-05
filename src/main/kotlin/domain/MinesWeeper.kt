61f2f28f94016083ee5b5e56fa11425b766aa0f0
celine.kk <celine.kk@kakaocorp.com>
2023-07-05 21:10:14 +0900

    feat. 리뷰 반영: 지뢰수에 대한 유효성 검사추가

--- src/main/kotlin/domain/MinesWeeper.kt
+++ src/main/kotlin/domain/MinesWeeper.kt
@@ -4,6 +4,8 @@ class MinesWeeper(val boards: List<Cell>) {
     companion object {
         fun of(height: Int, width: Int, count: Int): MinesWeeper {
 
+            require((height * width) > count) { COUNT_EXCEPTION }
+
             val mines = MineGenerator.create(height, width, count)
             val boards = (LOCATION_START_NUM until height)
                 .flatMap { y ->
@@ -16,6 +18,7 @@ class MinesWeeper(val boards: List<Cell>) {
             return MinesWeeper(boards)
         }
 
+        private const val COUNT_EXCEPTION = "지뢰수가 지뢰찾기게임의 칸수보다 적어야합니다."
         private const val LOCATION_START_NUM = 0
     }
 }
--- src/test/kotlin/domain/MinesWeeperTest.kt
+++ src/test/kotlin/domain/MinesWeeperTest.kt
@@ -8,6 +8,17 @@ import java.lang.IllegalArgumentException
 internal class MinesWeeperTest {
 
     @Test
+    internal fun `지뢰의 수는 지뢰찾기 게임의 칸수보다 적어야합니다`() {
+        val height = 2
+        val width = 2
+        val count = 4
+
+        shouldThrow<IllegalArgumentException> {
+            MinesWeeper.of(height, width, count)
+        }
+    }
+
+    @Test
     internal fun `지뢰찾기는 입력한 높이와 너비의 모양으로 생성된다`() {
         val height = 10
         val width = 5
