1e8359e701239d0eff93e36224b098d9c6fa712c
박세준 <sjpark@selab.co.kr>
2023-07-06 15:10:15 +0900

    refactor: MineSweeperGame 에서 상태를 관리하고 맵을 실제로 확인하는 역할도 하고 있었음
     - 이를 Game에서는 값 실행과 게임 상태 관리
     - 맵에서 실제로 맵을 오픈하는 역할을 가지고 해줌

--- src/main/kotlin/mine/sweeper/application/MineSweeperGame.kt
+++ src/main/kotlin/mine/sweeper/application/MineSweeperGame.kt
@@ -1,44 +1,22 @@
 package mine.sweeper.application
 
-import mine.sweeper.application.value.GameStatus
-import mine.sweeper.application.value.MineCount
-import mine.sweeper.domain.Vulture
-import mine.sweeper.view.dto.Fields
+import mine.sweeper.application.value.GameStatus.ON_PROGRESS
+import mine.sweeper.domain.Fields
 import mine.sweeper.view.dto.Position
 
-class MineSweeperGame(
-    private val fieldsManager: FieldsManager,
-    private val vulture: Vulture,
-) {
-    private var totalMine: Int = 0
-    fun setMine(mineCount: MineCount) {
-        this.totalMine = mineCount.value
-        val minePositions = vulture.findMinesPosition(mineCount)
-        fieldsManager.create(minePositions)
+class MineSweeperGame(private val map: MineSweeperMap) {
+    var status = ON_PROGRESS
+
+    fun open(position: Position) {
+        val field = map.findUncheckOrNull(position) ?: return
+        status = map.open(position, field)
     }
 
-    fun open(position: Position): GameStatus {
-        val field = fieldsManager.findOrNull(position) ?: return GameStatus.RE_TRY
-        val status = fieldsManager.open(field)
-        return determineGameStatus(status, position)
+    fun onProgress(): Boolean {
+        return status === ON_PROGRESS
     }
 
     fun getResult(): Fields {
-        return fieldsManager.toField()
-    }
-
-    private fun determineGameStatus(status: GameStatus, position: Position): GameStatus {
-        if (status === GameStatus.ON_PROGRESS) fieldsManager.gridOpen(position)
-
-        return when {
-            status === GameStatus.GAME_OVER -> GameStatus.GAME_OVER
-            isGameComplete() -> GameStatus.COMPLETE
-            status === GameStatus.ON_PROGRESS -> GameStatus.ON_PROGRESS
-            else -> throw IllegalStateException(status.name + " 은 지원되지 않는 상태입니다.")
-        }
-    }
-
-    private fun isGameComplete(): Boolean {
-        return totalMine == getResult().remainingFieldCount()
+        return map.fields
     }
 }
---
+++ src/main/kotlin/mine/sweeper/application/MineSweeperMap.kt
@@ -0,0 +1,37 @@
+package mine.sweeper.application
+
+import mine.sweeper.application.value.GameStatus
+import mine.sweeper.application.value.MineCount
+import mine.sweeper.domain.Field
+import mine.sweeper.domain.Fields
+import mine.sweeper.domain.SafeField
+import mine.sweeper.view.dto.Position
+
+class MineSweeperMap(
+    val fields: Fields,
+    private val mineCount: MineCount
+) {
+    fun findUncheckOrNull(position: Position): Field? {
+        val field = fields.findOrNull(position)
+        if (field == null || field.checked) return null
+        return field
+    }
+
+    fun open(position: Position, field: Field): GameStatus {
+        val status = field.open()
+        if (status === GameStatus.GAME_OVER) return GameStatus.GAME_OVER
+        if (isEmptyFields(field)) SurroundFieldManager.openEmptyFields(position, fields, HashSet())
+        if (isGameComplete()) return GameStatus.COMPLETE
+        return status
+    }
+
+    private fun isGameComplete(): Boolean {
+        return mineCount.value == fields.remainingFieldCount()
+    }
+
+    private fun isEmptyFields(
+        field: Field
+    ): Boolean {
+        return (field as SafeField).isEmpty()
+    }
+}
