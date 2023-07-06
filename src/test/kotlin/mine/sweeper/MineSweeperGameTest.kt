16534c777fe3feebfa0d3c858c9720b9ab1c5e96
박세준 <sjpark@selab.co.kr>
2023-07-06 15:13:13 +0900

    test: 게임 세부 테스트 추가

--- src/test/kotlin/mine/sweeper/MineSweeperGameTest.kt
+++ src/test/kotlin/mine/sweeper/MineSweeperGameTest.kt
@@ -4,33 +4,43 @@ import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
-import mine.sweeper.Fixture.Companion.MINE_SWEEPER_GAME
+import mine.sweeper.Fixture.createMineGame
+import mine.sweeper.application.MapInitializer
+import mine.sweeper.application.MineSweeperGame
 import mine.sweeper.application.value.GameStatus
 import mine.sweeper.application.value.Height
 import mine.sweeper.application.value.MineCount
 import mine.sweeper.application.value.Width
+import mine.sweeper.view.dto.MapSize
 import mine.sweeper.view.dto.Position
 
 class MineSweeperGameTest : StringSpec({
     "지뢰 찾기 게임에서 맵의 상태를 표출할 수 있다." {
-        val game = MINE_SWEEPER_GAME()
-        game.setMine(MineCount(3))
-
+        val game = createMineGame()
         shouldNotThrowAny {
             game.getResult()
         }
     }
 
     "지뢰가 맵 크기보다 커서 더이상 지뢰를 심을 수 없다면 IllegalStateException 에외가 발생한다." {
-        val game = MINE_SWEEPER_GAME()
         shouldThrow<IllegalStateException> {
-            game.setMine(MineCount(9999))
+            createMineGame(mineCount = MineCount(9999))
         }
     }
 
     "필드들에 지뢰들만 남았다면 승리한다." {
-        val game = MINE_SWEEPER_GAME(Height(3), Width(3))
-        val result = game.open(Position(1, 1))
-        result shouldBe GameStatus.COMPLETE
+        val mapSize = MapSize(Height(2), Width(1))
+        val map = MapInitializer(mapSize).create(setOf(Position(0, 0)))
+        val mineSweeperGame = MineSweeperGame(map)
+        mineSweeperGame.open(Position(1, 0))
+        mineSweeperGame.status shouldBe GameStatus.COMPLETE
+    }
+
+    "지뢰를 열면 게임이 오버된다." {
+        val mapSize = MapSize(Height(1), Width(1))
+        val map = MapInitializer(mapSize).create(setOf(Position(0, 0)))
+        val mineSweeperGame = MineSweeperGame(map)
+        mineSweeperGame.open(Position(0, 0))
+        mineSweeperGame.status shouldBe GameStatus.GAME_OVER
     }
 })
