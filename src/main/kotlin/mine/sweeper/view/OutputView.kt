2033892c78e7241924ff24528fb45ba353ab61a2
박세준 <sjpark@selab.co.kr>
2023-07-06 15:12:58 +0900

    refactor: Filed의 리스트를 전달 받는 것이 아닌 Feilds를 사용하도록 변경

--- src/main/kotlin/mine/sweeper/view/OutputView.kt
+++ src/main/kotlin/mine/sweeper/view/OutputView.kt
@@ -2,21 +2,17 @@ package mine.sweeper.view
 
 import mine.sweeper.application.value.GameStatus
 import mine.sweeper.domain.Field
+import mine.sweeper.domain.Fields
 import mine.sweeper.domain.MineField
 import mine.sweeper.domain.SafeField
-import mine.sweeper.view.dto.Fields
 
 object OutputView {
     fun noticeGameStart() {
         println("지뢰찾기 게임 시작")
     }
 
-    fun noticeWrongPosition() {
-        println("다시 입력해주세요.")
-    }
-
     fun printMap(fields: Fields) {
-        val sortedFields = fields.fields.sortedWith(compareBy({ it.position.x }, { it.position.y }))
+        val sortedFields = fields.toSortedList()
 
         var currentRow = sortedFields.first().position.x
         for (field in sortedFields) {
