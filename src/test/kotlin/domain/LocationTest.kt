7f0f31bb2caf7f51bbc4be23b8d23e3be9116f07
celine.kk <celine.kk@kakaocorp.com>
2023-07-06 19:02:31 +0900

    feat. location테스트

--- src/test/kotlin/domain/LocationTest.kt
+++ src/test/kotlin/domain/LocationTest.kt
@@ -1,7 +1,11 @@
 package domain
 
-import org.junit.jupiter.api.Assertions.*
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 
-internal class LocationTest{
-
+internal class LocationTest {
+    @Test
+    internal fun `x와 y값이 같으면 isSame이 true다`() {
+        Location(1, 2).isSame(2, 1) shouldBe true
+    }
 }
