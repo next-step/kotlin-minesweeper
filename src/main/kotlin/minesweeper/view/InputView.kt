acca23304044490e9fbbb15068c5e4071c82596f
waveofmymind <sjun0913@gmail.com>
2023-07-07 16:44:05 +0900

    Refactor: toIntOrNull로 변경

--- src/main/kotlin/minesweeper/view/InputView.kt
+++ src/main/kotlin/minesweeper/view/InputView.kt
@@ -7,18 +7,19 @@ object InputView {
             inputHeight(), inputWidth(), inputMineCount()
         )
     }
+
     private fun inputHeight(): Int {
         println("높이를 입력하세요.")
-        return readLine()?.toInt() ?: throw IllegalArgumentException("높이는 숫자여야 합니다.")
+        return readLine()?.toIntOrNull() ?: throw IllegalArgumentException("높이는 숫자여야 합니다.")
     }
 
     private fun inputWidth(): Int {
         println("너비를 입력하세요.")
-        return readLine()?.toInt() ?: throw IllegalArgumentException("너비 숫자여야 합니다.")
+        return readLine()?.toIntOrNull() ?: throw IllegalArgumentException("너비 숫자여야 합니다.")
     }
 
     private fun inputMineCount(): Int {
         println("지뢰는 몇 개인가요?")
-        return readLine()?.toInt() ?: throw IllegalArgumentException("지뢰 개수는 숫자여야 합니다.")
+        return readLine()?.toIntOrNull() ?: throw IllegalArgumentException("지뢰 개수는 숫자여야 합니다.")
     }
 }
