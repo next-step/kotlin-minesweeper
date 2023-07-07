21b6b6a805c76d2bc9eb5c3a7543436c31e94af2
peter <peter.shlee@kakaocorp.com>
2023-07-08 01:19:04 +0900

    feat(step1): 입력 받기 (높이, 너비, 지뢰 개수)

---
+++ src/main/kotlin/controller/Controller.kt
@@ -0,0 +1,9 @@
+package controller
+
+import presentation.InputView
+
+fun main() {
+    val height = InputView.getHeight()
+    val width = InputView.getWidth()
+    val mineCount = InputView.getMineCount()
+}
