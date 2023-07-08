7951abc790429fb6f5279eb38ce7d770519cb1c3
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-07-08 10:36:26 +0900

    refactor: 맵에서는 이제 더이상 게임 상태에 관심이 없고 값을 오픈해주는 것에만 관심을 둠

--- src/main/kotlin/mine/sweeper/application/MineSweeperMap.kt
+++ src/main/kotlin/mine/sweeper/application/MineSweeperMap.kt
@@ -1,37 +1,42 @@
 package mine.sweeper.application
 
-import mine.sweeper.domain.Field
 import mine.sweeper.domain.Fields
 import mine.sweeper.domain.SafeField
 import mine.sweeper.domain.value.GameStatus
 import mine.sweeper.domain.value.MineCount
+import mine.sweeper.domain.value.SurroundDirection
 import mine.sweeper.view.dto.Position
 
 class MineSweeperMap(
     val fields: Fields,
-    private val mineCount: MineCount
+    val mineCount: MineCount
 ) {
-    fun findUncheckOrNull(position: Position): Field? {
-        val field = fields.findOrNull(position)
-        if (field == null || field.checked) return null
-        return field
+    private val surroundOpener: SurroundOpener = SurroundOpener(fields)
+
+    fun open(position: Position): GameStatus {
+        val field = fields.get(position) ?: return GameStatus.ON_PROGRESS
+        val gameStatus = field.open()
+        if (field is SafeField && field.isEmpty) surroundOpener.open(position, HashSet())
+        return gameStatus
     }
 
-    fun open(position: Position, field: Field): GameStatus {
-        val openResult = field.open()
-        if (openResult === GameStatus.GAME_OVER) return GameStatus.GAME_OVER
-        if (isEmptyFields(field)) SurroundFieldManager.openEmptyFields(position, fields, HashSet())
-        if (isGameComplete()) return GameStatus.COMPLETE
-        return openResult
-    }
+    private inner class SurroundOpener(private val fields: Fields) {
+        fun open(position: Position, visited: HashSet<Position>) {
+            for (surround in SurroundDirection.values()) {
+                val target = Position(position.x + surround.x, position.y + surround.y)
+                if (visited.contains(target)) continue
+                val safeField = openSafeField(target, fields, visited)
+                safeField?.let {
+                    if (it.isEmpty) open(target, visited)
+                }
+            }
+        }
 
-    private fun isGameComplete(): Boolean {
-        return mineCount.value == fields.remainingFieldCount
-    }
-
-    private fun isEmptyFields(
-        field: Field
-    ): Boolean {
-        return (field as SafeField).isEmpty()
+        private fun openSafeField(target: Position, fields: Fields, visited: HashSet<Position>): SafeField? {
+            val safeField = fields.get(target) as? SafeField ?: return null
+            visited.add(target)
+            safeField.open()
+            return safeField
+        }
     }
 }
