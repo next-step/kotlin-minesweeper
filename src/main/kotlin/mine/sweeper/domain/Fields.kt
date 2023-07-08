f2d7a740d98a305e5d9cb8c3edb493846c93a169
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-07-08 10:33:45 +0900

    refactor: field 역할 분리 더이상 자신이 필드들을 만들지도, 지뢰를 설치하지도 않음

--- src/main/kotlin/mine/sweeper/domain/Fields.kt
+++ src/main/kotlin/mine/sweeper/domain/Fields.kt
@@ -1,15 +1,8 @@
 package mine.sweeper.domain
 
-import mine.sweeper.view.dto.MapSize
 import mine.sweeper.view.dto.Position
 
-class Fields(mapSize: MapSize) {
-    private var fields: List<Field> = List(mapSize.area) { index ->
-        val x = index / mapSize.width.value
-        val y = index % mapSize.width.value
-        SafeField(Position(x, y))
-    }
-
+class Fields(private val fields: List<Field>) {
     val remainingFieldCount: Int
         get() = fields.size - fields.count { it.checked }
 
@@ -19,15 +12,7 @@ class Fields(mapSize: MapSize) {
     val sortedList: List<Field>
         get() = fields.toList().sortedWith(compareBy({ it.position.x }, { it.position.y }))
 
-    infix fun get(position: Position): Field {
-        return fields.first { it.isSame(position) }
-    }
-
-    fun findOrNull(position: Position): Field? {
-        return fields.firstOrNull { it.isSame(position) }
-    }
-
-    fun setMine(position: Position) {
-        fields = fields.map { if (it.isSame(position)) MineField(position) else it }
+    infix fun get(position: Position): Field? {
+        return fields.find { it.isSame(position) }
     }
 }
