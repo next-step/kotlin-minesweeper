5fb67e0a863bfccdb325f7169973abc93074b085
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-07-08 10:35:25 +0900

    refactor: 맵 생성자가 이제 필드들을 생성, 지뢰도 생성, 이후 주변값 상승까지 책임을 짐

--- src/main/kotlin/mine/sweeper/application/MapInitializer.kt
+++ src/main/kotlin/mine/sweeper/application/MapInitializer.kt
@@ -1,15 +1,40 @@
 package mine.sweeper.application
 
+import mine.sweeper.domain.Field
 import mine.sweeper.domain.Fields
+import mine.sweeper.domain.MineField
+import mine.sweeper.domain.SafeField
 import mine.sweeper.domain.value.MineCount
+import mine.sweeper.domain.value.SurroundDirection
+import mine.sweeper.view.dto.MapSize
 import mine.sweeper.view.dto.Position
 
-class MapInitializer(private val fields: Fields) {
-    fun create(minePositions: Set<Position>): MineSweeperMap {
-        minePositions.forEach {
-            fields.setMine(it)
-            SurroundFieldManager.increase(it, fields)
+class MapInitializer(
+    private val mapSize: MapSize,
+    private val minePositions: Set<Position>
+) {
+    fun create(): MineSweeperMap {
+        val fieldList: MutableList<Field> = MutableList(mapSize.area) { index ->
+            val x = index / mapSize.width.value
+            val y = index % mapSize.width.value
+            if (minePositions.contains(Position(x, y))) {
+                MineField(Position(x, y))
+            } else {
+                SafeField(Position(x, y))
+            }
         }
-        return MineSweeperMap(fields, MineCount(minePositions.size))
+        val surroundIncrementor = SurroundIncrementor(fieldList)
+        minePositions.forEach(surroundIncrementor::increase)
+        return MineSweeperMap(Fields(fieldList), MineCount(minePositions.size))
+    }
+
+    private inner class SurroundIncrementor(private val fieldList: List<Field>) {
+        fun increase(position: Position) {
+            for (surround in SurroundDirection.values()) {
+                val target = Position(position.x + surround.x, position.y + surround.y)
+                val safeField = fieldList.find { it.isSame(target) } as? SafeField ?: continue
+                safeField.increase()
+            }
+        }
     }
 }
