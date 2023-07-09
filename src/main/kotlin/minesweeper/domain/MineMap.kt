0d1adf305b693e8e3e02f0c1d7cc5ed315899305
turner.lee <turner.lee@kakaocorp.com>
2023-07-09 18:21:48 +0900

    refactor: 객체지향원칙 적용

--- src/main/kotlin/minesweeper/domain/MineMap.kt
+++ src/main/kotlin/minesweeper/domain/MineMap.kt
@@ -7,19 +7,27 @@ class MineMap(height: Height, val width: Width, mineCount: MineCount) {
     init {
         require(size >= mineCount.value) { "지도 크기는 지뢰 개수이상이어야 합니다." }
 
-        val mineIndexes = (0..size).shuffled().take(mineCount.value)
+        val mineIndexes = (0..size)
+            .shuffled()
+            .take(mineCount.value)
 
         val mutableList = mutableListOf<Point>()
         (1..height.value).forEach { y ->
             (1..width.value).forEach { x ->
                 val index = (y - 1) * width.value + (x - 1)
 
-                val point = if (mineIndexes.contains(index)) MinePoint(x, y) else Point(x, y)
-
-                mutableList.add(point)
+                mutableList.add(point(mineIndexes.contains(index), x, y))
             }
         }
 
         map = mutableList.toList()
+    }
+
+    private fun point(isMine: Boolean, x: Int, y: Int): Point {
+        if (isMine) {
+            return MinePoint(x, y)
+        }
+
+        return Point(x, y)
     }
 }
