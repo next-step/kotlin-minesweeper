8ec8f9690350b143affb1c68010e8221311a46c3
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-07-09 09:27:31 +0900

    feat: 높이, 너비에 따라 필드들을 생성한다.

--- src/test/kotlin/mine/sweeper/GameInitializerTest.kt
+++ src/test/kotlin/mine/sweeper/GameInitializerTest.kt
@@ -2,6 +2,7 @@ package mine.sweeper
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 
 @JvmInline
 value class Height(val value: Int) {
@@ -25,25 +26,41 @@ value class MineCount(val value: Int) {
 }
 
 class GameOption(
-    val height: Height,
-    val width: Width,
+    height: Height,
+    width: Width,
     val mineCount: MineCount
-)
+) {
+    val area = height.value * width.value
+}
 
 class GameInitializer(
     val option: GameOption
 ) {
     fun create(): Game {
-        return Game()
+        return Game(Fields())
     }
 }
 
-class Game
+class Game(val fields: Fields) {
+
+}
+
+class Fields {
+
+    val size = 25
+
+}
 
 class GameInitializerTest : StringSpec({
     "게임 생성자는 높이 너비 마인수에 따라 맵을 생성한다." {
         val option = GameOption(Height(5), Width(5), MineCount(1))
         val game: Game = GameInitializer(option).create()
         game.shouldNotBeNull()
+    }
+
+    "높이, 너비에 따라 필드들을 생성한다." {
+        val option = GameOption(Height(5), Width(5), MineCount(1))
+        val game: Game = GameInitializer(option).create()
+        game.fields.size shouldBe option.area
     }
 })
