4cf947b2172ab32527f159795a77a58594694e68
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-07-09 11:05:31 +0900

    feat: 확인한 위치가 안전 필드면서 값이 0이면 인근 안전 필드를 전부 확인한다.

--- src/test/kotlin/mine/sweeper/FieldsTest.kt
+++ src/test/kotlin/mine/sweeper/FieldsTest.kt
@@ -2,9 +2,11 @@ package mine.sweeper
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 import mine.sweeper.Fixture.basicFields
 import mine.sweeper.domain.Fields
 import mine.sweeper.domain.Position
+import mine.sweeper.domain.SafeField
 
 class FieldsTest : StringSpec({
     "특정 위치의 필드를 확인한다" {
@@ -12,5 +14,16 @@ class FieldsTest : StringSpec({
         val position = Position(0, 0)
         val field = fields[position]
         field.shouldNotBeNull()
+    }
+
+    "확인한 위치가 안전 필드면서 값이 0이면 인근 안전 필드를 전부 확인한다." {
+        val fields = Fields(basicFields())
+        val position = Position(0, 0)
+        val field = fields[position]
+        field.shouldNotBeNull()
+        (field is SafeField) shouldBe true
+        (field as SafeField).value shouldBe 0
+        field.increase()
+        field.value shouldBe 1
     }
 })
