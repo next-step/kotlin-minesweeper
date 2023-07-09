dd9f47729da1d27c4d1e59b27c532ecbba560ae0
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-07-09 21:26:53 +0900

    refactor: let -> ?:로 처리하도록 변경

--- src/main/kotlin/mine/sweeper/application/FieldsInitializer.kt
+++ src/main/kotlin/mine/sweeper/application/FieldsInitializer.kt
@@ -1,18 +1,18 @@
 package mine.sweeper.application
 
 import mine.sweeper.domain.Field
-import mine.sweeper.domain.GameOption
 import mine.sweeper.domain.MineField
+import mine.sweeper.domain.RandomGameOption
 import mine.sweeper.domain.SafeField
 
-class FieldsInitializer(
-    private val option: GameOption
-) {
+class FieldsInitializer(private val option: RandomGameOption) {
     fun create(): MutableList<Field> {
         val minePositions = option.minePositions
         return MutableList(option.area) { index ->
             val position = option.positionBy(index)
-            minePositions.find { it == position }?.let { MineField(it) } ?: SafeField(position)
+            val minePosition = minePositions.find { it == position }
+                ?: return@MutableList SafeField(position)
+            MineField(minePosition)
         }
     }
 }
