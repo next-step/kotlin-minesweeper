b6ee144122ae2dee06ec40001bb89f073316fdae
judy.seo <judy.seo@kakaocorp.com>
2023-07-10 23:15:22 +0900

    refactor: MineSweeperGame 클래스 생성자 수정

--- src/main/kotlin/minesweeper/Config.kt
+++ src/main/kotlin/minesweeper/Config.kt
@@ -1,13 +1,11 @@
 package minesweeper
 
 import minesweeper.domain.MineSweeperGame
-import minesweeper.domain.RandomMinesGenerator
 import minesweeper.view.InputView
 import minesweeper.view.OutputView
 
 object Config {
     fun inputView() = InputView
     fun outputView() = OutputView
-    fun mineGame() = MineSweeperGame(minesGenerator())
-    fun minesGenerator() = RandomMinesGenerator
+    fun mineGame() = MineSweeperGame()
 }
--- src/main/kotlin/minesweeper/domain/MineSweeperGame.kt
+++ src/main/kotlin/minesweeper/domain/MineSweeperGame.kt
@@ -4,7 +4,9 @@ import minesweeper.domain.board.CellBoard
 import minesweeper.domain.board.GameBoard
 import minesweeper.domain.vo.PositiveNumber
 
-class MineSweeperGame(private val minesGenerator: RandomMinesGenerator) {
+class MineSweeperGame {
+    private val minesGenerator: RandomMinesGenerator = RandomMinesGenerator
+
     fun createBoard(height: PositiveNumber, width: PositiveNumber, mineCount: PositiveNumber): GameBoard {
         val randomMines = minesGenerator.generate(
             totalCount = height * width,
