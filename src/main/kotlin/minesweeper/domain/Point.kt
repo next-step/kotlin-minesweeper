936876319bfc358fdcbc750e304d2dadb68fc422
waveofmymind <sjun0913@gmail.com>
2023-07-11 10:54:16 +0900

    Feat: MinePoints에서 지뢰의 수를 세도록 변경

--- src/main/kotlin/minesweeper/domain/Point.kt
+++ src/main/kotlin/minesweeper/domain/Point.kt
@@ -4,4 +4,12 @@ data class Point(val row: Int, val col: Int) {
     operator fun plus(other: Point) = Point(row + other.row, col + other.col)
 
     fun isWithin(height: Int, width: Int) = row in 0 until height && col in 0 until width
+
+    fun getAdjacentPoints(): List<Point> {
+        return listOf(
+            Point(row - 1, col - 1), Point(row - 1, col), Point(row - 1, col + 1),
+            Point(row, col - 1), Point(row, col + 1),
+            Point(row + 1, col - 1), Point(row + 1, col), Point(row + 1, col + 1)
+        )
+    }
 }
