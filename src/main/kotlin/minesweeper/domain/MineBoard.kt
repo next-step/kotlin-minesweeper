31307fa9ac689b4844f5d951cebf4a7d8cf48c6c
waveofmymind <sjun0913@gmail.com>
2023-07-11 11:01:54 +0900

    Refactor: 사용하지 않는 함수 제거

--- src/main/kotlin/minesweeper/domain/MineBoard.kt
+++ src/main/kotlin/minesweeper/domain/MineBoard.kt
@@ -3,14 +3,9 @@ package minesweeper.domain
 class MineBoard(
     val boardInfo: List<BoardRow>,
 ) {
-    fun getCell(point: Point): Cell {
-        return boardInfo[point.row].rowInfo[point.col]
-    }
-
     val height: Int
         get() = boardInfo.size
 
     val width: Int
         get() = boardInfo[0].rowInfo.size
-
 }
