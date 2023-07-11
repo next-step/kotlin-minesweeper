732b454b4311a9e441eb690dcd4035db013cdbc7
jonghyunhub <chuwhdgus@gmail.com>
2023-07-11 21:23:30 +0900

    refact : GameBoardSquare sealed class로 변경

--- src/main/kotlin/minesweeper/domain/GameBoardSquare.kt
+++ src/main/kotlin/minesweeper/domain/GameBoardSquare.kt
@@ -1,8 +1,27 @@
 package minesweeper.domain
 
-data class GameBoardSquare(private val squareValueType: SquareValueType) {
+sealed class GameBoardSquare(val numOfNearByMine: Int) {
+    abstract fun isMine(): Boolean
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is GameBoardSquare) return false
 
-    fun isMine(): Boolean {
-        return squareValueType == SquareValueType.MINE
+        return numOfNearByMine == other.numOfNearByMine
+    }
+
+    override fun hashCode(): Int {
+        return numOfNearByMine
+    }
+
+    class MineSquare() : GameBoardSquare(0) {
+        override fun isMine(): Boolean = true
+    }
+
+    class NumberSquare(numOfNearByMine: Int) : GameBoardSquare(numOfNearByMine) {
+        override fun isMine(): Boolean = false
+
+        companion object {
+            fun createEmpty(): NumberSquare = NumberSquare(numOfNearByMine = 0)
+        }
     }
 }
