87798ba9f6d78de2fa0aa48ae1c0c669106e522b
waveofmymind <sjun0913@gmail.com>
2023-07-11 16:18:12 +0900

    Feat: 지뢰를 여는 로직을 BFS를 이용하여 구현

--- src/main/kotlin/minesweeper/domain/MineBoard.kt
+++ src/main/kotlin/minesweeper/domain/MineBoard.kt
@@ -1,5 +1,8 @@
 package minesweeper.domain
 
+import java.util.LinkedList
+import java.util.Queue
+
 class MineBoard(
     val boardInfo: List<BoardRow>,
 ) {
@@ -8,4 +11,41 @@ class MineBoard(
 
     val width: Int
         get() = boardInfo[0].rowInfo.size
+
+    fun openCell(point: Point): Result {
+        val queue: Queue<Cell> = LinkedList()
+        queue.add(getCell(point))
+
+        while (queue.isNotEmpty()) {
+            val cell = queue.poll()
+            if (cell.isOpen) continue
+            cell.isOpen = true
+
+            when (cell) {
+                is MineCell -> {
+                    return Result.LOSE
+                }
+
+                is EmptyCell -> {
+                    if (cell.mineCount == 0) {
+                        addCellsToQueue(queue, cell)
+                    }
+                }
+            }
+        }
+
+        return Result.CONTINUE
+    }
+
+    private fun addCellsToQueue(queue: Queue<Cell>, cell: Cell) {
+        cell.point.getAdjacentPoints().forEach { adjacentPoint ->
+            if (adjacentPoint.isWithin(height, width)) {
+                queue.add(getCell(adjacentPoint))
+            }
+        }
+    }
+
+    private fun getCell(point: Point): Cell {
+        return boardInfo[point.row].rowInfo[point.col]
+    }
 }
--- src/main/kotlin/minesweeper/domain/Point.kt
+++ src/main/kotlin/minesweeper/domain/Point.kt
@@ -3,6 +3,9 @@ package minesweeper.domain
 data class Point(val row: Int, val col: Int) {
     operator fun plus(other: Point) = Point(row + other.row, col + other.col)
 
+    fun isWithin(height: Int, width: Int): Boolean {
+        return row in 0 until height && col in 0 until width
+    }
     fun getAdjacentPoints(): Set<Point> {
         return setOf(
             Point(row - 1, col - 1), Point(row - 1, col), Point(row - 1, col + 1),
