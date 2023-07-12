e58f63bc4484e8af41b3509565ce270af029dc62
Songyi Kim <ksl2950@naver.com>
2023-07-12 16:35:11 +0900

    feat: 정해진 높이와 너비 내의 지뢰판을 생성할 수 있다.

--- src/test/kotlin/MineBoardGeneratorTest.kt
+++ src/test/kotlin/MineBoardGeneratorTest.kt
@@ -24,4 +24,25 @@ class MineBoardGeneratorTest : FunSpec({
             actual.values[it.y][it.x] shouldBe MINE
         }
     }
+
+    test("정해진 높이와 너비 내의 지뢰판을 생성할 수 있다.") {
+        // given
+        val width = 5
+        val height = 5
+        val boardSize = BoardSize(width, height)
+        val mineCount = 3
+        val boardInfoGenerator = BoardInfoGenerator(
+            boardSize,
+            mineCount,
+        )
+
+        // when
+        val actual = boardInfoGenerator.generate()
+
+        // then
+        actual.values.size shouldBe width
+        actual.values.forAll {
+            it.size shouldBe height
+        }
+    }
 })
