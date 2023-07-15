659685dd252e7b420e27ac7a09cae64bd21f647d
maenguin-h <maenguin.h@kakaocorp.com>
2023-07-15 17:42:05 +0900

    step4 : MineMapConfig의 너비와 높이는 1보다 커야하며 지뢰 개수는 0보다 커야 한다 그렇지 않으면 예외가 발생한다

--- src/main/kotlin/minesweeper/domain/MineMapConfig.kt
+++ src/main/kotlin/minesweeper/domain/MineMapConfig.kt
@@ -4,4 +4,10 @@ data class MineMapConfig(
     val width: Int,
     val height: Int,
     val mineCount: Int,
-)
+) {
+    init {
+        require(width > 1) { "width must be greater than 1" }
+        require(height > 1) { "height must be greater than 1" }
+        require(mineCount > 0) { "mineCount must be greater than zero" }
+    }
+}
--- src/test/kotlin/minesweeper/domain/MineMapConfigTest.kt
+++ src/test/kotlin/minesweeper/domain/MineMapConfigTest.kt
@@ -1,6 +1,7 @@
 package minesweeper.domain
 
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -24,5 +25,25 @@ internal class MineMapConfigTest {
         sut.width shouldBe widthValue
         sut.height shouldBe heightValue
         sut.mineCount shouldBe mindCountValue
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1, 3, 3",
+        "2, 1, 3",
+        "2, 2, 0",
+    )
+    internal fun `너비와 높이는 1보다 커야하며 지뢰 개수는 0보다 커야 한다 그렇지 않으면 예외가 발생한다`(
+        widthValue: Int,
+        heightValue: Int,
+        mindCountValue: Int,
+    ) {
+        assertThrows<IllegalArgumentException> {
+            MineMapConfig(
+                width = widthValue,
+                height = heightValue,
+                mineCount = mindCountValue,
+            )
+        }
     }
 }
