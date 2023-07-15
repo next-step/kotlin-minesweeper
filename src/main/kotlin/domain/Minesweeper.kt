3b1f437dd53bc6886e395f6ddfca3d3e0dba9fca
sjparkk <sjparkk0820@naver.com>
2023-07-15 18:16:52 +0900

    feat : 자신을 제외한 주변 8개 지뢰 찾기 기능 추가

--- src/main/kotlin/domain/Minesweeper.kt
+++ src/main/kotlin/domain/Minesweeper.kt
@@ -21,6 +21,48 @@ class Minesweeper(
         return this
     }
 
+    fun changeMineCountMap(): Minesweeper {
+        for (row in 0 until height) {
+            processRow(row)
+        }
+        return this
+    }
+
+    private fun processRow(row: Int) {
+        for (column in 0 until width) {
+            updateMineShape(row, column)
+        }
+    }
+
+    private fun updateMineShape(row: Int, column: Int) {
+        if (mineMap[row][column] == MinesweeperShape.BASIC) {
+            mineMap[row][column] = MinesweeperShape.values().find {
+                it.printShape == getMineNumber(row, column).toString().first()
+            } ?: MinesweeperShape.ZERO
+        }
+    }
+
+    private fun getMineNumber(row: Int, column: Int): Int {
+        var count = 0
+
+        if (isExistMine(row - 1, column - 1)) count++
+        if (isExistMine(row - 1, column)) count++
+        if (isExistMine(row - 1, column + 1)) count++
+        if (isExistMine(row, column - 1)) count++
+        if (isExistMine(row, column + 1)) count++
+        if (isExistMine(row + 1, column - 1)) count++
+        if (isExistMine(row + 1, column)) count++
+        if (isExistMine(row + 1, column + 1)) count++
+
+        return count
+    }
+
+    private fun isExistMine(row: Int, column: Int): Boolean {
+        return if (row < 0 || row >= height || column < 0 || column >= width) {
+            false
+        } else mineMap[row][column] == MinesweeperShape.MINE
+    }
+
     private fun minePositions(): MutableList<Pair<Int, Int>> {
         val minePositions = mutableListOf<Pair<Int, Int>>()
 
