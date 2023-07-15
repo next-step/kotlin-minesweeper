cf1e0f06216b1af0b0495f0814deb85d2289d934
maenguin-h <maenguin.h@kakaocorp.com>
2023-07-15 17:29:52 +0900

    step4 : Position은 최대 x값과 최대 y값이 주어졌을때 현재 위치에서 주변 8분면 중 탐색 가능한 위치를 추출할 수 있다

--- src/main/kotlin/minesweeper/domain/Position.kt
+++ src/main/kotlin/minesweeper/domain/Position.kt
@@ -8,4 +8,27 @@ data class Position(val x: Int, val y: Int) {
         require(x >= 0) { "x must be greater than and equal to zero" }
         require(y >= 0) { "y must be greater than and equal to zero" }
     }
+
+    /**
+     * ### 현재 위치에서 탐색 가능한 8분면 위치를 추출합니다.
+     */
+    fun nearby(maxX: Int, maxY: Int): List<Position> {
+        return OctantDelta.values()
+            .filter { this.x + it.dx in 0 until maxX && this.y + it.dy in 0 until maxY }
+            .map { Position(x = this.x + it.dx, y = this.y + it.dy) }
+    }
+}
+
+/**
+ * ### 특정 위치에서 8분면으로 탐색할 수 있는 좌표의 변화량을 정의한 Enum 입니다
+ */
+enum class OctantDelta(val dx: Int, val dy: Int) {
+    UP(0, -1),
+    UPPER_RIGHT(1, -1),
+    RIGHT(1, 0),
+    LOWER_RIGHT(1, 1),
+    DOWN(0, 1),
+    LOWER_LEFT(-1, 1),
+    LEFT(-1, 0),
+    UPPER_LEFT(-1, -1),
 }
--- src/test/kotlin/minesweeper/domain/PositionTest.kt
+++ src/test/kotlin/minesweeper/domain/PositionTest.kt
@@ -1,6 +1,8 @@
 package minesweeper.domain
 
+import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -25,5 +27,26 @@ internal class PositionTest {
     )
     internal fun `좌표가 0보다 작으면 예외가 발생한다`(valueX: Int, valueY: Int) {
         assertThrows<IllegalArgumentException> { Position(x = valueX, y = valueY) }
+    }
+
+    @Test
+    internal fun `최대 x값과 최대 y값이 주어졌을때 현재 위치에서 주변 8분면 중 탐색 가능한 위치를 추출할 수 있다`() {
+        // given : (1, 0)의 위치 준비
+        val sut = Position(x = 1, y = 0)
+
+        // when : X의 최대값이 3, Y의 최대값의 3일때 (1, 0)에서 탐색 가능한 8분면의 위치 추출
+        val postions = sut.nearby(maxX = 3, maxY = 3)
+
+        // then : 추출된 위치는 다음과 같다
+        // O * O
+        // O O O
+        // X X X
+        postions.shouldContainAll(
+            Position(0, 0),
+            Position(2, 0),
+            Position(0, 1),
+            Position(1, 1),
+            Position(2, 1),
+        )
     }
 }
