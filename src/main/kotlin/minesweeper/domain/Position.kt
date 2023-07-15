0413c3d6f04a24f0a7b46d4243e90cb5d6cf9d0a
maenguin-h <maenguin.h@kakaocorp.com>
2023-07-15 16:53:36 +0900

    step4 : Position의 좌표가 0보다 작으면 예외가 발생한다

--- src/main/kotlin/minesweeper/domain/Position.kt
+++ src/main/kotlin/minesweeper/domain/Position.kt
@@ -3,4 +3,9 @@ package minesweeper.domain
 /**
  * ### (x, y) 좌표계의 특정 위치를 표현합니다
  */
-data class Position(val x: Int, val y: Int)
+data class Position(val x: Int, val y: Int) {
+    init {
+        require(x >= 0) { "x must be greater than and equal to zero" }
+        require(y >= 0) { "y must be greater than and equal to zero" }
+    }
+}
--- src/test/kotlin/minesweeper/domain/PositionTest.kt
+++ src/test/kotlin/minesweeper/domain/PositionTest.kt
@@ -1,6 +1,7 @@
 package minesweeper.domain
 
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -14,5 +15,15 @@ internal class PositionTest {
         val sut = Position(x = expectedX, y = expectedY)
         sut.x shouldBe expectedX
         sut.y shouldBe expectedY
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "-1, -1",
+        "0, -1",
+        "-1, 0"
+    )
+    internal fun `좌표가 0보다 작으면 예외가 발생한다`(valueX: Int, valueY: Int) {
+        assertThrows<IllegalArgumentException> { Position(x = valueX, y = valueY) }
     }
 }
