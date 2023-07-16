ad496d52de3f7be32119e0ba2d5a444dd1c0ce71
waveofmymind <sjun0913@gmail.com>
2023-07-16 22:05:51 +0900

    Refactor: operator fun으로 변경

--- src/main/kotlin/minesweeper/domain/MinePoints.kt
+++ src/main/kotlin/minesweeper/domain/MinePoints.kt
@@ -1,7 +1,7 @@
 package minesweeper.domain
 
 class MinePoints(private val points: Set<Point>) {
-    fun contains(point: Point): Boolean = points.contains(point)
+    operator fun contains(point: Point): Boolean = points.contains(point)
 
     fun countNeighborMines(point: Point): Int {
         return point.getAdjacentPoints().count { it in points }
