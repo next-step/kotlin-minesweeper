b1e5efd257163c1d81c5fd2400b34a18e75b8ceb
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-19 01:29:16 +0900

    refactor: fixture 추가

--- src/test/kotlin/step4/domain/CellTest.kt
+++ src/test/kotlin/step4/domain/CellTest.kt
@@ -4,18 +4,19 @@ import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.throwable.shouldHaveMessage
+import step4.domain.CellFixture.aCell
 
 class CellTest : FunSpec({
 
     context("open") {
         test("이미 오픈된 좌표를 오픈하려하면 예외가 발생한다.") {
-            val cell = Cell(cellType = CellType.ZERO, isOpen = true)
+            val cell = aCell(isOpen = true)
             val exception = shouldThrowExactly<IllegalStateException> { cell.open() }
             exception shouldHaveMessage "이미 오픈된 좌표는 다시 오픈할 수 없습니다."
         }
 
         test("cell을 오픈한다.") {
-            val cell = Cell(cellType = CellType.ZERO, isOpen = false)
+            val cell = aCell(isOpen = false)
             cell.open()
 
             val actual = cell.isOpen
@@ -23,3 +24,13 @@ class CellTest : FunSpec({
         }
     }
 })
+
+object CellFixture {
+    fun aCell(
+        cellType: CellType = CellType.ZERO,
+        isOpen: Boolean = false,
+    ) = Cell(
+        cellType = cellType,
+        isOpen = isOpen,
+    )
+}
