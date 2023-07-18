969894c30f0d9e05a5a8482985e6dd98d17629e9
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-19 02:12:40 +0900

    feat: 지뢰 갯수가 보유한 cell보다 많은경우 예외 추가 및 테스트

--- src/main/kotlin/step4/domain/Cells.kt
+++ src/main/kotlin/step4/domain/Cells.kt
@@ -6,6 +6,7 @@ value class Cells(
 ) {
     fun installMine(mineCount: Int) {
         require(mineCount >= MINIMUM_MINE_COUNT) { "지뢰 갯수는 ${MINIMUM_MINE_COUNT}개 이상이어야 합니다." }
+        require(mineCount < values.size) { "보유한 cell보다 많은 지뢰를 설치할 수 없습니다." }
     }
 
     companion object {
--- src/test/kotlin/step4/domain/CellsTest.kt
+++ src/test/kotlin/step4/domain/CellsTest.kt
@@ -2,7 +2,6 @@ package step4.domain
 
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
 import io.kotest.matchers.throwable.shouldHaveMessage
 
 class CellsTest : FunSpec({
@@ -12,6 +11,12 @@ class CellsTest : FunSpec({
             val cells = Cells(mapOf())
             val exception = shouldThrowExactly<IllegalArgumentException> { cells.installMine(0) }
             exception shouldHaveMessage "지뢰 갯수는 1개 이상이어야 합니다."
+        }
+
+        test("입력된 지뢰 갯수가 보유한 cell보다 많은 경우 예외가 발생한다.") {
+            val cells = Cells(mapOf())
+            val exception = shouldThrowExactly<IllegalArgumentException> { cells.installMine(1) }
+            exception shouldHaveMessage "보유한 cell보다 많은 지뢰를 설치할 수 없습니다."
         }
     }
 })
