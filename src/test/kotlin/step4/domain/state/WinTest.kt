cae3961da793ca23ab634fb13a0944993f7f1f47
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-20 03:35:06 +0900

    test: 테스트 수정

--- src/test/kotlin/step4/domain/state/WinTest.kt
+++ src/test/kotlin/step4/domain/state/WinTest.kt
@@ -11,16 +11,16 @@ class WinTest : FunSpec({
 
     context("installMine") {
         test("지뢰를 설치하려하면 예외가 발생한다.") {
-            val running = Running(0, Cells(mapOf()))
-            val exception = shouldThrowExactly<IllegalStateException> { running.installMine(1) { Cell() } }
+            val win = Win()
+            val exception = shouldThrowExactly<IllegalStateException> { win.installMine(1) { Cell() } }
             exception shouldHaveMessage "지뢰를 설치할 수 있는 상태가 아닙니다."
         }
     }
 
     context("open") {
-        test("준비중에 cell을 열려고하면 예외가 발생한다.") {
-            val ready = Ready(0, Cells(mapOf()))
-            val exception = shouldThrowExactly<IllegalStateException> { ready.open(Coordinate(0, 0)) }
+        test("종료 상태에서 cell을 열려고하면 예외가 발생한다.") {
+            val win = Win()
+            val exception = shouldThrowExactly<IllegalStateException> { win.open(Coordinate(0, 0)) }
             exception shouldHaveMessage "종료 상태에선 cell을 열 수 없습니다."
         }
     }
