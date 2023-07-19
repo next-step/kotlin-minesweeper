f2ec9d70e1f0a185d52ae97b5104954919ec234b
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-20 04:45:02 +0900

    feat: 공통부분 제거

---
+++ src/main/kotlin/step4/domain/state/Finished.kt
@@ -0,0 +1,12 @@
+package step4.domain.state
+
+import step4.domain.Coordinate
+import step4.domain.strategy.CoordinateSelectStrategy
+
+abstract class Finished : MinesweeperState {
+
+    final override fun installMine(mineCount: Int, coordinateSelectStrategy: CoordinateSelectStrategy): MinesweeperState =
+        throw IllegalStateException("지뢰를 설치할 수 있는 상태가 아닙니다.")
+
+    final override fun open(coordinate: Coordinate): MinesweeperState = throw IllegalStateException("종료 상태에선 cell을 열 수 없습니다.")
+}
