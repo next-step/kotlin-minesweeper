4583f57449738834f398d5e4fae371d252331e48
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-21 07:05:21 +0900

    refactor: OutputView 매직넘버 상수화

--- src/main/kotlin/step4/view/OutputView.kt
+++ src/main/kotlin/step4/view/OutputView.kt
@@ -4,6 +4,9 @@ import step4.domain.CellInfo
 import step4.domain.CellInfos
 import step4.domain.state.MinesweeperState
 
+private const val ROW_DELIMITER = "\n"
+private const val COLUMN_DELIMITER = " "
+
 fun printStartGame() {
     println("지뢰찾기 게임 시작")
 }
@@ -17,10 +20,10 @@ fun printGameResult(gameState: MinesweeperState) {
 }
 
 private fun parseToPrintView(height: Int, cellInfos: List<CellInfo>): String =
-    (0 until height).joinToString("\n") { parseToRowPrintView(cellInfos.findCellInfosByRow(it)) }
+    (0 until height).joinToString(ROW_DELIMITER) { parseToRowPrintView(cellInfos.findCellInfosByRow(it)) }
 
 private fun List<CellInfo>.findCellInfosByRow(row: Int): List<CellInfo> = this.filter { row == it.row }
     .sortedBy { it.column }
 
 private fun parseToRowPrintView(cellInfos: List<CellInfo>): String =
-    cellInfos.joinToString(" ") { CellView.from(it.cellType).view }
+    cellInfos.joinToString(COLUMN_DELIMITER) { CellView.from(it.cellType).view }
