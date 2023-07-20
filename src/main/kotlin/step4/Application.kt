61792ba9e6c0ff075c7ee05f678c6ec8d67b45f4
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-07-21 00:48:57 +0900

    feat: 높이, 너비, 지뢰 갯수 입력 뷰 추가

---
+++ src/main/kotlin/step4/Application.kt
@@ -0,0 +1,11 @@
+package step4
+
+import step4.view.inputHeight
+import step4.view.inputMineCount
+import step4.view.inputWidth
+
+fun main() {
+    val height = inputHeight()
+    val width = inputWidth()
+    val mineCount = inputMineCount()
+}
---
+++ src/main/kotlin/step4/view/InputView.kt
@@ -0,0 +1,31 @@
+package step4.view
+
+fun inputHeight(): Int {
+    println("높이를 입력하세요.")
+    return readln().toIntOrNull() ?: retryInputHeight()
+}
+
+fun inputWidth(): Int {
+    println("너비를 입력하세요.")
+    return readln().toIntOrNull() ?: retryInputWidth()
+}
+
+fun inputMineCount(): Int {
+    println("지뢰는 몇 개인가요?")
+    return readln().toIntOrNull() ?: retryInputMineCount()
+}
+
+private fun retryInputHeight(): Int {
+    println("높이는 숫자만 입력하여야 합니다.")
+    return inputHeight()
+}
+
+private fun retryInputWidth(): Int {
+    println("너비는 숫자만 입력하여야 합니다.")
+    return inputWidth()
+}
+
+private fun retryInputMineCount(): Int {
+    println("지뢰는 숫자만 입력하여야 합니다.")
+    return inputMineCount()
+}
