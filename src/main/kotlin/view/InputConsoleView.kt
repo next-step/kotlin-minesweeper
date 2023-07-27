a3c4db2a09742327a3b338e7ed805a8a7e6d4994
rladowl92 <rladowl92@naver.com>
2023-07-27 23:29:30 +0900

    feat(mine): view 추가

---
+++ src/main/kotlin/view/InputConsoleView.kt
@@ -0,0 +1,18 @@
+package view
+
+class InputConsoleView {
+    fun inputHeight(): Int {
+        println("높이를 입력하세요.")
+        return readln().toInt()
+    }
+
+    fun inputWidth(): Int {
+        println("너비를 입력하세요.")
+        return readln().toInt()
+    }
+
+    fun inputMineCount(): Int {
+        println("지뢰는 몇 개인가요?")
+        return readln().toInt()
+    }
+}
