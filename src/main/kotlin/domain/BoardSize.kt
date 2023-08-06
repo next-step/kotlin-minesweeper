17699ecd64cdda8ec6623f917282271ce0c0ab13
songyi00 <ksl2950@naver.com>
2023-08-06 18:01:11 +0900

    feat: 지뢰판 크기 검증 로직 추가

--- src/main/kotlin/domain/BoardSize.kt
+++ src/main/kotlin/domain/BoardSize.kt
@@ -4,6 +4,11 @@ data class BoardSize(
     val width: Int,
     val height: Int
 ) {
+    init {
+        require(width > 0) { "너비는 0보다 커야 합니다." }
+        require(height > 0) { "높이는 0보다 커야 합니다." }
+    }
+
     val area: Int
         get() = width * height
 }
