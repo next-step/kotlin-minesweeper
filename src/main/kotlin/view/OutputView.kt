83dc65a16d8b374872978e40e908c055e0ce5b14
songyi00 <ksl2950@naver.com>
2023-08-06 18:04:44 +0900

    feat: cell 의 실제 문자값은 view에서 관리

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,16 +1,32 @@
 package view
 
+import domain.CellStatus
 import domain.MineBoard
+import domain.Row
 
 object OutputView {
+    const val MINE = "C "
+    const val EMPTY = "* "
+
     fun printStartGame() {
         println()
         println("지뢰찾기 게임 시작")
     }
 
     fun printMineBoard(mineBoard: MineBoard) {
-        mineBoard.info.values.forEach { cell ->
-            println(cell.map { it.value }.joinToString(" "))
+        mineBoard.info.layout.rows.forEach { row ->
+            print(row)
+            println()
+        }
+    }
+
+    private fun print(row: Row) {
+        for (cell in row.values) {
+            if (cell.status == CellStatus.MINE) {
+                print(MINE)
+            } else {
+                print(EMPTY)
+            }
         }
     }
 }
