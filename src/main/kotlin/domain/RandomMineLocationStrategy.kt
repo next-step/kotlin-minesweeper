cc224ee4ab7391d545916de4d15755555e75ca92
songyi00 <ksl2950@naver.com>
2023-08-06 18:03:31 +0900

    feat: 지뢰 위치 정보 set 으로 관리

--- src/main/kotlin/domain/RandomMineLocationStrategy.kt
+++ src/main/kotlin/domain/RandomMineLocationStrategy.kt
@@ -2,11 +2,17 @@ package domain
 
 class RandomMineLocationStrategy : MineLocationStrategy {
     override fun generateMineLocations(boardSize: BoardSize, mineCount: Int): MineLocations {
-        return MineLocations(List(mineCount) { randomPoint(boardSize) })
+        val locations: MutableSet<Point> = mutableSetOf()
+
+        while (locations.size < mineCount) {
+            locations.add(randomPoint(boardSize))
+        }
+
+        return MineLocations(locations.toSet())
     }
 
     private fun randomPoint(boardSize: BoardSize): Point {
-        val randomY = (1 until boardSize.height).random()
+        val randomY = (0 until boardSize.height).random()
         val randomX = (0 until boardSize.width).random()
         return Point(randomY, randomX)
     }
