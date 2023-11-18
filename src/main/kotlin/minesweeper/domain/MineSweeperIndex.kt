2ce9341f37eba34ce69499dacecc2893fd7573ff
원동재 <wdj0717@midasit.com>
2023-11-18 14:45:13 +0900

    step2: 지뢰 찾기(지뢰 개수) 피드백 반영 - backing 프로퍼티 삭제

--- src/main/kotlin/minesweeper/domain/MineSweeperIndex.kt
+++ src/main/kotlin/minesweeper/domain/MineSweeperIndex.kt
@@ -1,9 +1,7 @@
 package minesweeper.domain
 
-class MineSweeperIndex(position: Position) {
+class MineSweeperIndex(val position: Position) {
 
-    private val _position = position
-    val position get() = _position
     fun mineCount(mines: Mines): Int {
         if (mines.isMine(position)) return MINE
         return IndexSquare.values().count { mines.isMine(position + it.position) }
