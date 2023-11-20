49b9687ac2c67cf3963812e8dd5b7e66b413c6ec
원동재 <wdj0717@midasit.com>
2023-11-20 09:05:44 +0900

    step3: step2 피드백 반영 - IndexResult 클래스 생성

--- src/main/kotlin/minesweeper/view/OutputView.kt
+++ src/main/kotlin/minesweeper/view/OutputView.kt
@@ -1,16 +1,18 @@
 package minesweeper.view
 
+import minesweeper.domain.IndexResult
+
 object OutputView {
 
     fun printMineSweeperStart() {
         println(MINESWEEPER_START_MESSAGE)
     }
-    fun printMineSweeper(chunked: Int, result: List<Pair<Int, Boolean>>) {
+    fun printMineSweeper(chunked: Int, result: List<IndexResult>) {
         result.chunked(chunked).forEach { it.printMineSweeperRow() }
     }
 
-    private fun List<Pair<Int, Boolean>>.printMineSweeperRow() {
-        val convert = this.map { if (it.second) MINE else it.first.toString() }
+    private fun List<IndexResult>.printMineSweeperRow() {
+        val convert = this.map { if (it.isMine) MINE else it.mineCount.toString() }
         println(convert.joinToString(" "))
     }
 
