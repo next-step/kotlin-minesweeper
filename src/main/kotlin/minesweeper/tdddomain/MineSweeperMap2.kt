f00839d783ef700ebbc0a8a8f43aaea351116747
원동재 <wdj0717@midasit.com>
2023-11-22 14:05:13 +0900

    step4: 지뢰 찾기(리팩토링) - MineSweeperMap2 open함수 로직 추가

--- src/main/kotlin/minesweeper/tdddomain/MineSweeperMap2.kt
+++ src/main/kotlin/minesweeper/tdddomain/MineSweeperMap2.kt
@@ -1,16 +1,32 @@
 package minesweeper.tdddomain
 
 import minesweeper.domain.GameStatus
+import minesweeper.domain.IndexSquare
 import minesweeper.domain.Position
+import minesweeper.domain.PositionStatus
 
 class MineSweeperMap2(val mineSweeperIndexes: List<MineSweeperIndex2>) {
 
     fun open(position: Position): GameStatus {
         val mineSweeperIndex = mineSweeperIndexes.find { it.position == position } ?: throw IllegalArgumentException()
-        mineSweeperIndex.open()
         if (mineSweeperIndex.isMine()) {
             return GameStatus.LOSE
         }
+
+        mineSweeperIndex.open()
+        if (mineSweeperIndex.mineCount(mineSweeperIndexes) != 0) {
+            return GameStatus.CONTINUE
+        }
+
+        emptyIndex(mineSweeperIndex)
         return GameStatus.CONTINUE
+    }
+
+    private fun emptyIndex(mineSweeperIndex: MineSweeperIndex2) {
+        IndexSquare.squareIndex(mineSweeperIndex, mineSweeperIndexes)
+            .filter { it.openStatus == PositionStatus.CLOSED }
+            .forEach {
+                open(it.position)
+            }
     }
 }
--- src/test/kotlin/minesweeper/tdddomain/MineSweeperMap2Test.kt
+++ src/test/kotlin/minesweeper/tdddomain/MineSweeperMap2Test.kt
@@ -43,4 +43,27 @@ class MineSweeperMap2Test : BehaviorSpec({
             }
         }
     }
+
+    given("지뢰판(3x3)과 (1,2)가 지뢰일때") {
+        val mineSweeperIndexes = listOf(
+            MineSweeperIndex2(1, 1, MineStatus.NOT_MINE),
+            MineSweeperIndex2(1, 2, MineStatus.MINE),
+            MineSweeperIndex2(1, 3, MineStatus.NOT_MINE),
+            MineSweeperIndex2(2, 1, MineStatus.NOT_MINE),
+            MineSweeperIndex2(2, 2, MineStatus.NOT_MINE),
+            MineSweeperIndex2(2, 3, MineStatus.NOT_MINE),
+            MineSweeperIndex2(3, 1, MineStatus.NOT_MINE),
+            MineSweeperIndex2(3, 2, MineStatus.NOT_MINE),
+            MineSweeperIndex2(3, 3, MineStatus.NOT_MINE)
+        )
+
+        When("지뢰판의 (3,3)를 열면") {
+            val mineSweeperMap = MineSweeperMap2(mineSweeperIndexes)
+            val result = mineSweeperMap.open(Position(3, 3))
+            Then("해당 인덱스 상태는 열리고 근처 지뢰개수가 0인 인덱스 6개가 열린다.") {
+                result shouldBe GameStatus.CONTINUE
+                mineSweeperMap.mineSweeperIndexes.filter { it.openStatus == PositionStatus.OPENED }.size shouldBe 6
+            }
+        }
+    }
 })
