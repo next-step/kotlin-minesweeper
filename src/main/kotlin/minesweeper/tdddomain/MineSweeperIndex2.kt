314e1791a5ef236919c988b91541a7314054c154
원동재 <wdj0717@midasit.com>
2023-11-22 09:13:15 +0900

    step4: 지뢰 찾기(리팩토링) - MineSweeperIndex 클래스 openStatus 프로퍼티 추가

--- src/main/kotlin/minesweeper/tdddomain/MineSweeperIndex2.kt
+++ src/main/kotlin/minesweeper/tdddomain/MineSweeperIndex2.kt
@@ -2,5 +2,13 @@ package minesweeper.tdddomain
 
 import minesweeper.domain.MineStatus
 import minesweeper.domain.Position
+import minesweeper.domain.PositionStatus
 
-class MineSweeperIndex2(val position: Position, val mineStatus: MineStatus)
+class MineSweeperIndex2(val position: Position, val mineStatus: MineStatus) {
+    var openStatus: PositionStatus
+        private set
+
+    init {
+        openStatus = PositionStatus.CLOSED
+    }
+}
--- src/test/kotlin/minesweeper/tdddomain/MineSweeperIndex2Test.kt
+++ src/test/kotlin/minesweeper/tdddomain/MineSweeperIndex2Test.kt
@@ -4,10 +4,11 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import minesweeper.domain.MineStatus
 import minesweeper.domain.Position
+import minesweeper.domain.PositionStatus
 
 class MineSweeperIndex2Test : BehaviorSpec({
 
-    given("좌표(1,1)와 상태가 주어졌을 때") {
+    given("좌표(1,1)와 지뢰여부 상태가 주어졌을 때") {
         val position = Position(1, 1)
         val mineStatus = MineStatus.NOT_MINE
         When("지뢰 찾기 인덱스를 생성하면") {
@@ -15,6 +16,10 @@ class MineSweeperIndex2Test : BehaviorSpec({
             Then("좌표와 상태가 같다.") {
                 mineSweeperIndex.position shouldBe position
                 mineSweeperIndex.mineStatus shouldBe mineStatus
+            }
+
+            Then("열림 여부는 닫혀있다.") {
+                mineSweeperIndex.openStatus shouldBe PositionStatus.CLOSED
             }
         }
     }
