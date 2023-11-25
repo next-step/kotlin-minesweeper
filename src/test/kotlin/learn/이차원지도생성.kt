233317550bc00aca700d557d47b47c2e7231bc55
dong <dong.22@kakaoenterprise.com>
2023-11-25 14:06:50 +0900

    refactor : 불필요한 함수 호출 제거

--- src/test/kotlin/learn/이차원지도생성.kt
+++ src/test/kotlin/learn/이차원지도생성.kt
@@ -9,12 +9,10 @@ class 이차원지도생성 : StringSpec({
         val mapHeight = 10
         val mapWidth = 10
         (0 until mapHeight).forEach { row ->
-            run {
-                (0 until mapWidth).forEach { col ->
-                    run {
-                        val count = mapHeight * row + col + 1
-                        println("count=[$count] / row=[$row] / col=[$col]")
-                    }
+            (0 until mapWidth).forEach { col ->
+                run {
+                    val count = mapHeight * row + col + 1
+                    println("count=[$count] / row=[$row] / col=[$col]")
                 }
             }
         }
