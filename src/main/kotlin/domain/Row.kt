cb1ae7486f4272d2a1550305962ece8984ba9b5b
korjun1993 <corojoon93@naver.com>
2023-11-26 01:12:13 +0900

    refactor: Row 예외 문구 변경

--- src/main/kotlin/domain/Row.kt
+++ src/main/kotlin/domain/Row.kt
@@ -6,8 +6,8 @@ class Row(private val columns: SortedSet<Coordinate>) : SortedSet<Coordinate> by
     Comparable<Row> {
     init {
         require(columns.size >= MIN_SIZE) { "행에는 최소 ${MIN_SIZE}개 이상의 좌표가 포함되어야 합니다." }
-        require(columns.first().x == START_COLUMN) { "행의 첫번째 좌표의 x 값은 ${START_COLUMN}이어야 합니다." }
-        require(columns.all { it.y == columns.first().y }) { "행에 속한 좌표들의 y 값은 모두 동일해야 합니다." }
+        require(columns.first().x == START_COLUMN) { "첫번째 좌표의 x값은 ${START_COLUMN}이어야 합니다." }
+        require(columns.all { it.y == columns.first().y }) { "좌표들의 y 값은 모두 동일해야 합니다." }
         require(checkColumDiffIsOne()) { "좌표들의 x 값은 1씩 커져야합니다" }
     }
 
@@ -18,7 +18,7 @@ class Row(private val columns: SortedSet<Coordinate>) : SortedSet<Coordinate> by
     }
 
     override fun compareTo(other: Row): Int {
-        return columns.first().y - other.first().y
+        return first().y - other.first().y
     }
 
     companion object {
