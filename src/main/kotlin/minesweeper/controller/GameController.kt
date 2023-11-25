f385fba158a368c68a6d97b9e7046d4f98d397f0
parker1609 <psbum1609@gmail.com>
2023-11-26 07:14:55 +0900

    feat: 지뢰찾기 게임판의 높이를 입력받는다.

---
+++ src/main/kotlin/minesweeper/controller/GameController.kt
@@ -0,0 +1,7 @@
+package minesweeper.controller
+
+import minesweeper.view.ConsoleInput
+
+fun main() {
+    val boardHeight = ConsoleInput.inputBoardHeight()
+}
---
+++ src/main/kotlin/minesweeper/view/ConsoleInput.kt
@@ -0,0 +1,20 @@
+package minesweeper.view
+
+object ConsoleInput {
+    fun inputBoardHeight(): Int {
+        println("높이를 입력하세요.")
+
+        return inputNotEmptyString().toInt()
+    }
+
+    private fun inputNotEmptyString(): String {
+        var input: String
+
+        do {
+            input = readln()
+            if (input.isBlank()) println("공백을 입력할 수 없습니다. 다시 입력해주세요!")
+        } while (input.isBlank())
+
+        return input
+    }
+}
