3dbc2fe54ee744d868eabb21ee4581c1b2542e71
korjun1993 <corojoon93@naver.com>
2023-11-26 01:12:29 +0900

    feat: 맵에 속한 행들의 크기가 모두 동일해야 한다. 그렇지 않다면 예외가 발생한다.

--- src/main/kotlin/domain/Map.kt
+++ src/main/kotlin/domain/Map.kt
@@ -1,3 +1,11 @@
 package domain
 
-class Map(private val coordinates: Array<Row>)
+import java.util.*
+
+class Map(private val rows: SortedSet<Row>) : SortedSet<Row> by rows {
+    init {
+        require(rows.all { it.size == rows.first().size }) { "맵에 포함된 행들의 크기는 모두 동일해야 합니다." }
+    }
+
+    constructor(vararg rows: Row) : this(rows.toSortedSet())
+}
---
+++ src/test/kotlin/domain/MapTest.kt
@@ -0,0 +1,29 @@
+package domain
+
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+
+class MapTest : FunSpec({
+    context("맵에 속한 행들은") {
+        test("크기가 모두 동일해야 한다") {
+            shouldNotThrowAny {
+                Map(
+                    Row(Coordinate(x = 0, y = 0), Coordinate(x = 1, y = 0), Coordinate(x = 2, y = 0)),
+                    Row(Coordinate(x = 0, y = 1), Coordinate(x = 1, y = 1), Coordinate(x = 2, y = 1)),
+                    Row(Coordinate(x = 0, y = 2), Coordinate(x = 1, y = 2), Coordinate(x = 2, y = 2)),
+                )
+            }
+        }
+
+        test("크기가 다르다면 예외가 발생한다.") {
+            shouldThrow<IllegalArgumentException> {
+                Map(
+                    Row(Coordinate(x = 0, y = 0), Coordinate(x = 1, y = 0), Coordinate(x = 2, y = 0)),
+                    Row(Coordinate(x = 0, y = 1)),
+                    Row(Coordinate(x = 0, y = 2), Coordinate(x = 1, y = 2), Coordinate(x = 2, y = 2)),
+                )
+            }
+        }
+    }
+})
