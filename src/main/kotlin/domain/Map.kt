63cd49eb0112e2bf177b294f3b3a756d04963a21
korjun1993 <corojoon93@naver.com>
2023-11-26 01:53:13 +0900

    refactor: 무분별한 data 키워드 제거

--- src/main/kotlin/domain/Map.kt
+++ src/main/kotlin/domain/Map.kt
@@ -2,7 +2,7 @@ package domain
 
 import java.util.*
 
-data class Map(private val rows: SortedSet<Row>) : SortedSet<Row> by rows {
+class Map(private val rows: SortedSet<Row>) : SortedSet<Row> by rows {
     init {
         require(rows.all { it.size == first().size }) { "맵에 포함된 행들의 크기는 모두 동일해야 합니다." }
         require(rows.all { first().getY() == START_ROW }) { "맵의 행들의 y값은 ${START_ROW}부터 시작해야 합니다." }
