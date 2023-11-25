a53d8a4cb8670dc7c3af9b81f8702889cd6e0358
korjun1993 <corojoon93@naver.com>
2023-11-25 17:42:35 +0900

    feat: 같은 행에 속하는 좌표들은 y 값이 모두 동일해야 한다.

--- src/test/kotlin/domain/RowTest.kt
+++ src/test/kotlin/domain/RowTest.kt
@@ -8,5 +8,11 @@ class RowTest : FunSpec(
         test("행에 최소 1개 이상의 좌표가 존재한다") {
             shouldThrow<IllegalArgumentException> { Row() }
         }
+
+        test("같은 행에 속하는 좌표들은 y 값이 모두 동일해야 한다") {
+            shouldThrow<IllegalArgumentException> {
+                Row(Coordinate(x = 1, y = 1), Coordinate(x = 2, y = 2))
+            }
+        }
     }
 )
