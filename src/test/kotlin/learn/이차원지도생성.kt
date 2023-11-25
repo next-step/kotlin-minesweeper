2ec353ea473cd623faedda76010038baffc41392
dong <dong.22@kakaoenterprise.com>
2023-11-25 12:35:32 +0900

    test : 2중 포문을 코틀린스러운코드로 작성하는 학습테스트를 작성

--- src/test/kotlin/learn/이차원지도생성.kt
+++ src/test/kotlin/learn/이차원지도생성.kt
@@ -8,13 +8,12 @@ class 이차원지도생성 : StringSpec({
         println("ho")
         val mapHeight = 10
         val mapWidth = 10
-        (0 until  mapHeight).forEach { row ->
+        (0 until mapHeight).forEach { row ->
             run {
                 (0 until mapWidth).forEach { col ->
                     run {
-                        val count = mapHeight*row + col +1
+                        val count = mapHeight * row + col + 1
                         println("count=[$count] / row=[$row] / col=[$col]")
-
                     }
                 }
             }
