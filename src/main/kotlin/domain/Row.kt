bcfdaeca18b14fa2adee9acdb11668a128d72256
korjun1993 <corojoon93@naver.com>
2023-11-25 23:20:58 +0900

    feat: 행의 좌표들의 x값은 0부터 시작해야 한다. 그렇지 않으면 예외가 발생한다.

--- src/main/kotlin/domain/Row.kt
+++ src/main/kotlin/domain/Row.kt
@@ -5,6 +5,7 @@ import java.util.SortedSet
 class Row(private val columns: SortedSet<Coordinate>) : SortedSet<Coordinate> by columns {
     init {
         require(columns.size >= MIN_SIZE) { "행에는 최소 $MIN_SIZE 이상의 좌표가 포함되어야 합니다." }
+        require(columns.first().x == 0) { "행의 첫번째 좌표의 x 값은 0부터 시작해야 합니다." }
         require(columns.all { columns.first().y == it.y }) { "같은 행에 속한 좌표들의 y 값은 모두 동일해야 합니다." }
         require(checkColumDiffIsOne()) { "좌표들의 x 값은 1씩 커져야합니다" }
     }
--- src/test/kotlin/domain/RowTest.kt
+++ src/test/kotlin/domain/RowTest.kt
@@ -13,32 +13,54 @@ class RowTest : FunSpec(
 
         test("같은 행에 속하는 좌표들은 y 값이 모두 동일해야 한다") {
             shouldThrow<IllegalArgumentException> {
-                Row(Coordinate(x = 1, y = 1), Coordinate(x = 2, y = 2))
+                Row(Coordinate(x = 0, y = 1), Coordinate(x = 1, y = 2))
             }
         }
 
         test("행에 x, y 값이 동일한 좌표가 두 개 이상 포함될 수 없다") {
-            Row(Coordinate(x = 1, y = 1), Coordinate(x = 1, y = 1)).size shouldBe 1
+            Row(Coordinate(x = 0, y = 0), Coordinate(x = 0, y = 0)).size shouldBe 1
         }
 
         context("행의 좌표들의 x값은") {
-            test(" 1씩 커져야 한다") {
+            test("1씩 커져야 한다") {
                 shouldNotThrowAny {
                     Row(
+                        Coordinate(x = 0, y = 1),
                         Coordinate(x = 1, y = 1),
                         Coordinate(x = 2, y = 1),
-                        Coordinate(x = 3, y = 1),
+                        Coordinate(x = 3, y = 1)
+                    )
+                }
+            }
+            test("1씩 커지지 않으면 예외가 발생한다") {
+                shouldThrow<IllegalArgumentException> {
+                    Row(
+                        Coordinate(x = 0, y = 1),
+                        Coordinate(x = 1, y = 1),
+                        Coordinate(x = 2, y = 1),
                         Coordinate(x = 4, y = 1)
                     )
                 }
             }
-            test(" 그렇지 않으면 예외가 발생한다") {
+
+            test("0부터 시작해야 한다") {
+                shouldNotThrowAny {
+                    Row(
+                        Coordinate(x = 0, y = 1),
+                        Coordinate(x = 1, y = 1),
+                        Coordinate(x = 2, y = 1),
+                        Coordinate(x = 3, y = 1)
+                    )
+                }
+            }
+
+            test("0부터 시작하지 않으면 예외가 발생한다") {
                 shouldThrow<IllegalArgumentException> {
                     Row(
                         Coordinate(x = 1, y = 1),
                         Coordinate(x = 2, y = 1),
                         Coordinate(x = 3, y = 1),
-                        Coordinate(x = 5, y = 1)
+                        Coordinate(x = 4, y = 1)
                     )
                 }
             }
