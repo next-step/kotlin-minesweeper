6f57f7aaeea67c18f44d1fc9b44373ee13278ba0
ALbertIM0427 <guang7226961@naver.com>
2023-11-26 23:38:24 +0900

    feat: 위치를 추가할 때 조건에 만족하면 주변을 open 하는 기능 구현

--- src/main/kotlin/business/OpenedCells.kt
+++ src/main/kotlin/business/OpenedCells.kt
@@ -6,14 +6,22 @@ class OpenedCells(private val height: Int, private val width: Int, openedPoints:
     val openedPoints: List<Point>
         get() = _openedPoints.toList()
 
-    fun add(point: Point) {
+    fun add(point: Point, mines: Mines) {
         if (point.x < 0 || point.x >= height || point.y < 0 || point.y >= width) return
         if (_openedPoints.contains(point)) return
         _openedPoints.add(point)
+        if (mines.countMineAround(point) == 0) {
+            openAround(point, mines)
+        }
     }
 
-    fun openAround(point: Point, mines: Mines) = point.aroundPoints()
-        .filter { !mines.contains(it) }
-        .filter { !openedPoints.contains(it) }
-        .forEach { add(it) }
+    private fun openAround(point: Point, mines: Mines) =
+        point.aroundPoints()
+            .filter { !mines.contains(it) }
+            .filter { !openedPoints.contains(it) }
+            .forEach { add(it, mines) }
+
+    fun contains(point: Point): Boolean {
+        return _openedPoints.contains(point)
+    }
 }
--- src/test/kotlin/business/OpenedCellsTest.kt
+++ src/test/kotlin/business/OpenedCellsTest.kt
@@ -5,50 +5,80 @@ import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class OpenedCellsTest {
-    @Test
-    fun `open위치를 추가한다`() {
-        // given
-        val openedCells = OpenedCells(5, 5, listOf(Point(1, 1)))
-        val point = Point(1, 2)
-
-        // when
-        openedCells.add(point)
-
-        // then
-        openedCells.openedPoints shouldBe listOf(Point(1, 1), Point(1, 2))
-    }
-
+    // O - Opened
+    // C - Closed
+    // * - Mine
     // before
     // * C C C C
-    // C 1 C C C
+    // C C C C C
     // C C C C C
     // C C C C C
     // C C C C C
     // after openAround(1, 1)
-    // * 0 0 C C
-    // 0 1 0 C C
-    // 0 0 0 C C
+    // * C C C C
+    // C O C C C
     // C C C C C
     // C C C C C
+    // C C C C C
+    @Test
+    fun `open위치를 추가한다`() {
+        // given
+        val openedCells = OpenedCells(5, 5)
+        val mines = Mines(listOf(Mine(Point(0, 0))))
+
+        // when
+        openedCells.add(Point(1, 1), mines)
+
+        // then
+        openedCells.openedPoints shouldBe listOf(Point(1, 1))
+    }
+
+    // O - Opened
+    // C - Closed
+    // * - Mine
+    // before
+    // * C C * C
+    // C O C C *
+    // C C C * C
+    // C C C C C
+    // C C C * C
+    // after openAround(3, 1)
+    // * C C * C
+    // O O O C *
+    // O O O * C
+    // O O O C C
+    // O O O * C
     @Test
     fun `주변에 지뢰가 없는 위치를 open한다`() {
         // given
         val openedCells = OpenedCells(5, 5, listOf(Point(1, 1)))
-        val mines = Mines(listOf(Mine(Point(0, 0))))
+        val mines = Mines(
+            listOf(
+                Mine(Point(0, 0)),
+                Mine(Point(0, 3)),
+                Mine(Point(1, 4)),
+                Mine(Point(2, 3)),
+                Mine(Point(4, 3))
+            )
+        )
 
         // when
-        openedCells.openAround(Point(1, 1), mines)
+        openedCells.add(Point(3, 1), mines)
 
         // then
         openedCells.openedPoints shouldContainExactlyInAnyOrder listOf(
             Point(1, 0),
             Point(1, 1),
             Point(1, 2),
-            Point(0, 1),
-            Point(0, 2),
             Point(2, 0),
             Point(2, 1),
-            Point(2, 2)
+            Point(2, 2),
+            Point(3, 0),
+            Point(3, 1),
+            Point(3, 2),
+            Point(4, 0),
+            Point(4, 1),
+            Point(4, 2)
         )
     }
 }
