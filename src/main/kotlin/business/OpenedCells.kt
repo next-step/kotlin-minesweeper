f8ffa0db356b3a6238b71bb52231e52734a4a648
ALbertIM0427 <guang7226961@naver.com>
2023-11-26 22:55:43 +0900

    feat: 주변에 지뢰가 없는 위치를 open하는 기능 구현

--- src/main/kotlin/business/OpenedCells.kt
+++ src/main/kotlin/business/OpenedCells.kt
@@ -1,12 +1,19 @@
 package business
 
-class OpenedCells(openedPoints: List<Point> = listOf()) {
+class OpenedCells(private val height: Int, private val width: Int, openedPoints: List<Point> = listOf()) {
     private val _openedPoints = openedPoints.toMutableList()
 
     val openedPoints: List<Point>
         get() = _openedPoints.toList()
 
     fun add(point: Point) {
+        if (point.x < 0 || point.x >= height || point.y < 0 || point.y >= width) return
+        if (_openedPoints.contains(point)) return
         _openedPoints.add(point)
     }
+
+    fun openAround(point: Point, mines: Mines) = point.aroundPoints()
+        .filter { !mines.contains(it) }
+        .filter { !openedPoints.contains(it) }
+        .forEach { add(it) }
 }
--- src/test/kotlin/business/OpenedCellsTest.kt
+++ src/test/kotlin/business/OpenedCellsTest.kt
@@ -1,5 +1,6 @@
 package business
 
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -7,7 +8,7 @@ class OpenedCellsTest {
     @Test
     fun `open위치를 추가한다`() {
         // given
-        val openedCells = OpenedCells(listOf(Point(1, 1)))
+        val openedCells = OpenedCells(5, 5, listOf(Point(1, 1)))
         val point = Point(1, 2)
 
         // when
@@ -15,5 +16,39 @@ class OpenedCellsTest {
 
         // then
         openedCells.openedPoints shouldBe listOf(Point(1, 1), Point(1, 2))
+    }
+
+    // before
+    // * C C C C
+    // C 1 C C C
+    // C C C C C
+    // C C C C C
+    // C C C C C
+    // after openAround(1, 1)
+    // * 0 0 C C
+    // 0 1 0 C C
+    // 0 0 0 C C
+    // C C C C C
+    // C C C C C
+    @Test
+    fun `주변에 지뢰가 없는 위치를 open한다`() {
+        // given
+        val openedCells = OpenedCells(5, 5, listOf(Point(1, 1)))
+        val mines = Mines(listOf(Mine(Point(0, 0))))
+
+        // when
+        openedCells.openAround(Point(1, 1), mines)
+
+        // then
+        openedCells.openedPoints shouldContainExactlyInAnyOrder listOf(
+            Point(1, 0),
+            Point(1, 1),
+            Point(1, 2),
+            Point(0, 1),
+            Point(0, 2),
+            Point(2, 0),
+            Point(2, 1),
+            Point(2, 2)
+        )
     }
 }
