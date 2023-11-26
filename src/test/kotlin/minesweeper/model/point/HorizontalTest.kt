b2ade026de865480d88db3069b8443b1ecbb6ae5
dong <dong.22@kakaoenterprise.com>
2023-11-26 19:55:25 +0900

    test : 테스트코드 추가

--- src/test/kotlin/minesweeper/model/point/HorizontalTest.kt
+++ src/test/kotlin/minesweeper/model/point/HorizontalTest.kt
@@ -1,11 +1,24 @@
 package minesweeper.model.point
 
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 
 class HorizontalTest : StringSpec({
 
-    "음수가 입력되면 throw" {
-        TODO()
+    "Horizontal 의 value 는 0과 양수만을 허용 한다 " {
+        shouldNotThrow<IllegalArgumentException> {
+            Horizontal(0)
+            Horizontal(1)
+            Horizontal(100)
+            Horizontal(99999)
+        }
     }
 
+    "Horizontal 의 value 에 음수가 입력되면 throw IllegalArgumentException" {
+        shouldThrow<IllegalArgumentException> {
+            Horizontal(-1)
+            Horizontal(-2)
+        }
+    }
 })
--- src/test/kotlin/minesweeper/model/point/VerticalTest.kt
+++ src/test/kotlin/minesweeper/model/point/VerticalTest.kt
@@ -1,12 +1,24 @@
 package minesweeper.model.point
 
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
 
 class VerticalTest : StringSpec({
 
-    "음수가 입력되면 throw" {
-        TODO()
+    "Vertical 의 value 는 0과 양수만을 허용 한다 " {
+        shouldNotThrow<IllegalArgumentException> {
+            Vertical(0)
+            Vertical(1)
+            Vertical(100)
+            Vertical(99999)
+        }
     }
 
+    "Vertical 의 value 에 음수가 입력되면 throw IllegalArgumentException" {
+        shouldThrow<IllegalArgumentException> {
+            Vertical(-1)
+            Vertical(-2)
+        }
+    }
 })
