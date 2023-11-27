52dce7200688839ffe1bb1f5ba06055573cef002
dong <dong.22@kakaoenterprise.com>
2023-11-27 13:21:23 +0900

    bugfix : 최대로 생성 가능한 지뢰 숫자를 넘어서는 입력을 막는 코드 추가

--- src/main/kotlin/minesweeper/model/board/impl/EvenlyStrategy.kt
+++ src/main/kotlin/minesweeper/model/board/impl/EvenlyStrategy.kt
@@ -10,6 +10,7 @@ class EvenlyStrategy(
 ) : MineDeployStrategy {
 
     override fun deployPoints(verticalLimit: Int, horizontalLimit: Int): Points {
+        requireMineCountLimit(verticalLimit * horizontalLimit, countOfMines)
         val map = (0 until (verticalLimit * horizontalLimit))
             .asSequence()
             .shuffled()
@@ -17,6 +18,10 @@ class EvenlyStrategy(
             .map { coordinateOrderOf(it, verticalLimit, horizontalLimit) to Attribute.MINE }
             .toMap()
         return Points(map)
+    }
+
+    private fun requireMineCountLimit(limitMineCounts: Int, countOfMines: Int) {
+        require(limitMineCounts >= countOfMines) { "요청된 $countOfMines 개의 지뢰는 생성할 수 없습니다. 지뢰의 최대 생성 가능 수는 $limitMineCounts 개 입니다. " }
     }
 
     private fun coordinateOrderOf(order: Int, verticalLimit: Int, horizontalLimit: Int): Coordinate {
