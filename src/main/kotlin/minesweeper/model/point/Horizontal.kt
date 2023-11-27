bc86716f5cf4e79b1698a98a6599d9784976effb
dong <dong.22@kakaoenterprise.com>
2023-11-27 13:23:28 +0900

    refactor : 입력값을 에러메시지에 출력하도록 수정

--- src/main/kotlin/minesweeper/model/point/Horizontal.kt
+++ src/main/kotlin/minesweeper/model/point/Horizontal.kt
@@ -5,6 +5,6 @@ value class Horizontal(
     private val value: Int,
 ) {
     init {
-        require(value >= 0) { "음수는 허용되지 않습니다" }
+        require(value >= 0) { "Horizontal 에 입력된 ${value}으로 생성이 불가능합니다. 0 혹은 양의 정수만 허용됩니다" }
     }
 }
