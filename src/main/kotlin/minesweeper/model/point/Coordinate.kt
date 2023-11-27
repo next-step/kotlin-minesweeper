9f940f3d645c251629faa14e6d29e96e38f21644
dong <dong.22@kakaoenterprise.com>
2023-11-27 20:51:40 +0900

    refactor : 불필요한 주석 제거

--- src/main/kotlin/minesweeper/model/point/Coordinate.kt
+++ src/main/kotlin/minesweeper/model/point/Coordinate.kt
@@ -15,13 +15,6 @@ data class Coordinate(
         return vertical.movePossible(delta.verticalDelta, verticalLimit) && horizontal.movePossible(delta.horizontalDelta, horizontalLimit)
     }
 
-    // constructor(vertical: Int, horizontal: Int) : this(Vertical(vertical), Horizontal(horizontal))
-    /*
-    이슈 해결 필요
-    Platform declaration clash: The following declarations have the same JVM signature (<init>(II)V):
-    constructor Coordinate(vertical: Int, horizontal: Int) defined in minesweeper.model.point.Coordinate
-    constructor Coordinate(vertical: Vertical, horizontal: Horizontal) defined in minesweeper.model.point.Coordinate
-     */
     companion object {
         fun of(vertical: Int, horizontal: Int): Coordinate {
             return Coordinate(Vertical(vertical), Horizontal(horizontal))
