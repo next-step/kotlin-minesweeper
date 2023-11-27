fee4a5d2975e5dd1b741ec723df7e7ea9b52d4e5
dong <dong.22@kakaoenterprise.com>
2023-11-27 20:32:47 +0900

    feat : 너무 길이가 긴 메서드를 두줄로 분리

--- src/main/kotlin/minesweeper/Controller.kt
+++ src/main/kotlin/minesweeper/Controller.kt
@@ -3,11 +3,13 @@ package minesweeper
 import minesweeper.model.board.Board
 import minesweeper.view.InputView
 import minesweeper.view.OutputView
+import minesweeper.view.reder.impl.AdjacentMineCountRenderingStrategy
 
 fun main() {
     val mapHeight: Int = InputView.mapHeight()
     val mapWidth: Int = InputView.mapWidth()
     val minesCount: Int = InputView.countOfMines()
-    val board: Board = Board(minesCount, mapHeight, mapWidth)
-    OutputView().printMineMap(board)
+    val board = Board(minesCount, mapHeight, mapWidth)
+    val outputView = OutputView(AdjacentMineCountRenderingStrategy(board))
+    outputView.printMineMap(board)
 }
