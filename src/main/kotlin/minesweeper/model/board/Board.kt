ed2688a857e5d52c2013923710adcbe5b6beb2e4
dong <dong.22@kakaoenterprise.com>
2023-11-27 20:26:53 +0900

    feat : 주변 지뢰의 갯수를 리턴하는 메서드 기능 추가

--- src/main/kotlin/minesweeper/model/board/Board.kt
+++ src/main/kotlin/minesweeper/model/board/Board.kt
@@ -1,7 +1,10 @@
 package minesweeper.model.board
 
 import minesweeper.model.board.impl.EvenlyStrategy
+import minesweeper.model.point.Attribute
 import minesweeper.model.point.Coordinate
+import minesweeper.model.point.Delta
+import minesweeper.model.point.Delta.Companion.deltas
 import minesweeper.model.point.Points
 
 class Board(
@@ -24,7 +27,25 @@ class Board(
         return points.countOfMine()
     }
 
-    fun adjacentMineCount(coordinate: Coordinate) {
-        TODO("Not yet implemented")
+    fun adjacentMineCount(coordinate: Coordinate): Int {
+        return this.adjacentPointTraversal(coordinate)
+            .asSequence()
+            .map { points.attribute(it) }
+            .count { it == Attribute.MINE }
+    }
+
+    private fun adjacentPointTraversal(coordinate: Coordinate): List<Coordinate> {
+        return deltas.asSequence()
+            .filter { delta -> inRange(coordinate, delta) }
+            .map { coordinate.next(it) }
+            .toList()
+    }
+
+    private fun inRange(coordinate: Coordinate, delta: Delta): Boolean {
+        return coordinate.movePossible(
+            delta = delta,
+            verticalLimit = verticalSize,
+            horizontalLimit = horizontalSize
+        )
     }
 }
