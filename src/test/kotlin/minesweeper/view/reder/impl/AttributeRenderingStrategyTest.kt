80a33681f58ec2d5295b8b03f5b974c013ed0439
dong <dong.22@kakaoenterprise.com>
2023-11-27 14:16:31 +0900

    refactor : 어떻게 콘솔에 출력 될 것인지에 대한 테스트코드 추가

---
+++ src/test/kotlin/minesweeper/view/reder/impl/AttributeRenderingStrategyTest.kt
@@ -0,0 +1,15 @@
+package minesweeper.view.reder.impl
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import minesweeper.model.point.Attribute
+
+class AttributeRenderingStrategyTest : StringSpec({
+
+    "AttributeRenderingStrategy 전략은 Attribute 과 symbolString 이 1:1 매칭 되어야 한다" {
+        AttributeRenderingStrategy.symbol(Attribute.NONE) shouldBe "C"
+        AttributeRenderingStrategy.symbol(Attribute.MINE) shouldBe "*"
+        AttributeRenderingStrategy.symbol(Attribute.FLAG) shouldBe "F"
+    }
+
+})
