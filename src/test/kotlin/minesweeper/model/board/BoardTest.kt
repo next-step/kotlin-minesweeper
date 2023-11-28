8395dd4adb280a4ca463cb74b2c2e9fd2b85a9d0
dong <dong.22@kakaoenterprise.com>
2023-11-29 01:30:23 +0900

    refactor : named argment 와 trailling comma 를 사용하도록 수정

--- src/test/kotlin/minesweeper/model/board/BoardTest.kt
+++ src/test/kotlin/minesweeper/model/board/BoardTest.kt
@@ -6,22 +6,31 @@ import io.kotest.matchers.shouldBe
 class BoardTest : StringSpec({
     "입력한 수만큼 지뢰가 생성 되어야 한다" {
         val countOfMines = 22
-        val board = Board(countOfMines, 10, 10)
+        val board = Board(
+            mineCount = countOfMines,
+            limit = (10 to 10).toBoardLimit(),
+        )
 
         board.minesCount() shouldBe 22
     }
 
     "입력한 horizontal size 만큼의 지도가 생성 되어야 한다" {
         val horizontal = 17
-        val board = Board(5, 10, horizontal)
+        val board = Board(
+            mineCount = 5,
+            limit = (10 to horizontal).toBoardLimit(),
+        )
 
-        board.horizontalSize shouldBe horizontal
+        board.limit.horizontalLimit.value shouldBe horizontal
     }
 
     "입력한 vertical size 만큼의 지도가 생성 되어야 한다" {
         val vertical = 13
-        val board = Board(1, vertical, 10)
+        val board = Board(
+            mineCount = 1,
+            limit = (vertical to 10).toBoardLimit(),
+        )
 
-        board.verticalSize shouldBe vertical
+        board.limit.verticalLimit.value shouldBe vertical
     }
 })
