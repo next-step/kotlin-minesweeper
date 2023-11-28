99864921c4176e3c8b930e1c6c4feebc9ff8063d
jaylene shin <jaylene.shin@kakaocorp.com>
2023-11-28 15:07:11 +0900

    InputView로 받은 정보 맵핑할 DTO 생성

---
+++ src/test/kotlin/minesweeper/domain/MineMapGeneratorTest.kt
@@ -0,0 +1,32 @@
+package minesweeper.domain
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MineMapGeneratorTest {
+    @Test
+    fun `지뢰 맵을 구성할 위치 정보가 주어졌을 때 MineMap을 생성할 수 있다`() {
+        // given
+        val mineMapMeta = MineMapMeta(3, 3, 3)
+        val minePositions = setOf(
+            Position(1, 1),
+            Position(2, 2),
+            Position(3, 3)
+        )
+        val emptyPositions = setOf(
+            Position(1, 2),
+            Position(1, 3),
+            Position(2, 1),
+            Position(2, 3),
+            Position(3, 1),
+            Position(3, 2)
+        )
+
+        // when
+        val mineMapGenerator = MineMapGenerator(mineMapMeta)
+        val mineMap = mineMapGenerator.generate(minePositions, emptyPositions)
+
+        // then
+        assertEquals(mineMapMeta.getCellCount(), mineMap.values.size)
+    }
+}
