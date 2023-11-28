4bf62bc42228c11cdaecaaa55fb5b17c530f3f3f
dong <dong.22@kakaoenterprise.com>
2023-11-29 00:49:17 +0900

    refactor : 코틀린 컨벤션 순서 참조

--- src/main/kotlin/minesweeper/model/point/Vertical.kt
+++ src/main/kotlin/minesweeper/model/point/Vertical.kt
@@ -5,6 +5,10 @@ value class Vertical(
     private val value: Int,
 ) {
 
+    init {
+        require(value >= 0) { "Vertical 에 입력된 ${value}으로 생성이 불가능합니다. 0 혹은 양의 정수만 허용됩니다" }
+    }
+
     fun moveTo(delta: Int): Vertical {
         return Vertical(value + delta)
     }
@@ -13,7 +17,4 @@ value class Vertical(
         return (delta + value) in 0..verticalLimit
     }
 
-    init {
-        require(value >= 0) { "Vertical 에 입력된 ${value}으로 생성이 불가능합니다. 0 혹은 양의 정수만 허용됩니다" }
-    }
 }
