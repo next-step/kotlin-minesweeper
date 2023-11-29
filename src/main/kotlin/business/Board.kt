53121a5ecd175b14add433b0834f354d9826ace2
ALbertIM0427 <guang7226961@naver.com>
2023-11-29 20:56:25 +0900

    feat: 셀 주변에 지뢰인 칸의 수를 계산하는 기능

--- src/main/kotlin/business/Board.kt
+++ src/main/kotlin/business/Board.kt
@@ -3,4 +3,13 @@ package business
 class Board(cells: List<List<Cell>>) {
     private val _cells: List<List<Cell>> = cells
     fun isMine(point: Point): Boolean = _cells[point.x][point.y].isMine()
+    fun countMines(point: Point): Int = point.aroundPoints()
+        .filter { isValidPoint(it) }
+        .count { isMine(it) }
+
+    private fun isValidPoint(it: Point): Boolean = it.x in _cells.indices && it.y in _cells[FIRST_INDEX].indices
+
+    companion object {
+        const val FIRST_INDEX = 0
+    }
 }
--- src/test/kotlin/business/BoardTest.kt
+++ src/test/kotlin/business/BoardTest.kt
@@ -37,4 +37,38 @@ class BoardTest {
         // then
         result shouldBe false
     }
+
+    @Test
+    fun `특정 위치의 주변에 지뢰가 없으면 0`() {
+        // given
+        val board = Board(
+            listOf(
+                listOf(Cell(CellStatus.EMPTY), Cell(CellStatus.EMPTY)),
+                listOf(Cell(CellStatus.EMPTY), Cell(CellStatus.EMPTY)),
+            )
+        )
+
+        // when
+        val result = board.countMines(Point(0, 0))
+
+        // then
+        result shouldBe 0
+    }
+
+    @Test
+    fun `특정 위치의 주변에 지뢰가 개수를 계산한다`() {
+        // given
+        val board = Board(
+            listOf(
+                listOf(Cell(CellStatus.MINE), Cell(CellStatus.EMPTY)),
+                listOf(Cell(CellStatus.EMPTY), Cell(CellStatus.EMPTY)),
+            )
+        )
+
+        // when
+        val result = board.countMines(Point(0, 1))
+
+        // then
+        result shouldBe 1
+    }
 }
