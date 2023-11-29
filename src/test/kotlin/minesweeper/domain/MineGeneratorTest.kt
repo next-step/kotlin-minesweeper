8ba46fb05e5a1d443625bcbfd64f01892ccde00f
yeongun <gjduddns@gmail.com>
2023-11-29 20:04:31 +0900

    test: 지뢰 생성기 테스트에서 height, width을 given 절로 이동

--- src/test/kotlin/minesweeper/domain/MineGeneratorTest.kt
+++ src/test/kotlin/minesweeper/domain/MineGeneratorTest.kt
@@ -5,9 +5,11 @@ import io.kotest.matchers.shouldBe
 
 class MineGeneratorTest : BehaviorSpec({
     Given("지뢰 개수가 주어지면") {
+        val height = Size(10)
+        val width = Size(10)
         val count = Size(10)
         When("지뢰 생성기는") {
-            val mineGenerator = RandomMineGenerator(Size(10), Size(10))
+            val mineGenerator = RandomMineGenerator(height, width)
             val mines = mineGenerator.generate(count)
             Then("해당 개수에 맞는 지뢰들을 생성한다.") {
                 mines.mines.size shouldBe count.value
