3c74467975fc397a35bff6892a21e3df844c6bfe
korjun1993 <corojoon93@naver.com>
2023-11-29 14:58:30 +0900

    refactor: class layout convention

--- src/main/kotlin/domain/Coordinate.kt
+++ src/main/kotlin/domain/Coordinate.kt
@@ -5,11 +5,6 @@ open class Coordinate(val x: Int, val y: Int) : Comparable<Coordinate> {
         require(x >= MIN_X && y >= MIN_Y) { "x 좌표는 최소 ${MIN_X}, y 좌표는 최소 $MIN_Y 이상을 만족해야합니다." }
     }
 
-    companion object {
-        const val MIN_X = 0
-        const val MIN_Y = 0
-    }
-
     override fun compareTo(other: Coordinate): Int {
         return if (this.y != other.y) {
             this.y - other.y
@@ -34,5 +29,10 @@ open class Coordinate(val x: Int, val y: Int) : Comparable<Coordinate> {
         var result = x
         result = 31 * result + y
         return result
+    }
+
+    companion object {
+        const val MIN_X = 0
+        const val MIN_Y = 0
     }
 }
