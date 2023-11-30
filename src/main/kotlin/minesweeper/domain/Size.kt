cc9f3c6a4fd50445166e4b31ac8f4b19bb84bfd3
yeongun <gjduddns@gmail.com>
2023-11-30 22:33:21 +0900

    test: Size 테스트 추가

--- src/main/kotlin/minesweeper/domain/Size.kt
+++ src/main/kotlin/minesweeper/domain/Size.kt
@@ -7,25 +7,25 @@ data class Size(val value: Int) {
         require(value in 1..100) { "1 이상 100 이하의 숫자여야만 합니다." }
     }
 
-    operator fun times(other: Size): Size {
-        return Size(value * other.value)
-    }
-
     fun getNumbers(): List<Size> {
         return (1..value)
             .map { Size(it) }
     }
 
-    operator fun div(height: Size): Size {
-        return Size(value / height.value + 1)
-    }
-
-    operator fun rem(width: Size): Size {
-        return Size(value % width.value + 1)
-    }
-
     fun getRows(x: Size): List<Position> {
         return getNumbers()
             .map { Position(x, it) }
+    }
+
+    operator fun times(other: Size): Size {
+        return Size(value * other.value)
+    }
+
+    operator fun div(other: Size): Size {
+        return Size(value / other.value + 1)
+    }
+
+    operator fun rem(other: Size): Size {
+        return Size(value % other.value + 1)
     }
 }
--- src/test/kotlin/minesweeper/domain/SizeTest.kt
+++ src/test/kotlin/minesweeper/domain/SizeTest.kt
@@ -5,6 +5,7 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
+import io.kotest.matchers.shouldBe
 
 class SizeTest : BehaviorSpec({
     Given("1 이상 100 이하의 숫자가 주어지면") {
@@ -37,6 +38,36 @@ class SizeTest : BehaviorSpec({
                         Size(value)
                     }
                 }
+            }
+        }
+    }
+
+    Given("곱하려는 다른 사이즈가 주어지면") {
+        val other = Size(6)
+        When("사이즈는") {
+            val actual = Size(5) * other
+            Then("곱한 값 원소로 갖는 사이즈를 반환한다.") {
+                actual shouldBe Size(30)
+            }
+        }
+    }
+
+    Given("나누려는 다른 사이즈가 주어지면") {
+        val other = Size(5)
+        When("사이즈는") {
+            val actual = Size(10) / other
+            Then("몫에서 1을 더한 값을 원소로 갖는 사이즈를 반환한다.") {
+                actual shouldBe Size(3)
+            }
+        }
+    }
+
+    Given("나머지를 계산하려는 다른 사이즈가 주어지면") {
+        val other = Size(4)
+        When("사이즈는") {
+            val actual = Size(7) % other
+            Then("나머지에서 1을 더한 값을 원소로 갖는 사이즈를 반환한다.") {
+                actual shouldBe Size(4)
             }
         }
     }
