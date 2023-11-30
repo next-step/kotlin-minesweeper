e6681edf4c011c61b9c5e394094b4c7253c0453c
loren-hyuni <loren.hyuni@kakaocorp.com>
2023-11-30 16:24:58 +0900

    mission: step4 지뢰찾기 (리펙토링) spot을 열면 열린 상태가 된다

--- src/main/kotlin/domain/Spot.kt
+++ src/main/kotlin/domain/Spot.kt
@@ -1,8 +1,12 @@
 package domain
 
-class Spot(private val status: SpotStatus = SpotStatus.CLOSED) {
+class Spot(private var status: SpotStatus = SpotStatus.CLOSED) {
 
     fun isOpen(): Boolean {
         return status == SpotStatus.OPENED
+    }
+
+    fun open() {
+        status = SpotStatus.OPENED
     }
 }
--- src/test/kotlin/domain/SpotTest.kt
+++ src/test/kotlin/domain/SpotTest.kt
@@ -2,10 +2,17 @@ package domain
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
 
 class SpotTest : StringSpec({
     "Spot은 초기에 닫힌 상태이다" {
         val spot = Spot()
         spot.isOpen().shouldBeFalse()
+    }
+
+    "Spot을 열면 open 상태가 된다" {
+        val spot = Spot()
+        spot.open()
+        spot.isOpen().shouldBeTrue()
     }
 })
