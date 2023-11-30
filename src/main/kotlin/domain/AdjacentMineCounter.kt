56da5281fc29c0e99e77f40daa3a29372c7870b9
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-12-01 02:09:38 +0900

    [step2] (refactor) 주변 셀 위치를 위한 일급컬렐션 적용

--- src/main/kotlin/domain/AdjacentMineCounter.kt
+++ src/main/kotlin/domain/AdjacentMineCounter.kt
@@ -4,18 +4,7 @@ import inteface.MineCounter
 
 class AdjacentMineCounter : MineCounter {
     override fun countMinesAround(board: Board, position: Position): Int {
-        val neighborOffsets = listOf(
-            Position(-1, -1), Position(0, -1), Position(1, -1),
-            Position(-1, 0),  Position(1, 0),
-            Position(-1, 1), Position(0, 1), Position(1, 1)
-        )
-
-        return neighborOffsets
-            .map { offset -> Position(position.x + offset.x, position.y + offset.y) }
-            .filter { it.isWithinBoardBounds(board.height, board.width) && it != position }
-            .count { board.hasMineAt(it) }
+        val neighborPositions = NeighborPositions(position, board.height, board.width)
+        return neighborPositions.positions.count { board.hasMineAt(it) }
     }
-
-    private fun Position.isWithinBoardBounds(boardHeight: Int, boardWidth: Int): Boolean =
-        x in 0 until boardWidth && y in 0 until boardHeight
 }
