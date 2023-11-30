53b55a54aaa0a55ddf63331a9aaf3dffc3ccbe79
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-12-01 02:03:02 +0900

    [step2] (refactor) 주변 셀 및 x축 y축에 대한 네이밍 리팩토링

--- src/main/kotlin/domain/AdjacentMineCounter.kt
+++ src/main/kotlin/domain/AdjacentMineCounter.kt
@@ -4,19 +4,18 @@ import inteface.MineCounter
 
 class AdjacentMineCounter : MineCounter {
     override fun countMinesAround(board: Board, position: Position): Int {
-        return position.getNeighborPositions(board.height, board.width)
+        val neighborOffsets = listOf(
+            Position(-1, -1), Position(0, -1), Position(1, -1),
+            Position(-1, 0),  Position(1, 0),
+            Position(-1, 1), Position(0, 1), Position(1, 1)
+        )
+
+        return neighborOffsets
+            .map { offset -> Position(position.x + offset.x, position.y + offset.y) }
+            .filter { it.isWithinBoardBounds(board.height, board.width) && it != position }
             .count { board.hasMineAt(it) }
     }
 
-    private fun Position.getNeighborPositions(boardHeight: Int, boardWidth: Int): List<Position> {
-        return (-1..1).flatMap { dx ->
-            (-1..1).mapNotNull { dy ->
-                Position(x + dx, y + dy)
-                    .takeIf { it != this && it.isWithinBounds(boardHeight, boardWidth) }
-            }
-        }
-    }
-
-    private fun Position.isWithinBounds(boardHeight: Int, boardWidth: Int): Boolean =
+    private fun Position.isWithinBoardBounds(boardHeight: Int, boardWidth: Int): Boolean =
         x in 0 until boardWidth && y in 0 until boardHeight
 }
