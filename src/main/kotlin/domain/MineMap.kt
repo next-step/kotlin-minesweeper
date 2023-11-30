c082348d6fe264a9c98b77f7bd03916e689d38b2
loren-hyuni <loren.hyuni@kakaocorp.com>
2023-11-30 17:53:54 +0900

    mission: step4 지뢰찾기 (리펙토링) 랜덤한 지뢰 맵이 생성된다

--- src/main/kotlin/domain/MineMap.kt
+++ src/main/kotlin/domain/MineMap.kt
@@ -4,12 +4,25 @@ import kotlin.random.Random
 
 class MineMap(
     val height: Int,
-    val width: Int
+    val width: Int,
+    mineCount: Int = 10
 ) {
 
-    private val mineMap: Array<Array<Spot>> = Array(height) { Array(width) { Spot(Random.nextBoolean()) } }
+    private val mineMap: List<List<Spot>> =
+        (initialList(mineCount, true) + initialList(height * width - mineCount, false))
+            .shuffled()
+            .chunked(width)
+
+    init {
+        require(height > 0)
+        require(width > 0)
+        require(mineCount in 0 until height * width)
+    }
 
     fun get(x: Int, y: Int): Spot {
         return mineMap[y][x]
     }
+
+    private fun initialList(size: Int, isMine: Boolean): List<Spot> =
+        List(size) { Spot(isMine) }
 }
--- src/test/kotlin/domain/MineMapTest.kt
+++ src/test/kotlin/domain/MineMapTest.kt
@@ -1,32 +1,66 @@
 package domain
 
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 
-class MineMapTest : StringSpec({
-    "MineMap은 높이와 너비를 가진다" {
-        val mineMap = MineMap(10, 10)
-        mineMap.height shouldBe 10
-        mineMap.width shouldBe 10
+class MineMapTest : ShouldSpec({
+
+    should("1보다 작은 높이와 너비를 입력하면 에러가 발생한다") {
+        shouldThrow<IllegalArgumentException> {
+            MineMap(1, 0, 0)
+        }
+        shouldThrow<IllegalArgumentException> {
+            MineMap(0, 1, 0)
+        }
     }
 
-    "MineMap은 지뢰가 있는 지점이 있고, 없는 지점도 있다" {
+    should("맵의 칸보다 더 많은 수의 지뢰를 입력하면 에러가 발생한다") {
+        shouldThrow<IllegalArgumentException> {
+            MineMap(2, 2, 5)
+        }
+    }
+
+    context("10 X 10 map") {
         val height = 10
         val width = 10
-        var hasMine = false
-        var hasNotMine = false
-        val mineMap = MineMap(height, width)
-        repeat(height) { y ->
-            repeat(width) { x ->
-                if (mineMap.get(x, y).hasMine) {
-                    hasMine = true
-                } else {
-                    hasNotMine = true
-                }
-            }
+        should("MineMap은 높이와 너비를 가진다") {
+            val mineMap = MineMap(height, width, 10)
+            mineMap.height shouldBe 10
+            mineMap.width shouldBe 10
         }
 
-        hasMine shouldBe true
-        hasNotMine shouldBe true
+        should("MineMap은 지뢰가 있는 지점이 있고, 없는 지점도 있다") {
+            var hasMine = false
+            var hasNotMine = false
+            val mineMap = MineMap(height, width, 10)
+            repeat(height) { y ->
+                repeat(width) { x ->
+                    if (mineMap.get(x, y).hasMine) {
+                        hasMine = true
+                    } else {
+                        hasNotMine = true
+                    }
+                }
+            }
+
+            hasMine shouldBe true
+            hasNotMine shouldBe true
+        }
+
+        should("MineMap에 지뢰 개수를 입력하면 입력한 수만큼의 지뢰가 있다") {
+            val mineCount = 10
+            val mineMap = MineMap(height, width, mineCount)
+            var count = 0
+            repeat(height) { y ->
+                repeat(width) { x ->
+                    if (mineMap.get(x, y).hasMine) {
+                        count++
+                    }
+                }
+            }
+
+            count shouldBe mineCount
+        }
     }
 })
