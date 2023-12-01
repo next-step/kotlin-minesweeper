26b131c1dd3ebfb1329c493734e7ebceaf578928
jaylene shin <jaylene.shin@kakaocorp.com>
2023-12-02 08:11:47 +0900

    상대 좌표 구하도록 position 데이터 클래스 역할 확장

--- src/main/kotlin/minesweeper/domain/Position.kt
+++ src/main/kotlin/minesweeper/domain/Position.kt
@@ -5,7 +5,25 @@ data class Position(
     val x: Int
 ) {
     init {
-        require(y > 0) { "y는 0이거나 음수일 수 없습니다" }
-        require(x > 0) { "x는 0이거나 음수일 수 없습니다" }
+        require(y > 0) { "입력값: $y, y는 0이거나 음수일 수 없습니다" }
+        require(x > 0) { "입력값: $x, x는 0이거나 음수일 수 없습니다" }
     }
+
+    fun aroundPositions(): List<Position> {
+        return listOfNotNull(
+            topOrNull(),
+            bottomOrNull(),
+            leftOrNull(),
+            rightOrNull(),
+            topOrNull()?.leftOrNull(),
+            topOrNull()?.rightOrNull(),
+            bottomOrNull()?.leftOrNull(),
+            bottomOrNull()?.rightOrNull()
+        )
+    }
+
+    private fun leftOrNull(): Position? = runCatching { Position(y, x - 1) }.getOrNull()
+    private fun rightOrNull(): Position? = runCatching { Position(y, x + 1) }.getOrNull()
+    private fun topOrNull(): Position? = runCatching { Position(y - 1, x) }.getOrNull()
+    private fun bottomOrNull(): Position? = runCatching { Position(y + 1, x) }.getOrNull()
 }
