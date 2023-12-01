9d8936cb3ca06e905b39676e0a834333a9b5d548
Ahn-seokjoo <tjrwn0716@naver.com>
2023-12-02 02:06:25 +0900

    [step1] feature: test case 추가

--- src/test/kotlin/InputTest.kt
+++ src/test/kotlin/InputTest.kt
@@ -1,15 +1,22 @@
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 
 class InputTest : StringSpec({
     "width 가 1보다 작을 경우" {
-        
+        shouldThrow<IllegalArgumentException> {
+            MineSweeper(0, 1, 1)
+        }
     }
 
     "height 가 1보다 작을 경우" {
-
+        shouldThrow<IllegalArgumentException> {
+            MineSweeper(1, 0, 1)
+        }
     }
 
     "mine count 가 1보다 작을 경우" {
-
+        shouldThrow<IllegalArgumentException> {
+            MineSweeper(1, 1, 0)
+        }
     }
 })
