c547e54b51a84ba637ecab6b8aaeae3e50911afc
Ahn-seokjoo <tjrwn0716@naver.com>
2023-12-02 02:04:27 +0900

    [step1] feature: 테스트 케이스 추가

---
+++ src/test/kotlin/InputTest.kt
@@ -0,0 +1,15 @@
+import io.kotest.core.spec.style.StringSpec
+
+class InputTest : StringSpec({
+    "width 가 1보다 작을 경우" {
+        
+    }
+
+    "height 가 1보다 작을 경우" {
+
+    }
+
+    "mine count 가 1보다 작을 경우" {
+
+    }
+})
