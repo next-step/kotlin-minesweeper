e5dbc0fcbf864b92f9ab07b8f95bc8310ef746b9
dong <dong.22@kakaoenterprise.com>
2023-12-02 11:12:31 +0900

    feat : 새로 구현해야할 전략클래스 생성

---
+++ src/main/kotlin/minesweeper/view/render/impl/ExploringDefalutClosedAreaRenderingStrategy.kt
@@ -0,0 +1,11 @@
+package minesweeper.view.render.impl
+
+import minesweeper.model.point.Attribute
+import minesweeper.model.point.Coordinate
+import minesweeper.view.render.MineRenderingStrategy
+
+class ExploringDefalutClosedAreaRenderingStrategy:MineRenderingStrategy {
+    override fun symbol(attribute: Attribute, coordinate: Coordinate): String {
+        TODO("Not yet implemented")
+    }
+}
