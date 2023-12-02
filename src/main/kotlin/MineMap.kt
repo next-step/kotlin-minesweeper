e83577538a81692e4e57a04e027f587d3f29efe0
OYJ <oyj7677@gmail.com>
2023-12-02 21:46:14 +0900

    Feat: 지뢰 세팅 기능 구현

--- src/main/kotlin/MineMap.kt
+++ src/main/kotlin/MineMap.kt
@@ -21,8 +21,33 @@ class MineMap(val board: MutableList<MutableList<Cell>>) {
         require(board.isNotEmpty()) { ERR_MSG_EMPTY_LIST }
     }
 
-    companion object{
+    fun settingMine(number: Int) {
+        val rowIndex = getSelectRowIndex(number)
+        val columnIndex = getSelectColumIndex(number)
+
+        setMine(rowIndex, columnIndex)
+    }
+
+    private fun setMine(rowIndex: Int, columnIndex: Int) {
+        board[rowIndex][columnIndex] = Mine
+    }
+
+    private fun getSelectColumIndex(number: Int): Int {
+        val width = board.first().size
+        return when (val columnIndex = number % width) {
+            0 -> width - INDEX_OFFSET
+            else -> columnIndex - INDEX_OFFSET
+        }
+    }
+
+    private fun getSelectRowIndex(number: Int): Int {
+        val height = board.size
+        return number / height - INDEX_OFFSET
+    }
+
+    companion object {
         private const val ERR_MSG_EMPTY_LIST = "맵이 비어있습니다."
         private const val ERR_MSG_LINE_SIZE_DIFFERS = "라인의 크기가 다릅니다."
+        private const val INDEX_OFFSET = 1
     }
 }
--- src/test/kotlin/MineMapTest.kt
+++ src/test/kotlin/MineMapTest.kt
@@ -7,7 +7,7 @@ class MineMapTest {
         // given :
 
         // when : 행의 사이즈가 동일하지 않은 파라미터로 마인 맵이 생성 되었을 때
-        val actual = runCatching { MineMap(mutableListOf(mutableListOf(None,None,None), mutableListOf(None,None))) }.exceptionOrNull()
+        val actual = runCatching { MineMap(mutableListOf(mutableListOf(None, None, None), mutableListOf(None, None))) }.exceptionOrNull()
 
         // then : 예외를 던진다.
         assertThat(actual).isInstanceOf(IllegalArgumentException::class.java)
@@ -16,7 +16,7 @@ class MineMapTest {
     @Test
     fun `리스트가 비어져있고, 마인 맵이 생성될 때, 예외를 던진다`() {
         // given : 빈 리스트 생성
-        val emptyList: MutableList<MutableList<Cell>> =  mutableListOf()
+        val emptyList: MutableList<MutableList<Cell>> = mutableListOf()
 
         // when : 마인 맵 생성
         val actual = runCatching { MineMap(emptyList) }.exceptionOrNull()
@@ -25,6 +25,16 @@ class MineMapTest {
         assertThat(actual).isInstanceOf(IllegalArgumentException::class.java)
     }
 
+    @Test
+    fun `마인 맵이 생성되고, 지정된 곳에 지뢰를 생성할때, 해당 위치는 지뢰가 된다`() {
+        // given : 마인 맵이 생성된다.
+        val mineMap = CreateFactory.createMineMap(5, 5)
 
+        // when : 지정된 위치에 지뢰 세팅을 요청한다.
+        mineMap.settingMine(15)
+        val actual = mineMap.board[2][4]
 
+        // then : 해당 위치는 지뢰로 변경된다.
+        assertThat(actual).isInstanceOf(Mine::class.java)
+    }
 }
