87086a8cbc7826822dfef768358ffe8e45ee4c2c
seongmo-jeon <oeeen3@gmail.com>
2023-12-02 20:49:59 +0900

    test: 실패하는 테스트 수정

--- src/test/kotlin/minesweeper/domain/BoardTest.kt
+++ src/test/kotlin/minesweeper/domain/BoardTest.kt
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.assertThrows
 
 class BoardTest {
     @Test
-    fun `Board 는 높이, 너비를 이용하여 만들 수 있다`() {
+    fun `Board 는 높이 개수의 Row를 이용하여 만들 수 있다`() {
         val board = Board(Height(10), Width(10), MineCount(1))
-        board.rows.size shouldBe 100
+        board.rows.size shouldBe 10
     }
 
     @Test
