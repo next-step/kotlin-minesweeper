6a963e1db3ce806cf7dc995f2576ccc81d82381d
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-12-03 12:47:14 +0900

    (update) NeighborMineCount class -> value class 적용

--- src/main/kotlin/domain/NeighborMineCount.kt
+++ src/main/kotlin/domain/NeighborMineCount.kt
@@ -1,6 +1,7 @@
 package domain
 
-class NeighborMineCount(val count: Int) {
+@JvmInline
+value class NeighborMineCount(val count: Int) {
     init {
         require(count >= 0) { "count 는 음수일 수 없습니다." }
     }
--- src/test/kotlin/domain/NeighborMineCountTest.kt
+++ src/test/kotlin/domain/NeighborMineCountTest.kt
@@ -1,24 +1,9 @@
 package domain
 
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class NeighborMineCountTest {
-
-    private lateinit var neighborMineCount: NeighborMineCount
-    private lateinit var boardSettings: BoardSettings
-    private lateinit var board: MutableList<CellList>
-
-    @BeforeEach
-    fun setUp() {
-        neighborMineCount = NeighborMineCount(0)
-        boardSettings = BoardSettings(row = 3, col = 3, mineCount = 1)
-        board = (0 until boardSettings.row).map { row ->
-            CellList().createEmptyRow(row, boardSettings.col)
-        }.toMutableList()
-    }
-
     @Test
     fun `NeighborMineCount 생성시 음수가 들어오면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
