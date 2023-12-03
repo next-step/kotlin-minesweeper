45bb2bf2579b8fc00358606ecf0b2f5975cc9207
Ahn-seokjoo <tjrwn0716@naver.com>
2023-12-03 19:50:47 +0900

    [step1] refacotirng: mine width 추가

--- src/main/kotlin/minesweeper/MineSweeper.kt
+++ src/main/kotlin/minesweeper/MineSweeper.kt
@@ -1,6 +1,8 @@
 package minesweeper
 
+import minesweeper.domain.MineSweeperWidth
 import minesweeper.domain.board.size.MineSweeperBoardSize
+import minesweeper.domain.toMineSweeperWidth
 
 class MineSweeper(
     private val board: MineSweeperBoardSize,
@@ -10,25 +12,22 @@ class MineSweeper(
         require(mineCount > 0) { INVALID_VALUE }
     }
 
-    fun initialize(): List<List<String>> {
-        val initialList = (1..(board.getBoardFullSize() - mineCount)).map {
-            "C"
-        }
-        val mineList = (1..mineCount).map {
-            "*"
-        }
-        val mineSweeperList = (initialList + mineList).shuffled()
-        return getResultList(mineSweeperList)
-    }
+    fun initialize(): List<MineSweeperWidth> {
+        val notContainedMineCount = board.getBoardFullSize() - mineCount
+        val notContainedMineList = MineSweeperWidth.newInstance(widthSize = notContainedMineCount, value = NOT_MINE)
+        val mineList = MineSweeperWidth.newInstance(widthSize = mineCount, value = MINE)
 
-    private fun getResultList(initialList: List<String>): List<List<String>> {
+        val mineSweeperList = (notContainedMineList + mineList).shuffled()
+
         return (0 until board.width).map { height ->
             val line = height * board.width
-            initialList.slice(line until line + board.width)
+            mineSweeperList.slice(line until line + board.width).toMineSweeperWidth()
         }
     }
 
     companion object {
         private const val INVALID_VALUE = "잘못된 값을 입력하셨습니다."
+        private const val NOT_MINE = "C"
+        private const val MINE = "*"
     }
 }
---
+++ src/main/kotlin/minesweeper/domain/MineSweeperWidth.kt
@@ -0,0 +1,18 @@
+package minesweeper.domain
+
+class MineSweeperWidth(
+    private val widthList: List<String>,
+) : List<String> by widthList {
+
+    fun getWidthList(): List<String> = widthList
+
+    companion object {
+        fun newInstance(widthSize: Int, value: String): MineSweeperWidth {
+            return (1..widthSize).map {
+                value
+            }.toMineSweeperWidth()
+        }
+    }
+}
+
+fun List<String>.toMineSweeperWidth() = MineSweeperWidth(this)
