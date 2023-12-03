c437825d7ba120747884134d7e6a4cab7b46c25c
Ahn-seokjoo <tjrwn0716@naver.com>
2023-12-04 02:10:47 +0900

    [refactoring] step2: MineSweeperWidthTest 케이스 추가

--- src/test/kotlin/minesweeper/domain/MineSweeperWidthTest.kt
+++ src/test/kotlin/minesweeper/domain/MineSweeperWidthTest.kt
@@ -6,9 +6,16 @@ import minesweeper.domain.mine.MineSweeperShape
 
 class MineSweeperWidthTest : StringSpec({
     "생성한 MineSweeperWidth의 Shape는 기본적으로 MineSweeperShape.SHAPE 이다." {
-        val mineSweeperWidth = MineSweeperWidth.newInstance(4)
-        mineSweeperWidth.forEach { shape ->
+        val notMineSweeperWidth = MineSweeperWidth.newInstance(4)
+        notMineSweeperWidth.forEach { shape ->
             shape shouldBe MineSweeperShape.SHAPE.shape
+        }
+    }
+
+    "MineShape.MINE 을 주면, 마인을 가진 Width를 생성한다" {
+        val mineSweeperWidth = MineSweeperWidth.newInstance(widthSize = 4, mineSweeperShape = MineSweeperShape.MINE)
+        mineSweeperWidth.forEach { shape ->
+            shape shouldBe MineSweeperShape.MINE.shape
         }
     }
 
