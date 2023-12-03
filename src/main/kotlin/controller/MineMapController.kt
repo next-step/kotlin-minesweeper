47545056f4d094534035884c3a4263b978e96943
loren-hyuni <loren.hyuni@kakaocorp.com>
2023-12-04 07:17:11 +0900

    mission: step4 지뢰찾기 (리펙토링) run 함수를 의미있는 단위로 분할

--- src/main/kotlin/controller/MineMapController.kt
+++ src/main/kotlin/controller/MineMapController.kt
@@ -8,26 +8,34 @@ import view.View
 class MineMapController {
 
     fun run() {
+        val mineMap = inputMineMapData()
+        View.outputStartGame()
+
+        while (doRound(mineMap)) { }
+
+        outputResult(isWin = mineMap.isAllOpened(), mineMap)
+    }
+
+    private fun inputMineMapData(): MineMap {
         val height = View.inputHeight()
         val width = View.inputWidth()
         val mineCount = View.inputMineCount()
         val mapInfo = MineMapInfo(Point(height, width), mineCount)
-        val mineMap = MineMap(mapInfo)
+        return MineMap(mapInfo)
+    }
 
-        View.outputStartGame()
-
-        while (mineMap.isAllOpened().not()) {
-            View.outputMineMap(mineMap)
-            val openPoint = View.inputOpenSpot()
-            val openStatus = mineMap.open(openPoint)
-
-            if (openStatus.mineTrapped()) {
-                View.outputLoseGame()
-                return
-            }
-        }
-
+    private fun outputResult(isWin: Boolean, mineMap: MineMap) {
         View.outputMineMap(mineMap)
-        View.outputWinGame()
+        if (isWin) View.outputWinGame()
+        else View.outputLoseGame()
+    }
+
+    private fun doRound(mineMap: MineMap): Boolean {
+        if (mineMap.isAllOpened()) return false
+        View.outputMineMap(mineMap)
+        val openPoint = View.inputOpenSpot()
+        val openStatus = mineMap.open(openPoint)
+
+        return !openStatus.mineTrapped()
     }
 }
