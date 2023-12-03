15f4d51fd5a26866098ee23543a61c45cfbb1e5f
loren-hyuni <loren.hyuni@kakaocorp.com>
2023-12-04 07:24:03 +0900

    mission: step4 지뢰찾기 (리펙토링) MineMapInfo 테스트 수정

--- src/test/kotlin/domain/MineMapInfoTest.kt
+++ src/test/kotlin/domain/MineMapInfoTest.kt
@@ -2,29 +2,21 @@ package domain
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
 
 class MineMapInfoTest : StringSpec({
-    "높이가 0 이하면 예외가 발생한다" {
-        shouldThrow<IllegalArgumentException> {
-            MineMapInfo(Point(0, 10), 4)
-        }
-    }
 
-    "너비가 0 이하면 예외가 발생한다" {
-        shouldThrow<IllegalArgumentException> {
-            MineMapInfo(Point(10, 0), 4)
-        }
-    }
-
-    "지뢰 개수가 0 미만이면 예외가 발생한다" {
-        shouldThrow<IllegalArgumentException> {
-            MineMapInfo(Point(10, 10), -1)
-        }
-    }
-
-    "지뢰 개수가 높이와 너비의 곱보다 크거나 같으면 예외가 발생한다" {
-        shouldThrow<IllegalArgumentException> {
-            MineMapInfo(Point(10, 10), 100)
+    "잘못된 입력이 들어오면 예외가 발생한다" {
+        forAll(
+            row(0, 10, 4),
+            row(10, 0, 4),
+            row(10, 10, -1),
+            row(10, 10, 100)
+        ) { height, width, mineCount ->
+            shouldThrow<IllegalArgumentException> {
+                MineMapInfo(Point(height, width), mineCount)
+            }
         }
     }
 })
