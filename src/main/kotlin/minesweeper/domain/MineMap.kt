15f89f72582591bb2b2d7536cd8f06b7c098ae82
jaylene shin <jaylene.shin@kakaocorp.com>
2023-12-03 19:13:58 +0900

    open cell로 교체할 수 있게 MineMap 변경

--- src/main/kotlin/minesweeper/domain/MineMap.kt
+++ src/main/kotlin/minesweeper/domain/MineMap.kt
@@ -1,12 +1,37 @@
 package minesweeper.domain
 
 class MineMap(
-    private val values: Map<Position, Cell>
+    values: Map<Position, Cell>
 ) {
+    private val _values = values.toMutableMap()
+    val values: Map<Position, Cell>
+        get() = _values.toMap()
+
     val size: Int
-        get() = values.keys.size
+        get() = _values.keys.size
+
+    fun isEmptyCellClicked(position: Position): Boolean {
+        val cell = getCell(position)
+        return if (cell is Mine) {
+            false
+        } else {
+            openAroundCells(position)
+            true
+        }
+    }
 
     fun getCell(position: Position): Cell {
-        return values[position] ?: throw IllegalArgumentException("해당 위치에 셀이 없습니다")
+        return _values[position] ?: throw IllegalArgumentException("해당 위치에 셀이 없습니다")
+    }
+
+    private fun openAroundCells(position: Position) {
+        val aroundPositions = position.aroundPositions()
+        (aroundPositions + position).forEach(::openCell)
+    }
+
+    private fun openCell(position: Position) {
+        val cell = runCatching { getCell(position) }.getOrNull() ?: return
+        val newCell = cell.open()
+        _values[position] = newCell
     }
 }
