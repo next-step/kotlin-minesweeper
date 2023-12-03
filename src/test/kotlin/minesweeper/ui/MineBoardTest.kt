14e1c79f5086a49002088fe7d11297f7f9928868
Ahn-seokjoo <tjrwn0716@naver.com>
2023-12-04 01:08:29 +0900

    [refactoring] step2: 리뷰 반영 정상 케이스 추가 및 테스트 명 수정

--- src/test/kotlin/minesweeper/ui/MineBoardTest.kt
+++ src/test/kotlin/minesweeper/ui/MineBoardTest.kt
@@ -5,15 +5,19 @@ import io.kotest.core.spec.style.StringSpec
 import minesweeper.domain.board.size.MineSweeperBoardSize
 
 class MineBoardTest : StringSpec({
-    "width 가 1보다 작을 경우" {
+    "width 가 1보다 작을 경우 에러가 발생한다." {
         shouldThrow<IllegalArgumentException> {
             MineSweeperBoardSize(width = 0, height = 1)
         }
     }
 
-    "height 가 1보다 작을 경우" {
+    "height 가 1보다 작을 경우 에러가 발생한다." {
         shouldThrow<IllegalArgumentException> {
             MineSweeperBoardSize(width = 1, height = 0)
         }
+    }
+
+    "width와 height가 1 이상이면 아무 에러 없이 객체를 생성한다." {
+        MineSweeperBoardSize(width = 1, height = 1)
     }
 })
