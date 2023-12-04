d59f99576fff9203e0e871fb48075cba04163f79
dong <dong.22@kakaoenterprise.com>
2023-12-04 14:59:41 +0900

    feat : 출력전략 수정

--- src/main/kotlin/minesweeper/Controller.kt
+++ src/main/kotlin/minesweeper/Controller.kt
@@ -5,14 +5,14 @@ import minesweeper.model.board.Board
 import minesweeper.model.board.toBoardLimit
 import minesweeper.view.InputView
 import minesweeper.view.OutputView
-import minesweeper.view.render.impl.AdjacentMineCountRenderingStrategy
+import minesweeper.view.render.impl.ExploringDefaultClosedAreaRenderingStrategy
 
 fun main() {
     val mapHeight: Int = InputView.mapHeight()
     val mapWidth: Int = InputView.mapWidth()
     val minesCount: Int = InputView.countOfMines()
     val board = Board(minesCount, (mapHeight to mapWidth).toBoardLimit())
-    val outputView = OutputView(AdjacentMineCountRenderingStrategy)
+    val outputView = OutputView(ExploringDefaultClosedAreaRenderingStrategy)
     val game = MineSweeperGame(InputView, outputView)
     game.start(board)
 }
