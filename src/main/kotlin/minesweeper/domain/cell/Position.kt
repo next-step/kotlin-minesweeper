f5635af89c72a57529ca26bfd54aa167e5df3639
bongbong <kbk5190@naver.com>
2023-12-04 23:47:03 +0900

    [step1] feat: Position의 행과 열은 0이상

--- src/main/kotlin/minesweeper/domain/cell/Position.kt
+++ src/main/kotlin/minesweeper/domain/cell/Position.kt
@@ -3,4 +3,9 @@ package minesweeper.domain.cell
 data class Position(
     val row: Int,
     val column: Int,
-)
+) {
+    init {
+        require(row >= 0) { "행은 0이상만 가능합니다" }
+        require(column >= 0) { "열은 0이상만 가능합니다" }
+    }
+}
