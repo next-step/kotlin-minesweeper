bf8066bcbdd506d713f8e7859416c6ba127fe56b
dong <dong.22@kakaoenterprise.com>
2023-12-04 14:33:49 +0900

    refactor : 테스트코드에서 사용할 픽스쳐 함수 추가

--- src/test/kotlin/minesweeper/model/point/PointsFixture.kt
+++ src/test/kotlin/minesweeper/model/point/PointsFixture.kt
@@ -1,18 +1,7 @@
-package minesweeper.model.point
-
-object PointsFixture {
-    fun make(vararg pairs: Pair<Int, Int>): Points {
-        return Points(
-            pairs.asSequence()
-                .map { Coordinate(Vertical(it.first), Horizontal(it.second)) }
-                .map { it to TileType.MINE }
-                .toMap()
-        )
-    }
-}
 // package minesweeper.model.point
 //
 // import minesweeper.model.vison.VisionStrategy
+//
 // object PointsFixture {
 //    fun make(vararg pairs: Pair<Int, Int>): Points {
 //        return Points(
