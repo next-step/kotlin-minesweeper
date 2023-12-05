6de90e1c934234d6bec1237fadb628d87da45bd7
dong <dong.22@kakaoenterprise.com>
2023-12-05 17:23:48 +0900

    refactor : 스타일 맞춤

--- src/main/kotlin/minesweeper/model/point/Horizontal.kt
+++ src/main/kotlin/minesweeper/model/point/Horizontal.kt
@@ -4,6 +4,11 @@ package minesweeper.model.point
 value class Horizontal(
     val value: Int,
 ) {
+
+    init {
+        require(value >= 0) { "Horizontal 에 입력된 ${value}으로 생성이 불가능합니다. 0 혹은 양의 정수만 허용됩니다" }
+    }
+
     fun moveTo(delta: Int): Horizontal {
         return Horizontal(value + delta)
     }
@@ -14,9 +19,5 @@ value class Horizontal(
 
     fun range(): IntRange {
         return 0..value
-    }
-
-    init {
-        require(value >= 0) { "Horizontal 에 입력된 ${value}으로 생성이 불가능합니다. 0 혹은 양의 정수만 허용됩니다" }
     }
 }
--- src/test/kotlin/minesweeper/model/board/BoardTest.kt
+++ src/test/kotlin/minesweeper/model/board/BoardTest.kt
@@ -4,6 +4,7 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class BoardTest : StringSpec({
+
     "입력한 수만큼 지뢰가 생성 되어야 한다" {
         val countOfMines = 22
         val board = Board(
@@ -11,7 +12,7 @@ class BoardTest : StringSpec({
             limit = (10 to 10).toBoardLimit(),
         )
 
-        board.minesCount() shouldBe 22
+        board.minesCount shouldBe 22
     }
 
     "입력한 horizontal size 만큼의 지도가 생성 되어야 한다" {
