7323a6975157fa0353d02273987bed4e386848b9
dong <dong.22@kakaoenterprise.com>
2023-12-07 00:19:14 +0900

    test : 보드 범위 관련 테스트코드 추가

--- src/test/kotlin/minesweeper/model/board/BoardLimitTest.kt
+++ src/test/kotlin/minesweeper/model/board/BoardLimitTest.kt
@@ -1,10 +1,13 @@
 package minesweeper.model.board
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class BoardLimitTest : StringSpec({
 
     "입력된 Limit 정보를 바탕으로 area (영역의 크기) 를 반환해야 한다" {
-        TODO()
+        val limit = (7 to 4).toBoardLimit()
+
+        limit.area shouldBe 28
     }
 })
