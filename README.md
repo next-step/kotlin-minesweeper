cc7c170c4f9d814ecc294e9b6836df8bdd8aa40f
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-12-08 20:38:51 +0900

    test: added tests to GameMapCreatorTest

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 - [x] closed Cell should display correct value
 - [x] closed Cell can be opened
 - [x] Cell should have valid adjacent mine count value
-- [ ] game map should have valid width, height and mine count
-- [ ] game map creator should create correct map instance
+- [x] game map should have valid width, height and mine count
+- [x] game map creator should create correct map instance
--- src/test/kotlin/GameMapCreatorTest.kt
+++ src/test/kotlin/GameMapCreatorTest.kt
@@ -1,9 +1,12 @@
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import java.lang.IllegalArgumentException
+import java.lang.Integer.max
+import java.lang.Integer.min
 
-class GameMapCreatorTest : BehaviorSpec ({
+class GameMapCreatorTest : BehaviorSpec({
     given("came map creator") {
         `when`("invalid width") {
             then("should throw exception") {
@@ -36,17 +39,61 @@ class GameMapCreatorTest : BehaviorSpec ({
                 }.message.shouldContain("mine count cannot be larger than game map size")
             }
         }
+
+        `when`("valid size provided") {
+            then("should create game map with exact size") {
+                GameMapCreator().create(width = 2, height = 3, mineCount = 2).width shouldBe 2
+                GameMapCreator().create(width = 2, height = 3, mineCount = 2).height shouldBe 3
+            }
+        }
+
+        `when`("valid mine count provided") {
+            then("should create game map with exact mine count") {
+                GameMapCreator().create(width = 2, height = 2, mineCount = 2).mineCount() shouldBe 2
+            }
+        }
     }
 })
 
 class GameMapCreator {
-    fun create(width: Int, height: Int, mineCount: Int) : GameMap {
+    fun create(width: Int, height: Int, mineCount: Int): GameMap {
         require(width > 0) { "invalid game map width" }
         require(height > 0) { "invalid game map height" }
         require(mineCount > 0) { "game map should have at least 1 mine cell" }
         require(width * height > mineCount) { "mine count cannot be larger than game map size" }
-        return GameMap(
-            listOf(listOf(Cell()))
-        )
+
+        val initialCellParameters = MutableList(height) {
+            MutableList(width) {
+                Pair(false, 0)
+            }
+        }
+
+        while (initialCellParameters.flatten().count { it.first } != mineCount) {
+            val randomPosition = randomPosition(width, height)
+            val row = randomPosition.second
+            val col = randomPosition.first
+            val cell = initialCellParameters[row][col]
+
+            initialCellParameters[row][col] = cell.copy(first = true)
+        }
+
+        val initialGameMapScaffold = List(height) { row ->
+            List(width) { col ->
+                Cell(
+                    isMine = initialCellParameters[row][col].first,
+                    adjacentMineCount = initialCellParameters
+                        .filterIndexed { rowIdx, _ -> rowIdx in max(row - 1, 0)..min(row + 1, height - 1) }
+                        .map { it.slice(max(col - 1, 0)..min(col + 1, width - 1)) }
+                        .flatten()
+                        .count { it.first }
+                )
+            }
+        }
+
+
+        return GameMap(initialGameMapScaffold)
     }
+
+    private fun randomPosition(width: Int, height: Int): Pair<Int, Int> =
+        Pair((0 until width).random(), (0 until height).random())
 }
