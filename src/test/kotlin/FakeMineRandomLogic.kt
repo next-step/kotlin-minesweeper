21228589775a2a9e0338340f854ed80b3fbe72ea
OYJ <oyj7677@gmail.com>
2023-12-09 17:45:05 +0900

    Chore: 컨벤션 정리

--- src/test/kotlin/FakeMineRandomLogic.kt
+++ src/test/kotlin/FakeMineRandomLogic.kt
@@ -2,7 +2,6 @@ import ramdom.RandomInterface
 
 class FakeMineRandomLogic : RandomInterface {
     override fun createRandomNumList(count: Int, maxValue: Int): List<Int> {
-        return List(count) {index -> index}
+        return List(count) { index -> index }
     }
-
 }
