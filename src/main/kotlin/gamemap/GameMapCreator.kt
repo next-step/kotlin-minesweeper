326a0aeb53e2e76056470826916f813351a3fe53
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-12-14 17:17:27 +0900

    refactor: MinePositions 생성 및 생성로직 위임

--- src/main/kotlin/gamemap/GameMapCreator.kt
+++ src/main/kotlin/gamemap/GameMapCreator.kt
@@ -11,19 +11,16 @@ class GameMapCreator(
         require(mineCount > 0) { "game map should have at least 1 mine cell" }
         require(width * height > mineCount) { "mine count cannot be larger than game map size" }
     }
+
     fun create(): GameMap {
 
-        val minePositions = mutableSetOf<MinePosition>()
-
-        while (minePositions.size != mineCount) {
-            minePositions.add(randomPosition(width, height))
-        }
+        val minePositions = MinePositions.ofSize(mineCount) { MinePosition.ofRandom(width, height) }
 
         val initialGameMapScaffold = List(height) { row ->
             List(width) { col ->
                 Cell(
                     isMine = MinePosition(row = row, col = col) in minePositions,
-                    adjacentMineCount = minePositions.count { it.isAdjacentTo(cellRowIdx = row, cellColIdx = col) }
+                    adjacentMineCount = minePositions.countAdjacentTo(cellRowIdx = row, cellColIdx = col)
                 )
             }
         }
@@ -31,17 +28,49 @@ class GameMapCreator(
         return GameMap(initialGameMapScaffold)
     }
 
-    private fun randomPosition(width: Int, height: Int): MinePosition =
-        MinePosition(row = (0 until height).random(), col = (0 until width).random())
-
     private data class MinePosition(
         val row: Int,
         val col: Int,
     ) {
         fun isAdjacentTo(cellRowIdx: Int, cellColIdx: Int): Boolean {
-            return row in cellRowIdx - 1..cellRowIdx + 1 && col in cellColIdx - 1..cellColIdx + 1 && !isAtPosition(cellRowIdx, cellColIdx)
+            return (row in cellRowIdx - 1..cellRowIdx + 1) &&
+                (col in cellColIdx - 1..cellColIdx + 1) &&
+                !isAtPosition(cellRowIdx, cellColIdx)
         }
 
         private fun isAtPosition(cellRowIdx: Int, cellColIdx: Int) = row == cellRowIdx && col == cellColIdx
+
+        companion object {
+            fun ofRandom(width: Int, height: Int): MinePosition =
+                MinePosition(row = (0 until height).random(), col = (0 until width).random())
+        }
+    }
+
+    private class MinePositions(
+        private val _mineSet: MutableSet<MinePosition>
+    ) {
+        val size get() = _mineSet.size
+
+        fun add(minePosition: MinePosition) {
+            _mineSet.add(minePosition)
+        }
+
+        fun countAdjacentTo(cellRowIdx: Int, cellColIdx: Int) = _mineSet.count {
+            it.isAdjacentTo(cellRowIdx = cellRowIdx, cellColIdx = cellColIdx)
+        }
+
+        operator fun contains(minePosition: MinePosition): Boolean = _mineSet.contains(minePosition)
+
+        companion object {
+            fun ofSize(size: Int, creator: () -> MinePosition): MinePositions {
+                val minePositions = mutableSetOf<MinePosition>()
+
+                while (minePositions.size != size) {
+                    minePositions.add(creator())
+                }
+
+                return MinePositions(minePositions)
+            }
+        }
     }
 }
