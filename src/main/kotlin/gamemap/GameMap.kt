eecd2b4e740a5d4e86a13efeaafa89a5dc407a02
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-12-15 20:58:23 +0900

    refactor: GameMap 로직 정리

--- src/main/kotlin/gamemap/GameMap.kt
+++ src/main/kotlin/gamemap/GameMap.kt
@@ -3,7 +3,7 @@ package gamemap
 import java.util.LinkedList
 import java.util.Queue
 
-data class GameMap(
+class GameMap(
     private val gameMap: List<List<Cell>>
 ) {
     init {
@@ -11,80 +11,77 @@ data class GameMap(
         require(gameMap.first().isNotEmpty()) { "invalid game map width" }
         require(gameMap.flatten().any { cell -> cell.isMine }) { "game map should have at least 1 mine cell" }
     }
-    fun cellAt(r: Int, c: Int) = gameMap[r][c]
 
-    fun open(r: Int, c: Int): OpenResult {
-        val target = cellAt(r, c)
+    fun cellAt(row: Int, col: Int) = gameMap[row][col]
 
-        if (target.isOpen()) return OpenResult.Success
+    fun openCellAt(row: Int, col: Int): OpenCommandResult {
+        val target = cellAt(row, col)
+
+        if (target.isOpen()) return OpenCommandResult.Success
 
         if (target.isMine) {
             target.open()
-            return OpenResult.Fail
+            return OpenCommandResult.Fail
         }
 
-        openAllAdjacent(r, c)
+        openFloodFill(row, col)
 
-        if (remainingCellCount == 0) return OpenResult.Complete
+        if (remainingCellCount == 0) return OpenCommandResult.Complete
 
-        return OpenResult.Success
+        return OpenCommandResult.Success
     }
 
-    private fun openAllAdjacent(r: Int, c: Int) {
-        val queue: Queue<Pair<Int, Int>> = LinkedList()
-        val setOfVisited = mutableSetOf<Pair<Int, Int>>()
+    private data class TargetCellPosition(
+        val row: Int,
+        val col: Int,
+    ) {
+        fun adjacentPositionsIn(width: Int, height: Int): List<TargetCellPosition> {
+            val positions = mutableListOf<TargetCellPosition>()
 
-        queue.add(Pair(r, c))
+            val deltas = listOf(
+                Pair(-1, -1), Pair(-1, 0), Pair(-1, 1),
+                Pair(0, -1), Pair(0, 1),
+                Pair(1, -1), Pair(1, 0), Pair(1, 1),
+            )
+
+            deltas.forEach {
+                val targetRowIdx = row + it.first
+                val targetColIdx = col + it.second
+                if ((targetRowIdx in 0 until height) && (targetColIdx in 0 until width)) {
+                    positions.add(TargetCellPosition(row = targetRowIdx, col = targetColIdx))
+                }
+            }
+
+            return positions
+        }
+    }
+
+    private fun openFloodFill(row: Int, col: Int) {
+        val queue: Queue<TargetCellPosition> = LinkedList()
+        val setOfVisited = mutableSetOf<TargetCellPosition>()
+
+        queue.add(TargetCellPosition(row = row, col = col))
 
         while (queue.isNotEmpty()) {
-            val pair = queue.poll()
+            val targetPosition = queue.poll()
 
-            if (pair in setOfVisited) {
-                continue
-            }
+            if (targetPosition in setOfVisited) continue
 
-            val cell = gameMap[pair.first][pair.second]
+            val cell = gameMap[targetPosition.row][targetPosition.col]
 
             cell.open()
-            setOfVisited.add(pair)
-            if (cell.adjacentMineCount != 0) {
-                continue
-            }
+
+            setOfVisited.add(targetPosition)
+
+            if (cell.adjacentMineCount != 0) continue
+
             queue.addAll(
-                adjacentPositions(pair.first, pair.second)
-                    .filter { it !in setOfVisited && gameMap[it.first][it.second].isClose() && gameMap[it.first][it.second].isMine.not() }
+                targetPosition.adjacentPositionsIn(width, height)
+                    .filter {
+                        it !in setOfVisited && gameMap[it.row][it.col].hasToBeOpened()
+                    }
             )
         }
-    }
-
-    private fun adjacentPositions(r: Int, c: Int): List<Pair<Int, Int>> {
-        val positions = mutableListOf<Pair<Int, Int>>()
-        if (r + 1 < height) {
-            positions.add(Pair(r + 1, c))
-        }
-        if (r - 1 >= 0) {
-            positions.add(Pair(r - 1, c))
-        }
-        if (c + 1 < width) {
-            positions.add(Pair(r, c + 1))
-        }
-        if (c - 1 >= 0) {
-            positions.add(Pair(r, c - 1))
-        }
-        if (r + 1 < height && c + 1 < width) {
-            positions.add(Pair(r + 1, c + 1))
-        }
-        if (r + 1 < height && c - 1 >= 0) {
-            positions.add(Pair(r + 1, c - 1))
-        }
-        if (r - 1 >= 0 && c + 1 < width) {
-            positions.add(Pair(r - 1, c + 1))
-        }
-        if (r - 1 >= 0 && c - 1 >= 0) {
-            positions.add(Pair(r - 1, c - 1))
-        }
-
-        return positions
     }
 
     val mineCount = gameMap.flatten().count { cell -> cell.isMine }
@@ -93,9 +90,9 @@ data class GameMap(
 
     val height = gameMap.size
 
-    private val remainingCellCount get() = gameMap.flatten().count { cell -> cell.isMine.not() && cell.isClose() }
-}
+    private val remainingCellCount
+        get() = gameMap.flatten()
+            .count { cell -> cell.hasToBeOpened() }
 
-enum class OpenResult {
-    Success, Fail, Complete
+    private fun Cell.hasToBeOpened() = isMine.not() && isClose()
 }
