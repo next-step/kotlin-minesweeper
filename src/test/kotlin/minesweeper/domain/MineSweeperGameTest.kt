f7da4b2b8df146ae0eece7c220139ca1810f86fb
yeongun <gjduddns@gmail.com>
2023-12-29 17:36:25 +0900

    test: MineSweeperGame.open() 테스트 추가

--- src/test/kotlin/minesweeper/domain/MineSweeperGameTest.kt
+++ src/test/kotlin/minesweeper/domain/MineSweeperGameTest.kt
@@ -27,4 +27,30 @@ class MineSweeperGameTest : BehaviorSpec({
             }
         }
     }
+
+    Given("Open 하려는 위치가 주어지면") {
+        val height = Size(10)
+        val width = Size(10)
+        val mineCount = Size(10)
+
+        val minePositions = FixedPositionGenerator(height, width).generate(mineCount)
+        val cellFinder = CellFinder.init(height, width)
+        cellFinder.convert(minePositions)
+
+        val mineSweeperGame = MineSweeperGame(cellFinder)
+        When("지뢰찾기게임은") {
+            mineSweeperGame.open(Position(10, 10))
+            Then("지뢰가 없는 인접한 칸이 모두 열린다.") {
+                forAll(
+                    row(Position(1, 1), false),
+                    row(Position(-1, -1), false),
+                    row(Position(1, 2), false),
+                    row(Position(5, 5), true),
+                    row(Position(10, 10), true),
+                ) { position, expected ->
+                    mineSweeperGame.isOpen(position) shouldBe expected
+                }
+            }
+        }
+    }
 })
