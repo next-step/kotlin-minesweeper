e5280f955e9ee4632f7e944bf1c5b7a1ee337e12
SeongminKimKR <gvf156@gmail.com>
2024-12-05 01:11:06 +0900

    feat: 입력뷰 및 지뢰찾기 설정 클래스 작성

--- src/main/kotlin/minesweeper/MinesWeeperApplication.kt
+++ src/main/kotlin/minesweeper/MinesWeeperApplication.kt
@@ -1,10 +1,8 @@
 package minesweeper
 
-import minesweeper.domain.DefaultMineGenerator
-import minesweeper.domain.MinesWeeper
 import minesweeper.view.input.MinesWeeperSettingView
-import minesweeper.view.output.MinesWeeperStartView
 
 fun main() {
     val setting = MinesWeeperSettingView.parse()
+
 }
