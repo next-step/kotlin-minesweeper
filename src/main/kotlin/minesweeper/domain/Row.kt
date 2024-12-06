3c16fd69f5139526c3f6ac8ecdd0115b3aacee6e
jwahoonkim <kjhoon0330@snu.ac.kr>
2024-12-06 18:53:31 +0900

    refactor: property - init 순서로 배치

--- src/main/kotlin/minesweeper/domain/Row.kt
+++ src/main/kotlin/minesweeper/domain/Row.kt
@@ -2,11 +2,11 @@ package minesweeper.domain
 
 class Row(columns: Int) {
 
+    private val cells = List(columns) { Cell(isMine = false) }
+
     init {
         require(columns > 0) { "열은 1 이상이어야 합니다." }
     }
-
-    private val cells = List(columns) { Cell(isMine = false) }
 
     fun display(): String {
         return cells.joinToString(" ") { if (it.isMine) "X " else ". " }
