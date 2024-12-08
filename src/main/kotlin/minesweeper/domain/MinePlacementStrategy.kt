4fbe5bba29edad8b0e7c342b7b17233d56c917a0
jwahoonkim <kjhoon0330@snu.ac.kr>
2024-12-09 02:35:49 +0900

    refactor: MinePlacementStrategy 인터페이스 도입 및 구현 변경 + 테스트 추가

--- src/main/kotlin/minesweeper/domain/MinePlacementStrategy.kt
+++ src/main/kotlin/minesweeper/domain/MinePlacementStrategy.kt
@@ -10,11 +10,3 @@ class RandomMinePlacementStrategy : MinePlacementStrategy {
             .take(mineCount)
     }
 }
-
-class FixedMinePlacementStrategy(
-    private val minePlace: List<Int>
-) : MinePlacementStrategy {
-    override fun placeMines(height: Int, width: Int, mineCount: Int): List<Int> {
-        return minePlace
-    }
-}
--- src/test/kotlin/minesweeper/domain/BoardTest.kt
+++ src/test/kotlin/minesweeper/domain/BoardTest.kt
@@ -1,6 +1,8 @@
 package minesweeper.domain
 
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+import io.kotest.matchers.types.shouldBeSameInstanceAs
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -61,5 +63,36 @@ class BoardTest {
 
         val expected = board.countMines()
         mineCount shouldBe expected
+    }
+
+    @Test
+    fun `지뢰찾기 지뢰 개수를 정상적으로 센다`() {
+        // 아래와 같은 모양으로 지뢰를 심는다.
+        // X . .
+        // . . X
+        // X X .
+
+        val board = Board(
+            height = 3,
+            width = 3,
+            mineCount = 2,
+            minePlacementStrategy = FixedMinePlacementStrategy(listOf(0, 5, 6, 7)),
+        ).board
+
+        board[0][0].shouldBeInstanceOf<Mine>()
+        board[0][1].shouldBeInstanceOf<Land>()
+        board[0][2].shouldBeInstanceOf<Land>()
+        board[1][0].shouldBeInstanceOf<Land>()
+        board[1][1].shouldBeInstanceOf<Land>()
+        board[1][2].shouldBeInstanceOf<Mine>()
+        board[2][0].shouldBeInstanceOf<Mine>()
+        board[2][1].shouldBeInstanceOf<Mine>()
+        board[2][2].shouldBeInstanceOf<Land>()
+
+        (board[0][1] as Land).adjacentMines shouldBe 2
+        (board[0][2] as Land).adjacentMines shouldBe 1
+        (board[1][0] as Land).adjacentMines shouldBe 3
+        (board[1][1] as Land).adjacentMines shouldBe 4
+        (board[2][2] as Land).adjacentMines shouldBe 2
     }
 }
