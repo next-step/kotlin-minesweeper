09345c54f2e4d1e3476578ae568292cdac51c895
leekyuwon <kyuwoon369@gmail.com>
2024-12-09 22:06:07 +0900

    feat: 만든 지뢰게임 판을 출력하라

--- src/main/kotlin/minesweeper/entity/Cells.kt
+++ src/main/kotlin/minesweeper/entity/Cells.kt
@@ -3,6 +3,11 @@ package minesweeper.entity
 class Cells private constructor(
     val cells: List<Cell>,
 ) {
+    fun findCell(coordinate: Coordinate): Cell {
+        return cells.find { it.matches(coordinate) }
+            ?: throw IllegalArgumentException("셀을 찾을 수 없습니다: $coordinate")
+    }
+
     companion object {
         fun generate(
             height: Height,
