9c8783e6c4c7c7b8447b8b24ed18764f141f2245
leekyuwon <kyuwoon369@gmail.com>
2024-12-10 23:56:02 +0900

    refactor: require을 활용하라

--- src/main/kotlin/minesweeper/entity/MineField.kt
+++ src/main/kotlin/minesweeper/entity/MineField.kt
@@ -6,8 +6,8 @@ class MineField(
     private val _cells: Cells,
 ) {
     init {
-        if (cells.size != height.value * width.value) {
-            throw IllegalArgumentException("Cells의 개수가 보드 크기와 일치하지 않습니다.")
+        require(cells.size == height.value * width.value) {
+            "Cells의 개수가 보드 크기와 일치하지 않습니다."
         }
     }
 
