33c677daeb954cb89d8b1ae61443e62e1d716c59
leekyuwon <kyuwoon369@gmail.com>
2024-12-10 23:16:11 +0900

    refactor: Minesweeper에서 View 데이터 가공 책임 추가
    - Minesweeper가 View와 도메인(MineField)을 연결하는 역할 수행

--- src/main/kotlin/minesweeper/app/Minesweeper.kt
+++ src/main/kotlin/minesweeper/app/Minesweeper.kt
@@ -1,5 +1,6 @@
 package minesweeper.app
 
+import minesweeper.entity.Cell
 import minesweeper.entity.MineField
 import minesweeper.entity.MineFieldFactory
 import minesweeper.entity.RandomMineGenerator
@@ -20,6 +21,22 @@ class Minesweeper {
 
     fun gameStart(mineField: MineField) {
         outputView.printGameStart()
-        outputView.printMineField(mineField)
+        outputView.printMineField(mapToViewData(mineField))
+    }
+
+    private fun mapToViewData(mineField: MineField): List<List<Char>> {
+        val result = MutableList(mineField.height.value) { MutableList(mineField.width.value) { EMPTY_VIEW } }
+
+        for (cell in mineField.cells) {
+            val (x, y) = cell.coordinate
+            result[y][x] = if (cell is Cell.Mine) MINE_VIEW else EMPTY_VIEW
+        }
+
+        return result
+    }
+
+    companion object {
+        const val MINE_VIEW = '*'
+        const val EMPTY_VIEW = 'o'
     }
 }
