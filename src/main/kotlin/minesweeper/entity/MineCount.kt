4073d5d225ec2f2a099b695719682cb592343084
leekyuwon <kyuwoon369@gmail.com>
2024-12-11 21:00:20 +0900

    refactor: 지뢰 개수는 자연수를 받는다

--- src/main/kotlin/minesweeper/entity/MineCount.kt
+++ src/main/kotlin/minesweeper/entity/MineCount.kt
@@ -5,6 +5,6 @@ value class MineCount(
     val value: Int,
 ) {
     init {
-        require(value >= 0) { "지뢰 개수는 0보다 작을 수 없습니다." }
+        require(value > 0) { "지뢰 개수는 0보다 작을 수 없습니다." }
     }
 }
