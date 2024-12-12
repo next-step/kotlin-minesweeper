efe3450f1a1516b75bd736e60c138768a3d7c266
gmltkddl1 <sky1994601@hanmail.net>
2024-12-12 17:46:04 +0900

    feat: 지뢰 찾기(지뢰 개수)
    요구사항 '지뢰 찾기 칸은 주변의 지뢰 수를 가진다'

--- src/test/kotlin/domain/MineSweeperMapBlockTest.kt
+++ src/test/kotlin/domain/MineSweeperMapBlockTest.kt
@@ -6,7 +6,13 @@ import org.junit.jupiter.api.Test
 class MineSweeperMapBlockTest {
     @Test
     fun `지뢰찾기 칸은 지뢰인지 여부를 가진다`() {
-        val mineSweeperBlock = MineSweeperMapBlock(isMine = true)
+        val mineSweeperBlock = MineSweeperMapBlock(true)
         mineSweeperBlock.isMine shouldBe true
+    }
+
+    @Test
+    fun `지뢰찾기 칸은 주변의 지뢰 수를 가진다`() {
+        val mineSweeperMapBlock = MineSweeperMapBlock(false, 1)
+        mineSweeperMapBlock.mineAroundCount shouldBe 1
     }
 }
