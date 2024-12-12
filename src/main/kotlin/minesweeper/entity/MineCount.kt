91525e2a1cc1bcfa2ee0492064f2768f1bf1dfe9
leekyuwon <kyuwoon369@gmail.com>
2024-12-12 22:22:33 +0900

    refactor: MineCount에 자신의 값이 유효한지 확인하는 책임 위임

--- src/main/kotlin/minesweeper/entity/MineCount.kt
+++ src/main/kotlin/minesweeper/entity/MineCount.kt
@@ -7,4 +7,10 @@ value class MineCount(
     init {
         require(value > 0) { "지뢰 개수는 0보다 작을 수 없습니다." }
     }
+
+    fun validate(totalCells: Int) {
+        require(value < totalCells) {
+            "지뢰 개수는 전체 셀 수를 초과할 수 없습니다. (total: $totalCells, mineCount: $value)"
+        }
+    }
 }
