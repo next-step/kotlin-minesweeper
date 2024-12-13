6c2557a4adaf0c32202a1041b35f953badcbb897
Jihoi-Kang <jihoi.kang@gmail.com>
2024-12-13 13:24:34 +0900

    refactor: separate function

--- src/main/kotlin/minsweeper/domain/BoardLinesGenerator.kt
+++ src/main/kotlin/minsweeper/domain/BoardLinesGenerator.kt
@@ -6,11 +6,29 @@ class BoardLinesGenerator(
 
     fun generate(boardParam: BoardParam): BoardLines {
         val minePositions = minePositionsGenerator.generate(boardParam)
-        return BoardLines(List(boardParam.height) { row ->
-            BoardLine(List(boardParam.width) { col ->
-                Cell.Mine.takeIf { row * boardParam.width + col in minePositions } ?: Cell.Island
-            })
-        })
+        return createBoardLines(boardParam, minePositions)
     }
+
+    private fun createBoardLines(
+        boardParam: BoardParam,
+        minePositions: List<Int>,
+    ): BoardLines = BoardLines(List(boardParam.height) { row ->
+        createBoardLine(boardParam, row, minePositions)
+    })
+
+    private fun createBoardLine(
+        boardParam: BoardParam,
+        row: Int,
+        minePositions: List<Int>,
+    ): BoardLine = BoardLine(List(boardParam.width) { col ->
+        createCell(row, boardParam, col, minePositions)
+    })
+
+    private fun createCell(
+        row: Int,
+        boardParam: BoardParam,
+        col: Int,
+        minePositions: List<Int>,
+    ): Cell = Cell.Mine.takeIf { row * boardParam.width + col in minePositions } ?: Cell.Island
 
 }
