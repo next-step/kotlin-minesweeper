3d2f7a2e2d75b082c7d57fa81f39812042ba0455
Jihoi-Kang <jihoi.kang@gmail.com>
2024-12-13 13:19:42 +0900

    refactor: remove if else

--- src/main/kotlin/minsweeper/domain/BoardLinesGenerator.kt
+++ src/main/kotlin/minsweeper/domain/BoardLinesGenerator.kt
@@ -8,10 +8,7 @@ class BoardLinesGenerator(
         val minePositions = minePositionsGenerator.generate(boardParam)
         return BoardLines(List(boardParam.height) { row ->
             BoardLine(List(boardParam.width) { col ->
-                val position = row * boardParam.width + col
-
-                if (position in minePositions) Cell.Mine
-                else Cell.Island
+                Cell.Mine.takeIf { row * boardParam.width + col in minePositions } ?: Cell.Island
             })
         })
     }
