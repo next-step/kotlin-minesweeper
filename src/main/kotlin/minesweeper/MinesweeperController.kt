ac3f6ee629b075ed1168beb294e060691c8b4bd9
Giwan Kim <giwan.kim@gmail.com>
2024-12-13 15:12:16 +0900

    feat: 애플리케이션 부트스트랩에 필요한 메인 함수와 MVC 컨트롤러 클래스를 추가한다.

---
+++ src/main/kotlin/minesweeper/MinesweeperController.kt
@@ -0,0 +1,24 @@
+package minesweeper
+
+import minesweeper.application.GenerateMinesweeperCommand
+import minesweeper.application.MinesweeperService
+import minesweeper.ui.InputView
+
+class MinesweeperController(
+    private val minesweeperService: MinesweeperService,
+) {
+    fun start() {
+        val height = InputView.getHeight()
+        val width = InputView.getWidth()
+        val mineCount = InputView.getMineCount()
+
+        val board =
+            minesweeperService.generateBoard(
+                GenerateMinesweeperCommand(
+                    height = height,
+                    width = width,
+                    mineCount = mineCount,
+                ),
+            )
+    }
+}
