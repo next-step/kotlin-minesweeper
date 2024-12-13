461f08c0c76ece05c1e020a1cf6a003e189eed5d
Giwan Kim <giwan.kim@gmail.com>
2024-12-13 12:30:26 +0900

    feat: 지뢰 찾기 판을 생성할 수 있다.
    - 지정한 높이와 너비의 판을 생성할 수 있다.
    - 지정한 지뢰 개수를 랜덤하게 배치한다.

---
+++ src/main/kotlin/minesweeper/application/RandomBoardGenerator.kt
@@ -0,0 +1,33 @@
+package minesweeper.application
+
+import minesweeper.domain.Board
+import minesweeper.domain.Coordinate
+import minesweeper.domain.EmptyCell
+import minesweeper.domain.MinedCell
+
+class RandomBoardGenerator : BoardGenerator {
+    override fun generate(command: GenerateMinesweeperCommand): Board {
+        val width = command.width
+        val area = command.area
+        val mineCount = command.mineCount
+
+        val shuffledIndices = (0 until area).shuffled()
+
+        val minedCells =
+            shuffledIndices
+                .take(mineCount)
+                .map { MinedCell(toCoordinate(it, width)) }
+
+        val emptyCellCoordinates =
+            shuffledIndices
+                .drop(mineCount)
+                .map { EmptyCell(toCoordinate(it, width)) }
+
+        return Board((minedCells + emptyCellCoordinates).toSet())
+    }
+
+    private fun toCoordinate(
+        index: Int,
+        width: Int,
+    ): Coordinate = Coordinate(index / width, index % width)
+}
