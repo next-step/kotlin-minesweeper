7ee846f82ce11484fd11e578e6a2e7cd9f9f4a26
wooobo <irooori89@gmail.com>
2024-12-16 00:06:34 +0900

    feat: draw 함수  추가

--- src/main/kotlin/minesweeper/Board.kt
+++ src/main/kotlin/minesweeper/Board.kt
@@ -7,6 +7,18 @@ class Board(
         return cells.checkMine(position)
     }
 
+    fun draw(): List<List<CellType>> {
+        val result = mutableListOf<List<CellType>>() // 최종 결과를 담을 리스트
+        val cellSize = cells.rowSize()
+        while (cellSize > result.size) {
+            val row = cells.rowAt(result.size) ?: break
+
+            result.add(row)
+        }
+
+        return result
+    }
+
     companion object {
         fun initializeBoard(dimensions: Dimensions, mineCount: Int, fixture: PositionProvider): Board {
             val cells = Cells(fixture.provide(dimensions, mineCount))
