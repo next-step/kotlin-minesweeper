8b59f78f270c3cd7d8b9d31c224815c984702dda
Giwan Kim <giwan.kim@gmail.com>
2024-12-15 17:39:14 +0900

    feat: 좌표(`Coordinate`) 주변 8개의 좌표를 구한다.

--- src/main/kotlin/minesweeper/domain/Coordinate.kt
+++ src/main/kotlin/minesweeper/domain/Coordinate.kt
@@ -4,13 +4,28 @@ data class Coordinate(
     val y: Int,
     val x: Int,
 ) {
+    val neighbors: List<Coordinate> by lazy { neighbors() }
+
     init {
         require(y in 0..MAX_VALUE) { "y축 좌표는 0 이상이어야 합니다." }
         require(x in 0..MAX_VALUE) { "x축 좌표는 0 이상이어야 합니다." }
     }
 
+    private fun neighbors(): List<Coordinate> =
+        Direction
+            .entries
+            .mapNotNull {
+                val nextY = y + it.dy
+                val nextX = x + it.dx
+                if (nextY in 0..MAX_VALUE && nextX in 0..MAX_VALUE) {
+                    Coordinate(y = nextY, x = nextX)
+                } else {
+                    null
+                }
+            }
+
     companion object {
-        private const val MAX_VALUE = 1_000_000
         // https://www.perplexity.ai/search/largest-ever-minesweeper-game-EXup1APhRFK6xGo6j8Ps1g#0
+        const val MAX_VALUE = 1_000_000
     }
 }
