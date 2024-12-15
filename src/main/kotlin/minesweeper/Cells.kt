5fd6a089fd99368288cd0fef9f67e61f52d83d1f
wooobo <irooori89@gmail.com>
2024-12-16 00:15:38 +0900

    feat: require 제거

--- src/main/kotlin/minesweeper/Cells.kt
+++ src/main/kotlin/minesweeper/Cells.kt
@@ -1,10 +1,6 @@
 package minesweeper
 
 class Cells(private val values: MutableMap<Position, CellType>) {
-    init {
-        require(values.keys.size == values.keys.toSet().size) { "중복된 Position 이 존재합니다." }
-    }
-
     fun checkMine(position: Position): Boolean {
         return at(position).isMine()
     }
