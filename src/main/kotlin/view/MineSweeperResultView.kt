8d56fb6fc18ffc60d553832dbdea7e441fb3c98a
gmltkddl1 <sky1994601@hanmail.net>
2024-12-16 14:53:36 +0900

    refactor: 지뢰 찾기(게임 실행)
    else 삭제

--- src/main/kotlin/view/MineSweeperResultView.kt
+++ src/main/kotlin/view/MineSweeperResultView.kt
@@ -6,8 +6,8 @@ object MineSweeperResultView {
     fun drawResult(game: MineSweeperGame) {
         if (game.isWin()) {
             MineSweeperMapView.drawWinResult(game.mineSweeperMap)
-        } else {
-            println("Lose Game")
+            return
         }
+        println("Lose Game")
     }
 }
