ee691e6c8208e5a00d759a63c5651df5178333e3
Sara Han <sarahan774@gmail.com>
2024-12-18 08:10:36 +0900

    [코드리뷰][step1] MineCountTest, RandomMineGeneratorTest Junit 의존성 제거

--- src/test/kotlin/minesweeper/domain/MineCountTest.kt
+++ src/test/kotlin/minesweeper/domain/MineCountTest.kt
@@ -1,17 +1,17 @@
 package minesweeper.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.assertThrows
+import io.kotest.matchers.shouldBe
 
 class MineCountTest : StringSpec({
     "지뢰 개수가 0 보다 크다면 유효한 지뢰 개수" {
         val mineCount = MineCount(10)
-        assertEquals(10, mineCount.count)
+        mineCount.count shouldBe 10
     }
 
     "지뢰 개수가 0 이라면 예외를 던진다" {
-        assertThrows<IllegalArgumentException> {
+        shouldThrow<IllegalArgumentException> {
             MineCount(0)
         }
     }
--- src/test/kotlin/minesweeper/domain/RandomMineGeneratorTest.kt
+++ src/test/kotlin/minesweeper/domain/RandomMineGeneratorTest.kt
@@ -1,9 +1,8 @@
 package minesweeper.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.assertThrows
+import io.kotest.matchers.shouldBe
 
 class RandomMineGeneratorTest : StringSpec({
     "주어진 지뢰 개수만큼 랜덤하게 지뢰를 생성" {
@@ -11,12 +10,12 @@ class RandomMineGeneratorTest : StringSpec({
         val mineCount = 10
         val minePositions = RandomMineGenerator().generateMinePositions(totalCells, mineCount)
 
-        assertEquals(mineCount, minePositions.size)
-        assertTrue(minePositions.all { it in 0 until totalCells })
+        minePositions.size shouldBe mineCount
+        minePositions.all { it in 0 until totalCells } shouldBe true
     }
 
     "가용한 Cell 개수를 초과해서 지뢰를 생성하고자 하는 경우 예외를 던짐" {
-        assertThrows<IllegalArgumentException> {
+        shouldThrow<IllegalArgumentException> {
             RandomMineGenerator().generateMinePositions(10, 20)
         }
     }
