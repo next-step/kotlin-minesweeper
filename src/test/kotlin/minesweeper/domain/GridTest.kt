3ac9dc97c73a3c50a7086014fe4c218c7e024b99
Sara Han <sarahan774@gmail.com>
2024-12-18 08:09:08 +0900

    [코드리뷰][step1] GridTest 테스트 보강, Junit 의존성 제거

--- src/test/kotlin/minesweeper/domain/GridTest.kt
+++ src/test/kotlin/minesweeper/domain/GridTest.kt
@@ -1,7 +1,7 @@
 package minesweeper.domain
 
 import io.kotest.core.spec.style.StringSpec
-import org.junit.jupiter.api.Assertions.assertEquals
+import io.kotest.matchers.shouldBe
 
 class GridTest : StringSpec({
     "Grid 는 주어진 높이와 너비를 곱한 개수만큼 초기화 된다" {
@@ -9,8 +9,8 @@ class GridTest : StringSpec({
         val mineCount = MineCount(10)
         val grid = Grid(dimension, mineCount)
 
-        assertEquals(10, grid.cells.size)
-        assertEquals(10, grid.cells[0].size)
+        grid.cells.size shouldBe  10
+        grid.cells[0].size shouldBe 10
     }
 
     "Grid 는 주어진 지뢰의 개수만큼 지뢰를 가진채로 초기화 된다" {
@@ -21,6 +21,20 @@ class GridTest : StringSpec({
         val allCells = grid.cells.flatten()
         val mineCountInGrid = allCells.count { it.isMine() }
 
-        assertEquals(5, mineCountInGrid)
+        mineCountInGrid shouldBe 5
+    }
+
+    "Grid 의 알맞은 위치에 지뢰가 생성된다" {
+        val dimension = Dimension(height = 5, width = 5)
+        val mineCount = MineCount(5)
+        val minePositions = setOf(1, 2, 3, 4, 5)
+        val mineGenerator = object : MineGenerator {
+            override fun generateMinePositions(totalCells: Int, mineCount: Int): Set<Int> {
+                return minePositions
+            }
+        }
+        val grid = Grid(dimension, mineCount, mineGenerator)
+        val allCells = grid.cells.flatten()
+        allCells.take(5).all { it.isMine() } shouldBe true
     }
 })
