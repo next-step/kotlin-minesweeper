c0188e147c664a6400410fdbf1cda2b6da6b2655
KimDabomi <cdabomi@nate.com>
2024-12-18 20:39:31 +0900

    [step1] refactor: named arguments 적용

--- src/test/kotlin/domain/PositionTest.kt
+++ src/test/kotlin/domain/PositionTest.kt
@@ -6,13 +6,27 @@ import io.kotest.matchers.shouldBe
 
 class PositionTest : StringSpec({
     "유효한 row와 col 값을 가질 수 있다." {
-        val position = Position(1, 2)
+        val position =
+            Position(
+                row = 1,
+                col = 2,
+            )
         position.row shouldBe 1
         position.col shouldBe 2
     }
 
     "row나 col이 음수이면 예외를 발생시킨다." {
-        shouldThrow<IllegalArgumentException> { Position(-1, 0) }
-        shouldThrow<IllegalArgumentException> { Position(0, -1) }
+        shouldThrow<IllegalArgumentException> {
+            Position(
+                row = -1,
+                col = 0,
+            )
+        }
+        shouldThrow<IllegalArgumentException> {
+            Position(
+                row = 0,
+                col = -1,
+            )
+        }
     }
 })
