e52e37f2fa644afd12696977b6e9786caa0e4efc
wooobo <irooori89@gmail.com>
2024-12-18 11:23:10 +0900

    fix: 사용안하는 코드 제거

--- src/main/kotlin/minesweeper/Cells.kt
+++ src/main/kotlin/minesweeper/Cells.kt
@@ -1,32 +1,5 @@
 package minesweeper
 
-//class Cells(private val values: MutableMap<Position, CellType>) {
-//    val mineCount: Int
-//        get() = values.values.count { it.isMine() }
-//
-//    fun checkMine(position: Position): Boolean {
-//        return at(position).isMine()
-//    }
-//
-//    fun rowSize(): Int {
-//        return values.keys
-//            .filter { it.x == 0 }
-//            .size
-//    }
-//
-//    fun rowAt(rowIndex: Int): List<CellType>? {
-//        return values.entries
-//            .filter { it.key.y == rowIndex }
-//            .map { it.value }
-//            .takeIf { it.isNotEmpty() }
-//    }
-//
-//    private fun at(position: Position): CellType {
-//        return values[position] ?: throw IllegalArgumentException("존재 하지 않는 위치 입니다.")
-//    }
-//}
-
-
 class Cells(private val values: Map<Int, Cell>) {
     val mineCount: Int
         get() = values.values.count { it.isMine }
