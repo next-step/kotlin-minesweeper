327ef9ec2afcdd5341c15f76c53f3768d61b616d
Giwan Kim <giwan.kim@gmail.com>
2024-12-18 12:24:36 +0900

    feat: 입력 받은 좌표를 연 결과와 게임 결과를 출력한다.

--- src/main/kotlin/minesweeper/MinesweeperController.kt
+++ src/main/kotlin/minesweeper/MinesweeperController.kt
@@ -2,6 +2,8 @@ package minesweeper
 
 import minesweeper.application.GenerateMinesweeperCommand
 import minesweeper.application.MinesweeperService
+import minesweeper.domain.CompletedGame
+import minesweeper.domain.PlayableGame
 import minesweeper.ui.InputView
 import minesweeper.ui.ResultView
 
@@ -13,14 +15,25 @@ class MinesweeperController(
         val width = InputView.getWidth()
         val mineCount = InputView.getMineCount()
 
-        val board =
-            minesweeperService.generateBoard(
+        var game =
+            minesweeperService.newGame(
                 GenerateMinesweeperCommand(
                     height = height,
                     width = width,
                     mineCount = mineCount,
                 ),
             )
-        ResultView.render(board)
+
+        ResultView.start()
+
+        while (game is PlayableGame) {
+            val (row, col) = InputView.getCoordinates()
+            game = game.open(y = row - 1, x = col - 1)
+            if (game is PlayableGame) {
+                ResultView.render(game)
+            }
+        }
+
+        ResultView.result(game as CompletedGame)
     }
 }
--- src/main/kotlin/minesweeper/ui/ResultView.kt
+++ src/main/kotlin/minesweeper/ui/ResultView.kt
@@ -1,10 +1,12 @@
 package minesweeper.ui
 
+import minesweeper.domain.ClosedEmptyCell
 import minesweeper.domain.CompletedGame
-import minesweeper.domain.EmptyCell
-import minesweeper.domain.MineExplodedGame
+import minesweeper.domain.MineDetonatedGame
 import minesweeper.domain.MinedCell
+import minesweeper.domain.OpenedEmptyCell
 import minesweeper.domain.PlayableBoard
+import minesweeper.domain.PlayableGame
 import minesweeper.domain.PlayerWonGame
 
 object ResultView {
@@ -12,16 +14,21 @@ object ResultView {
     private const val GAME_START = "지뢰찾기 게임 시작"
     private const val SPACE = " "
     private const val MINE = "*"
+    private const val CLOSED = "C"
 
     fun start() {
-        buildString {
-            appendLine()
-            appendLine(GAME_START)
-        }.also { print(it) }
+        println(NEWLINE + GAME_START)
     }
 
-    fun render(board: PlayableBoard) {
-        println(board.render())
+    fun render(game: PlayableGame) {
+        println(game.board.render())
+    }
+
+    fun result(game: CompletedGame) {
+        when (game) {
+            is PlayerWonGame -> println("Won Game.")
+            is MineDetonatedGame -> println("Lose Game.")
+        }
     }
 
     private fun PlayableBoard.render(): String {
@@ -31,17 +38,11 @@ object ResultView {
             (0..maxX).joinToString(SPACE) { x ->
                 when (get(y, x)) {
                     is MinedCell -> MINE
-                    is EmptyCell -> countMines(y, x).toString()
+                    is ClosedEmptyCell -> CLOSED
+                    is OpenedEmptyCell -> countMines(y, x).toString()
                     null -> error("정상적으로 판이 생성되었으면 도달할 수 없는 코드")
                 }
             }
-        }
-    }
-
-    fun result(game: CompletedGame) {
-        when (game) {
-            is PlayerWonGame -> println("Won Game.")
-            is MineExplodedGame -> println("Lose Game.")
         }
     }
 }
