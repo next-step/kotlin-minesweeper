588579529b80e2d43733b6a1c5078ea47f8add29
Giwan Kim <giwan.kim@gmail.com>
2024-12-18 20:52:22 +0900

    feat: 지뢰와 열기 전의 칸은 `C`로 표기한다.

--- src/main/kotlin/minesweeper/ui/ResultView.kt
+++ src/main/kotlin/minesweeper/ui/ResultView.kt
@@ -13,7 +13,6 @@ object ResultView {
     private val NEWLINE = System.lineSeparator()
     private const val GAME_START = "지뢰찾기 게임 시작"
     private const val SPACE = " "
-    private const val MINE = "*"
     private const val CLOSED = "C"
 
     fun start() {
@@ -37,8 +36,7 @@ object ResultView {
         return (0..maxY).joinToString(NEWLINE) { y ->
             (0..maxX).joinToString(SPACE) { x ->
                 when (get(y, x)) {
-                    is MinedCell -> MINE
-                    is ClosedEmptyCell -> CLOSED
+                    is MinedCell, is ClosedEmptyCell -> CLOSED
                     is OpenedEmptyCell -> countMines(y, x).toString()
                     null -> error("정상적으로 판이 생성되었으면 도달할 수 없는 코드")
                 }
