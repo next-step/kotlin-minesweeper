babeffe47966806f6eba7adf72db116d6009cc2b
devtaebong <thk98k@gmail.com>
2024-12-19 17:26:01 +0900

    test: 지뢰찾기 게임 메타데이터를 관리하는 클래스 사용

--- src/main/kotlin/domain/MineGameMetric.kt
+++ src/main/kotlin/domain/MineGameMetric.kt
@@ -1,3 +1,12 @@
 package domain
 
-class MineGameMetric(val boardHeightSize: Int, val boardWidthSize: Int, val mineCount: Int)
+import constants.MineSweeperConstants.MINIMUM_HEIGHT
+import constants.MineSweeperConstants.MINIMUM_WIDTH
+
+class MineGameMetric(val boardHeightSize: Int, val boardWidthSize: Int, val mineCount: Int) {
+    init {
+        require(boardHeightSize > MINIMUM_HEIGHT)
+        require(boardWidthSize > MINIMUM_WIDTH)
+        require(boardWidthSize * boardWidthSize >= mineCount)
+    }
+}
