afe4fb66a8e7dee8a06efbf755838f996d01dd5a
devtaebong <thk98k@gmail.com>
2024-12-19 18:35:20 +0900

    feat: 비어있는 셀에 주변 지뢰 셀의 개수를 계산하는 메서드를 제공한다.

--- src/main/kotlin/domain/MineBoard.kt
+++ src/main/kotlin/domain/MineBoard.kt
@@ -3,4 +3,26 @@ package domain
 class MineBoard(
     private val mineGameMetric: MineGameMetric,
     private val cells: Cells,
-)
+) {
+    fun countAdjacentMines(cell: Cell): Int {
+        var numberOfMines = 0
+        val dr = arrayOf(-1, 1, 0, 0, -1, -1, 1, 1)
+        val dc = arrayOf(0, 0, -1, 1, -1, 1, -1, 1)
+
+        for (i in dr.indices) {
+            val nr = dr[i] + cell.coordinate.r.value
+            val nc = dc[i] + cell.coordinate.c.value
+            if (0 > nr || nr > mineGameMetric.boardHeightSize || 0 > nc || nc > mineGameMetric.boardWidthSize) {
+                continue
+            }
+
+            val nextCoordinate = cells.getCoordinateIs(Coordinate(Row(nr), Col(nc)))
+
+            if (nextCoordinate.isMineCell()) {
+                numberOfMines++
+            }
+        }
+
+        return numberOfMines
+    }
+}
