1eff0e08838593c6016f3b40053916f678b83ae5
devtaebong <thk98k@gmail.com>
2024-12-19 18:43:56 +0900

    refactor: 매직넘버 제거

--- src/main/kotlin/domain/MineBoard.kt
+++ src/main/kotlin/domain/MineBoard.kt
@@ -1,18 +1,18 @@
 package domain
 
+import constants.MineSweeperConstants.MINIMUM_HEIGHT
+import constants.MineSweeperConstants.MINIMUM_WIDTH
+
 class MineBoard(
     private val mineGameMetric: MineGameMetric,
     private val cells: Cells,
 ) {
     fun countAdjacentMines(cell: Cell): Int {
         var numberOfMines = 0
-        val dr = arrayOf(-1, 1, 0, 0, -1, -1, 1, 1)
-        val dc = arrayOf(0, 0, -1, 1, -1, 1, -1, 1)
-
-        for (i in dr.indices) {
-            val nr = dr[i] + cell.coordinate.r.value
-            val nc = dc[i] + cell.coordinate.c.value
-            if (0 > nr || nr > mineGameMetric.boardHeightSize || 0 > nc || nc > mineGameMetric.boardWidthSize) {
+        for (i in DIRECTION_START_COUNT..DIRECTION_COUNT) {
+            val nr = ROW_OFFSETS[i] + cell.coordinate.r.value
+            val nc = COL_OFFSETS[i] + cell.coordinate.c.value
+            if (MINIMUM_HEIGHT > nr || nr > mineGameMetric.boardHeightSize || MINIMUM_WIDTH > nc || nc > mineGameMetric.boardWidthSize) {
                 continue
             }
 
@@ -24,5 +24,12 @@ class MineBoard(
         }
 
         return numberOfMines
+    }
+
+    companion object {
+        private const val DIRECTION_START_COUNT = 0
+        private const val DIRECTION_COUNT = 8
+        private val ROW_OFFSETS = arrayOf(-1, 1, 0, 0, -1, -1, 1, 1)
+        private val COL_OFFSETS = arrayOf(0, 0, -1, 1, -1, 1, -1, 1)
     }
 }
