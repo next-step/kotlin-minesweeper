c6bf2b8f95e50c9fc28f1784b2c2edd7c319629b
wooobo <irooori89@gmail.com>
2024-12-19 11:54:48 +0900

    fix: 리팩토링 발생 오류 수정

--- src/test/kotlin/minesweeper/BoardTest.kt
+++ src/test/kotlin/minesweeper/BoardTest.kt
@@ -1,7 +1,6 @@
 package minesweeper
 
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 
@@ -16,29 +15,13 @@ class BoardTest {
                     Cell.MineCell(Position(0, 2)),
                 ),
             )
-        val board = Board.initializeBoard(Dimensions(3, 3, 2), fixture)
+        val board = Board.initializeBoard(Dimensions(3, 3), fixture)
 
         assertAll(
             { assertThat(board.checkMine(Position(0, 0))).isTrue() },
             { assertThat(board.checkMine(Position(0, 1))).isFalse() },
             { assertThat(board.checkMine(Position(0, 2))).isTrue() },
         )
-    }
-
-    @Test
-    fun `지뢰 갯수가 일치하지 않으면 예외가 발생한다`() {
-        val fixture =
-            testFixture(
-                listOf(
-                    Cell.MineCell(Position(0, 0)),
-                    Cell.NumberCell(Position(1, 0)),
-                    Cell.NumberCell(Position(2, 0)),
-                ),
-            )
-
-        assertThatIllegalArgumentException()
-            .isThrownBy { Board.initializeBoard(Dimensions(3, 1, 2), fixture) }
-            .withMessage("지뢰 갯수가 일치하지 않습니다.")
     }
 
     private fun testFixture(data: List<Cell>) =
