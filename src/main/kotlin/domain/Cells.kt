8ec947d177a705fc833433dd72ddd5230c973e44
devtaebong <thk98k@gmail.com>
2024-12-20 21:27:09 +0900

    refactor: 일급컬렉션(`Cells`) inline 클래스로 변경

--- src/main/kotlin/domain/Cells.kt
+++ src/main/kotlin/domain/Cells.kt
@@ -6,7 +6,8 @@ import domain.Cell.EmptyCell
 import domain.Cell.MineCell
 import domain.strategy.MineCellGenerator
 
-class Cells(val cells: List<Cell>) {
+@JvmInline
+value class Cells(val cells: List<Cell>) {
     fun mineCells(): List<MineCell> = cells.filterIsInstance<MineCell>()
 
     fun emptyCells(): List<EmptyCell> = cells.filterIsInstance<EmptyCell>()
