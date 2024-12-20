b17bbd3fd418e562de712f77a82bb505b049c18b
sian <chicori3@naver.com>
2024-12-21 00:57:21 +0900

    feat: 지뢰 보드 포매터 정의

---
+++ src/main/kotlin/application/CellBoardFormatter.kt
@@ -0,0 +1,44 @@
+package application
+
+import cell.Cell
+import cell.CellBoard
+import cell.Coordinate
+import cell.MineCell
+
+fun format(board: CellBoard): String {
+    val cells = board.cells
+    val height = cells.keys.maxOf { it.y.value } + 1
+    val width = cells.keys.maxOf { it.x.value } + 1
+    val boardArray = initializeBoardArray(height, width)
+
+    populateBoardArray(boardArray, cells)
+
+    return boardArrayToString(boardArray)
+}
+
+private fun initializeBoardArray(
+    height: Int,
+    width: Int,
+): Array<CharArray> {
+    return Array(height) { CharArray(width) { 'C' } }
+}
+
+private fun populateBoardArray(
+    boardArray: Array<CharArray>,
+    cells: Map<Coordinate, Cell>,
+) {
+    cells.forEach { (coordinate, cell) ->
+        val x = coordinate.x.value
+        val y = coordinate.y.value
+
+        boardArray[y][x] =
+            when (cell) {
+                is MineCell -> 'X'
+                else -> 'C'
+            }
+    }
+}
+
+private fun boardArrayToString(boardArray: Array<CharArray>): String {
+    return boardArray.joinToString("\n") { it.joinToString(" ") }
+}
