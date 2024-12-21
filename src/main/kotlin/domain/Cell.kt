d33019f67cece8001e60fb82d837b749a4f1362e
Kim Dabomi <110092175+KimDabomi@users.noreply.github.com>
2024-12-21 22:41:22 +0900

    [step2] refactor: addNumberHint 메서드 추상화

--- src/main/kotlin/domain/Cell.kt
+++ src/main/kotlin/domain/Cell.kt
@@ -3,34 +3,41 @@ package domain
 sealed class Cell {
     abstract val id: CellId
 
-    fun addNumberHint(row: Int, col: Int, allCells: Cells): Cell {
-        if (this is Empty) {
-            val adjacentMineCount = countAdjacentMines(row, col, allCells)
-            return if (adjacentMineCount > 0) NumberCell(adjacentMineCount) else this
-        }
-        return this
-    }
-
-    private fun countAdjacentMines(row: Int, col: Int, allCells: Cells): Int {
-        return DIRECTIONS.count { (dr, dc) ->
-            val newRow = row + dr
-            val newCol = col + dc
-            if (newRow in 0 until allCells.size && newCol in 0 until allCells[newRow].size) {
-                allCells[newRow][newCol] is MineCell
-            } else false
-        }
-    }
+    abstract fun addNumberHint(row: Int, col: Int, allCells: Cells): Cell
 
     data object Empty : Cell() {
         override val id = CellId.EMPTY
+
+        override fun addNumberHint(row: Int, col: Int, allCells: Cells): Cell {
+            val adjacentMineCount = countAdjacentMines(row, col, allCells)
+            return if (adjacentMineCount > 0) NumberCell(adjacentMineCount) else this
+        }
+
+        private fun countAdjacentMines(row: Int, col: Int, allCells: Cells): Int {
+            return DIRECTIONS.count { (dr, dc) ->
+                val newRow = row + dr
+                val newCol = col + dc
+                if (newRow in 0 until allCells.size && newCol in 0 until allCells[newRow].size) {
+                    allCells[newRow][newCol] is MineCell
+                } else false
+            }
+        }
     }
 
     data object MineCell : Cell() {
         override val id = CellId.MINE
+
+        override fun addNumberHint(row: Int, col: Int, allCells: Cells): Cell {
+            return this // Mines do not change to another type.
+        }
     }
 
     data class NumberCell(val count: Int) : Cell() {
         override val id = CellId.NUMBER
+
+        override fun addNumberHint(row: Int, col: Int, allCells: Cells): Cell {
+            return this // Mines do not change to another type.
+        }
     }
 
     companion object {
