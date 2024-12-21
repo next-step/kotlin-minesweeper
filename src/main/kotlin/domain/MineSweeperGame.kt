1f77070f5d8f0b813275d8b3b7a191b78d57e917
devtaebong <thk98k@gmail.com>
2024-12-22 01:13:31 +0900

    refactor: isContinueGame 함수 when 식으로 변경

--- src/main/kotlin/domain/MineSweeperGame.kt
+++ src/main/kotlin/domain/MineSweeperGame.kt
@@ -1,15 +1,13 @@
 package domain
 
 class MineSweeperGame(private val mineBoard: MineBoard, var result: GameResult = GameResult.FAILURE) {
-    // todo: when 식으로 refactor
     fun isContinueGame(): Boolean {
-        if (mineBoard.isAnyMineCellOpened()) {
-            return false
-        }
-
-        if (mineBoard.isAllEmptyCellsOpened()) {
-            result = GameResult.SUCCESS
-            return false
+        when {
+            mineBoard.isAnyMineCellOpened() -> return false
+            mineBoard.isAllEmptyCellsOpened() -> {
+                result = GameResult.SUCCESS
+                return false
+            }
         }
         return true
     }
